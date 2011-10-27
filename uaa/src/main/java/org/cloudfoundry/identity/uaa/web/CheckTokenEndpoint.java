23cb8ae18bf3f7d2996b1741fd66dc284b37189d
Luke Taylor <luke.taylor@springsource.com>
2011-10-28 00:07:12 +0100

    CFID-28: Revert CheckTokenEndpoint changes which don't work with AuthzAuthenticationProvider.

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/web/CheckTokenEndpoint.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/web/CheckTokenEndpoint.java
@@ -1,19 +1,8 @@
 package org.cloudfoundry.identity.uaa.web;
 
-import java.security.Principal;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.cloudfoundry.identity.uaa.authentication.UaaUser;
-import org.cloudfoundry.identity.uaa.authentication.UaaUserService;
+import org.cloudfoundry.identity.uaa.authentication.UaaPrincipal;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -23,66 +12,45 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Controller which decodes access tokens for resource owners who are not able to do so (or where opaque token
+ * Controller which decodes access tokens for clients who are not able to do so (or where opaque token
  * values are used).
  *
  * @author Luke Taylor
- * @author Dave Syer
  */
 @Controller
 public class CheckTokenEndpoint implements InitializingBean {
-	private final Log logger = LogFactory.getLog(getClass());
-
 	private TokenStore tokenStore;
-	private UaaUserService userService;
 
 	public void afterPropertiesSet() throws Exception {
 		Assert.notNull(tokenStore, "tokenServices must be set");
-		Assert.notNull(userService, "userService must be set");
 	}
 
 	@RequestMapping (value = "/check_token")
 	@ResponseBody
-	public Map<String,Object> checkToken(@RequestParam ("token") String value, Principal resourceOwner) {
-		logger.debug("check_token called by " + resourceOwner);
-		Map<String,Object> response = new HashMap<String,Object>();
+	public Map<String,String> checkToken(@RequestParam ("token") String value) {
+		Map<String,String> response = new HashMap<String,String>();
 		OAuth2AccessToken token = tokenStore.readAccessToken(value);
 
 		if (token == null) {
 			response.put("error", "invalid_token");
 			return response;
 		}
-		// TODO: Check token scope matches the identity of the caller principal
+
 		OAuth2Authentication authentication = tokenStore.readAuthentication(token);
-		User principal = (User) authentication.getUserAuthentication().getPrincipal();
+		UaaPrincipal principal = (UaaPrincipal) authentication.getUserAuthentication().getPrincipal();
 
-		UaaUser user = userService.getUser(principal.getUsername());
-
-		response.put("user_id", userService.getPrincipal(user).getId());
-		response.put("user_name", user.getUsername());
-		response.put("user_email", user.getEmail());
-		response.put("scope", token.getScope());
-		response.put("user_authorities", getAuthorities(authentication.getAuthorities()));
+		response.put("user_id", principal.getId());
+		response.put("user_name", principal.getName());
 
 		return response;
-	}
-
-	private Collection<String> getAuthorities(Collection<GrantedAuthority> authorities) {
-		HashSet<String> result = new HashSet<String>();
-		for (GrantedAuthority authority : authorities) {
-			result.add(authority.getAuthority());
-		}
-		return result;
 	}
 
 	@Autowired
 	public void setTokenStore(TokenStore tokenStore) {
 		this.tokenStore = tokenStore;
-	}
-
-	@Autowired
-	public void setUserService(UaaUserService userService) {
-		this.userService = userService;
 	}
 }
--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/CheckTokenEndpointIntegrationTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/CheckTokenEndpointIntegrationTests.java
@@ -2,12 +2,14 @@ package org.cloudfoundry.identity.uaa.integration;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.codec.Base64;
 import org.springframework.util.LinkedMultiValueMap;
@@ -39,15 +41,17 @@ public class CheckTokenEndpointIntegrationTests {
 		ResponseEntity<Map> response = serverRunning.postForMap("/cloudfoundry-identity-uaa/oauth/token", formData);
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		String token = (String) response.getBody().get("access_token");
-		
+
 		formData = new LinkedMultiValueMap<String, String>();
 		formData.add("token", token);
 		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
 		headers.set("Authorization", "Basic " + new String(Base64.encode("app:appclientsecret".getBytes("UTF-8"))));
 		response = serverRunning.postForMap("/cloudfoundry-identity-uaa/check_token", formData, headers);
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		System.err.println(response.getBody());
-		
+
 		@SuppressWarnings("unchecked")
 		Map<String,String> map = response.getBody();
 		assertEquals("marissa", map.get("user_name"));
