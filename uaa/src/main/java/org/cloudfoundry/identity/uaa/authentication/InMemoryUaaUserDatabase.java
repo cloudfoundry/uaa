6888d3b5d4ff0c56d280409b4096c6ef6bcd5bbb
Luke Taylor <luke.taylor@springsource.com>
2011-11-04 13:58:13 +0000

    CFID-34: Update UAA database to support SCIM users.

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/authentication/InMemoryUaaUserDatabase.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/authentication/InMemoryUaaUserDatabase.java
@@ -1,20 +1,41 @@
 package org.cloudfoundry.identity.uaa.authentication;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.cloudfoundry.identity.uaa.scim.ScimException;
+import org.cloudfoundry.identity.uaa.scim.ScimUser;
+import org.cloudfoundry.identity.uaa.scim.ScimUserProvisioning;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.codec.Hex;
+import org.springframework.util.Assert;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
+ * In-memory user account information storage.
+ *
+ * @author Luke Taylor
  */
-public class InMemoryUaaUserDatabase implements UaaUserService {
+public class InMemoryUaaUserDatabase implements UaaUserService, ScimUserProvisioning {
+	private final Log logger = LogFactory.getLog(getClass());
+	private int counter = 1;
+
 	private final Map<String, UaaUser> userDb = new HashMap<String,UaaUser>();
 
 	public InMemoryUaaUserDatabase(List<UaaUser> users) {
 		for (UaaUser user : users) {
-			userDb.put(user.getUsername(), user);
+			addUser(user);
 		}
+	}
+
+	private UaaUser addUser(UaaUser user) {
+		userDb.put(user.getUsername(), user.id(counter++));
+
+		return userDb.get(user.getUsername());
 	}
 
 	@Override
@@ -31,5 +52,37 @@ public class InMemoryUaaUserDatabase implements UaaUserService {
 	@Override
 	public UaaPrincipal getPrincipal(UaaUser user) {
 		return new UaaPrincipal(String.valueOf(user.getUsername().hashCode()), user.getUsername(), user.getEmail());
+	}
+
+	// Scim interface
+
+	@Override
+	public ScimUser retrieveUser(String id) {
+		for (UaaUser user : userDb.values()) {
+			if (user.getId().equals(id)) {
+				return user.scimUser();
+			}
+		}
+		throw new ScimException("User " + id + " does not exist", HttpStatus.NOT_FOUND);
+	}
+
+	@Override
+	public ScimUser createUser(ScimUser scim) {
+		Assert.isTrue(!userDb.containsKey(scim.getUserName()), "A user with name '" + scim.getUserName() +
+				"' already exists");
+		Assert.notEmpty(scim.getEmails(), "At least one email is required");
+
+		try {
+			UaaUser uaaUser = addUser(new UaaUser(scim));
+
+			return uaaUser.scimUser();
+		} catch(IllegalArgumentException e) {
+			throw new ScimException(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@Override
+	public ScimUser updateUser(String id, ScimUser user) {
+		return null;
 	}
 }
--- uaa/src/main/java/org/cloudfoundry/identity/uaa/authentication/UaaUser.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/authentication/UaaUser.java
@@ -1,17 +1,23 @@
 package org.cloudfoundry.identity.uaa.authentication;
 
+import org.cloudfoundry.identity.uaa.scim.ScimUser;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.crypto.codec.Hex;
+import org.springframework.util.Assert;
 
+import java.security.SecureRandom;
 import java.util.List;
+import java.util.Random;
 
 /**
  * User data for authentication against UAA's internal authentication provider.
- * 
+ *
  * @author Luke Taylor
  * @author Dave Syer
  */
 public class UaaUser {
+	private final String id;
 
 	private final String username;
 
@@ -21,14 +27,43 @@ public class UaaUser {
 
 	private List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_USER");
 
-	public UaaUser(String username, String password) {
+	UaaUser(String username, String password) {
 		this(username, password, null);
 	}
 
-	public UaaUser(String username, String password, String email) {
+	UaaUser(String username, String password, String email) {
+		this("NaN", username, password, email);
+	}
+
+	UaaUser(String id, String username, String password, String email) {
+		Assert.hasText(username, "Username cannot be empty");
+		Assert.hasText(id, "Id cannot be null");
+		Assert.hasText(email, "Email is required");
+		this.id = id;
 		this.username = username;
 		this.password = password;
+		// TODO: Canonicalize email?
 		this.email = email;
+	}
+
+	/**
+	 * Create a new user from Scim data.
+	 */
+	UaaUser(ScimUser scim) {
+		// TODO: Will password be passed in SCIM request or what?
+		this(scim.getUserName(), generatePassword(), scim.getPrimaryEmail().getValue());
+	}
+
+	private static final Random passwordGenerator = new SecureRandom();
+
+	private static String generatePassword() {
+		byte[] bytes = new byte[16];
+		passwordGenerator.nextBytes(bytes);
+		return new String(Hex.encode(bytes));
+	}
+
+	public String getId() {
+		return id;
 	}
 
 	public String getUsername() {
@@ -45,5 +80,22 @@ public class UaaUser {
 
 	public List<GrantedAuthority> getAuthorities() {
 		return authorities;
+	}
+
+	public UaaUser id(int id) {
+		if ("Nan".equals(this.id)) {
+			throw new IllegalStateException("Id already set");
+		}
+		return new UaaUser(Integer.toString(id), username, password, email);
+	}
+
+	/**
+	 * Convert to SCIM data for use in JSON responses.
+	 */
+	ScimUser scimUser() {
+		ScimUser scim = new ScimUser(getId());
+		scim.addEmail(getEmail());
+		scim.setUserName(getUsername());
+		return scim;
 	}
 }
