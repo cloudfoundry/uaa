277e180030ade6f68509b17258f8907de14d9148
Dave Syer <dsyer@vmware.com>
2011-11-09 14:28:52 +0000

    CFID-36: /app now authenticates with /uaa via /userinfo

--- app/src/main/java/org/cloudfoundry/identity/app/web/OpenIdClientFilter.java
+++ app/src/main/java/org/cloudfoundry/identity/app/web/OpenIdClientFilter.java
@@ -1,13 +1,56 @@
 package org.cloudfoundry.identity.app.web;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.cloudfoundry.identity.openid.user.CustomUserDetails;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.web.client.RestOperations;
 
-public class OpenIdClientFilter {
+public class OpenIdClientFilter extends AbstractAuthenticationProcessingFilter {
 
 	public RestOperations restTemplate;
 
+	private String userInfoUrl;
+
 	public void setRestTemplate(RestOperations restTemplate) {
-		this.restTemplate = restTemplate;		
+		this.restTemplate = restTemplate;
+	}
+
+	public void setUserInfoUrl(String userInfoUrl) {
+		this.userInfoUrl = userInfoUrl;
+	}
+
+	public OpenIdClientFilter(String defaultFilterProcessesUrl) {
+		super(defaultFilterProcessesUrl);
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException, IOException, ServletException {
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = restTemplate.getForObject(userInfoUrl, Map.class);
+		String userName = map.get("user_name");
+		List<GrantedAuthority> authorities = Arrays.<GrantedAuthority> asList(new SimpleGrantedAuthority("ROLE_USER"));
+		return new UsernamePasswordAuthenticationToken(new CustomUserDetails(userName,authorities), null,
+				authorities);
+	}
+	
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException failed) throws IOException, ServletException {
+		throw failed;
 	}
 
 }
