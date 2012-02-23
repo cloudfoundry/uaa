f5ec16677415450c020171e9a4d4bc216cb0f00d
Dale Olds <olds@vmware.com>
2012-02-23 13:40:35 -0800

    update JWT token decoder
    Change-Id: I50dafd303c8becc27f3ea1500bad505128fadbf7

--- gem/lib/uaa/token_decoder.rb
+++ gem/lib/uaa/token_decoder.rb
@@ -4,6 +4,11 @@
 # do not initiate their creation with the AS. The AcceptToken class is for
 # this use. This may be the only class the cloud controller needs.
 
+require 'uaa/http'
+require 'uaa/error'
+require 'jwt'
+require 'base64'
+
 class Cloudfoundry::Uaa::TokenDecoder
 
   include Cloudfoundry::Uaa::Http
@@ -12,8 +17,8 @@ class Cloudfoundry::Uaa::TokenDecoder
     @target, @resource_id, @secret = target, resource_id, secret
   end
 
-  # returns hash of decoded values or parsed error from server
-  def decode(auth_header)
+  # returns hash of values from server that are associated with the opaque token
+  def issuer_decode(auth_header)
     unless auth_header && (tkn = auth_header.split).length == 2
       raise AuthError, "invalid authentication header: #{auth_header}"
     end
@@ -21,6 +26,24 @@ class Cloudfoundry::Uaa::TokenDecoder
     reply = json_get("/check_token?token_type=#{tkn[0]}&token=#{tkn[1]}", res_auth)
     return reply if reply[:resource_ids].include?(@resource_id)
     raise AuthError, "invalid resource audience: #{reply[:resource_ids]}"
+  end
+
+  # returns hash of values decoded from the token contents
+  def decode(auth_header)
+    unless auth_header && (tkn = auth_header.split).length == 2 && tkn[0] =~ /bearer/i
+      raise AuthError, "invalid authentication header: #{auth_header}"
+    end
+    reply = JWT.decode(tkn[1], @secret)
+
+    # TODO: the following line only symbolizes the top level hash keys, but rather
+    # than introduce a large funciton to do it, we should get the JWT.decode call
+    # to support the symbolize option to JSON.parse.
+    reply = reply.each_with_object({}){|(k,v), h| h[k.to_sym] = v}
+
+    return reply if reply[:resource_ids].include?(@resource_id)
+    raise AuthError, "invalid resource audience: #{reply[:resource_ids]}"
+  rescue JWT::DecodeError, NotImplementedError
+    raise AuthError, "invalid authentication token"
   end
 
 end
--- gem/spec/token_decoder_spec.rb
+++ gem/spec/token_decoder_spec.rb
@@ -1,40 +1,39 @@
 require 'spec_helper'
+require 'uaa/token_decoder'
+require 'webmock/rspec'
 
 describe Cloudfoundry::Uaa::TokenDecoder do
-
-  before :each do
-    subject.stub!(:perform_http_request) do
-      @response # [status, body, headers]
-    end
-    # subject.trace = true
-  end
+  include WebMock::API
 
   subject { Cloudfoundry::Uaa::TokenDecoder.new("http://localhost:8080/uaa",
       "test_resource", "test_secret") }
 
   before :each do
+    @stub_req = stub_request(:get, "http://test_resource:test_secret@localhost:8080/uaa/check_token")
+        .with(:headers => {'Accept' => 'application/json'},
+            :query => {'token' => 'two', 'token_type' => 'one'})
     subject.trace = true
   end
 
   it "should raise an auth error if the given auth header is bad" do
-    @response = [401, nil, nil]
-    expect { subject.decode(nil).should raise_exception(AuthError) }
-    expect { subject.decode("one two three").should raise_exception(AuthError) }
+    @stub_req.to_return(File.new(spec_asset('check_token_success.txt')))
+    expect { subject.issuer_decode(nil) }.to raise_exception(Cloudfoundry::Uaa::TokenDecoder::AuthError)
+    expect { subject.issuer_decode("one two three") }.to raise_exception(Cloudfoundry::Uaa::TokenDecoder::AuthError)
   end
 
   it "should raise a target error if the response is not json" do
-    @response = [200, "foo bar", nil]
-    expect { subject.decode("one two").should raise_exception(BadTarget) }
+    @stub_req.to_return(File.new(spec_asset('text_reply.txt')))
+    expect { subject.issuer_decode("one two")}.should raise_exception(Cloudfoundry::Uaa::TokenDecoder::BadTarget)
   end
 
   it "should raise a target json error if the response is 400 with valid oauth json error" do
-    @response = [400, '{"error":"bad_request","description":"Bad request"}', nil]
-    expect { subject.decode("one two").should raise_exception(TargetJsonError) }
+    @stub_req.to_return(File.new(spec_asset('oauth_error_reply.txt')))
+    expect { subject.issuer_decode("one two")}.should raise_exception(Cloudfoundry::Uaa::TokenDecoder::TargetJsonError)
   end
 
   it "should GET decoded token hash from the check_token endpoint" do
-    @response = [200, '{"resource_ids": ["one_resource", "test_resource", "other_resource"],"email":"derek@gmail.com"}', nil]
-    info = subject.decode("one two")
+    @stub_req.to_return(File.new(spec_asset('check_token_success.txt')))
+    info = subject.issuer_decode("one two")
     info.should include(:resource_ids)
     info[:resource_ids].should include("test_resource")
     info.should include(:email => "derek@gmail.com")
