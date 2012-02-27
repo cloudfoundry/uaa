a56b5e4f9c85a5274e3dfacc75cda47429eb604e
Luke Taylor <ltaylor@vmware.com>
2012-02-27 22:27:35 +0000

    CFID-144: Add expires_at time (s) to JWT token.
    Change-Id: I8e8d79deaca9c6c6a6b8b12c3a3897b5a311646e

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/oauth/DefaultTokenConverter.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/oauth/DefaultTokenConverter.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2006-2011 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -53,6 +53,10 @@ public class DefaultTokenConverter implements AccessTokenConverter {
 		response.put(OAuth2AccessToken.SCOPE, token.getScope());
 		if (token.getExpiresIn() > 0) {
 			response.put(OAuth2AccessToken.EXPIRES_IN, token.getExpiresIn());
+		}
+
+		if (token.getExpiration() != null) {
+			response.put("expires_at", token.getExpiration().getTime()/1000);
 		}
 
 		response.put("client_id", clientToken.getClientId());
