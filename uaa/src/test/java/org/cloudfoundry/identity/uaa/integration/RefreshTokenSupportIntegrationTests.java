ac020838a4edd73b8cf20777130f73c6cf7afeba
Dave Syer <dsyer@vmware.com>
2012-03-09 12:26:39 +0000

    CFID-192: upgrade to Spring 3.1.1
    Change-Id: I83f86c5694189c4202a36b6e7a26ecdbe1706558

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/RefreshTokenSupportIntegrationTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/RefreshTokenSupportIntegrationTests.java
@@ -1,14 +1,11 @@
 /**
- * Cloud Foundry 2012.02.03 Beta
- * Copyright (c) [2009-2012] VMware, Inc. All Rights Reserved.
- *
- * This product is licensed to you under the Apache License, Version 2.0 (the "License").
- * You may not use this product except in compliance with the License.
- *
- * This product includes a number of subcomponents with
- * separate copyright notices and license terms. Your use of these
- * subcomponents is subject to the terms and conditions of the
- * subcomponent's license, as noted in the LICENSE file.
+ * Cloud Foundry 2012.02.03 Beta Copyright (c) [2009-2012] VMware, Inc. All Rights Reserved.
+ * 
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License"). You may not use this product
+ * except in compliance with the License.
+ * 
+ * This product includes a number of subcomponents with separate copyright notices and license terms. Your use of these
+ * subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE file.
  */
 package org.cloudfoundry.identity.uaa.integration;
 
@@ -78,7 +75,8 @@ public class RefreshTokenSupportIntegrationTests {
 		assertEquals("no-store", response.getHeaders().getFirst("Cache-Control"));
 		@SuppressWarnings("unchecked")
 		OAuth2AccessToken newAccessToken = OAuth2AccessToken.valueOf(response.getBody());
-		assertFalse(newAccessToken.getValue().equals(accessToken.getValue()));
+		assertFalse("TODO: This occasionally fails because the expiry time has 1 second accuracy.", newAccessToken.getValue()
+				.equals(accessToken.getValue()));
 
 	}
 }
