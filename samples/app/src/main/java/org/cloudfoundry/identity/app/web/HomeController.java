813ca8d3c4eade42a4ece78e2481d19d75f75f6c
Dave Syer <dsyer@vmware.com>
2012-03-14 13:33:08 +0000

    CFID-141: switch to jso library for browser client demo
    Change-Id: I73b834e557810f997d1c645bd170f4933bab6077

--- samples/app/src/main/java/org/cloudfoundry/identity/app/web/HomeController.java
+++ samples/app/src/main/java/org/cloudfoundry/identity/app/web/HomeController.java
@@ -21,6 +21,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+	private String userAuthorizationUri = "http://localhost:8080/uaa/oauth/authorize";
+	
+	private String dataUri = "http://localhost:8080/api/apps";
+	
+	/**
+	 * @param userAuthorizationUri the userAuthorizationUri to set
+	 */
+	public void setUserAuthorizationUri(String userAuthorizationUri) {
+		this.userAuthorizationUri = userAuthorizationUri;
+	}
+
+	/**
+	 * @param userInfoUri the userInfoUri to set
+	 */
+	public void setDataUri(String dataUri) {
+		this.dataUri = dataUri;
+	}
+
+	@RequestMapping("/browse")
+	public String browse(Model model) {
+		model.addAttribute("userAuthorizationUri", userAuthorizationUri);
+		model.addAttribute("dataUri", dataUri);
+		return "browse";
+	}
+
 	@RequestMapping("/home")
 	public String home(Model model, Principal principal) {
 		model.addAttribute("principal", principal);
