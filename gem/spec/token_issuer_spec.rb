2fd9d0a42c08b34076b4571f50cec92a6dec0f02
Luke Taylor <ltaylor@vmware.com>
2012-03-21 13:58:21 +0000

    CFID-134: Exclude license headers from gem rdoc
    Change-Id: I40e47c80dfcaad785bdcc764585eefcb5584665d

--- gem/spec/token_issuer_spec.rb
+++ gem/spec/token_issuer_spec.rb
@@ -1,4 +1,4 @@
-#
+#--
 # Cloud Foundry 2012.02.03 Beta
 # Copyright (c) [2009-2012] VMware, Inc. All Rights Reserved.
 #
@@ -9,7 +9,7 @@
 # separate copyright notices and license terms. Your use of these
 # subcomponents is subject to the terms and conditions of the
 # subcomponent's license, as noted in the LICENSE file.
-#
+#++
 
 require 'spec_helper'
 require 'uaa/token_issuer'
