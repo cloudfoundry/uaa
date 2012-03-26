f7ea2dea1f414e23b38b8a0d834779fa957c59ba
Luke Taylor <ltaylor@vmware.com>
2012-03-26 20:39:31 +0100

    CFID-221: Relax rules for passwords of 20 characters or more
    Change-Id: Icf270b72168796977dd437fea12fee712a653ee6

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/scim/DefaultPasswordValidator.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/scim/DefaultPasswordValidator.java
@@ -44,6 +44,7 @@ import edu.vt.middleware.password.UsernameRule;
  */
 public class DefaultPasswordValidator implements PasswordValidator {
 	private final List<Rule> defaultRules;
+    private final List<Rule> shortRules;
 
 	public DefaultPasswordValidator() {
 		List<Rule> rules = new ArrayList<Rule>(6);
@@ -53,13 +54,18 @@ public class DefaultPasswordValidator implements PasswordValidator {
 		rules.add(new UsernameRule(true, true));
 		// Try and catch variations on "password" as a password
 		rules.add(new RegexRule("[pP]+[aA@&]*[sSzZ$]+[wW]+[oO0]*[rR]*[dD]*"));
-		rules.add(new RepeatCharacterRegexRule());
-		rules.add(new NumericalSequenceRule());
-		rules.add(new AlphabeticalSequenceRule());
 		rules.add(new QwertySequenceRule());
 
-		defaultRules = Collections.unmodifiableList(rules);
-	}
+        defaultRules = Collections.unmodifiableList(rules);
+
+        rules = new ArrayList<Rule>(3);
+
+        rules.add(new NumericalSequenceRule());
+        rules.add(new RepeatCharacterRegexRule());
+        rules.add(new AlphabeticalSequenceRule());
+
+        shortRules = Collections.unmodifiableList(rules);
+    }
 
 	@Override
 	public void validate(String password, ScimUser user) throws InvalidPasswordException {
@@ -70,13 +76,16 @@ public class DefaultPasswordValidator implements PasswordValidator {
 
 		// Build dictionary rule based on Scim data
 		rules = new ArrayList<Rule>(defaultRules);
+        if (password.length() < 20) {
+            // Check sequences only in "short" passwords (see CFID-221)
+            rules.addAll(shortRules);
+        }
 		String[] userWords = user.wordList().toArray(new String[user.wordList().size()]);
 		Arrays.sort(userWords, WordLists.CASE_INSENSITIVE_COMPARATOR);
 		rules.add(new DictionarySubstringRule(new WordListDictionary(new ArrayWordList(userWords, false))));
 
 		edu.vt.middleware.password.PasswordValidator validator =
 				new edu.vt.middleware.password.PasswordValidator(rules);
-
 
 		RuleResult result = validator.validate(passwordData);
 
