93ef9ac4154867e8e5eb647f7a81c8b5be200822
Luke Taylor <ltaylor@vmware.com>
2012-04-25 20:30:42 +0100

    CFID-242: Add support for RSA key in JwtTokenEnhancer and UAA
    This mainly builds on changes to the JWT module in
    Spring Security OAuth. An RSA key in ssh format can
    be injected instead of an HMAC key and will be used
    for signing tokens.
    
    The default uaa.yml file now also specifies an RSA
    key pair.
    
    Change-Id: Ie2efdc5bd825b7885f7fed20a6978f27f57a3d69

--- common/src/main/java/org/cloudfoundry/identity/uaa/oauth/JwtTokenEnhancer.java
+++ common/src/main/java/org/cloudfoundry/identity/uaa/oauth/JwtTokenEnhancer.java
@@ -14,50 +14,93 @@ package org.cloudfoundry.identity.uaa.oauth;
 
 import java.util.Collections;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.security.jwt.JwtHelper;
-import org.springframework.security.jwt.crypto.sign.MacSigner;
+import org.springframework.security.jwt.crypto.sign.*;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.util.RandomValueStringGenerator;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * OAuth2 token services that produces JWT encoded token values.
  * 
  * @author Dave Syer
+ * @author Luke Taylor
  */
-public class JwtTokenEnhancer implements TokenEnhancer {
+public class JwtTokenEnhancer implements TokenEnhancer, InitializingBean {
+	protected final Log logger = LogFactory.getLog(getClass());
 
 	private AccessTokenConverter tokenConverter = new DefaultTokenConverter();
 
 	private ObjectMapper objectMapper = new ObjectMapper();
 
-	private String key = new RandomValueStringGenerator().generate();
-	
+	private String verifierKey = new RandomValueStringGenerator().generate();
+
+	private Signer signer = new MacSigner(verifierKey);
+
+	private String signingKey = verifierKey;
+
 	/**
-	 * @return the key used when signing tokens
+	 * @return the key used to verify tokens
 	 */
 	@RequestMapping(value = "/token_key", method = RequestMethod.GET)
 	@ResponseBody
 	public String getKey() {
-		return key;
+		return verifierKey;
 	}
 
 	/**
-	 * @param key the key to use when signing tokens
+	 * Sets the JWT signing key. It can be either a simple MAC key or an RSA key. RSA keys should be in OpenSSH format,
+	 * as produced by <tt>ssh-keygen</tt>.
+	 * 
+	 * @param key the key to be used for signing JWTs.
 	 */
-	@RequestMapping(value = "/token_key", method = RequestMethod.POST)
-	@ResponseBody
-	public void setKey(@RequestParam String key) {
-		this.key = key;
+	public void setSigningKey(String key) {
+		Assert.hasText(key);
+		key = key.trim();
+
+		this.signingKey = key;
+
+		if (key.startsWith("-----BEGIN")) {
+			signer = new RsaSigner(key);
+			logger.info("Configured with RSA signing key");
+		}
+		else {
+			// Assume it's an HMAC key
+			verifierKey = key;
+			signer = new MacSigner(key);
+		}
 	}
-	
+
+	/**
+	 * The key used for verifying signatures produced by this class. This is not used but is returned from the endpoint
+	 * to allow resource servers to obtain the key.
+	 * 
+	 * For an HMAC key it will be the same value as the signing key and does not need to be set. For and RSA key, it
+	 * should be set to the String representation of the public key, in a standard format (e.g. OpenSSH keys)
+	 * 
+	 * @param key the signature verification key (typically an RSA public key)
+	 */
+	public void setVerifierKey(String key) {
+		this.verifierKey = key;
+		try {
+			new RsaSigner(verifierKey);
+			throw new IllegalArgumentException("Private key cannot be set as verifierKey property");
+		}
+		catch (Exception expected) {
+			// Expected
+		}
+	}
+
 	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
 		DefaultOAuth2AccessToken result = new DefaultOAuth2AccessToken(accessToken);
 		String tokenId = result.getValue();
@@ -73,8 +116,36 @@ public class JwtTokenEnhancer implements TokenEnhancer {
 		catch (Exception e) {
 			throw new IllegalStateException("Cannot convert access token to JSON", e);
 		}
-		String token = JwtHelper.encode(content, new MacSigner(key)).getEncoded();
+		String token = JwtHelper.encode(content, signer).getEncoded();
 		return token;
 	}
 
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// Check the signing and verification keys match
+		if (signer instanceof RsaSigner) {
+			RsaVerifier verifier;
+			try {
+				verifier = new RsaVerifier(verifierKey);
+			}
+			catch (Exception e) {
+				logger.warn("Unable to create an RSA verifier from verifierKey");
+				return;
+			}
+
+			byte[] test = "test".getBytes();
+			try {
+				verifier.verify(test, signer.sign(test));
+				logger.info("Signing and verification RSA keys match");
+			}
+			catch (InvalidSignatureException e) {
+				logger.error("Signing and verification RSA keys do not match");
+			}
+		}
+		else {
+			// Avoid a race condition where
+			Assert.state(this.signingKey == this.verifierKey,
+					"For MAC signing you do not need to specify the verifier key separately, and if you do it must match the signing key");
+		}
+	}
 }
--- common/src/test/java/org/cloudfoundry/identity/uaa/oauth/JwtTokenEnhancerTests.java
+++ common/src/test/java/org/cloudfoundry/identity/uaa/oauth/JwtTokenEnhancerTests.java
@@ -9,12 +9,16 @@
  */
 package org.cloudfoundry.identity.uaa.oauth;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.cloudfoundry.identity.uaa.authentication.UaaAuthentication;
 import org.cloudfoundry.identity.uaa.authentication.UaaAuthenticationTestFactory;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.jwt.crypto.sign.RsaVerifier;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.AuthorizationRequest;
@@ -22,7 +26,7 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 
 /**
  * @author Dave Syer
- * 
+ * @author Luke Taylor
  */
 public class JwtTokenEnhancerTests {
 
@@ -42,6 +46,41 @@ public class JwtTokenEnhancerTests {
 				new AuthorizationRequest("foo", null, null, null), userAuthentication);
 		OAuth2AccessToken token = tokenEnhancer.enhance(new DefaultOAuth2AccessToken("FOO"), authentication);
 		assertNotNull(token.getValue());
+	}
+
+
+	@Test
+	public void rsaKeyCreatesValidRsaSignedTokens() throws Exception {
+		String rsaKey = " -----BEGIN RSA PRIVATE KEY-----  \n" +
+				"  MIIBywIBAAJhAOTeb4AZ+NwOtPh+ynIgGqa6UWNVe6JyJi+loPmPZdpHtzoqubnC \n" +
+				" wEs6JSiSZ3rButEAw8ymgLV6iBY02hdjsl3h5Z0NWaxx8dzMZfXe4EpfB04ISoqq\n" +
+				"    hZCxchvuSDP4eQIDAQABAmEAqUuYsuuDWFRQrZgsbGsvC7G6zn3HLIy/jnM4NiJK\n" +
+				" t0JhWNeN9skGsR7bqb1Sak2uWqW8ZqnqgAC32gxFRYHTavJEk6LTaHWovwDEhPqc\n" +
+				" Zs+vXd6tZojJQ35chR/slUEBAjEA/sAd1oFLWb6PHkaz7r2NllwUBTvXL4VcMWTS\n" +
+				" pN+5cU41i9fsZcHw6yZEl+ZCicDxAjEA5f3R+Bj42htNI7eylebew1+sUnFv1xT8\n" +
+				" jlzxSzwVkoZo+vef7OD6OcFLeInAHzAJAjEAs6izolK+3ETa1CRSwz0lPHQlnmdM\n" +
+				" Y/QuR5tuPt6U/saEVuJpkn4LNRtg5qt6I4JRAjAgFRYTG7irBB/wmZFp47izXEc3\n" +
+				" gOdvA1hvq3tlWU5REDrYt24xpviA0fvrJpwMPbECMAKDKdiDi6Q4/iBkkzNMefA8\n" +
+				"  7HX27b9LR33don/1u/yvzMUo+lrRdKAFJ+9GPE9XFA== \n" +
+				"-----END RSA PRIVATE KEY----- ";
+		tokenEnhancer.setSigningKey(rsaKey);
+		OAuth2Authentication authentication = new OAuth2Authentication(
+				new AuthorizationRequest("foo", null, null, null), userAuthentication);
+		OAuth2AccessToken token = tokenEnhancer.enhance(new DefaultOAuth2AccessToken("FOO"), authentication);
+		JwtHelper.decodeAndVerify(token.getValue(), new RsaVerifier(rsaKey));
+	}
+
+	@Test
+	public void publicKeyStringIsReturnedFromTokenKeyEndpoint() throws Exception {
+		tokenEnhancer.setVerifierKey("someKey");
+		assertEquals("someKey", tokenEnhancer.getKey());
+	}
+
+	@Test(expected=IllegalStateException.class)
+	public void keysNotMatchingWithMacSigner() throws Exception {
+		tokenEnhancer.setSigningKey("aKey");
+		tokenEnhancer.setVerifierKey("someKey");
+		tokenEnhancer.afterPropertiesSet();
 	}
 
 }
