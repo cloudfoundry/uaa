33c279b78cfe051da5d2c103918bb043827141fc
Luke Taylor <ltaylor@vmware.com>
2012-04-28 17:20:58 +0100

    CFID-256: Add denyAll filter chain.
    The UI filter chain is now configured to only match the relevant
    specific patterns which it can handle and an extra filter chain
    has been added which any other request will fall-through to, getting
    a 403 response code.
    
    Change-Id: I6c71ab679d6359303f1389f8b89584ecbeb50856

---
+++ common/src/main/java/org/cloudfoundry/identity/uaa/security/web/DelegatingRequestMatcher.java
@@ -0,0 +1,43 @@
+/*
+ * Cloud Foundry 2012.02.03 Beta
+ * Copyright (c) [2009-2012] VMware, Inc. All Rights Reserved.
+ *
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ * You may not use this product except in compliance with the License.
+ *
+ * This product includes a number of subcomponents with
+ * separate copyright notices and license terms. Your use of these
+ * subcomponents is subject to the terms and conditions of the
+ * subcomponent's license, as noted in the LICENSE file.
+ */
+
+package org.cloudfoundry.identity.uaa.security.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.web.util.RequestMatcher;
+
+/**
+ * @author Luke Taylor
+ */
+public class DelegatingRequestMatcher implements RequestMatcher {
+	private final List<RequestMatcher> matchers;
+
+	public DelegatingRequestMatcher(List<RequestMatcher> matchers) {
+		this.matchers = new ArrayList<RequestMatcher>(matchers);
+	}
+
+	@Override
+	public boolean matches(HttpServletRequest request) {
+		for (RequestMatcher m: matchers) {
+			if (m.matches(request)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+}
