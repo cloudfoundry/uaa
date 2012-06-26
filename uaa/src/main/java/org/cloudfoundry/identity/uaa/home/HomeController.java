19de06ff8c94a3b63ddcc51c766488fa24be27c0
Dave Syer <dsyer@vmware.com>
2012-06-26 08:59:11 +0100

    CFID-331: remove unused stuff from HomeController
    Change-Id: I25004429d29a30772c796f744fbf2512dd1dff2a

--- uaa/src/main/java/org/cloudfoundry/identity/uaa/home/HomeController.java
+++ uaa/src/main/java/org/cloudfoundry/identity/uaa/home/HomeController.java
@@ -15,33 +15,22 @@ package org.cloudfoundry.identity.uaa.home;
 import java.security.Principal;
 import java.util.Map;
 
-import org.springframework.security.oauth2.provider.AuthorizationRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.context.request.WebRequest;
 
 /**
- * Controller for retrieving the model for demo home page.
+ * Controller for retrieving the model for home page.
  *
  * @author Dave Syer
  */
 @Controller
-@SessionAttributes(types = AuthorizationRequest.class)
 public class HomeController {
 
 	@RequestMapping(value = { "/", "/home" })
-	public String homePage(Map<String, Object> model, WebRequest request, Principal principal) {
-
-		Object error = request.getAttribute("SPRING_SECURITY_403_EXCEPTION", WebRequest.SCOPE_REQUEST);
-		if (error != null) {
-			model.put("error", "You don't have access to this resource (" + error + ")");
-		}
+	public String homePage(Map<String, Object> model, Principal principal) {
 		model.put("message", "You are logged in.  Log out by sending a GET to the location provided.");
-		model.put("location", "http:" + request.getHeader("Host") + request.getContextPath() + "/logout.do");
 		model.put("principal", principal);
 		return "home";
-
 	}
 
 }
