164a64bf7d67118b28add4dde62deb065d56d6e8
Dave Syer <dsyer@vmware.com>
2012-09-27 11:36:02 +0100

    Return code for default vmc should be 302
    The problem turned out to be that I was running tests locally
    against a postgres database that had a non-default vmc registration.
    In this database vmc had a registered callback (by default it does
    not), and so the response was 401 because the redirect sent by the
    test did not match.
    
    [Fixes #36770647] UAA login authentication integration test fails (302 instead of 401)
    
    Change-Id: I6774716660ba3b0063fc62a826ed8c124eac1325

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/LoginServerSecurityIntegrationTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/LoginServerSecurityIntegrationTests.java
@@ -35,6 +35,7 @@ import org.springframework.security.oauth2.client.test.BeforeOAuth2Context;
 import org.springframework.security.oauth2.client.test.OAuth2ContextConfiguration;
 import org.springframework.security.oauth2.client.test.OAuth2ContextSetup;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitResourceDetails;
 import org.springframework.security.oauth2.common.util.RandomValueStringGenerator;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -153,7 +154,8 @@ public class LoginServerSecurityIntegrationTests {
 	@Test
 	@OAuth2ContextConfiguration(LoginClient.class)
 	public void testMissingUsernameIsError() throws Exception {
-		((RestTemplate) serverRunning.getRestTemplate()).setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+		((RestTemplate) serverRunning.getRestTemplate())
+				.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
 		params.set("client_id", testAccounts.getDefaultImplicitResource().getClientId());
 		params.remove("username");
 		// Some of the user info is there but not enough to determine a username
@@ -170,17 +172,20 @@ public class LoginServerSecurityIntegrationTests {
 	@Test
 	@OAuth2ContextConfiguration(LoginClient.class)
 	public void testWrongUsernameIsError() throws Exception {
-		((RestTemplate) serverRunning.getRestTemplate()).setRequestFactory(new HttpComponentsClientHttpRequestFactory());
-		params.set("client_id", testAccounts.getDefaultImplicitResource().getClientId());
+		((RestTemplate) serverRunning.getRestTemplate())
+				.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+		ImplicitResourceDetails resource = testAccounts.getDefaultImplicitResource();
+		params.set("client_id", resource.getClientId());
 		params.set("username", "bogus");
+		String redirect = resource.getPreEstablishedRedirectUri();
+		if (redirect != null) {
+			params.set("redirect_uri", redirect);
+		}
 		@SuppressWarnings("rawtypes")
 		ResponseEntity<Map> response = serverRunning.postForMap(serverRunning.getAuthorizationUri(), params, headers);
-		// TODO: should be 302
-		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-		// String results = response.getHeaders().getLocation().getFragment();
-		@SuppressWarnings("unchecked")
-		Map<String, String> results = response.getBody();
-		assertNotNull("There should be an error: " + results, results.containsKey("error"));
+		assertEquals(HttpStatus.FOUND, response.getStatusCode());
+		String results = response.getHeaders().getLocation().getFragment();
+		assertNotNull("There should be an error: " + results, results.contains("error"));
 	}
 
 	private static class LoginClient extends ClientCredentialsResourceDetails {
