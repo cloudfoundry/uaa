6b3b1d04e3a118b05dad537ab75153b92b897d24
Vidya Valmikinathan <vidya@rbcon.com>
2012-12-18 14:50:32 -0800

    [cfid-256] Implement group member roles as a legitimate SCIM extension.
    The SCIM spec allows for extension to the core schema in the form of new top-level
    attributes, but not sub-attributes for existing top-level attributes. This makes the
    'authorities' sub-attribute of the SCIM group's 'members' attribute a spec violation.
    
    This change [#38709619] makes the SCIM Groups API spec compliant by removing the
    authorities sub-attribute and instead adding top-level attributes for different type of
    group members, like 'readers', 'writers' etc.
    
    In addition, this change partially addresses [cfid-48][#35352689] - by having the custom
    JSON deserializer for ScimGroup treat field names in a case-insensitive manner.
    
    Change-Id: Icabb46ab2a2dcb3d3913bd6c48549e730d22a314

---
+++ common/src/main/java/org/cloudfoundry/identity/uaa/scim/util/json/ScimGroupJsonDeserializer.java
@@ -0,0 +1,71 @@
+package org.cloudfoundry.identity.uaa.scim.util.json;
+
+import org.cloudfoundry.identity.uaa.scim.ScimGroup;
+import org.cloudfoundry.identity.uaa.scim.ScimGroupMember;
+import org.cloudfoundry.identity.uaa.scim.ScimMeta;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonProcessingException;
+import org.codehaus.jackson.JsonToken;
+import org.codehaus.jackson.map.DeserializationContext;
+import org.codehaus.jackson.map.JsonDeserializer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ScimGroupJsonDeserializer extends JsonDeserializer<ScimGroup> {
+
+	@Override
+	public ScimGroup deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+		ScimGroup group = new ScimGroup();
+
+		List<ScimGroupMember> members = null;
+
+		Map<ScimGroup.Authority, List<ScimGroupMember>> roles = new HashMap<ScimGroup.Authority, List<ScimGroupMember>>();
+		for (ScimGroup.Authority authority : ScimGroup.Authority.values()) {
+			roles.put(authority, new ArrayList<ScimGroupMember>());
+		}
+
+		while(jp.nextToken() != JsonToken.END_OBJECT) {
+			if (jp.getCurrentToken() == JsonToken.FIELD_NAME) {
+				String fieldName = jp.getCurrentName();
+				jp.nextToken();
+
+				if ("id".equalsIgnoreCase(fieldName)) {
+					group.setId(jp.readValueAs(String.class));
+				} else if ("displayname".equalsIgnoreCase(fieldName)) {
+					group.setDisplayName(jp.readValueAs(String.class));
+				} else if ("meta".equalsIgnoreCase(fieldName)) {
+					group.setMeta(jp.readValueAs(ScimMeta.class));
+				} else if ("schemas".equalsIgnoreCase(fieldName)) {
+					group.setSchemas(jp.readValueAs(String[].class));
+				} else if ("members".equalsIgnoreCase(fieldName)) {
+					members = Arrays.asList(jp.readValueAs(ScimGroupMember[].class));
+				} else {
+					for (ScimGroup.Authority authority : ScimGroup.Authority.values()) {
+						if (fieldName.equalsIgnoreCase(authority.getRoleName()+"s")) {
+							roles.get(authority).addAll(Arrays.asList(jp.readValueAs(ScimGroupMember[].class)));
+						}
+					}
+				}
+			}
+		}
+
+		for (ScimGroupMember member : members) {
+			member.setAuthorities(new ArrayList<ScimGroup.Authority>());
+			for (ScimGroup.Authority authority : roles.keySet()) {
+				if (roles.get(authority).contains(member)) {
+					member.getAuthorities().add(authority);
+				}
+			}
+			if (!member.getAuthorities().contains(ScimGroup.Authority.READ)) {
+				member.getAuthorities().add(ScimGroup.Authority.READ);
+			}
+		}
+		group.setMembers(members);
+		return group;
+	}
+}
