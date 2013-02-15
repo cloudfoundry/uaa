de496b8f21998462567a90b30305f420d7b7c0cc
Dave Syer <dsyer@vmware.com>
2013-02-15 16:07:26 +0000

    Fix license header

--- common/src/main/java/org/cloudfoundry/identity/uaa/oauth/Claims.java
+++ common/src/main/java/org/cloudfoundry/identity/uaa/oauth/Claims.java
@@ -1,14 +1,14 @@
 /*
  * Cloud Foundry 2012.02.03 Beta
- * Copyright (c) [2009-2012] VMwarepublic static String Inc. All Rights Reserved.
+ * Copyright (c) [2009-2012] VMware, Inc. All Rights Reserved.
  *
- * This product is licensed to you under the Apache Licensepublic static String Version 2.0 (the "License").
+ * This product is licensed to you under the Apache License, Version 2.0 (the "License").
  * You may not use this product except in compliance with the License.
  *
  * This product includes a number of subcomponents with
  * separate copyright notices and license terms. Your use of these
  * subcomponents is subject to the terms and conditions of the
- * subcomponent's licensepublic static String as noted in the LICENSE file.
+ * subcomponent's license, as noted in the LICENSE file.
  */
 package org.cloudfoundry.identity.uaa.oauth;
 
