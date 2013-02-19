b1a95abb08694f5ed1658b9e145b542091ce41da
Joel D'sa <jdsa@vmware.com>
2013-02-19 14:19:03 -0800

    Fix the refresh token integration test

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/RefreshTokenSupportIntegrationTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/RefreshTokenSupportIntegrationTests.java
@@ -1,25 +1,32 @@
 /**
  * Cloud Foundry 2012.02.03 Beta Copyright (c) [2009-2012] VMware, Inc. All Rights Reserved.
- * 
+ *
  * This product is licensed to you under the Apache License, Version 2.0 (the "License"). You may not use this product
  * except in compliance with the License.
- * 
+ *
  * This product includes a number of subcomponents with separate copyright notices and license terms. Your use of these
  * subcomponents is subject to the terms and conditions of the subcomponent's license, as noted in the LICENSE file.
  */
 package org.cloudfoundry.identity.uaa.integration;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.Map;
 
+import org.cloudfoundry.identity.uaa.oauth.approval.Approval;
+import org.cloudfoundry.identity.uaa.oauth.approval.Approval.ApprovalStatus;
 import org.cloudfoundry.identity.uaa.test.TestAccountSetup;
 import org.cloudfoundry.identity.uaa.test.UaaTestAccounts;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -75,6 +82,18 @@ public class RefreshTokenSupportIntegrationTests {
 
 		// now use the refresh token to get a new access token.
 		assertNotNull(accessToken.getRefreshToken());
+
+		// add an approval for the scope requested
+		HttpHeaders approvalHeaders = new HttpHeaders();
+		approvalHeaders.set("Authorization", "bearer " + accessToken.getValue());
+		ResponseEntity<Approval[]> approvals = serverRunning.getRestTemplate().exchange(
+				serverRunning.getUrl("/approvals"),
+				HttpMethod.PUT,
+				new HttpEntity<Approval[]>((new Approval[]{new Approval(resource.getUsername(), resource.getClientId(),
+						"cloud_controller.read", 50000, ApprovalStatus.APPROVED)}), approvalHeaders), Approval[].class);
+
+		assertEquals(HttpStatus.OK, approvals.getStatusCode());
+
 		formData = new LinkedMultiValueMap<String, String>();
 		formData.add("grant_type", "refresh_token");
 		formData.add("refresh_token", accessToken.getRefreshToken().getValue());
