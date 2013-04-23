21791fc35cb43feb7ee5de05d8c6baf638ade105
Dave Syer <dsyer@vmware.com>
2013-04-23 13:47:53 +0100

    Fix gatling maven config

--- gatling/src/test/scala/Engine.scala
+++ gatling/src/test/scala/Engine.scala
@@ -1,14 +1,13 @@
 import com.excilys.ebi.gatling.app.Gatling
 import com.excilys.ebi.gatling.core.config.GatlingPropertiesBuilder
-import com.excilys.ebi.gatling.core.util.PathHelper.path2string
 
 object Engine extends App {
 	
 	val props = new GatlingPropertiesBuilder
-	props.dataDirectory(IDEPathHelper.dataDirectory)
-	props.resultsDirectory(IDEPathHelper.resultsDirectory)
-	props.requestBodiesDirectory(IDEPathHelper.requestBodiesDirectory)
-	props.binariesDirectory(IDEPathHelper.mavenBinariesDirectory)
+	props.dataDirectory(IDEPathHelper.dataDirectory.toString)
+	props.resultsDirectory(IDEPathHelper.resultsDirectory.toString)
+	props.requestBodiesDirectory(IDEPathHelper.requestBodiesDirectory.toString)
+	props.binariesDirectory(IDEPathHelper.mavenBinariesDirectory.toString)
 
 	Gatling.fromMap(props.build)
 }
