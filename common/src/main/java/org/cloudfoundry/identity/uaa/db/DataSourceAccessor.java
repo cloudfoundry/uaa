2e322d0c4c0a500e0aed8ce5763aa1f563321320
Filip Hanik <fhanik@gopivotal.com>
2014-03-28 07:27:17 -0600

    https://www.pivotaltracker.com/n/projects/997278/stories/67714674 Reapplying the DDL to postgres to bring legacy databases up to date requires that we do it without a transaction.

--- common/src/main/java/org/cloudfoundry/identity/uaa/db/DataSourceAccessor.java
+++ common/src/main/java/org/cloudfoundry/identity/uaa/db/DataSourceAccessor.java
@@ -10,27 +10,23 @@
  *     subcomponents is subject to the terms and conditions of the
  *     subcomponent's license, as noted in the LICENSE file.
  *******************************************************************************/
-package org.cloudfoundry.identity.uaa.db.postgresql;
+package org.cloudfoundry.identity.uaa.db;
 
-import java.sql.Connection;
+import javax.sql.DataSource;
 
-import org.cloudfoundry.identity.uaa.db.DataSourceAccessor;
-import org.cloudfoundry.identity.uaa.db.InitialPreDatabaseVersioningSchemaCreator;
+public class DataSourceAccessor {
 
-public class V1_5_3__InitialDBScript extends InitialPreDatabaseVersioningSchemaCreator {
-    public V1_5_3__InitialDBScript() {
-        super("postgresql");
-    }
-
-    @Override
-    public void migrate(Connection connection) throws Exception {
-        Connection con = DataSourceAccessor.getDataSource().getConnection();
-        try {
-            super.migrate(con);
-        } finally {
-            try { con.close(); } catch (Exception ignore) {}
-        }
+    private static DataSource dataSource;
+    
+    public DataSourceAccessor() {
     }
     
+    public void setDataSource(DataSource ds) {
+        dataSource = ds;
+    }
     
+    public static DataSource getDataSource() {
+        return dataSource;
+    }
+
 }
