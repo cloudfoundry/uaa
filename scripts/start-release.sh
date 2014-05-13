1e08645c6a963a3ee119a06edcce7167e9202863
Filip Hanik and Philip Kuryloski <pair+fhanik+pkuryloski@pivotallabs.com>
2014-05-13 10:04:14 -0700

    Add deploy scripts
    [#71195408] https://www.pivotaltracker.com/story/show/71195408

---
+++ scripts/start-release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+cd `dirname $0`/..
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $(basename $0) uaa_release_version"
+fi
+
+echo Creating UAA release $1
+
+set -x
+
+git checkout develop
+git checkout -b releases/$1
+./scripts/set-version.sh $1 $2
+git commit -am "Bump release version to $1"
+git push --set-upstream origin releases/$1
+
+set +x
+
+echo Release branch created from develop branch
+echo
+echo Check the version number changes and ammend if necessary
+echo
+echo Deploy and finish the release with deploy-and-finish-release.sh
