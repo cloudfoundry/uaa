1bf45068c09d2be01e0f343a72134133919f0cd7
Filip Hanik <fhanik@gopivotal.com>
2014-06-09 17:03:11 -0700

    [#72731888] https://www.pivotaltracker.com/story/show/72731888 Implement proper SCIM filter parsing by using an external library. Turns out we have been doing it wrong. There is still an instance where we may have to conver a JSON string to a Java string properly. Comparisons are case insensitive. So all LOWER functions must be used, but may invalidate any indices

--- scim/src/test/java/org/cloudfoundry/identity/uaa/scim/jdbc/ScimSearchQueryConverterTests.java
+++ scim/src/test/java/org/cloudfoundry/identity/uaa/scim/jdbc/ScimSearchQueryConverterTests.java
@@ -12,66 +12,68 @@
  *******************************************************************************/
 package org.cloudfoundry.identity.uaa.scim.jdbc;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import org.cloudfoundry.identity.uaa.rest.SimpleAttributeNameMapper;
+import org.cloudfoundry.identity.uaa.rest.jdbc.SearchQueryConverter.ProcessedFilter;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.cloudfoundry.identity.uaa.rest.SimpleAttributeNameMapper;
-import org.cloudfoundry.identity.uaa.rest.jdbc.SearchQueryConverter.ProcessedFilter;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ScimSearchQueryConverterTests {
 
     private ScimSearchQueryConverter filterProcessor = new ScimSearchQueryConverter();
 
-    @Test
-    public void canConvertValidFilters() throws Exception {
-        validate(filterProcessor.convert("username pr", null, false), "username is not null", 0);
-        validate(filterProcessor.convert("username eq 'joe'", null, false), "lower(username) = :value0", 1);
-        validate(filterProcessor.convert("username eq 'bar", null, false), "username = 'bar", 0);
-        validate(filterProcessor.convert("displayName eq \"openid\"", null, false), "lower(displayName) = :value0", 1);
-        validate(filterProcessor.convert("USERNAME eq 'joe'", null, false), "lower(USERNAME) = :value0", 1);
-        validate(filterProcessor.convert("username EQ 'joe'", null, false), "lower(username) = :value0", 1);
-        validate(filterProcessor.convert("username eq 'Joe'", null, false), "lower(username) = :value0", 1);
-        validate(filterProcessor.convert("displayName co 'write'", null, false), "lower(displayName) like :value0", 1);
-        validate(filterProcessor.convert("displayName sw 'scim.'", null, false), "lower(displayName) like :value0", 1);
-        validate(filterProcessor.convert("username gt 'joe'", null, false), "username > 'joe'", 0);
-        validate(filterProcessor.convert("userName eq 'joe' and meta.version eq 0", null, false),
-                        "lower(userName) = :value0 and version = 0", 1);
-        validate(filterProcessor.convert("meta.created gt '1970-01-01T00:00:00.000Z'", null, false),
-                        "created > :value0", 1);
-        validate(filterProcessor.convert("username pr and active eq true", null, false),
-                        "username is not null and active = :value0", 1);
-        validate(filterProcessor.convert("username pr", "username", true),
-                        "username is not null order by username asc", 0);
-        validate(filterProcessor.convert("displayName pr", "displayName", false),
-                        "displayName is not null order by displayName desc", 0);
-        validate(filterProcessor.convert("username pr and emails.value co '.com'", null, false),
-                        "username is not null and emails.lower(value) like :value0", 1);
-        validate(filterProcessor.convert("username eq 'joe' or emails.value co '.com'", null, false),
-                        "lower(username) = :value1 or emails.lower(value) like :value0", 2);
-    }
-
-    @Test
-    public void canConvertWithReplacePatterns() {
-        Map<String, String> replaceWith = new HashMap<String, String>();
+    @Before
+    public void setUp() {
+        Map<String, String> replaceWith = new HashMap<>();
         replaceWith.put("emails\\.value", "email");
         replaceWith.put("groups\\.display", "authorities");
         replaceWith.put("phoneNumbers\\.value", "phoneNumber");
         filterProcessor.setAttributeNameMapper(new SimpleAttributeNameMapper(replaceWith));
+    }
 
-        validate(filterProcessor.convert("emails.value sw 'joe'", null, false), "lower(email) like :value0", 1);
-        validate(filterProcessor.convert("groups.display co 'org.foo'", null, false),
-                        "lower(authorities) like :value0", 1);
-        validate(filterProcessor.convert("phoneNumbers.value sw '+1-222'", null, false),
-                        "lower(phoneNumber) like :value0", 1);
-        validate(filterProcessor.convert("username pr", "emails.value", true),
-                        "username is not null order by email asc", 0);
-        validate(filterProcessor.convert("emails.type eq 'bar'", "emails.type", false),
-                        "emails.lower(type) = :value0 order by emails.type desc", 1);
+    @Test
+    public void canConvertValidFilters() throws Exception {
+        validate(filterProcessor.convert("username pr", null, false), "username IS NOT NULL", 0);
+        validate(filterProcessor.convert("username eq \"joe\"", null, false), "LOWER(username) = LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("username eq \"'bar\"", null, false), "LOWER(username) = LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("displayName eq \"openid\"", null, false), "LOWER(displayName) = LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("USERNAME eq \"joe\"", null, false), "LOWER(USERNAME) = LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("username EQ \"joe\"", null, false), "LOWER(username) = LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("username eq \"Joe\"", null, false), "LOWER(username) = LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("username eq \"Joe\"", null, false), "LOWER(username) = LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("displayName co \"write\"", null, false), "LOWER(displayName) LIKE LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("displayName sw \"scim.\"", null, false), "LOWER(displayName) LIKE LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("username gt \"joe\"", null, false), "LOWER(username) > LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("userName eq \"joe\" and meta.version eq 0", null, false),"(LOWER(userName) = LOWER(:__value_0) AND version = :__value_1)", 2);
+        validate(filterProcessor.convert("meta.created gt \"1970-01-01T00:00:00.000Z\"", null, false),"created > :__value_0", 1);
+        validate(filterProcessor.convert("username pr and active eq true", null, false),"(username IS NOT NULL AND active = :__value_0)", 1);
+        validate(filterProcessor.convert("username pr", "username", true),"username IS NOT NULL ORDER BY username ASC", 0);
+        validate(filterProcessor.convert("displayName pr", "displayName", false),"displayName IS NOT NULL ORDER BY displayName DESC", 0);
+        validate(filterProcessor.convert("username pr and emails.value co \".com\"", null, false),"(username IS NOT NULL AND LOWER(email) LIKE LOWER(:__value_0))", 1);
+        validate(filterProcessor.convert("username eq \"joe\" or emails.value co \".com\"", null, false),"(LOWER(username) = LOWER(:__value_0) OR LOWER(email) LIKE LOWER(:__value_1))", 2);
+    }
 
+    @Test
+    public void canConvertWithReplacePatterns() {
+
+
+        validate(filterProcessor.convert("emails.value sw \"joe\"", null, false), "LOWER(email) LIKE LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("groups.display co \"org.foo\"", null, false),"LOWER(authorities) LIKE LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("phoneNumbers.value sw \"+1-222\"", null, false),"LOWER(phoneNumber) LIKE LOWER(:__value_0)", 1);
+        validate(filterProcessor.convert("username pr", "emails.value", true),"username IS NOT NULL ORDER BY email ASC", 0);
+        
+
+    }
+
+    @Test
+    public void testFilterWithApostrophe() throws Exception {
+        validate(filterProcessor.convert("username eq \"marissa'@test.org\"", null, false),
+                "LOWER(username) = LOWER(:__value_0)", 1);
     }
 
     private void validate(ProcessedFilter filter, String expectedSql, int expectedParamCount) {
@@ -79,5 +81,4 @@ public class ScimSearchQueryConverterTests {
         assertEquals(expectedSql, filter.getSql());
         assertEquals(expectedParamCount, filter.getParams().size());
     }
-
 }
