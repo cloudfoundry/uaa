3496205d9cae1341499e66efe74e62af37b23553
Filip Hanik <fhanik@gopivotal.com>
2014-07-10 18:05:21 +0200

    Formally override any profile that may be passed in. And reset after the end of the test

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/ldap/LdapMockMvcTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/ldap/LdapMockMvcTests.java
@@ -106,11 +106,7 @@ public class LdapMockMvcTests {
     @BeforeClass
     public static void startApacheDS() throws Exception {
         originalProfile = System.getProperty(SPRING_PROFILES_ACTIVE);
-        if (originalProfile==null || originalProfile.trim().length()==0) {
-            System.setProperty(SPRING_PROFILES_ACTIVE, "ldap,default");
-        } else {
-            System.setProperty(SPRING_PROFILES_ACTIVE, originalProfile+",ldap");
-        }
+        System.setProperty(SPRING_PROFILES_ACTIVE, "ldap,default");
         System.out.println("LdapMockMvcTests Profile:"+System.getProperty(SPRING_PROFILES_ACTIVE));
         tmpDir = new File(System.getProperty("java.io.tmpdir")+"/apacheds/"+new RandomValueStringGenerator().generate());
         tmpDir.deleteOnExit();
