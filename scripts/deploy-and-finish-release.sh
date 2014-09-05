5fd34bb349677337ba9bdb96ddd131ce79a2974b
Filip Hanik <fhanik@pivotal.io>
2014-09-04 20:42:29 -0600

    git diff should be non interactive

--- scripts/deploy-and-finish-release.sh
+++ scripts/deploy-and-finish-release.sh
@@ -22,7 +22,7 @@ git merge releases/$1 --no-ff -m "Merge branch 'releases/$1' into develop"
 git branch -d releases/$1
 ./scripts/set-version.sh $2
 git commit -am "Bump next developer version"
-git diff origin/develop
+git diff --exit-code origin/develop
 git push origin develop
 
 set +x
