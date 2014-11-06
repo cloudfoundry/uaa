f03ce2ce8e92a441d253e440f0ca9b5a6b7124ee
Will Tran <wtran@pivotal.io>
2014-11-06 16:37:23 -0500

    added validation routine which will error out if any id column data won't fint into char(36) ascii

--- common/src/main/resources/org/cloudfoundry/identity/uaa/db/mysql/V2_0_1__CleanUpIdColumnTypes.sql
+++ common/src/main/resources/org/cloudfoundry/identity/uaa/db/mysql/V2_0_1__CleanUpIdColumnTypes.sql
@@ -1,5 +1,40 @@
 -- making everything CHAR(36) ascii will make indexes 1/3 the size,
 -- so you can do more in memory and comparisons are proportionately faster.
+
+-- first validate all the values will fit into char(36) ascii
+DROP TABLE IF EXISTS uuid_validate;
+CREATE TABLE uuid_validate (
+  id CHAR(36) CHARACTER SET ascii
+);
+
+INSERT INTO `uuid_validate` SELECT `user_id` FROM `authz_approvals`; 
+INSERT INTO `uuid_validate` SELECT `client_id` FROM `authz_approvals`; 
+ 
+INSERT INTO `uuid_validate` SELECT `client_id` FROM `client_idp`; 
+INSERT INTO `uuid_validate` SELECT `identity_provider_id` FROM `client_idp`; 
+
+INSERT INTO `uuid_validate` SELECT `group_id` FROM `external_group_mapping`; 
+
+INSERT INTO `uuid_validate` SELECT `group_id` FROM `group_membership`; 
+INSERT INTO `uuid_validate` SELECT `member_id` FROM `group_membership`; 
+
+INSERT INTO `uuid_validate` SELECT `id` FROM `groups`; 
+
+INSERT INTO `uuid_validate` SELECT `id` FROM `identity_provider`; 
+INSERT INTO `uuid_validate` SELECT `identity_zone_id` FROM `identity_provider`; 
+
+INSERT INTO `uuid_validate` SELECT `id` FROM `identity_zone`; 
+INSERT INTO `uuid_validate` SELECT `service_instance_id` FROM `identity_zone`; 
+
+INSERT INTO `uuid_validate` SELECT `client_id` FROM `oauth_client_details`; 
+INSERT INTO `uuid_validate` SELECT `identity_zone_id` FROM `oauth_client_details`; 
+
+INSERT INTO `uuid_validate` SELECT `id` FROM `users`; 
+INSERT INTO `uuid_validate` SELECT `identity_provider_id` FROM `users`; 
+
+DROP TABLE uuid_validate;
+
+-- now alter columns
 ALTER TABLE `authz_approvals` 
 MODIFY COLUMN `user_id` CHAR(36) CHARACTER SET ascii,
 MODIFY COLUMN `client_id` CHAR(36) CHARACTER SET ascii;
