0c8983f87001a0df1094e05efd53c69430c3facb
Will Tran <wtran@pivotallabs.com>
2014-11-28 15:34:53 -0500

    Running ID column cleanup at the very end so we can keep writing mysql migrations the same way (varcahr(36))

--- common/src/main/java/org/cloudfoundry/identity/uaa/db/mysql/V2_0_9__CleanUpIdColumnTypes.java
+++ common/src/main/java/org/cloudfoundry/identity/uaa/db/mysql/V2_0_9__CleanUpIdColumnTypes.java
@@ -10,12 +10,12 @@ import org.springframework.jdbc.datasource.init.ScriptException;
 
 import com.googlecode.flyway.core.api.migration.jdbc.JdbcMigration;
 
-public class V2_0_1__CleanUpIdColumnTypes implements JdbcMigration {
-    private static final Log logger = LogFactory.getLog(V2_0_1__CleanUpIdColumnTypes.class);
+public class V2_0_9__CleanUpIdColumnTypes implements JdbcMigration {
+    private static final Log logger = LogFactory.getLog(V2_0_9__CleanUpIdColumnTypes.class);
     @Override
     public void migrate(Connection connection) throws Exception {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScript(new ClassPathResource("org/cloudfoundry/identity/uaa/db/mysql/V2_0_1__CleanUpIdColumnTypes.txt"));
+        populator.addScript(new ClassPathResource(V2_0_9__CleanUpIdColumnTypes.class.getName().replace('.', '/')+".txt"));
         populator.setIgnoreFailedDrops(true);
         try {
             populator.populate(connection);
--- common/src/main/resources/org/cloudfoundry/identity/uaa/db/mysql/V2_0_3__PostBootstrapIdentityZones.sql
+++ common/src/main/resources/org/cloudfoundry/identity/uaa/db/mysql/V2_0_3__PostBootstrapIdentityZones.sql
@@ -1,2 +1,2 @@
-ALTER TABLE oauth_client_details MODIFY COLUMN `identity_zone_id` char(36) NOT NULL;
-ALTER TABLE users MODIFY COLUMN `identity_zone_id` CHAR(36) CHARACTER SET ascii NOT NULL;
+ALTER TABLE oauth_client_details MODIFY COLUMN `identity_zone_id` varchar(36) NOT NULL;
+ALTER TABLE users MODIFY COLUMN `identity_zone_id` varchar(36) NOT NULL;
