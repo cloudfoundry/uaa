7c019adfca6d085a7ade09574a52cc3608157017
Will Tran <wtran@pivotal.io>
2014-12-12 16:46:22 -0800

    Refactor sample app.
    [#72723618] https://www.pivotaltracker.com/story/show/72723618
    
    Signed-off-by: Madhura Bhave <mbhave@pivotal.io>

--- samples/oauth-showcase/src/main/java/org/cloudfoundry/identity/oauth2showcase/Application.java
+++ samples/oauth-showcase/src/main/java/org/cloudfoundry/identity/oauth2showcase/Application.java
@@ -2,47 +2,29 @@ package org.cloudfoundry.identity.oauth2showcase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.security.oauth2.sso.EnableOAuth2Sso;
-import org.springframework.cloud.security.oauth2.sso.OAuth2SsoConfigurerAdapter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
-import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
-import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.security.web.util.UrlUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-@EnableConfigurationProperties
-@EnableOAuth2Sso
+@Controller
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    OAuth2SsoConfigurerAdapter oAuth2SsoConfigurerAdapter() {
-        return new OAuth2SsoConfigurerAdapter() {
-            @Override
-            public void match(RequestMatchers matchers) {
-                matchers.antMatchers("/authorization_code/**");
-            }
-
-            public void configure(HttpSecurity http) throws Exception {
-                http.authorizeRequests()
-                    .antMatchers("/").permitAll();
-            }
-        };
+    @RequestMapping("/")
+    public String index(HttpServletRequest request, Model model) {
+        request.getSession().invalidate();
+        model.addAttribute("thisUrl", UrlUtils.buildFullRequestUrl(request));
+        return "index";
     }
 }
--- samples/oauth-showcase/src/main/java/org/cloudfoundry/identity/oauth2showcase/ClientCredentials.java
+++ samples/oauth-showcase/src/main/java/org/cloudfoundry/identity/oauth2showcase/ClientCredentials.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
@@ -33,6 +34,7 @@ public class ClientCredentials {
     }
 
     @Configuration
+    @EnableConfigurationProperties
     @EnableOAuth2Client
     public static class Config {
         @Bean
