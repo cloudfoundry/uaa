ec13c27e13889e91b7c48684389ccdcf8dcf6e0c
Chris Dutra <cdeoliveira@pivotal.io>
2015-01-16 10:12:30 -0800

    Add tests for admin driven user management in zones
    [#82406754] https://www.pivotaltracker.com/story/show/82406754
    
    Signed-off-by: Madhura Bhave <mbhave@pivotal.io>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/zones/IdentityZoneEndpointsMockMvcTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/zones/IdentityZoneEndpointsMockMvcTests.java
@@ -1,38 +1,22 @@
 package org.cloudfoundry.identity.uaa.mock.zones;
 
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.UUID;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import com.googlecode.flyway.core.Flyway;
 import org.apache.commons.codec.binary.Base64;
 import org.cloudfoundry.identity.uaa.authentication.Origin;
-import org.cloudfoundry.identity.uaa.scim.ScimGroup;
-import org.cloudfoundry.identity.uaa.scim.ScimGroupMember;
-import org.cloudfoundry.identity.uaa.scim.ScimGroupMembershipManager;
-import org.cloudfoundry.identity.uaa.scim.ScimGroupProvisioning;
 import org.cloudfoundry.identity.uaa.scim.ScimUser;
-import org.cloudfoundry.identity.uaa.scim.ScimUserProvisioning;
-import org.cloudfoundry.identity.uaa.scim.endpoints.ScimUserEndpoints;
 import org.cloudfoundry.identity.uaa.test.TestClient;
 import org.cloudfoundry.identity.uaa.test.YamlServletProfileInitializerContextInitializer;
+import org.cloudfoundry.identity.uaa.util.SetServerNameRequestPostProcessor;
 import org.cloudfoundry.identity.uaa.zone.IdentityProvider;
 import org.cloudfoundry.identity.uaa.zone.IdentityProviderProvisioning;
 import org.cloudfoundry.identity.uaa.zone.IdentityZone;
 import org.cloudfoundry.identity.uaa.zone.IdentityZoneCreationRequest;
 import org.cloudfoundry.identity.uaa.zone.IdentityZoneHolder;
 import org.cloudfoundry.identity.uaa.zone.MultitenancyFixture;
+import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -46,16 +30,33 @@ import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.StringUtils;
 import org.springframework.web.context.support.XmlWebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class IdentityZoneEndpointsMockMvcTests {
     private static XmlWebApplicationContext webApplicationContext;
     private static MockMvc mockMvc;
     private static String identityAdminToken = null;
     private static TestClient testClient = null;
+    private RandomValueStringGenerator generator = new RandomValueStringGenerator();
 
     @BeforeClass
     public static void setUp() throws Exception {
@@ -91,39 +92,40 @@ public class IdentityZoneEndpointsMockMvcTests {
         IdentityZoneHolder.clear();
     }
 
-    private ScimUser createUser(String scopes) throws Exception {
+    private ScimUser createUser(String token, String subdomain) throws Exception {
+        ScimUser user = getScimUser();
 
-        ScimUserProvisioning userProvisioning = webApplicationContext.getBean(ScimUserProvisioning.class);
-        ScimGroupProvisioning groupProvisioning = webApplicationContext.getBean(ScimGroupProvisioning.class);
-        ScimGroupMembershipManager membershipManager = webApplicationContext.getBean(ScimGroupMembershipManager.class);
-        ScimUserEndpoints userEndpoints = webApplicationContext.getBean(ScimUserEndpoints.class);
+        byte[] requestBody = new ObjectMapper().writeValueAsBytes(user);
+        MockHttpServletRequestBuilder post = post("/Users")
+                .header("Authorization", "Bearer " + token)
+                .contentType(APPLICATION_JSON)
+                .content(requestBody);
+        if (subdomain != null && !subdomain.equals("")) post.with(new SetServerNameRequestPostProcessor(subdomain + ".localhost"));
 
-        //create the scope
-        List<ScimGroup> scopeGroups = new LinkedList<>();
-        for (String scope : StringUtils.commaDelimitedListToSet(scopes)) {
-            ScimGroup group = new ScimGroup(new RandomValueStringGenerator().generate(), scope);
-            scopeGroups.add(groupProvisioning.create(group));
-        }
+        MvcResult result = mockMvc.perform(post)
+                .andExpect(status().isCreated())
+                .andExpect(header().string("ETag", "\"0\""))
+                .andExpect(jsonPath("$.userName").value(user.getUserName()))
+                .andExpect(jsonPath("$.emails[0].value").value(user.getUserName()))
+                .andExpect(jsonPath("$.name.familyName").value(user.getFamilyName()))
+                .andExpect(jsonPath("$.name.givenName").value(user.getGivenName()))
+                .andReturn();
 
-        //create the user
-        String username = new RandomValueStringGenerator().generate();
-        ScimUser user = new ScimUser(null, username, "Given", "Family");
-        user.setPrimaryEmail(username+"@test.org");
-        user.setOrigin(Origin.UAA);
-        user = userProvisioning.createUser(user, "secret");
+        return new ObjectMapper().readValue(result.getResponse().getContentAsString(), ScimUser.class);
+    }
 
-        //associate user with scope
-        for (ScimGroup group : scopeGroups) {
-            ScimGroupMember member = new ScimGroupMember(user.getId());
-            membershipManager.addMember(group.getId(), member);
-        }
-
-        return (ScimUser)userEndpoints.findUsers(null, "username eq \""+user.getUserName()+"\"", null, "ascending", 0, 100).getResources().iterator().next();
+    private ScimUser getScimUser() {
+        String email = "joe@"+generator.generate().toLowerCase()+".com";
+        ScimUser user = new ScimUser();
+        user.setUserName(email);
+        user.setName(new ScimUser.Name("Joe", "User"));
+        user.addEmail(email);
+        return user;
     }
 
     @Test
     public void testCreateZone() throws Exception {
-        String id = new RandomValueStringGenerator().generate();
+        String id = generator.generate();
         MvcResult result = createZone(id, HttpStatus.CREATED, identityAdminToken);
         IdentityZone zone = new ObjectMapper().readValue(result.getResponse().getContentAsByteArray(), IdentityZone.class);
         assertEquals(id, zone.getId());
@@ -132,20 +134,20 @@ public class IdentityZoneEndpointsMockMvcTests {
 
     @Test
     public void testCreateDuplicateZoneReturns409() throws Exception {
-        String id = new RandomValueStringGenerator().generate();
+        String id = generator.generate();
         createZone(id, HttpStatus.CREATED, identityAdminToken);
         createZone(id, HttpStatus.CONFLICT, identityAdminToken);
     }
 
     @Test
     public void testUpdateNonExistentReturns404() throws Exception {
-        String id = new RandomValueStringGenerator().generate();
+        String id = generator.generate();
         updateZone(id, HttpStatus.NOT_FOUND, identityAdminToken);
     }
 
     @Test
     public void testUpdateExistentReturns200() throws Exception {
-        String id = new RandomValueStringGenerator().generate();
+        String id = generator.generate();
         createZone(id, HttpStatus.CREATED, identityAdminToken);
         updateZone(id, HttpStatus.OK, identityAdminToken);
     }
@@ -300,6 +302,135 @@ public class IdentityZoneEndpointsMockMvcTests {
                 .andExpect(status().isOk())
                 .andReturn();
         
+    }
+
+    @Test
+    public void testSuccessfulUserManagementInZone() throws Exception {
+        String subdomain = generator.generate();
+        createOtherIdentityZone(subdomain);
+        String zoneAdminToken = testClient.getClientCredentialsOAuthAccessToken("admin", "admin-secret", "scim.write,scim.read", subdomain);
+        ScimUser user = createUser(zoneAdminToken, subdomain);
+
+        user.setUserName("updated-username@test.com");
+        MockHttpServletRequestBuilder put = put("/Users/" + user.getId())
+                                            .header("Authorization", "Bearer " + zoneAdminToken)
+                                            .header("If-Match", "\"" + user.getVersion() + "\"")
+                                            .contentType(APPLICATION_JSON)
+                                            .content(new ObjectMapper().writeValueAsString(user))
+                                            .with(new SetServerNameRequestPostProcessor(subdomain + ".localhost"));
+
+        MvcResult result = mockMvc.perform(put)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userName").value(user.getUserName()))
+                .andReturn();
+        user = new ObjectMapper().readValue(result.getResponse().getContentAsString(), ScimUser.class);
+
+        List<ScimUser> users = getUsersInZone(subdomain, zoneAdminToken);
+        assertTrue(users.contains(user));
+        assertEquals(1, users.size());
+
+        MockHttpServletRequestBuilder delete = delete("/Users/" + user.getId())
+                .header("Authorization", "Bearer " + zoneAdminToken)
+                .header("If-Match", "\"" + user.getVersion() + "\"")
+                .contentType(APPLICATION_JSON)
+                .with(new SetServerNameRequestPostProcessor(subdomain + ".localhost"));
+
+        mockMvc.perform(delete)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(user.getId()))
+                .andReturn();
+
+        users = getUsersInZone(subdomain, zoneAdminToken);
+        assertEquals(0, users.size());
+    }
+
+    private List<ScimUser> getUsersInZone(String subdomain, String token) throws Exception {
+        MockHttpServletRequestBuilder get = get("/Users").header("Authorization", "Bearer " + token);
+        if (subdomain != null && !subdomain.equals("")) get.with(new SetServerNameRequestPostProcessor(subdomain + ".localhost"));
+
+        MvcResult mvcResult = mockMvc.perform(get).andExpect(status().isOk()).andReturn();
+
+        JsonNode root = new ObjectMapper().readTree(mvcResult.getResponse().getContentAsString());
+        return new ObjectMapper().readValue(root.get("resources").toString(), new TypeReference<List<ScimUser>>() {});
+    }
+
+    @Test
+    public void testCreateAndListUsersInOtherZoneIsUnauthorized() throws Exception {
+        String subdomain = generator.generate();
+        createOtherIdentityZone(subdomain);
+
+        String zoneAdminToken = testClient.getClientCredentialsOAuthAccessToken("admin", "adminsecret", "scim.write,scim.read", null);
+
+        ScimUser user = getScimUser();
+
+        byte[] requestBody = new ObjectMapper().writeValueAsBytes(user);
+        MockHttpServletRequestBuilder post = post("/Users")
+                .with(new SetServerNameRequestPostProcessor(subdomain + ".localhost"))
+                .header("Authorization", "Bearer " + zoneAdminToken)
+                .contentType(APPLICATION_JSON)
+                .content(requestBody);
+
+        mockMvc.perform(post).andExpect(status().isUnauthorized());
+
+        MockHttpServletRequestBuilder get = get("/Users").header("Authorization", "Bearer " + zoneAdminToken);
+        if (subdomain != null && !subdomain.equals("")) get.with(new SetServerNameRequestPostProcessor(subdomain + ".localhost"));
+
+        mockMvc.perform(get).andExpect(status().isUnauthorized()).andReturn();
+    }
+
+    @Test
+    public void testModifyandDeleteUserInOtherZoneIsUnauthorized() throws Exception {
+        String defaultZoneAdminToken = testClient.getClientCredentialsOAuthAccessToken("admin", "adminsecret", "scim.write", null);
+        ScimUser user = createUser(defaultZoneAdminToken, null);
+
+        String subdomain = generator.generate();
+        createOtherIdentityZone(subdomain);
+
+        String zoneAdminToken = testClient.getClientCredentialsOAuthAccessToken("admin", "admin-secret", "scim.write,scim.read", subdomain);
+        user.setUserName("updated-user@defaultzone.com");
+
+        MockHttpServletRequestBuilder put = put("/Users/" + user.getId())
+                .header("Authorization", "Bearer " + zoneAdminToken)
+                .header("If-Match", "\"" + user.getVersion() + "\"")
+                .contentType(APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(user));
+
+        mockMvc.perform(put)
+                .andExpect(status().isUnauthorized())
+                .andReturn();
+
+        MockHttpServletRequestBuilder delete = delete("/Users/" + user.getId())
+                .header("Authorization", "Bearer " + zoneAdminToken)
+                .header("If-Match", "\"" + user.getVersion() + "\"")
+                .contentType(APPLICATION_JSON);
+
+        mockMvc.perform(delete)
+                .andExpect(status().isUnauthorized())
+                .andReturn();
+    }
+
+    private IdentityZone createOtherIdentityZone(String subdomain) throws Exception {
+
+        String identityToken = testClient.getClientCredentialsOAuthAccessToken("identity", "identitysecret", "zones.create", null);
+
+        IdentityZone identityZone = MultitenancyFixture.identityZone(subdomain, subdomain);
+        IdentityZoneCreationRequest creationRequest = new IdentityZoneCreationRequest();
+        creationRequest.setIdentityZone(identityZone);
+
+        List<BaseClientDetails> clientDetails = new ArrayList<>();
+        BaseClientDetails client = new BaseClientDetails("admin", null,null, "client_credentials", "clients.admin,scim.read,scim.write");
+        client.setClientSecret("admin-secret");
+        clientDetails.add(client);
+        creationRequest.setClientDetails(clientDetails);
+
+        mockMvc.perform(post("/identity-zones")
+                .header("Authorization", "Bearer " + identityToken)
+                .contentType(APPLICATION_JSON)
+                .accept(APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(creationRequest)))
+                .andExpect(status().isCreated());
+
+        return identityZone;
     }
     
     private String getBasicAuthHeaderValue(String clientId, String clientSecret) {
