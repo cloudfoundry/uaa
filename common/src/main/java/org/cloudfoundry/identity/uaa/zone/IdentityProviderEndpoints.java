2defa0a5aee38aea57cfd22396d9b26764a4d930
Chris Dutra <cdutra@pivotal.io>
2015-02-03 09:54:37 -0800

    Fix starred import. [#84601188] https://www.pivotaltracker.com/story/show/84601188
    Signed-off-by: Rob Gallagher <rgallagher@pivotal.io>

--- common/src/main/java/org/cloudfoundry/identity/uaa/zone/IdentityProviderEndpoints.java
+++ common/src/main/java/org/cloudfoundry/identity/uaa/zone/IdentityProviderEndpoints.java
@@ -17,12 +17,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-import static org.springframework.web.bind.annotation.RequestMethod.*;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 @RequestMapping("/identity-providers/")
 @RestController
