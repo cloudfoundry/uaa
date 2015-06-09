784918272e2d9e522b4b906f3f5188bbe4733a02
Filip Hanik <fhanik@pivotal.io>
2015-06-09 20:35:17 +0200

    NotificationService should get local access token https://www.pivotaltracker.com/story/show/96504282 [#96504282]

---
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/login/util/LocalUaaRestTemplateMockMvcTests.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ *     Cloud Foundry
+ *     Copyright (c) [2009-2014] Pivotal Software, Inc. All Rights Reserved.
+ *
+ *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ *     You may not use this product except in compliance with the License.
+ *
+ *     This product includes a number of subcomponents with
+ *     separate copyright notices and license terms. Your use of these
+ *     subcomponents is subject to the terms and conditions of the
+ *     subcomponent's license, as noted in the LICENSE file.
+ *******************************************************************************/
+package org.cloudfoundry.identity.uaa.login.util;
+
+
+import org.cloudfoundry.identity.uaa.mock.InjectedMockContextTest;
+import org.junit.Test;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Method;
+import java.net.URI;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class LocalUaaRestTemplateMockMvcTests extends InjectedMockContextTest {
+
+    @Test
+    public void testLocalUaaRestTemplateAcquireToken() {
+        LocalUaaRestTemplate restTemplate = getWebApplicationContext().getBean(LocalUaaRestTemplate.class);
+        OAuth2AccessToken token = restTemplate.acquireAccessToken(null);
+        assertTrue("Scopes should contain oauth.login", token.getScope().contains("oauth.login"));
+        assertTrue("Scopes should contain notifications.write", token.getScope().contains("notifications.write"));
+        assertTrue("Scopes should contain critical_notifications.write", token.getScope().contains("critical_notifications.write"));
+    }
+
+    @Test
+    public void testUaaRestTemplateContainsBearerHeader() throws Exception {
+        LocalUaaRestTemplate restTemplate = getWebApplicationContext().getBean(LocalUaaRestTemplate.class);
+        OAuth2AccessToken token = restTemplate.acquireAccessToken(restTemplate.getOAuth2ClientContext());
+        Method createRequest = OAuth2RestTemplate.class.getDeclaredMethod("createRequest",URI.class, HttpMethod.class);
+        ReflectionUtils.makeAccessible(createRequest);
+        ClientHttpRequest request = (ClientHttpRequest)createRequest.invoke(restTemplate, new URI("http://localhost/oauth/token"), HttpMethod.POST);
+        assertEquals("authorization bearer header should be present", 1, request.getHeaders().get("Authorization").size());
+        assertNotNull("authorization bearer header should be present", request.getHeaders().get("Authorization").get(0));
+        assertEquals("bearer "+token.getValue(), request.getHeaders().get("Authorization").get(0));
+
+    }
+}
