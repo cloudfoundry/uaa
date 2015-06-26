cd212cec87a1049fe2861ce4cea40645954ab585
Chris Dutra <cdutra@pivotal.io>
2015-06-25 18:32:20 -0700

    /password_change endpoint allowed the caller to specify JSON parameters that should never have been allowed.
    [#95916214] https://www.pivotaltracker.com/story/show/95916214
    
    Signed-off-by: Rick Kawala <rkawala@pivotal.io>

---
+++ scim/src/main/java/org/cloudfoundry/identity/uaa/scim/endpoints/PasswordReset.java
@@ -0,0 +1,33 @@
+package org.cloudfoundry.identity.uaa.scim.endpoints;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PasswordReset {
+
+    @JsonProperty("code")
+    private String code;
+
+    @JsonProperty("new_password")
+    private String newPassword;
+
+    public PasswordReset(String code, String newPassword) {
+        this.code = code;
+        this.newPassword = newPassword;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
