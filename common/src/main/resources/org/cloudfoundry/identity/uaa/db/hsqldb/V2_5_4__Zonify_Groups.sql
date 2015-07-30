4aabcd2d983862d674ecf5744be52a15ce6792b3
Filip Hanik <fhanik@pivotal.io>
2015-07-30 08:07:10 -0600

    Zonify Group Migration Script - Fix null value on group_membership.identity_zone_id - For all groups in the UAA zone, duplicate them in other zones - Migrate group memberships where users and groups belong to different zones

--- common/src/main/resources/org/cloudfoundry/identity/uaa/db/hsqldb/V2_5_4__Zonify_Groups.sql
+++ common/src/main/resources/org/cloudfoundry/identity/uaa/db/hsqldb/V2_5_4__Zonify_Groups.sql
@@ -10,10 +10,6 @@
 -- subcomponents is subject to the terms and conditions of the
 -- subcomponent's license, as noted in the LICENSE file.
 --
--- add zone id to the groups table
-ALTER TABLE groups ADD COLUMN identity_zone_id varchar(36) DEFAULT 'uaa' NOT NULL ;
-ALTER TABLE groups DROP CONSTRAINT unique_uk_2;
-CREATE UNIQUE INDEX groups_unique_key ON groups (displayname,identity_zone_id);
 
 -- remove zone id from the group_membership table - it is derived from group_id
 DROP INDEX group_membership_unique_key;
@@ -24,16 +20,3 @@ CREATE UNIQUE INDEX group_membership_unique_key ON group_membership (member_id,g
 DROP INDEX external_group_unique_key;
 ALTER TABLE external_group_mapping DROP COLUMN identity_zone_id;
 CREATE UNIQUE INDEX external_group_unique_key ON external_group_mapping (origin,external_group,group_id);
-
---
--- ALTER TABLE external_group_mapping ADD COLUMN identity_zone_id varchar(36);
--- ALTER TABLE external_group_mapping ADD COLUMN origin varchar(36);
--- ALTER TABLE external_group_mapping DROP CONSTRAINT external_group_mapping_pkey;
---
--- UPDATE group_membership SET identity_zone_id = (SELECT identity_zone_id FROM users where users.id = group_membership.member_id);
--- UPDATE group_membership SET identity_zone_id = (SELECT 'uaa' FROM groups where groups.id = group_membership.member_id);
---
--- UPDATE external_group_mapping SET identity_zone_id = 'uaa', origin='ldap';
---
--- CREATE UNIQUE INDEX group_membership_unique_key ON group_membership (identity_zone_id,member_id,group_id);
--- CREATE UNIQUE INDEX external_group_unique_key ON external_group_mapping (identity_zone_id,origin,external_group,group_id);
