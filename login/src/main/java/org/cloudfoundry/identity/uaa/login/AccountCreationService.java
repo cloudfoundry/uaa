gaybro8777/cg-uaa

Client side Password Validation
For a better user experience, users would like to know the password
requirements prior to the first submission of credentials.
Work card:
https://trello.com/c/tc2CAVkh/439-ui-changing-setting-password-should-show-requirements-beforehand

login/src/main/java/org/cloudfoundry/identity/uaa/login/AccountCreationService.java
@@ -1,6 +1,7 @@
 package org.cloudfoundry.identity.uaa.login;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.cloudfoundry.identity.uaa.config.PasswordPolicy;
 import org.cloudfoundry.identity.uaa.scim.ScimUser;
 
 import java.io.IOException;
@@ -13,6 +14,8 @@ public interface AccountCreationService {
     ScimUser createUser(String username, String password, String origin);
 
     String getDefaultRedirect() throws IOException;
+
+    PasswordPolicy getPasswordPolicy();
 
     class ExistingUserResponse {
         @JsonProperty

login/src/main/java/org/cloudfoundry/identity/uaa/login/ChangePasswordService.java
@@ -12,6 +12,9 @@
  *******************************************************************************/
 package org.cloudfoundry.identity.uaa.login;
 
+import org.cloudfoundry.identity.uaa.config.PasswordPolicy;
+
 public interface ChangePasswordService {
     void changePassword(String username, String currentPassword, String newPassword);
+    PasswordPolicy getPasswordPolicy();
 }

login/src/main/java/org/cloudfoundry/identity/uaa/login/EmailAccountCreationService.java
@@ -6,6 +6,7 @@ import org.apache.commons.logging.LogFactory;
 import org.cloudfoundry.identity.uaa.authentication.Origin;
 import org.cloudfoundry.identity.uaa.codestore.ExpiringCode;
 import org.cloudfoundry.identity.uaa.codestore.ExpiringCodeStore;
+import org.cloudfoundry.identity.uaa.config.PasswordPolicy;
 import org.cloudfoundry.identity.uaa.error.UaaException;
 import org.cloudfoundry.identity.uaa.scim.ScimUser;
 import org.cloudfoundry.identity.uaa.scim.ScimUserProvisioning;
@@ -156,6 +157,11 @@ public class EmailAccountCreationService implements AccountCreationService {
             }
             throw new UaaException("Couldn't create user:"+username, x);
         }
+    }
+
+    @Override
+    public PasswordPolicy getPasswordPolicy() {
+        return passwordValidator.getPasswordPolicy();
     }
 
     private String getSubjectText() {

scim/src/main/java/org/cloudfoundry/identity/uaa/login/ResetPasswordService.java
@@ -13,6 +13,7 @@
 package org.cloudfoundry.identity.uaa.login;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.cloudfoundry.identity.uaa.config.PasswordPolicy;
 import org.cloudfoundry.identity.uaa.scim.ScimUser;
 import org.cloudfoundry.identity.uaa.scim.exception.InvalidPasswordException;
 
@@ -22,6 +23,8 @@ public interface ResetPasswordService {
     ForgotPasswordInfo forgotPassword(String email, String clientId, String redirectUri);
 
     ResetPasswordResponse resetPassword(String code, String password) throws InvalidPasswordException;
+
+    PasswordPolicy getPasswordPolicy();
 
     class ResetPasswordResponse {
         @JsonProperty("user")

scim/src/main/java/org/cloudfoundry/identity/uaa/scim/validate/NullPasswordValidator.java
@@ -12,6 +12,7 @@
  *******************************************************************************/
 package org.cloudfoundry.identity.uaa.scim.validate;
 
+import org.cloudfoundry.identity.uaa.config.PasswordPolicy;
 import org.cloudfoundry.identity.uaa.scim.exception.InvalidPasswordException;
 
 /**
@@ -24,5 +25,11 @@ public class NullPasswordValidator implements PasswordValidator {
     @Override
     public void validate(String password) throws InvalidPasswordException {
         //noop
+    }
+
+    @Override
+    public PasswordPolicy getPasswordPolicy() {
+        //noop
+        return null;
     }
 }

cloudfoundry/uaa

login/src/main/java/org/cloudfoundry/identity/uaa/login/AccountCreationService.java
@@ -1,6 +1,7 @@
 package org.cloudfoundry.identity.uaa.login;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.cloudfoundry.identity.uaa.provider.PasswordPolicy;
 import org.cloudfoundry.identity.uaa.scim.ScimUser;
 
 import java.io.IOException;
@@ -13,6 +14,8 @@ public interface AccountCreationService {
     ScimUser createUser(String username, String password, String origin);
 
     String getDefaultRedirect() throws IOException;
+
+    PasswordPolicy getPasswordPolicy();
 
     class ExistingUserResponse {
         @JsonProperty

login/src/main/java/org/cloudfoundry/identity/uaa/login/ChangePasswordService.java
@@ -12,6 +12,9 @@
  *******************************************************************************/
 package org.cloudfoundry.identity.uaa.login;
 
+import org.cloudfoundry.identity.uaa.provider.PasswordPolicy;
+
 public interface ChangePasswordService {
     void changePassword(String username, String currentPassword, String newPassword);
+    PasswordPolicy getPasswordPolicy();
 }

login/src/main/java/org/cloudfoundry/identity/uaa/login/EmailAccountCreationService.java
@@ -7,6 +7,7 @@ import org.cloudfoundry.identity.uaa.codestore.ExpiringCode;
 import org.cloudfoundry.identity.uaa.codestore.ExpiringCodeStore;
 import org.cloudfoundry.identity.uaa.constants.OriginKeys;
 import org.cloudfoundry.identity.uaa.error.UaaException;
+import org.cloudfoundry.identity.uaa.provider.PasswordPolicy;
 import org.cloudfoundry.identity.uaa.scim.ScimUser;
 import org.cloudfoundry.identity.uaa.scim.ScimUserProvisioning;
 import org.cloudfoundry.identity.uaa.scim.exception.ScimResourceAlreadyExistsException;
@@ -155,6 +156,11 @@ public class EmailAccountCreationService implements AccountCreationService {
             }
             throw new UaaException("Couldn't create user:"+username, x);
         }
+    }
+
+    @Override
+    public PasswordPolicy getPasswordPolicy() {
+        return passwordValidator.getPasswordPolicy();
     }
 
     private String getSubjectText() {

scim/src/main/java/org/cloudfoundry/identity/uaa/login/ResetPasswordService.java
@@ -13,6 +13,7 @@
 package org.cloudfoundry.identity.uaa.login;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.cloudfoundry.identity.uaa.provider.PasswordPolicy;
 import org.cloudfoundry.identity.uaa.scim.ScimUser;
 import org.cloudfoundry.identity.uaa.scim.exception.InvalidPasswordException;
 
@@ -22,6 +23,8 @@ public interface ResetPasswordService {
     ForgotPasswordInfo forgotPassword(String email, String clientId, String redirectUri);
 
     ResetPasswordResponse resetPassword(String code, String password) throws InvalidPasswordException;
+
+    PasswordPolicy getPasswordPolicy();
 
     class ResetPasswordResponse {
         @JsonProperty("user")

scim/src/main/java/org/cloudfoundry/identity/uaa/scim/validate/NullPasswordValidator.java
@@ -12,6 +12,7 @@
  *******************************************************************************/
 package org.cloudfoundry.identity.uaa.scim.validate;
 
+import org.cloudfoundry.identity.uaa.provider.PasswordPolicy;
 import org.cloudfoundry.identity.uaa.scim.exception.InvalidPasswordException;
 
 /**
@@ -24,5 +25,11 @@ public class NullPasswordValidator implements PasswordValidator {
     @Override
     public void validate(String password) throws InvalidPasswordException {
         //noop
+    }
+
+    @Override
+    public PasswordPolicy getPasswordPolicy() {
+        //noop
+        return null;
     }
 }