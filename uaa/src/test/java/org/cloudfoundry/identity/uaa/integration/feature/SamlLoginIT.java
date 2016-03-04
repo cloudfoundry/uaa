02eba7a8b7d262644a77726745a2716a3d2ffd80
Brittany <brittany.johnson@ge.com>
2016-03-04 10:58:24 -0800

    [Dario] Included addNew property linked to addShadowUserOnLogin property for backward compatibility. Change formatting in SamlLoginIT.

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/SamlLoginIT.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/SamlLoginIT.java
@@ -470,6 +470,7 @@ public class SamlLoginIT {
         if (null == userId) {
             return;
         }
+        if (userId == null) return;
 
         IntegrationTestUtils.deleteUser(zoneAdminToken, baseUrl, userId);
     }
