4ab01844707934154f4c83f4934ef5e4dc0102ab
Filip Hanik <fhanik@pivotal.io>
2016-03-09 15:16:16 -0700

    Add support for skipping SSL validation on the client-lib Oauth2RestTemplate

---
+++ client-lib/src/test/java/org/cloudfoundry/identity/client/integration/HttpsIntegrationTest.java
@@ -0,0 +1,83 @@
+/*
+ * *****************************************************************************
+ *      Cloud Foundry
+ *      Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
+ *
+ *      This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ *      You may not use this product except in compliance with the License.
+ *
+ *      This product includes a number of subcomponents with
+ *      separate copyright notices and license terms. Your use of these
+ *      subcomponents is subject to the terms and conditions of the
+ *      subcomponent's license, as noted in the LICENSE file.
+ * *****************************************************************************
+ */
+
+package org.cloudfoundry.identity.client.integration;
+
+import org.cloudfoundry.identity.client.UaaContext;
+import org.cloudfoundry.identity.client.UaaContextFactory;
+import org.cloudfoundry.identity.client.token.GrantType;
+import org.cloudfoundry.identity.client.token.TokenRequest;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
+import org.springframework.web.client.ResourceAccessException;
+
+import java.net.URI;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class HttpsIntegrationTest {
+
+    public static String uaaURI = "https://login.identity.cf-app.com";
+
+    private UaaContextFactory factory;
+
+    @Rule
+    public IsUAAListeningRule uaaListeningRule = new IsUAAListeningRule(uaaURI, false);
+
+    @Before
+    public void setUp() throws Exception {
+        factory =
+            UaaContextFactory.factory(new URI(uaaURI))
+                .authorizePath("/oauth/authorize")
+                .tokenPath("/oauth/token");
+    }
+
+    @Test
+    public void test_ignore_self_signed_cert_happy_path() throws Exception {
+        test_self_signed_cert(true);
+    }
+
+    @Test
+    public void test_self_signed_cert_should_fail() throws Exception {
+        try {
+            test_self_signed_cert(false);
+            fail("Self signed cert should not pass this test");
+        }catch (OAuth2AccessDeniedException x) {
+            assertEquals(ResourceAccessException.class, x.getCause().getClass());
+        }
+    }
+
+    protected void test_self_signed_cert(boolean skipSslValidation) {
+        TokenRequest clientCredentials = factory.tokenRequest()
+            .setClientId("oauth_showcase_client_credentials")
+            .setClientSecret("secret")
+            .setGrantType(GrantType.CLIENT_CREDENTIALS)
+            .setSkipSslValidation(skipSslValidation);
+
+        UaaContext context = factory.authenticate(clientCredentials);
+        assertNotNull(context);
+        assertTrue(context.hasAccessToken());
+        assertFalse(context.hasIdToken());
+        assertFalse(context.hasRefreshToken());
+        assertTrue(context.getToken().getScope().contains("uaa.resource"));
+    }
+
+}
