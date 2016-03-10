3055967f53605b76bed07e04adec15e145183bd3
Filip Hanik <fhanik@pivotal.io>
2016-03-10 11:32:12 -0700

    Implement plumbing and test cases to fetch id tokens and token from an authorization code Add support to skip SSL validation Add cookie support for integration tests to run in a CF environment
    [#115082069] https://www.pivotaltracker.com/story/show/115082069

--- client-lib/src/main/java/org/cloudfoundry/identity/client/UaaContextFactory.java
+++ client-lib/src/main/java/org/cloudfoundry/identity/client/UaaContextFactory.java
@@ -15,16 +15,28 @@
 package org.cloudfoundry.identity.client;
 
 
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.ProtocolException;
+import org.apache.http.client.RedirectStrategy;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.conn.ssl.SSLContextBuilder;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.protocol.HttpContext;
 import org.cloudfoundry.identity.client.token.TokenRequest;
 import org.cloudfoundry.identity.uaa.oauth.token.CompositeAccessToken;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.BaseOAuth2ProtectedResourceDetails;
@@ -43,12 +55,15 @@ import org.springframework.security.oauth2.client.token.grant.password.ResourceO
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.exceptions.UnsupportedGrantTypeException;
 import org.springframework.security.oauth2.common.util.OAuth2Utils;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpMessageConverterExtractor;
 import org.springframework.web.client.ResponseExtractor;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.net.ssl.SSLContext;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
@@ -61,6 +76,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import static java.lang.String.format;
 import static org.cloudfoundry.identity.client.token.GrantType.AUTHORIZATION_CODE;
 import static org.cloudfoundry.identity.client.token.GrantType.PASSWORD_WITH_PASSCODE;
 import static org.springframework.security.oauth2.common.AuthenticationScheme.header;
@@ -113,18 +129,34 @@ public class UaaContextFactory {
     }
 
     /**
+     * Returns the authorize URI
+     * @return the UAA authorization URI
+     */
+    public URI getAuthorizeUri() {
+        UriComponentsBuilder authorizationURI = UriComponentsBuilder.newInstance();
+        authorizationURI.uri(uaaURI);
+        authorizationURI.path(authorizePath);
+        return authorizationURI.build().toUri();
+    }
+
+    /**
+     * Returns the URI
+     * @return
+     */
+    public URI getTokenUri() {
+        UriComponentsBuilder tokenURI = UriComponentsBuilder.newInstance();
+        tokenURI.uri(uaaURI);
+        tokenURI.path(tokenPath);
+        return tokenURI.build().toUri();
+    }
+
+    /**
      * Creates a new {@link TokenRequest} object.
      * The object will have the token an authorize endpoints already configured.
      * @return the new token request that can be used for an access token request.
      */
     public TokenRequest tokenRequest() {
-        UriComponentsBuilder tokenURI = UriComponentsBuilder.newInstance();
-        tokenURI.uri(uaaURI);
-        tokenURI.path(tokenPath);
-        UriComponentsBuilder authorizationURI = UriComponentsBuilder.newInstance();
-        authorizationURI.uri(uaaURI);
-        authorizationURI.path(authorizePath);
-        return new TokenRequest(tokenURI.build().toUri(), authorizationURI.build().toUri());
+        return new TokenRequest(getTokenUri(), getAuthorizeUri());
     }
 
 
@@ -149,8 +181,41 @@ public class UaaContextFactory {
             case PASSWORD_WITH_PASSCODE: return authenticatePassword(request);
             case AUTHORIZATION_CODE: return authenticateAuthCode(request);
             case AUTHORIZATION_CODE_WITH_TOKEN: return authenticateAuthCodeWithToken(request);
+            case FETCH_TOKEN_FROM_CODE: return fetchTokenFromCode(request);
             default: throw new UnsupportedGrantTypeException("Not implemented:"+request.getGrantType());
         }
+    }
+
+    protected UaaContext fetchTokenFromCode(final TokenRequest request) {
+        String clientBasicAuth = null;
+        try {
+            byte[] autbytes = Base64.encode(format("%s:%s", request.getClientId(),request.getClientSecret()).getBytes("UTF-8"));
+            String base64 = new String(autbytes);
+            clientBasicAuth = format("Basic %s", base64);
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+        RestTemplate template = new RestTemplate();
+        if (request.isSkipSslValidation()) {
+            template.setRequestFactory(getNoValidatingClientHttpRequestFactory());
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, clientBasicAuth);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String,String> form = new LinkedMultiValueMap<>();
+        form.add(OAuth2Utils.GRANT_TYPE, "authorization_code");
+        form.add(OAuth2Utils.REDIRECT_URI, request.getRedirectUri().toString());
+        String responseType = "token";
+        if (request.wantsIdToken()) {
+            responseType += " id_token";
+        }
+        form.add(OAuth2Utils.RESPONSE_TYPE, responseType);
+        form.add("code", request.getAuthorizationCode());
+
+        ResponseEntity<CompositeAccessToken> token = template.exchange(request.getTokenEndpoint(), HttpMethod.POST, new HttpEntity<>(form, headers), CompositeAccessToken.class);
+        return new UaaContextImpl(request, null, token.getBody());
     }
 
     /**
@@ -160,7 +225,7 @@ public class UaaContextFactory {
      */
     protected UaaContext authenticateAuthCode(final TokenRequest tokenRequest) {
         AuthorizationCodeResourceDetails details = new AuthorizationCodeResourceDetails();
-        details.setPreEstablishedRedirectUri(tokenRequest.getRedirectUriRedirectUri().toString());
+        details.setPreEstablishedRedirectUri(tokenRequest.getRedirectUri().toString());
         details.setUserAuthorizationUri(tokenRequest.getAuthorizationEndpoint().toString());
         configureResourceDetails(tokenRequest, details);
         setClientCredentials(tokenRequest, details);
@@ -197,7 +262,7 @@ public class UaaContextFactory {
         );
         enhanceRequestParameters(tokenRequest, providers.get(0));
         AuthorizationCodeResourceDetails details = new AuthorizationCodeResourceDetails();
-        details.setPreEstablishedRedirectUri(tokenRequest.getRedirectUriRedirectUri().toString());
+        details.setPreEstablishedRedirectUri(tokenRequest.getRedirectUri().toString());
         configureResourceDetails(tokenRequest, details);
         setClientCredentials(tokenRequest, details);
         setRequestScopes(tokenRequest, details);
@@ -337,19 +402,7 @@ public class UaaContextFactory {
     protected void skipSslValidation(TokenRequest tokenRequest, OAuth2RestTemplate template, List<OAuth2AccessTokenSupport> existingProviders)  {
         ClientHttpRequestFactory requestFactory = null;
         if (tokenRequest.isSkipSslValidation()) {
-
-            SSLContext sslContext;
-            try {
-                sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            } catch (KeyManagementException e) {
-                throw new RuntimeException(e);
-            } catch (KeyStoreException e) {
-                throw new RuntimeException(e);
-            }
-            CloseableHttpClient httpClient = HttpClients.custom().setSslcontext(sslContext).build();
-            requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+            requestFactory = getNoValidatingClientHttpRequestFactory();
         }
         List<OAuth2AccessTokenSupport> accessTokenProviders =
             existingProviders!=null ? existingProviders :
@@ -368,6 +421,43 @@ public class UaaContextFactory {
         }
         AccessTokenProviderChain chain = new AccessTokenProviderChain(providers);
         template.setAccessTokenProvider(chain);
+    }
+
+    public static ClientHttpRequestFactory getNoValidatingClientHttpRequestFactory() {
+        return getNoValidatingClientHttpRequestFactory(true);
+    }
+    public static ClientHttpRequestFactory getNoValidatingClientHttpRequestFactory(boolean followRedirects) {
+        ClientHttpRequestFactory requestFactory;
+        SSLContext sslContext;
+        try {
+            sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (KeyManagementException e) {
+            throw new RuntimeException(e);
+        } catch (KeyStoreException e) {
+            throw new RuntimeException(e);
+        }
+        //
+        CloseableHttpClient httpClient =
+            HttpClients.custom()
+                .setSslcontext(sslContext)
+                .setRedirectStrategy(
+                    followRedirects ? new DefaultRedirectStrategy() : new RedirectStrategy() {
+                        @Override
+                        public boolean isRedirected(HttpRequest request, HttpResponse response, HttpContext context) throws ProtocolException {
+                            return false;
+                        }
+
+                        @Override
+                        public HttpUriRequest getRedirect(HttpRequest request, HttpResponse response, HttpContext context) throws ProtocolException {
+                            return null;
+                        }
+                    }
+                ).build();
+
+        requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        return requestFactory;
     }
 
 }
--- client-lib/src/test/java/org/cloudfoundry/identity/client/integration/HttpsIntegrationTest.java
+++ client-lib/src/test/java/org/cloudfoundry/identity/client/integration/HttpsIntegrationTest.java
@@ -20,6 +20,7 @@ import org.cloudfoundry.identity.client.UaaContextFactory;
 import org.cloudfoundry.identity.client.token.GrantType;
 import org.cloudfoundry.identity.client.token.TokenRequest;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
@@ -41,9 +42,15 @@ public class HttpsIntegrationTest {
 
     @Rule
     public IsUAAListeningRule uaaListeningRule = new IsUAAListeningRule(uaaURI, false);
+    private String clientId;
+    private String clientSecret;
+    private String redirectUri;
 
     @Before
     public void setUp() throws Exception {
+        redirectUri = "https://uaa.identity.cf-app.com";
+        clientId = "xxx";
+        clientSecret = "xxx";
         factory =
             UaaContextFactory.factory(new URI(uaaURI))
                 .authorizePath("/oauth/authorize")
@@ -64,6 +71,19 @@ public class HttpsIntegrationTest {
             assertEquals(ResourceAccessException.class, x.getCause().getClass());
         }
     }
+
+    @Test
+    @Ignore("Ignored until we have valid client information for acceptance that we can use")
+    public void test_fetch_token_from_authorization_code() throws Exception {
+        ClientAPITokenIntegrationTest.test_fetch_token_from_authorization_code(factory, uaaURI, false, true, clientId, clientSecret, redirectUri);
+    }
+
+    @Test
+    @Ignore("Ignored until we have valid client information for acceptance that we can use")
+    public void test_fetch_token_from_authorization_code_with_id_token() throws Exception {
+        ClientAPITokenIntegrationTest.test_fetch_token_from_authorization_code(factory, uaaURI, true, true, clientId, clientSecret, redirectUri);
+    }
+
 
     protected void test_self_signed_cert(boolean skipSslValidation) {
         TokenRequest clientCredentials = factory.tokenRequest()
