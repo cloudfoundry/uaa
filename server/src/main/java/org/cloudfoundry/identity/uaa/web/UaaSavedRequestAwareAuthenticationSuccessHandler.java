f691a199d31eaba0b7afd5e9d17b749e5c15ce4f
Filip Hanik <fhanik@pivotal.io>
2016-10-11 08:23:37 -0700

    Implement a security filter for password reset. The reason for a security filter is because of the requirement that the user is logged in after the password reset. [#131688463] https://www.pivotaltracker.com/story/show/131688463

---
+++ server/src/main/java/org/cloudfoundry/identity/uaa/web/UaaSavedRequestAwareAuthenticationSuccessHandler.java
@@ -0,0 +1,40 @@
+/*
+ * ****************************************************************************
+ *     Cloud Foundry
+ *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
+ *
+ *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ *     You may not use this product except in compliance with the License.
+ *
+ *     This product includes a number of subcomponents with
+ *     separate copyright notices and license terms. Your use of these
+ *     subcomponents is subject to the terms and conditions of the
+ *     subcomponent's license, as noted in the LICENSE file.
+ * ****************************************************************************
+ */
+
+package org.cloudfoundry.identity.uaa.web;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UaaSavedRequestAwareAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+    public static final String URI_OVERRIDE_ATTRIBUTE = "override.redirect_uri";
+
+    private static Log logger = LogFactory.getLog(UaaSavedRequestAwareAuthenticationSuccessHandler.class);
+
+    @Override
+    public String determineTargetUrl(HttpServletRequest request, HttpServletResponse response) {
+        Object redirectAttribute = request.getAttribute(URI_OVERRIDE_ATTRIBUTE);
+        if (redirectAttribute !=null) {
+            logger.debug("Returning redirectAttribute saved URI:"+redirectAttribute);
+            return (String) redirectAttribute;
+        } else {
+            return super.determineTargetUrl(request, response);
+        }
+    }
+}
