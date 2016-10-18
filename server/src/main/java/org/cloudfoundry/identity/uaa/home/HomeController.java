b1d0129a03d324b939259284909ee2cc34a7680e
Ilya Lipkind <lipkind@ge.com>
2016-10-18 13:44:54 -0700

    Removing possibility of NPE in case of SAML error

--- server/src/main/java/org/cloudfoundry/identity/uaa/home/HomeController.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/home/HomeController.java
@@ -14,7 +14,6 @@ package org.cloudfoundry.identity.uaa.home;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.http.client.utils.URIBuilder;
 import org.cloudfoundry.identity.uaa.client.ClientMetadata;
 import org.cloudfoundry.identity.uaa.client.JdbcClientMetadataProvisioning;
 import org.cloudfoundry.identity.uaa.constants.OriginKeys;
@@ -29,11 +28,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -136,7 +131,11 @@ public class HomeController {
     @RequestMapping("/saml_error")
     public String error401(Model model, HttpServletRequest request) {
         AuthenticationException exception = (AuthenticationException) request.getSession().getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
-        model.addAttribute("saml_error", exception.getMessage());
+        if (exception == null) {
+            model.addAttribute("saml_error", "The server received a request for '/saml_error' but there was no SAML error information stored for the session.");
+        } else {
+            model.addAttribute("saml_error", exception.getMessage());
+        }
         return "external_auth_error";
     }
 
