de063f8289e47c08e42e1ffd4afd491957662af1
Filip Hanik <fhanik@pivotal.io>
2016-12-07 13:48:50 -0800

    Fork the syslog appender so we can correctly append headers to each message [#131567727] https://www.pivotaltracker.com/story/show/131567727

--- server/src/main/java/org/cloudfoundry/identity/uaa/util/log/LineAwareLayout.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/util/log/LineAwareLayout.java
@@ -1,26 +1,23 @@
 /*
- * ******************************************************************************
- *       Cloud Foundry Copyright (c) [2009-2015] Pivotal Software, Inc. All Rights Reserved.
+ * ****************************************************************************
+ *     Cloud Foundry
+ *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
  *
- *       This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *       You may not use this product except in compliance with the License.
+ *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ *     You may not use this product except in compliance with the License.
  *
- *       This product includes a number of subcomponents with
- *       separate copyright notices and license terms. Your use of these
- *       subcomponents is subject to the terms and conditions of the
- *       subcomponent's license, as noted in the LICENSE file.
- * ******************************************************************************
+ *     This product includes a number of subcomponents with
+ *     separate copyright notices and license terms. Your use of these
+ *     subcomponents is subject to the terms and conditions of the
+ *     subcomponent's license, as noted in the LICENSE file.
+ * ****************************************************************************
  */
 
-package org.cloudfoundry.identity.uaa.util;
+package org.cloudfoundry.identity.uaa.util.log;
 
 import org.apache.log4j.Layout;
 import org.apache.log4j.spi.LoggingEvent;
-import org.springframework.util.StringUtils;
 
-/**
- * Created by pivotal on 10/28/15.
- */
 public class LineAwareLayout extends Layout {
     private Layout messageLayout;
     private Layout lineLayout;
