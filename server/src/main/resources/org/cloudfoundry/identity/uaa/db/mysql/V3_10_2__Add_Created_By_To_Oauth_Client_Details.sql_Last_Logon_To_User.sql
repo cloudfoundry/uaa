731b49a1cb0471c7b817cbc9209800c5fd1b88e7
Priyata Agrawal <pagrawal@pivotal.io>
2017-01-12 15:22:50 -0800

    Added sql server flyway script to add created_by column in oauth_client and renamed existing file for other databases
    [#136376883] https://www.pivotaltracker.com/story/show/136376883
    
    Signed-off-by: Bharath Sekar <bharath.sekar@ge.com>

--- server/src/main/resources/org/cloudfoundry/identity/uaa/db/mysql/V3_10_2__Add_Created_By_To_Oauth_Client_Details.sql_Last_Logon_To_User.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE oauth_client_details ADD COLUMN created_by CHAR(36) DEFAULT NULL;
-ALTER TABLE oauth_client_details ADD COLUMN last_updated_by CHAR(36) DEFAULT NULL;
-
---
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/sqlserver/V3_10_2__Add_Created_By_To_Oauth_Client_Details.sql
@@ -0,0 +1,1 @@
+ALTER TABLE oauth_client_details ADD created_by CHAR(36) DEFAULT NULL;
