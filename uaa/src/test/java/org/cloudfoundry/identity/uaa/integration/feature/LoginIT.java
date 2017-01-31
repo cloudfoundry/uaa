cc0e7001435cf2553b94bf12d37c26135973c7a9
Bharath Sekar <bharath.sekar@ge.com>
2017-01-31 12:22:23 -0800

    Fix failing tests for footer_last_login
    [#137988919] https://www.pivotaltracker.com/story/show/137988919
    
    Signed-off-by: Mikhail Vyshegorodtsev <medvedzver@inbox.ru>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/LoginIT.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/LoginIT.java
@@ -139,16 +139,6 @@ public class LoginIT {
     }
 
     @Test
-    public void testSuccessfulLogin() throws Exception {
-        webDriver.get(baseUrl + "/login");
-        assertEquals("Cloud Foundry", webDriver.getTitle());
-        attemptLogin(testAccounts.getUserName(), testAccounts.getPassword());
-        assertThat(webDriver.findElement(By.cssSelector("h1")).getText(), Matchers.containsString("Where to?"));
-        assertThat(webDriver.findElement(By.cssSelector(".footer")).getText(), Matchers.containsString("Last Login"));
-        IntegrationTestUtils.validateAccountChooserCookie(baseUrl, webDriver);
-    }
-
-    @Test
     public void testSuccessfulLoginNewUser() throws Exception {
         String newUserEmail = createAnotherUser();
         webDriver.get(baseUrl + "/logout.do");
