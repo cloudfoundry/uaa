21be693dab1fca371b8f6ea949b959364967c6b9
Filip Hanik <fhanik@pivotal.io>
2017-03-06 14:49:14 -0800

    Add indices to improve sec_audit performance
    [#141169701] https://www.pivotaltracker.com/story/show/141169701

---
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/postgresql/V4_0_1__Add_Index_To_Sec_Audit.sql
@@ -0,0 +1,17 @@
+--
+-- Cloud Foundry
+-- Copyright (c) [2016] Pivotal Software, Inc. All Rights Reserved.
+--
+-- This product is licensed to you under the Apache License, Version 2.0 (the "License").
+-- You may not use this product except in compliance with the License.
+--
+-- This product includes a number of subcomponents with
+-- separate copyright notices and license terms. Your use of these
+-- subcomponents is subject to the terms and conditions of the
+-- subcomponent's license, as noted in the LICENSE file.
+--
+
+
+-- create an index to match the query
+CREATE INDEX sec_audit_principal_idx ON sec_audit(principal_id);
+CREATE INDEX sec_audit_created_idx ON sec_audit(created);
