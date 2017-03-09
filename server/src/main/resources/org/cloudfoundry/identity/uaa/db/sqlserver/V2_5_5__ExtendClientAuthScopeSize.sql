d82193c79623c06140fc4261a3ca20b313f4b0b0
Roop <rupinder.guron@ge.com>
2017-03-09 14:44:25 -0800

    Fix sql server syntax for flywar script.
    Signed-off-by: Brittany <brittany.johnson@ge.com>

--- server/src/main/resources/org/cloudfoundry/identity/uaa/db/sqlserver/V2_5_5__ExtendClientAuthScopeSize.sql
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/sqlserver/V2_5_5__ExtendClientAuthScopeSize.sql
@@ -1,3 +1,4 @@
-ALTER TABLE oauth_client_details ALTER COLUMN scope TYPE NVARCHAR(MAX);
-ALTER TABLE oauth_client_details ALTER COLUMN authorities TYPE NVARCHAR(MAX);
+ALTER TABLE oauth_client_details ALTER COLUMN scope NVARCHAR(MAX);
+ALTER TABLE oauth_client_details ALTER COLUMN authorities NVARCHAR(MAX);
+
 
