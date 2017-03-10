e2b846ac1e7514b908726cecaa2b2368ea6c5fd1
Bharath Sekar <bharath.sekar@ge.com>
2017-03-09 16:58:25 -0800

    Test csrf token reset
    [#141240857] https://www.pivotaltracker.com/story/show/141240857

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/LoginIT.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/LoginIT.java
@@ -55,6 +55,7 @@ import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
@@ -216,6 +217,15 @@ public class LoginIT {
             String.class);
         assertEquals(HttpStatus.FORBIDDEN, loginResponse.getStatusCode());
         assertTrue("CSRF message should be shown", loginResponse.getBody().contains("Invalid login attempt, request does not meet our security standards, please try again."));
+    }
+
+    @Test
+    public void testCsrfIsResetDuringLoginPageReload() {
+        webDriver.get(baseUrl + "/login");
+        String csrf1 = webDriver.manage().getCookieNamed(CookieBasedCsrfTokenRepository.DEFAULT_CSRF_COOKIE_NAME).getValue();
+        webDriver.get(baseUrl + "/login");
+        String csrf2 = webDriver.manage().getCookieNamed(CookieBasedCsrfTokenRepository.DEFAULT_CSRF_COOKIE_NAME).getValue();
+        assertNotEquals(csrf1, csrf2);
     }
 
     @Test
