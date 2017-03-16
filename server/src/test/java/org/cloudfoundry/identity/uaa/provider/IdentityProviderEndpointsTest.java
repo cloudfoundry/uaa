2eeea05781fa5574b24738676750a553ffbcddb5
Filip Hanik <fhanik@pivotal.io>
2017-03-16 15:29:14 -0700

    Redact LDAP bind password from API operations.
    Patch LDAP bind password on update.
    
    [#141161179] https://www.pivotaltracker.com/story/show/141161179

--- server/src/test/java/org/cloudfoundry/identity/uaa/provider/IdentityProviderEndpointsTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/provider/IdentityProviderEndpointsTest.java
@@ -1,19 +1,34 @@
 package org.cloudfoundry.identity.uaa.provider;
 
-import org.cloudfoundry.identity.uaa.authentication.Origin;
 import org.cloudfoundry.identity.uaa.constants.OriginKeys;
 import org.cloudfoundry.identity.uaa.provider.saml.SamlIdentityProviderConfigurator;
 import org.cloudfoundry.identity.uaa.scim.ScimGroupExternalMembershipManager;
 import org.cloudfoundry.identity.uaa.scim.ScimGroupProvisioning;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.cloudfoundry.identity.uaa.constants.OriginKeys.LDAP;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class IdentityProviderEndpointsTest {
@@ -27,8 +42,159 @@ public class IdentityProviderEndpointsTest {
 
     @Before
     public void setup() {
+
+        configValidator = mock(IdentityProviderConfigValidationDelegator.class);
         identityProviderProvisioning = mock(IdentityProviderProvisioning.class);
         identityProviderEndpoints = new IdentityProviderEndpoints(identityProviderProvisioning, scimGroupExternalMembershipManager, scimGroupProvisioning, samlConfigurator, configValidator);
+    }
+
+    public IdentityProvider<LdapIdentityProviderDefinition> getLdapDefinition() {
+        String ldapProfile = "ldap-search-and-bind.xml";
+        //String ldapProfile = "ldap-search-and-compare.xml";
+        String ldapGroup = "ldap-groups-null.xml";
+        LdapIdentityProviderDefinition definition = new LdapIdentityProviderDefinition();
+        definition.setLdapProfileFile("ldap/" + ldapProfile);
+        definition.setLdapGroupFile("ldap/" + ldapGroup);
+        definition.setMaxGroupSearchDepth(10);
+        definition.setBaseUrl("ldap://localhost");
+        definition.setBindUserDn("cn=admin,ou=Users,dc=test,dc=com");
+        definition.setBindPassword("adminsecret");
+        definition.setSkipSSLVerification(true);
+        definition.setTlsConfiguration("none");
+        definition.setMailAttributeName("mail");
+        definition.setReferral("ignore");
+
+        IdentityProvider<LdapIdentityProviderDefinition> ldapProvider = new IdentityProvider<>();
+        ldapProvider.setOriginKey(LDAP);
+        ldapProvider.setConfig(definition);
+        ldapProvider.setType(LDAP);
+        ldapProvider.setId("id");
+        return ldapProvider;
+    }
+
+    @Test
+    public void retrieve_ldap_provider_by_id_redacts_password() throws Exception {
+        retrieve_ldap_provider_by_id("");
+    }
+
+    public IdentityProvider<LdapIdentityProviderDefinition> retrieve_ldap_provider_by_id(String id) throws Exception {
+        when(identityProviderProvisioning.retrieve(anyString())).thenReturn(getLdapDefinition());
+        ResponseEntity<IdentityProvider> ldap = identityProviderEndpoints.retrieveIdentityProvider(id, true);
+        assertNotNull(ldap);
+        assertEquals(200,ldap.getStatusCode().value());
+        assertNotNull(ldap.getBody());
+        assertNotNull(ldap.getBody().getConfig());
+        assertTrue(ldap.getBody().getConfig() instanceof LdapIdentityProviderDefinition);
+        assertNull(((LdapIdentityProviderDefinition)ldap.getBody().getConfig()).getBindPassword());
+        return ldap.getBody();
+    }
+
+    @Test
+    public void remove_bind_password() throws Exception {
+        IdentityProvider provider = getLdapDefinition();
+        LdapIdentityProviderDefinition spy = Mockito.spy((LdapIdentityProviderDefinition)provider.getConfig());
+        provider.setConfig(spy);
+        identityProviderEndpoints.removeBindPassword(provider);
+        verify(spy, times(1)).setBindPassword(Matchers.isNull(String.class));
+    }
+
+    @Test
+    public void remove_bind_password_non_ldap() throws Exception {
+        IdentityProvider provider = getLdapDefinition();
+        LdapIdentityProviderDefinition spy = Mockito.spy((LdapIdentityProviderDefinition)provider.getConfig());
+        provider.setConfig(spy);
+        provider.setType(OriginKeys.UNKNOWN);
+        identityProviderEndpoints.removeBindPassword(provider);
+        verify(spy, never()).setBindPassword(Matchers.isNull(String.class));
+    }
+
+    @Test
+    public void patch_bind_password() throws Exception {
+        IdentityProvider provider = getLdapDefinition();
+        LdapIdentityProviderDefinition def = (LdapIdentityProviderDefinition) provider.getConfig();
+        def.setBindPassword(null);
+        LdapIdentityProviderDefinition spy = Mockito.spy(def);
+        provider.setConfig(spy);
+        reset(identityProviderProvisioning);
+        when(identityProviderProvisioning.retrieve(eq(provider.getId()))).thenReturn(getLdapDefinition());
+        identityProviderEndpoints.patchBindPassword(provider.getId(), provider);
+        verify(spy, times(1)).setBindPassword(eq(getLdapDefinition().getConfig().getBindPassword()));
+    }
+
+    @Test
+    public void patch_bind_password_non_ldap() throws Exception {
+        IdentityProvider provider = getLdapDefinition();
+        LdapIdentityProviderDefinition spy = Mockito.spy((LdapIdentityProviderDefinition)provider.getConfig());
+        provider.setConfig(spy);
+        provider.setType(OriginKeys.UNKNOWN);
+        identityProviderEndpoints.removeBindPassword(provider);
+        verify(spy, never()).setBindPassword(anyObject());
+    }
+
+    @Test
+    public void retrieve_ldap_providers_redacts_password() throws Exception {
+        when(identityProviderProvisioning.retrieveAll(anyBoolean(), anyString())).thenReturn(Arrays.asList(getLdapDefinition()));
+        ResponseEntity<List<IdentityProvider>> ldapList = identityProviderEndpoints.retrieveIdentityProviders("false", true);
+        assertNotNull(ldapList);
+        assertNotNull(ldapList.getBody());
+        assertEquals(1, ldapList.getBody().size());
+        IdentityProvider<LdapIdentityProviderDefinition> ldap = ldapList.getBody().get(0);
+        assertNotNull(ldap);
+        assertNotNull(ldap.getConfig());
+        assertTrue(ldap.getConfig() instanceof LdapIdentityProviderDefinition);
+        assertNull(ldap.getConfig().getBindPassword());
+    }
+
+    @Test
+    public void update_ldap_provider_patches_password() throws Exception {
+        IdentityProvider<LdapIdentityProviderDefinition> provider = retrieve_ldap_provider_by_id("id");
+        provider.getConfig().setBindPassword(null);
+        LdapIdentityProviderDefinition spy = Mockito.spy(provider.getConfig());
+        provider.setConfig(spy);
+        reset(identityProviderProvisioning);
+        IdentityProvider<LdapIdentityProviderDefinition> existingConfig = getLdapDefinition();
+        when(identityProviderProvisioning.retrieve(eq(provider.getId()))).thenReturn(existingConfig);
+        when(identityProviderProvisioning.update(anyObject())).thenReturn(existingConfig);
+        identityProviderEndpoints.updateIdentityProvider(provider.getId(), provider, true);
+        verify(spy, times(1)).setBindPassword(eq(existingConfig.getConfig().getBindPassword()));
+        ArgumentCaptor<IdentityProvider> captor = ArgumentCaptor.forClass(IdentityProvider.class);
+        verify(identityProviderProvisioning, times(1)).update(captor.capture());
+        assertNotNull(captor.getValue());
+        assertEquals(1, captor.getAllValues().size());
+        assertEquals(existingConfig.getConfig().getBindPassword(), ((LdapIdentityProviderDefinition)captor.getValue().getConfig()).getBindPassword());
+    }
+
+    @Test
+    public void update_ldap_provider_takes_new_password() throws Exception {
+        IdentityProvider<LdapIdentityProviderDefinition> provider = retrieve_ldap_provider_by_id("id");
+        LdapIdentityProviderDefinition spy = Mockito.spy(provider.getConfig());
+        provider.setConfig(spy);
+        spy.setBindPassword("newpassword");
+        reset(identityProviderProvisioning);
+        IdentityProvider<LdapIdentityProviderDefinition> existingConfig = getLdapDefinition();
+        when(identityProviderProvisioning.retrieve(eq(provider.getId()))).thenReturn(existingConfig);
+        when(identityProviderProvisioning.update(anyObject())).thenReturn(existingConfig);
+        identityProviderEndpoints.updateIdentityProvider(provider.getId(), provider, true);
+        verify(spy, times(1)).setBindPassword(eq("newpassword"));
+        ArgumentCaptor<IdentityProvider> captor = ArgumentCaptor.forClass(IdentityProvider.class);
+        verify(identityProviderProvisioning, times(1)).update(captor.capture());
+        assertNotNull(captor.getValue());
+        assertEquals(1, captor.getAllValues().size());
+        assertEquals("newpassword", ((LdapIdentityProviderDefinition)captor.getValue().getConfig()).getBindPassword());
+    }
+
+    @Test
+    public void create_ldap_provider_removes_password() throws Exception {
+        IdentityProvider<LdapIdentityProviderDefinition> ldapDefinition = getLdapDefinition();
+        assertNotNull(ldapDefinition.getConfig().getBindPassword());
+        when(identityProviderProvisioning.create(anyObject())).thenReturn(ldapDefinition);
+        ResponseEntity<IdentityProvider> response = identityProviderEndpoints.createIdentityProvider(ldapDefinition, true);
+        IdentityProvider created = response.getBody();
+        assertNotNull(created);
+        assertEquals(LDAP, created.getType());
+        assertNotNull(created.getConfig());
+        assertTrue(created.getConfig() instanceof LdapIdentityProviderDefinition);
+        assertNull(((LdapIdentityProviderDefinition)created.getConfig()).getBindPassword());
     }
 
     @Test
