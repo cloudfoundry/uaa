b459367fee2f068348430bf1e90d8f0ee44f3413
Filip Hanik <fhanik@pivotal.io>
2017-05-11 15:33:41 -0700

    Add column to code table
    [#145313303] https://www.pivotaltracker.com/story/show/145313303

---
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/postgresql/V4_0_10__ExpiringCode_Add_ZoneId.sql
@@ -0,0 +1,15 @@
+--
+-- Cloud Foundry
+-- Copyright (c) [2015] Pivotal Software, Inc. All Rights Reserved.
+--
+-- This product is licensed to you under the Apache License, Version 2.0 (the "License").
+-- You may not use this product except in compliance with the License.
+--
+-- This product includes a number of subcomponents with
+-- separate copyright notices and license terms. Your use of these
+-- subcomponents is subject to the terms and conditions of the
+-- subcomponent's license, as noted in the LICENSE file.
+--
+
+-- add zone id to the expiring_code_store table
+ALTER TABLE expiring_code_store ADD COLUMN identity_zone_id varchar(36) DEFAULT 'uaa' NOT NULL ;
