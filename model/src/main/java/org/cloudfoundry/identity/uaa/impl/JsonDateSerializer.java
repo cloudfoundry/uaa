06c1d811d9df3e6f057cecd6df5825aa471ea022
Filip Hanik <fhanik@pivotal.io>
2017-07-25 07:07:25 -0700

    Convert threadlocal to local objects for simplicity
    Format tabs to spaces
    Remove not used variables
    
    [#148609563] https://www.pivotaltracker.com/story/show/148609563

--- model/src/main/java/org/cloudfoundry/identity/uaa/impl/JsonDateSerializer.java
+++ model/src/main/java/org/cloudfoundry/identity/uaa/impl/JsonDateSerializer.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
- *     Cloud Foundry 
- *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
+ *     Cloud Foundry
+ *     Copyright (c) [2009-2017] Pivotal Software, Inc. All Rights Reserved.
  *
  *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
  *     You may not use this product except in compliance with the License.
@@ -10,10 +10,10 @@
  *     subcomponents is subject to the terms and conditions of the
  *     subcomponent's license, as noted in the LICENSE file.
  *******************************************************************************/
+
 package org.cloudfoundry.identity.uaa.impl;
 
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
@@ -21,31 +21,17 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static org.cloudfoundry.identity.uaa.impl.JsonDateDeserializer.DATE_FORMATTER;
+
 /**
  * JSON serializer for Jackson to handle regular date instances as timestamps in
  * ISO format.
- * 
- * @author Dave Syer
- * 
  */
 public class JsonDateSerializer extends JsonSerializer<Date> {
 
-   
-    private static final ThreadLocal<SimpleDateFormat> DATE_FORMATTER = 
-    	    new ThreadLocal<SimpleDateFormat>() {
-    	        @Override
-    	        protected SimpleDateFormat initialValue() {
-    	            return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-    	        }
-    	    };
-
-    public static SimpleDateFormat getDateFormatter() {
-        return DATE_FORMATTER.get();
-    }
     @Override
-    public void serialize(Date date, JsonGenerator generator, SerializerProvider provider) throws IOException,
-        JsonProcessingException {
-        String formatted = getDateFormatter().format(date);
+    public void serialize(Date date, JsonGenerator generator, SerializerProvider provider) throws IOException {
+        String formatted = new SimpleDateFormat(DATE_FORMATTER).format(date);
         generator.writeString(formatted);
     }
 
