09548dea0db0c5811093238b81655f94b40aa99e
Can Berk Güder <cbguder@pivotal.io>
2017-07-28 14:19:31 -0700

    Change CSRF cookie age to 24 hours
    [#148112051] https://www.pivotaltracker.com/story/show/148112051

--- server/src/main/java/org/cloudfoundry/identity/uaa/security/web/CookieBasedCsrfTokenRepository.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/security/web/CookieBasedCsrfTokenRepository.java
@@ -29,7 +29,7 @@ public class CookieBasedCsrfTokenRepository implements CsrfTokenRepository {
 
     public static final String DEFAULT_CSRF_HEADER_NAME = "X-CSRF-TOKEN";
     public static final String DEFAULT_CSRF_COOKIE_NAME = "X-Uaa-Csrf";
-    public static final int DEFAULT_COOKIE_MAX_AGE = 60 * 60 * 24 *30;
+    public static final int DEFAULT_COOKIE_MAX_AGE = 60 * 60 * 24;
 
     // 22 characters of the 62-ary codec gives about 131 bits of entropy, 62 ^ 22 ~ 2^ 130.9923
     private RandomValueStringGenerator generator = new RandomValueStringGenerator(22);
