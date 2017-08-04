4a018b49f24adcf3b74add4fa7593e2e46e0dbe0
Bharath <bharath.sekar@ge.com>
2017-08-04 10:03:20 -0700

    Revert flyway script that restricts scope length to 4000

--- server/src/main/resources/org/cloudfoundry/identity/uaa/db/hsqldb/V4_0_9__ExtendClientAuthScopeSize.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE oauth_client_details ALTER COLUMN scope VARCHAR(4000);
-ALTER TABLE oauth_client_details ALTER COLUMN authorities VARCHAR(4000);
-ALTER TABLE revocable_tokens ALTER COLUMN scope VARCHAR(4000);
--- server/src/main/resources/org/cloudfoundry/identity/uaa/db/mysql/V4_0_9__ExtendClientAuthScopeSize.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE oauth_client_details MODIFY scope VARCHAR(4000);
-ALTER TABLE oauth_client_details MODIFY authorities VARCHAR(4000);
-ALTER TABLE revocable_tokens MODIFY scope VARCHAR(4000);
