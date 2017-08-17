57adf09314522d9d33046285d4e2625da6cd6a85
Bharath Sekar <bharath.sekar@ge.com>
2017-08-17 15:09:54 -0700

    add integration test, change link color conditional
    [#150108577] https://www.pivotaltracker.com/story/show/150108577
    Signed-off-by: Henry zhao <henryzh16@gmail.com>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/LoginIT.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/LoginIT.java
@@ -16,6 +16,8 @@ import com.dumbster.smtp.SimpleSmtpServer;
 import com.dumbster.smtp.SmtpMessage;
 import org.cloudfoundry.identity.uaa.integration.util.IntegrationTestUtils;
 import org.cloudfoundry.identity.uaa.security.web.CookieBasedCsrfTokenRepository;
+import org.cloudfoundry.identity.uaa.zone.BrandingInformation;
+import org.cloudfoundry.identity.uaa.zone.BrandingInformation.Banner;
 import org.cloudfoundry.identity.uaa.zone.IdentityZoneConfiguration;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
@@ -57,6 +59,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
@@ -165,6 +168,39 @@ public class LoginIT {
         assertEquals(zoneUrl + "/login?discoveryPerformed=true&providedUsername=someUser", webDriver.getCurrentUrl());
         assertEquals("someUser", webDriver.findElement(By.cssSelector("input[name=username]")).getAttribute("value"));
         assertEquals("true", webDriver.findElement(By.cssSelector("input[name=password]")).getAttribute("autofocus"));
+    }
+
+    @Test
+    public void testBannerFunctionalityInDiscoveryPage() {
+        String zoneId = "testzone3";
+
+        RestTemplate identityClient = IntegrationTestUtils.getClientCredentialsTemplate(
+            IntegrationTestUtils.getClientCredentialsResource(baseUrl, new String[]{"zones.write", "zones.read", "scim.zones"}, "identity", "identitysecret")
+        );
+        IdentityZoneConfiguration config = new IdentityZoneConfiguration();
+        config.setIdpDiscoveryEnabled(true);
+        Banner banner = new Banner();
+        banner.setText("test banner");
+        banner.setBackgroundColor("rgba(68, 68, 68, 1)");
+        banner.setTextColor("rgba(17, 17, 17, 1)");
+        config.setBranding(new BrandingInformation());
+        config.getBranding().setBanner(banner);
+        IntegrationTestUtils.createZoneOrUpdateSubdomain(identityClient, baseUrl, zoneId, zoneId, config);
+
+        String zoneUrl = baseUrl.replace("localhost",zoneId+".localhost");
+        webDriver.get(zoneUrl);
+        assertEquals("test banner", webDriver.findElement(By.cssSelector(".login-header span")).getText());
+        assertEquals("rgba(68, 68, 68, 1)", webDriver.findElement(By.cssSelector(".login-header")).getCssValue("background-color"));
+        assertEquals("rgba(17, 17, 17, 1)", webDriver.findElement(By.cssSelector(".login-header span")).getCssValue("color"));
+
+        String base64Val = "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAATBJREFUeNqk008og3Ecx/HNnrJSu63kIC5qKRe7KeUiOSulTHJUTrsr0y5ycFaEgyQXElvt5KDYwU0uO2hSUy4KoR7v7/qsfmjPHvzq1e/XU8/39/3zPFHf9yP/WV7jED24nGRbxDFWUAsToM05zyKFLG60d/wmQBxWzwyOlMU1phELEyCmtPeRQRoVbKOM0VYB6q0QW+3IYQpJFFDEYFCAiMqwNY857Ko3SxjGBTbRXb+xMUamcMbWh148YwJvOHSCdyqTAdxZo72ADGwKT98C9CChcxUPQSVYLz50toae4Fy9WcAISl7AiN/RhS1N5RV5rOLxx5eom90pvGAI/VjHMm6bfspK18a1gXvsqM41XDVL052C1Tim56cYd/rR+mdSrXGluxfm5S8Z/HV9CjAAvQZLXoa5mpgAAAAASUVORK5CYII=";
+        banner.setLogo(base64Val);
+
+        IntegrationTestUtils.createZoneOrUpdateSubdomain(identityClient, baseUrl, zoneId, zoneId, config);
+        webDriver.get(zoneUrl);
+
+        assertEquals("data:image/png;base64," + base64Val, webDriver.findElement(By.cssSelector(".login-header img")).getAttribute("src"));
+        assertEquals(2, webDriver.findElement(By.cssSelector(".login-header")).findElements(By.xpath(".//*")).size());
     }
 
     @Test
