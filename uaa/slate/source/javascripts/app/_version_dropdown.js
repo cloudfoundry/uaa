45a392c05944aee96e4e6a7dad3ae688a89c87e0
Henry Zhao <henryzh16@gmail.com>
2017-08-29 15:54:42 -0700

    WIP: version compile works, drop down inserted but not functional.
    [#150449948] https://www.pivotaltracker.com/story/show/150449948
    Signed-off-by: Shash Reddy <sreddy@pivotal.io>

---
+++ uaa/slate/source/javascripts/app/_version_dropdown.js
@@ -0,0 +1,106 @@
+//= require ../lib/_lunr
+
+
+(function() {
+    'use strict';
+
+    var originalSearchResults, originalDropdownLinks;
+    var searchResults, dropdownLinks;
+
+    var index = new lunr.Index();
+
+    index.ref('id');
+    index.field('title', {boost: 10});
+    index.field('body');
+    index.pipeline.add(lunr.trimmer, lunr.stopWordFilter);
+
+    $(setupVersionsDropdown);
+
+    function captureOriginal() {
+        originalSearchResults = $('#version-list').clone();
+        originalDropdownLinks = originalSearchResults.find('.version-link').clone();
+    }
+
+    function bindDropdownPointers() {
+        dropdownLinks = $('.dropdown-content .version-link');
+    }
+
+    function restoreOriginalContent() {
+        searchResults.html(originalSearchResults.html());
+    }
+
+    function bindFilter() {
+        searchResults = $('#version-list');
+        $('#version-select').on('keyup', filterDropdown);
+    }
+
+    function findMatchingLinkFromOriginal(id) {
+        var match = originalDropdownLinks.filter("#" + id)[0];
+        return match;
+    }
+
+    function displaySearchResults(results) {
+        $.each(results, function (index, result) {
+            var matchingLink = findMatchingLinkFromOriginal(result.ref);
+            searchResults.append(matchingLink);
+        });
+    }
+
+    function populateFilter() {
+        dropdownLinks.each(function() {
+            var link = $(this);
+
+            index.add({
+                id: link.prop('id'),
+                title: link.text(),
+                href: link.href
+            });
+        });
+    }
+
+    function filterDropdown(event) {
+        // ESC clears the field
+        if (event.keyCode === 27) this.value = '';
+
+        if (this.value) {
+            var results = index.search(this.value).filter(function(r) {
+                return r.score > 0.0001;
+            });
+            if (results.length) {
+                searchResults.empty();
+                displaySearchResults(results);
+            } else {
+                searchResults.html('<li>' + 'No Results Found for "' + this.value + '"' + '</li>');
+            }
+        } else {
+            searchResults.empty();
+            restoreOriginalContent();
+            populateFilter();
+        }
+    }
+
+    function setupVersionsDropdown() {
+        // $.get(
+        //     "/versions.json",
+        //     function(data) {
+                // var versions = data.versions 
+                var versions =
+                  [
+                    "4.6.0-SNAPSHOT",
+                    "release-candidate"
+                  ];
+                for(var i = 0; i < versions.length; i++) {
+                    var version = versions[i];
+                    if(version == "release-candidate") { continue; }
+                    var li = '<li><a id="version-link-' + version + '"' + ' class="version-link" href="/version/' + version + '">' + version + '</a></li>';
+                    $('#version-list').append(li);
+                }
+
+                captureOriginal();
+                bindDropdownPointers();
+                populateFilter();
+                bindFilter();
+        //     }
+        // );
+    }
+})();
