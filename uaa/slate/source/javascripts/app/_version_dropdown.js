de5ba04ec8b6b0009b6ab6412eeca1ef59dd35ec
Henry Zhao <henryzh16@gmail.com>
2017-09-21 15:23:55 -0700

    Docs fix: version dropdown links respect folder structure
    [#151191551] https://www.pivotaltracker.com/story/show/151191551
    
    Signed-off-by: Aakash Shah <ashah@pivotal.io>

--- uaa/slate/source/javascripts/app/_version_dropdown.js
+++ uaa/slate/source/javascripts/app/_version_dropdown.js
@@ -70,7 +70,7 @@
                 for(var i = 0; i < versions.length; i++) {
                     var version = versions[i];
                     if(version == "release-candidate") { continue; }
-                    var li = '<li><a id="version-link-' + version + '"' + ' class="version-link" href="/version/' + version + '">' + version + '</a></li>';
+                    var li = '<li><a id="version-link-' + version + '"' + ' class="version-link" href="../../version/' + version + '">' + version + '</a></li>';
                     $('#version-list').append(li);
                 }
 
