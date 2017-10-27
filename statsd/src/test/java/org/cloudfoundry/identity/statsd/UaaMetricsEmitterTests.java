f47f17908a291083c24a28a8e44141917575ecc1
Henry Zhao <henryzh16@gmail.com>
2017-10-27 12:52:43 -0700

    Fix statsd notification reliance on UAA
    - Previous refactor broke the assumption about uaa being independent of
      statds so this commit restores the behavior.
    
    [#152093262] https://www.pivotaltracker.com/story/show/152093262
    
    Signed-off-by: Shash Reddy <sreddy@pivotal.io>

--- statsd/src/test/java/org/cloudfoundry/identity/statsd/UaaMetricsEmitterTests.java
+++ statsd/src/test/java/org/cloudfoundry/identity/statsd/UaaMetricsEmitterTests.java
@@ -157,6 +157,7 @@ public class UaaMetricsEmitterTests {
 
     @Test
     public void testNotifications() throws Exception {
+        uaaMetricsEmitter.enableNotification();
         emitter.sendNotification(new Notification("/api", 45L, 0));
         Mockito.verify(statsDClient).time("requests.api.latency", 45L);
     }
