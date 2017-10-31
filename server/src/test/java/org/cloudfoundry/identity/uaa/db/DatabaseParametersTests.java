5759cc3644a63eea50b04a8a35308e9ec363905c
Filip Hanik <fhanik@pivotal.io>
2017-10-31 11:21:29 -0700

    Have the default string use 127.0.0.1 connecting

--- server/src/test/java/org/cloudfoundry/identity/uaa/db/DatabaseParametersTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/db/DatabaseParametersTests.java
@@ -19,80 +19,60 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.cloudfoundry.identity.uaa.test.JdbcTestBase;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.springframework.mock.env.MockEnvironment;
+import org.springframework.util.StringUtils;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-import static org.cloudfoundry.identity.uaa.db.Vendor.hsqldb;
-import static org.cloudfoundry.identity.uaa.db.Vendor.mysql;
-import static org.cloudfoundry.identity.uaa.db.Vendor.postgresql;
-import static org.cloudfoundry.identity.uaa.db.Vendor.sqlserver;
 import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
-public class DatabaseParametersTests {
+public class DatabaseParametersTests extends JdbcTestBase {
 
 
-    @Parameterized.Parameters(name = "{index}: database[{0}]")
-    public static Object[][] parameters() {
-        return new Object[][] {{mysql}, {postgresql}, {hsqldb}, {sqlserver}};
-    }
+    private Vendor vendor;
 
-    private final Vendor vendor;
-    private UrlTester tester;
-
-    public DatabaseParametersTests(Vendor vendor) {
-        this.vendor = vendor;
-    }
-
+    @Override
     @Before
-    public void setup() throws Exception {
+    public void setUp() throws Exception {
         MockEnvironment environment = new MockEnvironment();
         environment.setProperty("database.initialsize", "0");
         environment.setProperty("database.validationquerytimeout", "5");
         environment.setProperty("database.connecttimeout", "5");
-        environment.setActiveProfiles(vendor.name());
-        tester = new UrlTester();
-        tester.setUp(environment);
-
-    }
-
-    @After
-    public void teardown() throws Exception {
-        tester.teardown();
+        if (System.getProperty("spring.profiles.active")!=null) {
+            environment.setActiveProfiles(StringUtils.commaDelimitedListToStringArray(System.getProperty("spring.profiles.active")));
+        }
+        super.setUp(environment);
+        vendor = webApplicationContext.getBean(DatabaseUrlModifier.class).getDatabaseType();
     }
 
     @Test
     public void initial_size() throws Exception {
-        assertEquals(0, tester.getDataSource().getInitialSize());
+        assertEquals(0, getDataSource().getInitialSize());
     }
 
     @Test
     public void validation_query_timeout() throws Exception {
-        assertEquals(5, tester.getDataSource().getValidationQueryTimeout());
+        assertEquals(5, getDataSource().getValidationQueryTimeout());
     }
 
     @Test
     public void connection_timeout_property_set() throws Exception {
         switch (vendor) {
             case mysql : {
-                assertEquals("5000", tester.getUrlParameter("connectTimeout"));
+                assertEquals("5000", getUrlParameter("connectTimeout"));
                 break;
             }
             case postgresql : {
-                assertEquals("5", tester.getUrlParameter("connectTimeout"));
+                assertEquals("5", getUrlParameter("connectTimeout"));
                 break;
             }
             case sqlserver : {
-                assertEquals("5", tester.getUrlParameter("loginTimeout"));
+                assertEquals("5", getUrlParameter("loginTimeout"));
                 break;
             }
             case hsqldb : {break;}
@@ -101,36 +81,20 @@ public class DatabaseParametersTests {
 
     }
 
-    public static class UrlTester extends JdbcTestBase {
-        public void teardown() throws Exception {
-            super.tearDown(true);
-        }
+    public DataSource getDataSource() {
+        return (DataSource)dataSource;
+    }
 
-        @Override
-        public void setUp() throws Exception {
-            //no op
-        }
-
-        @Override
-        public void setUp(MockEnvironment environment) throws Exception {
-            super.setUp(environment);
-        }
-
-        public DataSource getDataSource() {
-            return (DataSource)dataSource;
-        }
-
-        public String getUrlParameter(String name) throws URISyntaxException {
-            String dburl = getDataSource().getUrl();
-            URI uri = URI.create("http://localhost" + dburl.substring(dburl.indexOf("?")));
-            List<NameValuePair> pairs = URLEncodedUtils.parse(uri, StandardCharsets.UTF_8);
-            for (NameValuePair p : pairs) {
-                if (name.equals(p.getName())) {
-                    return p.getValue();
-                }
+    public String getUrlParameter(String name) throws URISyntaxException {
+        String dburl = getDataSource().getUrl();
+        URI uri = URI.create("http://localhost" + dburl.substring(dburl.indexOf("?")));
+        List<NameValuePair> pairs = URLEncodedUtils.parse(uri, StandardCharsets.UTF_8);
+        for (NameValuePair p : pairs) {
+            if (name.equals(p.getName())) {
+                return p.getValue();
             }
-            return null;
         }
+        return null;
     }
 
 }
