fe326cced4344a70bcf97456488bc0cd7d4f20d7
Torsten Luh <torsten.luh@sap.com>
2018-03-08 10:16:13 +0000

    Check for prompt=login and max_age in ReAuthFilter
    [#155800120] https://www.pivotaltracker.com/story/show/155800120
    
    Signed-off-by: Florian Tack <florian.tack@sap.com>

--- server/src/main/java/org/cloudfoundry/identity/uaa/authentication/ReAuthenticationRequiredFilter.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/authentication/ReAuthenticationRequiredFilter.java
@@ -1,5 +1,6 @@
 package org.cloudfoundry.identity.uaa.authentication;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -12,6 +13,16 @@ import java.io.IOException;
 public class ReAuthenticationRequiredFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        boolean reAuthenticationRequired = false;
+        if ("login".equals(request.getParameter("prompt"))) {
+            reAuthenticationRequired = true;
+        }
+        if (request.getParameter("max_age") != null && SecurityContextHolder.getContext().getAuthentication() instanceof UaaAuthentication) {
+            UaaAuthentication auth = (UaaAuthentication) SecurityContextHolder.getContext().getAuthentication();
+            if ((System.currentTimeMillis() - auth.getAuthenticatedTime()) > (Long.valueOf(request.getParameter("max_age"))*1000)) {
+                reAuthenticationRequired = true;
+            }
+        }
         filterChain.doFilter(request, response);
     }
 }
