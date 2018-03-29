bca3371a3a7c79fe1c1f4b490efbc3a64a699bbd
Jennifer Hamon <jhamon@pivotal.io>
2018-03-29 16:43:10 -0700

    Add migration test to cover previously ran db migrations correctly add primary key columns
    [#155725419] https://www.pivotaltracker.com/story/show/155725419
    
    Signed-off-by: Dennis Leon <dleon@pivotal.io>

--- server/src/test/java/org/cloudfoundry/identity/uaa/db/DbMigrationIntegrationTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/db/DbMigrationIntegrationTest.java
@@ -6,15 +6,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@Profile("mysql")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:/spring/data-source.xml", "classpath*:/spring/env.xml"})
 public class DbMigrationIntegrationTest {
@@ -49,19 +53,55 @@ public class DbMigrationIntegrationTest {
             @Override
             public void runAssertions() throws Exception {
                 int count = jdbcTemplate.queryForObject(checkPrimaryKeyExists, Integer.class, jdbcTemplate.getDataSource().getConnection().getCatalog(), "oauth_code");
-                assertThat(count, is(1));
+                assertThat("oauth_code is missing primary key", count, is(1));
 
                 count = jdbcTemplate.queryForObject(checkPrimaryKeyExists, Integer.class, jdbcTemplate.getDataSource().getConnection().getCatalog(), "group_membership");
-                assertThat(count, is(1));
+                assertThat("group_membership is missing primary key", count, is(1));
 
                 count = jdbcTemplate.queryForObject(checkPrimaryKeyExists, Integer.class, jdbcTemplate.getDataSource().getConnection().getCatalog(), "sec_audit");
-                assertThat(count, is(1));
+                assertThat("sec_audit is missing primary key", count, is(1));
 
                 count = jdbcTemplate.queryForObject(checkPrimaryKeyExists, Integer.class, jdbcTemplate.getDataSource().getConnection().getCatalog(), "external_group_mapping");
-                assertThat(count, is(1));
+                assertThat("external_group_membership is missing primary key", count, is(1));
             }
         };
 
         migrationTestRunner.run(migrationTest);
+    }
+
+    /*
+        We have had to modify existing db migrations. This means that some uaa deploys will not apply these 'modified' migration scripts. We want to test that in these cases that primary key columns are still created
+        See: https://www.pivotaltracker.com/story/show/155725419
+    */
+    @Test
+    public void insertMissingPrimaryKeys_whenOldMigrationWithoutPrimaryKeyModificationHasAlreadyRun() throws SQLException {
+        List<MigrationTest> migrationTest = Arrays.asList(new MigrationTest() {
+            @Override
+            public String getTargetMigration() {
+                return "2.4.1";
+            }
+
+            @Override
+            public void runAssertions() throws Exception {
+                jdbcTemplate.execute("ALTER TABLE group_membership drop column id");
+                jdbcTemplate.execute("ALTER TABLE external_group_mapping drop column id");
+            }
+        }, new MigrationTest() {
+            @Override
+            public String getTargetMigration() {
+                return "4.9.2";
+            }
+
+            @Override
+            public void runAssertions() throws Exception {
+                int count = jdbcTemplate.queryForObject(checkPrimaryKeyExists, Integer.class, jdbcTemplate.getDataSource().getConnection().getCatalog(), "group_membership");
+                assertThat("group_membership is missing primary key", count, is(1));
+
+                count = jdbcTemplate.queryForObject(checkPrimaryKeyExists, Integer.class, jdbcTemplate.getDataSource().getConnection().getCatalog(), "external_group_mapping");
+                assertThat("external_group_mapping is missing primary key", count, is(1));
+            }
+        });
+
+        migrationTestRunner.run(migrationTest.toArray(new MigrationTest[]{}));
     }
 }
