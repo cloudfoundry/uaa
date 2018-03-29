463763669e6ec280493239027652fa5290dff194
Dennis Leon <dleon@pivotal.io>
2018-03-29 16:43:19 -0700

    Add db migration and db migration tests for sqlserver
    [#155725419] https://www.pivotaltracker.com/story/show/155725419
    
    Signed-off-by: Jaskanwal Pawar <jspawar@pivotal.io>

--- server/src/test/java/org/cloudfoundry/identity/uaa/db/SqlServerDbMigrationIntegrationTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/db/SqlServerDbMigrationIntegrationTest.java
@@ -19,18 +19,18 @@ import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.hasSize;
 
-@Profile("postgresql")
+@Profile("sqlserver")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:/spring/data-source.xml", "classpath*:/spring/env.xml"})
-public class PostgresDbMigrationIntegrationTest {
+public class SqlServerDbMigrationIntegrationTest {
     @Autowired
     private Flyway flyway;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    private String checkPrimaryKeyExists = "SELECT COUNT(*) FROM information_schema.KEY_COLUMN_USAGE WHERE TABLE_CATALOG = ? AND TABLE_NAME = LOWER(?) AND CONSTRAINT_NAME LIKE LOWER(?)";
-    private String getAllTableNames = "SELECT distinct TABLE_NAME from information_schema.KEY_COLUMN_USAGE where TABLE_CATALOG = ? and TABLE_NAME != 'schema_version'";
+    private String checkPrimaryKeyExists = "SELECT COUNT(*) FROM information_schema.KEY_COLUMN_USAGE WHERE TABLE_CATALOG = ? AND TABLE_NAME = LOWER(?) AND CONSTRAINT_NAME like 'PK_%'";
+    private String getAllTableNames = "SELECT distinct TABLE_NAME from information_schema.tables WHERE TABLE_CATALOG = ? and TABLE_NAME != 'schema_version'";
 
     @Before
     public void setup() {
@@ -49,7 +49,7 @@ public class PostgresDbMigrationIntegrationTest {
         List<String> tableNames = jdbcTemplate.queryForList(getAllTableNames, String.class, jdbcTemplate.getDataSource().getConnection().getCatalog());
         assertThat(tableNames, hasSize(greaterThan(0)));
         for (String tableName : tableNames) {
-            int count = jdbcTemplate.queryForObject(checkPrimaryKeyExists, Integer.class, jdbcTemplate.getDataSource().getConnection().getCatalog(), tableName, "%" + tableName + "_pk%");
+            int count = jdbcTemplate.queryForObject(checkPrimaryKeyExists, Integer.class, jdbcTemplate.getDataSource().getConnection().getCatalog(), tableName);
             assertThat(format("%s is missing primary key", tableName), count, greaterThanOrEqualTo(1));
         }
     }
