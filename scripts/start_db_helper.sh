4e8e31cb586d1084720a1f1139f09b1007de9a30
Jennifer Hamon <jhamon@pivotal.io>
2018-04-04 14:41:17 -0700

    Parameterize volume mount location
    Needed since travis and concourse will mount the uaa code in
    different locations
    
    [#154653975] https://www.pivotaltracker.com/story/show/154653975
    
    Signed-off-by: Jaskanwal Pawar <jpawar@pivotal.io>

--- scripts/start_db_helper.sh
+++ scripts/start_db_helper.sh
@@ -16,7 +16,7 @@ function bootDB {
   elif [ "$db" = "sqlserver" ]; then
     launchDB="(/opt/mssql/bin/sqlservr &> /var/log/sqlserver-boot.log) &"
     testConnection="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'changemeCHANGEME1234!' -d master -Q \"select 'hello'\""
-    initDB="pushd /root/uaa; ./gradlew -b mssql.gradle createSQLServerUAA; popd"
+    initDB="pushd $UAA_DIR; ./gradlew -b mssql.gradle createSQLServerUAA; popd"
   else
     echo "skipping database"
     return 0
