c789f6772f0b2d16c7ef1a7cabdbe34dfa82e975
Jennifer Hamon <jhamon@pivotal.io>
2018-04-03 17:16:10 -0700

    Add some gradle flags to hopefully speed up test runs

--- scripts/integration-tests.sh
+++ scripts/integration-tests.sh
@@ -25,5 +25,5 @@ pushd $(dirname $DIR)
   ./scripts/ldap/configure-manifest.sh
   ldapadd -Y EXTERNAL -H ldapi:/// -f ./uaa/src/main/resources/ldap_db_init.ldif
   ldapadd -x -D 'cn=admin,dc=test,dc=com' -w password -f ./uaa/src/main/resources/ldap_init.ldif
-  ./gradlew "-Dspring.profiles.active=$TESTENV" jacocoRootReportIntegrationTest
+  ./gradlew "-Dspring.profiles.active=$TESTENV" jacocoRootReportIntegrationTest --no-daemon --stacktrace --console=plain -x :cloudfoundry-identity-samples:assemble -x javadoc -x javadocJar
 popd
