6f47f2bb8f82529a0b9f323b10d021e14466dd04
Bruce Ricard <bricard@pivotal.io>
2018-04-12 11:30:09 -0700

    Fix test scripts by giving the uaa root path as argument to the updated inner script.
    Signed-off-by: Jennifer Hamon <jhamon@pivotal.io>

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -4,5 +4,6 @@ set -xeu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 DB_IMAGE_NAME="$1"
+CONTAINER_SCRIPT_DIR='/root/uaa'
 
-docker run --privileged -t -i --shm-size=1G  -v ${SCRIPT_DIR}:/root/uaa cfidentity/uaa-${DB_IMAGE_NAME} /root/uaa/scripts/integration-tests.sh ${DB_IMAGE_NAME},default
+docker run --privileged -t -i --shm-size=1G  -v "${SCRIPT_DIR}":"${CONTAINER_SCRIPT_DIR}" "cfidentity/uaa-${DB_IMAGE_NAME}" /root/uaa/scripts/integration-tests.sh "${DB_IMAGE_NAME}",default "${CONTAINER_SCRIPT_DIR}"
