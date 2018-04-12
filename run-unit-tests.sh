0743e6a76f0afcc3cba5da08b829ca83a7b8181d
Jennifer Hamon <jhamon@pivotal.io>
2018-04-12 11:37:34 -0700

    Allow the manual test scripts to be run in parallel.
    Signed-off-by: Bruce Ricard <bricard@pivotal.io>

--- run-unit-tests.sh
+++ run-unit-tests.sh
@@ -5,5 +5,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 DB_IMAGE_NAME="$1"
 CONTAINER_SCRIPT_DIR='/root/uaa'
+GRADLE_LOCK_DIR='/root/uaa/.gradle/'
 
-docker run --privileged -t -i --shm-size=1G  -v "${SCRIPT_DIR}":"${CONTAINER_SCRIPT_DIR}" "cfidentity/uaa-${DB_IMAGE_NAME}" /root/uaa/scripts/unit-tests.sh "${DB_IMAGE_NAME}",default "${CONTAINER_SCRIPT_DIR}"
+docker run --privileged -t -i --shm-size=1G  -v "${SCRIPT_DIR}":"${CONTAINER_SCRIPT_DIR}" -v "${GRADLE_LOCK_DIR}" "cfidentity/uaa-${DB_IMAGE_NAME}" /root/uaa/scripts/unit-tests.sh "${DB_IMAGE_NAME}",default "${CONTAINER_SCRIPT_DIR}"
