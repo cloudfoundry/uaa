c555ba703abb59e6b28dcef81faa0733faceb242
Dennis Leon <dleon@pivotal.io>
2018-04-19 16:25:15 -0700

    Adding validation logic for new encryption key config.
    [#156445728] https://www.pivotaltracker.com/story/show/156445728
    
    Signed-off-by: Bruce Ricard <bruce.ricard@gmail.com>

--- server/src/main/java/org/cloudfoundry/identity/uaa/cypto/ActiveEncryptionKeyService.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/cypto/ActiveEncryptionKeyService.java
@@ -1,15 +1,40 @@
 package org.cloudfoundry.identity.uaa.cypto;
 
+import org.apache.directory.api.util.Strings;
+
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ActiveEncryptionKeyService {
     private final EncryptionKey activeKeyPassphrase;
 
     public ActiveEncryptionKeyService(String activeKeyLabel, List<EncryptionKey> encryptionKeys) {
-        activeKeyPassphrase = encryptionKeys.stream().filter(v -> v.getLabel().equals(activeKeyLabel)).findFirst().orElseGet(() -> {
-            throw new NoActiveEncryptionKeyProvided(String.format("%s was not provided as an encryption key", activeKeyLabel));
-        });
+        if (Strings.isEmpty(activeKeyLabel)) {
+            throw new NoActiveEncryptionKeyProvided(
+              "UAA cannot be started without encryption key value uaa.encryption.active_key_label"
+            );
+        }
+
+        List<EncryptionKey> keysWithoutPassphrase = encryptionKeys.stream().filter(encryptionKey -> Strings.isEmpty(encryptionKey.getPassphrase())).collect(Collectors.toList());
+
+        if (!keysWithoutPassphrase.isEmpty()) {
+            throw new NoActiveEncryptionKeyProvided(
+              String.format("UAA cannot be started as encryption key passphrase for uaa.encryption.encryption_keys/[%s] is undefined",
+                String.join(", ", keysWithoutPassphrase.stream().map(s -> "label=" + s.getLabel()).collect(Collectors.toList()))
+              )
+            );
+        }
+
+        activeKeyPassphrase =
+          encryptionKeys.stream()
+            .filter(v -> v.getLabel().equals(activeKeyLabel))
+            .findFirst()
+            .orElseGet(() -> {
+                throw new NoActiveEncryptionKeyProvided(
+                  String.format("UAA cannot be started as encryption key passphrase for uaa.encryption.encryption_keys/[label=%s] is undefined", activeKeyLabel)
+                );
+            });
     }
 
     public String getPassphrase() {
--- server/src/test/java/org/cloudfoundry/identity/uaa/cypto/ActiveEncryptionKeyServiceTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/cypto/ActiveEncryptionKeyServiceTest.java
@@ -1,5 +1,6 @@
 package org.cloudfoundry.identity.uaa.cypto;
 
+import com.google.common.collect.Lists;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -43,9 +44,45 @@ public class ActiveEncryptionKeyServiceTest {
 
     @Test
     public void shouldThrowErrorIfActiveEncryptionKeyHasNotBeenProvided() {
+        String activeKeyLabel = "active-key" + System.currentTimeMillis();
         expectedException.expect(NoActiveEncryptionKeyProvided.class);
-        expectedException.expectMessage("active-key was not provided as an encryption key");
+        expectedException.expectMessage(String.format("UAA cannot be started as encryption key passphrase for uaa.encryption.encryption_keys/[label=%s] is undefined", activeKeyLabel));
 
-        activeEncryptionKeyService = new ActiveEncryptionKeyService("active-key", new ArrayList<>());
+        activeEncryptionKeyService = new ActiveEncryptionKeyService(activeKeyLabel, new ArrayList<>());
+    }
+
+    @Test
+    public void shouldThrowErrorIfNoActiveKeyLabelIsProvided() {
+        expectedException.expect(NoActiveEncryptionKeyProvided.class);
+        expectedException.expectMessage("UAA cannot be started without encryption key value uaa.encryption.active_key_label");
+
+        activeEncryptionKeyService = new ActiveEncryptionKeyService("", new ArrayList<>());
+    }
+
+    @Test
+    public void eachEncryptionKeyShouldHaveANonEmptyPassphrase() {
+        String key2 = "key2" + System.currentTimeMillis();
+        String key3 = "key3" + System.currentTimeMillis();
+
+        expectedException.expect(NoActiveEncryptionKeyProvided.class);
+        expectedException.expectMessage(
+          String.format("UAA cannot be started as encryption key passphrase for uaa.encryption.encryption_keys/[label=%s, label=%s] is undefined", key2, key3)
+        );
+
+        ActiveEncryptionKeyService.EncryptionKey activeEncryptionKey = new ActiveEncryptionKeyService.EncryptionKey();
+        activeEncryptionKey.put("label", "key1");
+        activeEncryptionKey.put("passphrase", "123456789");
+
+        ActiveEncryptionKeyService.EncryptionKey encryptionKey2 = new ActiveEncryptionKeyService.EncryptionKey();
+        encryptionKey2.put("label", key2);
+        encryptionKey2.put("passphrase", "");
+
+
+        ActiveEncryptionKeyService.EncryptionKey encryptionKey3 = new ActiveEncryptionKeyService.EncryptionKey();
+        encryptionKey3.put("label", key3);
+        encryptionKey3.put("passphrase", "");
+
+        activeEncryptionKeyService =
+          new ActiveEncryptionKeyService("key1", Lists.newArrayList(activeEncryptionKey, encryptionKey2, encryptionKey3));
     }
 }
