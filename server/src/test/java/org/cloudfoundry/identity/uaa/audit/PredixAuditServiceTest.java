642770c26b6774ce194a0cf91943a00b68bcbdbd
Brittany <brittany.johnson@ge.com>
2018-04-26 10:31:24 -0700

    Exclude additional jackson core, databind, and annotations dependencies. Exclude event-hub-sdk which brings in jackson < 2.9.0. Predix auditing profile is temporarily commented out and is unusable.

--- server/src/test/java/org/cloudfoundry/identity/uaa/audit/PredixAuditServiceTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/audit/PredixAuditServiceTest.java
@@ -1,183 +1,183 @@
-package org.cloudfoundry.identity.uaa.audit;
-
-import com.ge.predix.audit.sdk.AuditClient;
-import com.ge.predix.audit.sdk.exception.AuditException;
-import com.ge.predix.audit.sdk.message.AuditEnums;
-import com.ge.predix.audit.sdk.message.AuditEventV2;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatcher;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.junit.Assert.fail;
-import static org.mockito.ArgumentMatchers.argThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-@RunWith(MockitoJUnitRunner.class)
-public class PredixAuditServiceTest {
-
-    private static final String DATA = "data";
-    private static final String IDENTITY_ZONE_ID = "12345678-1234-1234-1234-123456789012";
-    private static final String CORRELATION_ID = "abcdeabc-abcd-abcd-abcd-abcdeabcdeab";
-
-    @Mock
-    private AuditClient mockAuditClient;
-
-    @Spy
-    @InjectMocks
-    PredixAuditService auditService = new PredixAuditService();
-
-    @Test
-    public void testAuditNoCorrelationId() {
-        String expectedData = AuditEventType.ClientAuthenticationFailure
-                .toString() + ": " + DATA;
-        AuditEvent auditEvent = new AuditEvent(
-                AuditEventType.ClientAuthenticationFailure, "princialID",
-                "origin", DATA, System.currentTimeMillis(), IDENTITY_ZONE_ID,
-                null, null);
-        try {
-            auditService.log(auditEvent, IDENTITY_ZONE_ID);
-            verify(mockAuditClient)
-                    .audit(argThat(new AuditServiceOutputValidator(
-                            AuditEnums.CategoryType.AUTHENTICATIONS,
-                            AuditEnums.EventType.LOGIN_FAILURE,
-                            AuditEnums.Classifier.FAILURE, null,
-                            IDENTITY_ZONE_ID, expectedData)));
-        } catch (AuditException e) {
-            fail("threw auditException: " + e.getMessage());
-        }
-    }
-
-    @Test
-    public void testAuditNoIdentityZone() {
-        String expectedData = "Z: null "
-                + AuditEventType.ClientAuthenticationFailure.toString() + ": "
-                + DATA;
-        AuditEvent auditEvent = new AuditEvent(
-                AuditEventType.ClientAuthenticationFailure, "princialID",
-                "origin", DATA, System.currentTimeMillis(), null,
-                null, null);
-        when(auditService.getCorrelationId()).thenReturn(CORRELATION_ID);
-        try {
-            auditService.log(auditEvent, null);
-            verify(mockAuditClient)
-                    .audit(argThat(new AuditServiceOutputValidator(
-                            AuditEnums.CategoryType.AUTHENTICATIONS,
-                            AuditEnums.EventType.LOGIN_FAILURE,
-                            AuditEnums.Classifier.FAILURE, CORRELATION_ID, null,
-                            expectedData)));
-        } catch (AuditException e) {
-            fail("threw auditException: " + e.getMessage());
-        }
-    }
-
-    @Test
-    public void testAuditIdentityZoneNotUUID() {
-        String identityZoneId = "not-a-uuid";
-        String expectedData = "Z: " + identityZoneId + " "
-                + AuditEventType.ClientAuthenticationFailure.toString() + ": "
-                + DATA;
-        AuditEvent auditEvent = new AuditEvent(
-                AuditEventType.ClientAuthenticationFailure, "princialID",
-                "origin", DATA, System.currentTimeMillis(), identityZoneId,
-                null, null);
-        when(auditService.getCorrelationId()).thenReturn(CORRELATION_ID);
-        try {
-            auditService.log(auditEvent, identityZoneId);
-            verify(mockAuditClient)
-                    .audit(argThat(new AuditServiceOutputValidator(
-                            AuditEnums.CategoryType.AUTHENTICATIONS,
-                            AuditEnums.EventType.LOGIN_FAILURE,
-                            AuditEnums.Classifier.FAILURE, CORRELATION_ID, null,
-                            expectedData)));
-        } catch (AuditException e) {
-            fail("threw auditException: " + e.getMessage());
-        }
-    }
-
-    @Test
-    public void testAuditIDPDeleted() {
-        String data = "('Class:org.cloudfoundry.identity.uaa.provider.IdentityProvider; ID:')";
-        String expectedData = AuditEventType.EntityDeletedEvent + ": " + data;
-        AuditEvent auditEvent = new AuditEvent(
-                AuditEventType.EntityDeletedEvent, "princialID", "origin", data,
-                System.currentTimeMillis(), IDENTITY_ZONE_ID,
-                null, null);
-        when(auditService.getCorrelationId()).thenReturn(CORRELATION_ID);
-        try {
-            auditService.log(auditEvent, IDENTITY_ZONE_ID);
-            verify(mockAuditClient)
-                    .audit(argThat(new AuditServiceOutputValidator(
-                            AuditEnums.CategoryType.AUTHORIZATION,
-                            AuditEnums.EventType.ACCOUNT_PRIVILEGE_SUCCESS_MODIFICATION,
-                            AuditEnums.Classifier.SUCCESS, CORRELATION_ID,
-                            IDENTITY_ZONE_ID, expectedData)));
-        } catch (AuditException e) {
-            fail("threw auditException: " + e.getMessage());
-        }
-    }
-
-    @Test
-    public void testAuditIdentityZoneDeleted() {
-        String data = "('Class:org.cloudfoundry.identity.uaa.zone.IdentityZone; ID:')";
-        String expectedData = AuditEventType.EntityDeletedEvent + ": " + data;
-        AuditEvent auditEvent = new AuditEvent(
-                AuditEventType.EntityDeletedEvent, "princialID", "origin", data,
-                System.currentTimeMillis(), IDENTITY_ZONE_ID,
-                null, null);
-        when(auditService.getCorrelationId()).thenReturn(CORRELATION_ID);
-        try {
-            auditService.log(auditEvent, IDENTITY_ZONE_ID);
-            verify(mockAuditClient)
-                    .audit(argThat(new AuditServiceOutputValidator(
-                            AuditEnums.CategoryType.ADMINISTRATIONS,
-                            AuditEnums.EventType.CHANGE_CONFIGURATIONS_SUCCESS,
-                            AuditEnums.Classifier.SUCCESS, CORRELATION_ID,
-                            IDENTITY_ZONE_ID, expectedData)));
-        } catch (AuditException e) {
-            fail("threw auditException: " + e.getMessage());
-        }
-    }
-
-    private class AuditServiceOutputValidator
-            implements
-                ArgumentMatcher<AuditEventV2> {
-        private String correlationId;
-        private String identityZoneId;
-        private AuditEnums.Classifier status;
-        private AuditEnums.EventType eventType;
-        private AuditEnums.CategoryType categoryType;
-        private String payload;
-
-        public AuditServiceOutputValidator(AuditEnums.CategoryType categoryType,
-                AuditEnums.EventType eventType, AuditEnums.Classifier status,
-                String correlationId, String identityZoneId, String payload) {
-            this.correlationId = correlationId;
-            this.identityZoneId = identityZoneId;
-            this.status = status;
-            this.eventType = eventType;
-            this.categoryType = categoryType;
-            this.payload = payload;
-        }
-
-        public String toString() {
-            // printed in verification errors
-            return "audit service did not match expected";
-        }
-
-        @Override
-        public boolean matches(AuditEventV2 actualAuditEvent) {
-            return actualAuditEvent.getCategoryType() == categoryType
-                    && actualAuditEvent.getEventType() == eventType
-                    && actualAuditEvent.getClassifier() == status
-                    && actualAuditEvent.getTenantUuid() == identityZoneId
-                    && actualAuditEvent.getCorrelationId() == correlationId
-                    && actualAuditEvent.getPayload().equals(payload);
-        }
-    }
-
-}
+//package org.cloudfoundry.identity.uaa.audit;
+//
+//import com.ge.predix.audit.sdk.AuditClient;
+//import com.ge.predix.audit.sdk.exception.AuditException;
+//import com.ge.predix.audit.sdk.message.AuditEnums;
+//import com.ge.predix.audit.sdk.message.AuditEventV2;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.ArgumentMatcher;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.Spy;
+//import org.mockito.junit.MockitoJUnitRunner;
+//
+//import static org.junit.Assert.fail;
+//import static org.mockito.ArgumentMatchers.argThat;
+//import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.when;
+//
+//@RunWith(MockitoJUnitRunner.class)
+//public class PredixAuditServiceTest {
+//
+//    private static final String DATA = "data";
+//    private static final String IDENTITY_ZONE_ID = "12345678-1234-1234-1234-123456789012";
+//    private static final String CORRELATION_ID = "abcdeabc-abcd-abcd-abcd-abcdeabcdeab";
+//
+//    @Mock
+//    private AuditClient mockAuditClient;
+//
+//    @Spy
+//    @InjectMocks
+//    PredixAuditService auditService = new PredixAuditService();
+//
+//    @Test
+//    public void testAuditNoCorrelationId() {
+//        String expectedData = AuditEventType.ClientAuthenticationFailure
+//                .toString() + ": " + DATA;
+//        AuditEvent auditEvent = new AuditEvent(
+//                AuditEventType.ClientAuthenticationFailure, "princialID",
+//                "origin", DATA, System.currentTimeMillis(), IDENTITY_ZONE_ID,
+//                null, null);
+//        try {
+//            auditService.log(auditEvent, IDENTITY_ZONE_ID);
+//            verify(mockAuditClient)
+//                    .audit(argThat(new AuditServiceOutputValidator(
+//                            AuditEnums.CategoryType.AUTHENTICATIONS,
+//                            AuditEnums.EventType.LOGIN_FAILURE,
+//                            AuditEnums.Classifier.FAILURE, null,
+//                            IDENTITY_ZONE_ID, expectedData)));
+//        } catch (AuditException e) {
+//            fail("threw auditException: " + e.getMessage());
+//        }
+//    }
+//
+//    @Test
+//    public void testAuditNoIdentityZone() {
+//        String expectedData = "Z: null "
+//                + AuditEventType.ClientAuthenticationFailure.toString() + ": "
+//                + DATA;
+//        AuditEvent auditEvent = new AuditEvent(
+//                AuditEventType.ClientAuthenticationFailure, "princialID",
+//                "origin", DATA, System.currentTimeMillis(), null,
+//                null, null);
+//        when(auditService.getCorrelationId()).thenReturn(CORRELATION_ID);
+//        try {
+//            auditService.log(auditEvent, null);
+//            verify(mockAuditClient)
+//                    .audit(argThat(new AuditServiceOutputValidator(
+//                            AuditEnums.CategoryType.AUTHENTICATIONS,
+//                            AuditEnums.EventType.LOGIN_FAILURE,
+//                            AuditEnums.Classifier.FAILURE, CORRELATION_ID, null,
+//                            expectedData)));
+//        } catch (AuditException e) {
+//            fail("threw auditException: " + e.getMessage());
+//        }
+//    }
+//
+//    @Test
+//    public void testAuditIdentityZoneNotUUID() {
+//        String identityZoneId = "not-a-uuid";
+//        String expectedData = "Z: " + identityZoneId + " "
+//                + AuditEventType.ClientAuthenticationFailure.toString() + ": "
+//                + DATA;
+//        AuditEvent auditEvent = new AuditEvent(
+//                AuditEventType.ClientAuthenticationFailure, "princialID",
+//                "origin", DATA, System.currentTimeMillis(), identityZoneId,
+//                null, null);
+//        when(auditService.getCorrelationId()).thenReturn(CORRELATION_ID);
+//        try {
+//            auditService.log(auditEvent, identityZoneId);
+//            verify(mockAuditClient)
+//                    .audit(argThat(new AuditServiceOutputValidator(
+//                            AuditEnums.CategoryType.AUTHENTICATIONS,
+//                            AuditEnums.EventType.LOGIN_FAILURE,
+//                            AuditEnums.Classifier.FAILURE, CORRELATION_ID, null,
+//                            expectedData)));
+//        } catch (AuditException e) {
+//            fail("threw auditException: " + e.getMessage());
+//        }
+//    }
+//
+//    @Test
+//    public void testAuditIDPDeleted() {
+//        String data = "('Class:org.cloudfoundry.identity.uaa.provider.IdentityProvider; ID:')";
+//        String expectedData = AuditEventType.EntityDeletedEvent + ": " + data;
+//        AuditEvent auditEvent = new AuditEvent(
+//                AuditEventType.EntityDeletedEvent, "princialID", "origin", data,
+//                System.currentTimeMillis(), IDENTITY_ZONE_ID,
+//                null, null);
+//        when(auditService.getCorrelationId()).thenReturn(CORRELATION_ID);
+//        try {
+//            auditService.log(auditEvent, IDENTITY_ZONE_ID);
+//            verify(mockAuditClient)
+//                    .audit(argThat(new AuditServiceOutputValidator(
+//                            AuditEnums.CategoryType.AUTHORIZATION,
+//                            AuditEnums.EventType.ACCOUNT_PRIVILEGE_SUCCESS_MODIFICATION,
+//                            AuditEnums.Classifier.SUCCESS, CORRELATION_ID,
+//                            IDENTITY_ZONE_ID, expectedData)));
+//        } catch (AuditException e) {
+//            fail("threw auditException: " + e.getMessage());
+//        }
+//    }
+//
+//    @Test
+//    public void testAuditIdentityZoneDeleted() {
+//        String data = "('Class:org.cloudfoundry.identity.uaa.zone.IdentityZone; ID:')";
+//        String expectedData = AuditEventType.EntityDeletedEvent + ": " + data;
+//        AuditEvent auditEvent = new AuditEvent(
+//                AuditEventType.EntityDeletedEvent, "princialID", "origin", data,
+//                System.currentTimeMillis(), IDENTITY_ZONE_ID,
+//                null, null);
+//        when(auditService.getCorrelationId()).thenReturn(CORRELATION_ID);
+//        try {
+//            auditService.log(auditEvent, IDENTITY_ZONE_ID);
+//            verify(mockAuditClient)
+//                    .audit(argThat(new AuditServiceOutputValidator(
+//                            AuditEnums.CategoryType.ADMINISTRATIONS,
+//                            AuditEnums.EventType.CHANGE_CONFIGURATIONS_SUCCESS,
+//                            AuditEnums.Classifier.SUCCESS, CORRELATION_ID,
+//                            IDENTITY_ZONE_ID, expectedData)));
+//        } catch (AuditException e) {
+//            fail("threw auditException: " + e.getMessage());
+//        }
+//    }
+//
+//    private class AuditServiceOutputValidator
+//            implements
+//                ArgumentMatcher<AuditEventV2> {
+//        private String correlationId;
+//        private String identityZoneId;
+//        private AuditEnums.Classifier status;
+//        private AuditEnums.EventType eventType;
+//        private AuditEnums.CategoryType categoryType;
+//        private String payload;
+//
+//        public AuditServiceOutputValidator(AuditEnums.CategoryType categoryType,
+//                AuditEnums.EventType eventType, AuditEnums.Classifier status,
+//                String correlationId, String identityZoneId, String payload) {
+//            this.correlationId = correlationId;
+//            this.identityZoneId = identityZoneId;
+//            this.status = status;
+//            this.eventType = eventType;
+//            this.categoryType = categoryType;
+//            this.payload = payload;
+//        }
+//
+//        public String toString() {
+//            // printed in verification errors
+//            return "audit service did not match expected";
+//        }
+//
+//        @Override
+//        public boolean matches(AuditEventV2 actualAuditEvent) {
+//            return actualAuditEvent.getCategoryType() == categoryType
+//                    && actualAuditEvent.getEventType() == eventType
+//                    && actualAuditEvent.getClassifier() == status
+//                    && actualAuditEvent.getTenantUuid() == identityZoneId
+//                    && actualAuditEvent.getCorrelationId() == correlationId
+//                    && actualAuditEvent.getPayload().equals(payload);
+//        }
+//    }
+//
+//}
