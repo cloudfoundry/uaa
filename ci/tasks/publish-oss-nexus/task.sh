e8e4a1496a1fc90ea34a1215ff70c1bde42ad3b1
Bruce Ricard <bruce.ricard@gmail.com>
2018-05-04 14:47:43 -0700

    Add ci task to publish artifacts to Nexus
    [#157060251] https://www.pivotaltracker.com/story/show/157060251
    
    Signed-off-by: Dennis Leon <dleon@pivotal.io>

---
+++ ci/tasks/publish-oss-nexus/task.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -xeu
+
+pushd uaa
+    sed -i "s/nexusUsername=/nexusUsername=${NEXUS_USERNAME}" ./gradle.properties
+    sed -i "s/nexusUsername=/nexusPassword=${NEXUS_PASSWORD}" ./gradle.properties
+    ./gradlew publish
+popd
