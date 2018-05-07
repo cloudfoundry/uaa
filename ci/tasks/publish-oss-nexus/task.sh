4779cb00a9542829beac0ddf813f3070fe28e0a6
Dennis Leon <dleon@pivotal.io>
2018-05-07 14:49:26 -0700

    Fix typo in publish-oss-nexus publish task
    [#157060251] https://www.pivotaltracker.com/story/show/157060251

--- ci/tasks/publish-oss-nexus/task.sh
+++ ci/tasks/publish-oss-nexus/task.sh
@@ -3,7 +3,7 @@
 set -xeu
 
 pushd uaa
-    sed -i "s/nexusUsername=/nexusUsername=${NEXUS_USERNAME}/" ./gradle.properties
-    sed -i "s/nexusUsername=/nexusPassword=${NEXUS_PASSWORD}/" ./gradle.properties
+    sed -i "s#nexusUsername=#nexusUsername=${NEXUS_USERNAME}#" ./gradle.properties
+    sed -i "s#nexusPassword=#nexusPassword=${NEXUS_PASSWORD}#" ./gradle.properties
     ./gradlew publish
 popd
