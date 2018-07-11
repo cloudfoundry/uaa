7c650f430b850c381c5addf8f339e04faf0d3187
Bruce Ricard <bricard@pivotal.io>
2018-07-11 16:26:11 -0700

    Updated integration test script with correct MySQL syntax
    Signed-off-by: Jaskanwal Pawar <jpawar@pivotal.io>

--- scripts/start_db_helper.sh
+++ scripts/start_db_helper.sh
@@ -13,7 +13,7 @@ function bootDB {
   elif [ "$db" = "mysql" ]  || [ "$db" = "mysql-5.6" ]; then
     launchDB="(MYSQL_ROOT_PASSWORD=changeme /entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
     testConnection="echo '\s;' | mysql -h 127.0.0.1 -u root --password='changeme' &>/dev/null"
-    initDB="mysql -uroot -pchangeme -e 'drop database if exists uaa;'; mysql -uroot -pchangeme -e 'CREATE DATABASE uaa DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;'; mysql -uroot -pchangeme -e \"SET PASSWORD FOR 'root'@'localhost' = PASSWORD('changeme');\";"
+    initDB="mysql -uroot -pchangeme -e 'drop database if exists uaa;'; mysql -uroot -pchangeme -e 'CREATE DATABASE uaa DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;'; mysql -uroot -pchangeme -e \"SET PASSWORD FOR 'root'@'localhost' = 'changeme';\";"
   elif [ "$db" = "sqlserver" ]; then
     launchDB="(/opt/mssql/bin/sqlservr &> /var/log/sqlserver-boot.log) &"
     testConnection="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'changemeCHANGEME1234!' -d master -Q \"select 'hello'\""
