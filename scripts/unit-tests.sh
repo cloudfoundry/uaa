ed8120b95f6ba182dcc665cbac21dea1c50ca030
Jen Hamon <jhamon@gmail.com>
2018-08-18 19:01:49 -0700

    Rip out jacoco configuration to improve test speed

--- scripts/unit-tests.sh
+++ scripts/unit-tests.sh
@@ -24,5 +24,5 @@ pushd $(dirname $SCRIPT_DIR)
   ./scripts/ldap/configure-manifest.sh
   ldapadd -Y EXTERNAL -H ldapi:/// -f ./uaa/src/main/resources/ldap_db_init.ldif
   ldapadd -x -D 'cn=admin,dc=test,dc=com' -w password -f ./uaa/src/main/resources/ldap_init.ldif
-  ./gradlew "-Dspring.profiles.active=$TESTENV" jacocoRootReportTest --no-daemon --stacktrace --console=plain -x :cloudfoundry-identity-samples:assemble -x javadoc -x javadocJar
+  ./gradlew "-Dspring.profiles.active=$TESTENV" test --no-daemon --stacktrace --console=plain -x :cloudfoundry-identity-samples:assemble -x javadoc -x javadocJar
 popd
