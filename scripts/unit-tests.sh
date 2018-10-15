714891e15ef7d3db382c90e680b0ad9b8c6b0bc4
Jaskanwal Pawar <jpawar@pivotal.io>
2018-10-15 16:36:24 -0700

    Allow test scripts to run without Honeycomb params
    Signed-off-by: Natalie Bennett <nbennett@pivotal.io>
    Co-authored-by: Natalie Bennett <nbennett@pivotal.io>

--- scripts/unit-tests.sh
+++ scripts/unit-tests.sh
@@ -6,8 +6,13 @@ source $SCRIPT_DIR/start_db_helper.sh
 source $SCRIPT_DIR/start_ldap_helper.sh
 
 TESTENV="$1"
-HONEYCOMB_KEY="$2"
-HONEYCOMB_DATASET="$3"
+HONEYCOMB_KEY="${2:-}"
+HONEYCOMB_DATASET="${3:-}"
+
+HONEYCOMB_PARAMS=""
+if [[ -n ${HONEYCOMB_KEY} ]] && [[ -n ${HONEYCOMB_DATASET} ]] ; then
+  HONEYCOMB_PARAMS="-Dhoneycomb.writekey=${HONEYCOMB_KEY} -Dhoneycomb.dataset=${HONEYCOMB_DATASET}"
+fi
 
 cat <<EOF >>/etc/hosts
 127.0.0.1 testzone1.localhost
@@ -26,5 +31,5 @@ pushd $(dirname $SCRIPT_DIR)
   ./scripts/ldap/configure-manifest.sh
   ldapadd -Y EXTERNAL -H ldapi:/// -f ./uaa/src/main/resources/ldap_db_init.ldif
   ldapadd -x -D 'cn=admin,dc=test,dc=com' -w password -f ./uaa/src/main/resources/ldap_init.ldif
-  ./gradlew "-Dspring.profiles.active=${TESTENV}" test "-Dhoneycomb.writekey=${HONEYCOMB_KEY}" "-Dhoneycomb.dataset=${HONEYCOMB_DATASET}" --no-daemon --stacktrace --console=plain -x :cloudfoundry-identity-samples:assemble
+  ./gradlew "-Dspring.profiles.active=${TESTENV}" test "${HONEYCOMB_PARAMS}" --no-daemon --stacktrace --console=plain -x :cloudfoundry-identity-samples:assemble
 popd
