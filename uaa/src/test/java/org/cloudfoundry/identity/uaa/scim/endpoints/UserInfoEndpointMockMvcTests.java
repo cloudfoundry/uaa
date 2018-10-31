d0bddaa8779cf6426382e9711b3b5ee381dba3aa
Dennis Leon <dleon@pivotal.io>
2018-10-31 08:42:20 -0700

    Test Refactor
    Signed-off-by: Joshua Casey <jcasey@pivotal.io>
    Co-authored-by: Joshua Casey <jcasey@pivotal.io>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/scim/endpoints/UserInfoEndpointMockMvcTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/scim/endpoints/UserInfoEndpointMockMvcTests.java
@@ -12,71 +12,96 @@
  *******************************************************************************/
 package org.cloudfoundry.identity.uaa.scim.endpoints;
 
+import org.cloudfoundry.identity.uaa.TestSpringContext;
 import org.cloudfoundry.identity.uaa.account.UserInfoResponse;
-import org.cloudfoundry.identity.uaa.mock.InjectedMockContextTest;
+import org.cloudfoundry.identity.uaa.mock.util.MockMvcUtils;
 import org.cloudfoundry.identity.uaa.scim.ScimUser;
+import org.cloudfoundry.identity.uaa.test.HoneycombAuditEventTestListenerExtension;
+import org.cloudfoundry.identity.uaa.test.HoneycombJdbcInterceptorExtension;
+import org.cloudfoundry.identity.uaa.test.TestClient;
 import org.cloudfoundry.identity.uaa.user.UaaUserDatabase;
 import org.cloudfoundry.identity.uaa.user.UserInfo;
 import org.cloudfoundry.identity.uaa.util.JsonUtils;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.oauth2.common.util.RandomValueStringGenerator;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import static org.cloudfoundry.identity.uaa.mock.util.MockMvcUtils.utils;
 import static org.cloudfoundry.identity.uaa.oauth.token.ClaimConstants.ROLES;
 import static org.cloudfoundry.identity.uaa.oauth.token.ClaimConstants.USER_ATTRIBUTES;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class UserInfoEndpointMockMvcTests extends InjectedMockContextTest {
+@ExtendWith(SpringExtension.class)
+@ExtendWith(HoneycombJdbcInterceptorExtension.class)
+@ExtendWith(HoneycombAuditEventTestListenerExtension.class)
+@ActiveProfiles("default")
+@WebAppConfiguration
+@ContextConfiguration(classes = TestSpringContext.class)
+class UserInfoEndpointMockMvcTests {
 
     private RandomValueStringGenerator generator = new RandomValueStringGenerator();
     private String clientId = generator.generate().toLowerCase();
     private String clientSecret = generator.generate().toLowerCase();
 
-    private String adminToken;
-
     private ScimUser user;
-    private String userName;
 
     private List<String> roles;
     private MultiValueMap<String, String> userAttributes;
 
-    @Before
-    public void setUp() throws Exception {
-        adminToken = testClient.getClientCredentialsOAuthAccessToken(
-            "admin",
-            "adminsecret",
-            "clients.read clients.write clients.secret scim.read scim.write clients.admin"
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+    private TestClient testClient;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        FilterChainProxy springSecurityFilterChain = webApplicationContext.getBean("springSecurityFilterChain", FilterChainProxy.class);
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(springSecurityFilterChain)
+                .build();
+        testClient = new TestClient(mockMvc);
+
+        String adminToken = testClient.getClientCredentialsOAuthAccessToken(
+                "admin",
+                "adminsecret",
+                "clients.read clients.write clients.secret scim.read scim.write clients.admin"
         );
         String authorities = "scim.read,scim.write,password.write,oauth.approvals,scim.create,openid";
-        utils().createClient(
-            this.getMockMvc(),
-            adminToken,
-            clientId,
-            clientSecret,
-            Collections.singleton("oauth"),
-            Arrays.asList("openid", USER_ATTRIBUTES, ROLES),
-            Arrays.asList("client_credentials", "password"),
-            authorities
+        MockMvcUtils.createClient(
+                mockMvc,
+                adminToken,
+                clientId,
+                clientSecret,
+                Collections.singleton("oauth"),
+                Arrays.asList("openid", USER_ATTRIBUTES, ROLES),
+                Arrays.asList("client_credentials", "password"),
+                authorities
         );
-        userName = new RandomValueStringGenerator().generate() + "@test.org";
+        String userName = new RandomValueStringGenerator().generate() + "@test.org";
         user = new ScimUser(null, userName, "PasswordResetUserFirst", "PasswordResetUserLast");
         user.setPrimaryEmail(user.getUserName());
         user.setPassword("secr3T");
-        user = utils().createUser(getMockMvc(), adminToken, user);
-        getWebApplicationContext().getBean(UaaUserDatabase.class).updateLastLogonTime(user.getId());
+        user = MockMvcUtils.createUser(mockMvc, adminToken, user);
+        webApplicationContext.getBean(UaaUserDatabase.class).updateLastLogonTime(user.getId());
 
         userAttributes = new LinkedMultiValueMap<>();
         userAttributes.add("single", "1");
@@ -85,14 +110,14 @@ public class UserInfoEndpointMockMvcTests extends InjectedMockContextTest {
 
         roles = Arrays.asList("role1", "role2", "role3");
         UserInfo userInfo = new UserInfo()
-            .setUserAttributes(userAttributes)
-            .setRoles(roles);
+                .setUserAttributes(userAttributes)
+                .setRoles(roles);
 
-        getWebApplicationContext().getBean(UaaUserDatabase.class).storeUserInfo(user.getId(), userInfo);
+        webApplicationContext.getBean(UaaUserDatabase.class).storeUserInfo(user.getId(), userInfo);
     }
 
     @Test
-    public void testGetUserInfo() throws Exception {
+    void testGetUserInfo() throws Exception {
         UserInfoResponse userInfoResponse = getUserInfo("openid");
 
         assertEquals(user.getUserName(), userInfoResponse.getUserName());
@@ -102,12 +127,12 @@ public class UserInfoEndpointMockMvcTests extends InjectedMockContextTest {
 
         String userId = userInfoResponse.getUserId();
         assertNotNull(userId);
-        Long dbPreviousLogonTime = getWebApplicationContext().getBean(UaaUserDatabase.class).retrieveUserById(userId).getPreviousLogonTime();
+        Long dbPreviousLogonTime = webApplicationContext.getBean(UaaUserDatabase.class).retrieveUserById(userId).getPreviousLogonTime();
         assertEquals(dbPreviousLogonTime, userInfoResponse.getPreviousLogonSuccess());
     }
 
     @Test
-    public void attributesWithRolesAndUserAttributes() throws Exception {
+    void attributesWithRolesAndUserAttributes() throws Exception {
         UserInfoResponse userInfo = getUserInfo("openid user_attributes roles");
         Map<String, List<String>> uas = userInfo.getUserAttributes();
         assertNotNull(uas);
@@ -119,7 +144,7 @@ public class UserInfoEndpointMockMvcTests extends InjectedMockContextTest {
     }
 
     @Test
-    public void attributesWithNoExtraScopes() throws Exception {
+    void attributesWithNoExtraScopes() throws Exception {
         UserInfoResponse userInfo = getUserInfo("openid");
         assertNull(userInfo.getUserAttributes());
         assertNull(userInfo.getRoles());
@@ -127,22 +152,22 @@ public class UserInfoEndpointMockMvcTests extends InjectedMockContextTest {
 
     private UserInfoResponse getUserInfo(String scopes) throws Exception {
         String userInfoToken = testClient.getUserOAuthAccessToken(
-            clientId,
-            clientSecret,
-            user.getUserName(),
-            "secr3T",
-            scopes
+                clientId,
+                clientSecret,
+                user.getUserName(),
+                "secr3T",
+                scopes
         );
 
-        MockHttpServletResponse response = getMockMvc().perform(
-            get("/userinfo")
-                .header("Authorization", "Bearer " + userInfoToken))
-            .andExpect(status().isOk())
-            .andReturn().getResponse();
+        MockHttpServletResponse response = mockMvc.perform(
+                get("/userinfo")
+                        .header("Authorization", "Bearer " + userInfoToken))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
 
         return JsonUtils.readValue(
-            response.getContentAsString(),
-            UserInfoResponse.class
+                response.getContentAsString(),
+                UserInfoResponse.class
         );
     }
 
