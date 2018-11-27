a4eb98393183b1f997683198f0f42599056ce249
Ryan Richard <rrichard@pivotal.io>
2018-11-27 15:57:57 -0800

    Move percona database logic responsible for creating a user after connection has been validated
    Signed-off-by: Dennis Leon <dleon@pivotal.io>
    Co-authored-by: Dennis Leon <dleon@pivotal.io>

--- scripts/start_db_helper.sh
+++ scripts/start_db_helper.sh
@@ -11,9 +11,19 @@ function bootDB {
     testConnection="psql -h localhost -U postgres -c '\conninfo' &>/dev/null"
     initDB="psql -c 'drop database if exists uaa;' -U postgres; psql -c 'create database uaa;' -U postgres; psql -c 'drop user if exists root;' --dbname=uaa -U postgres; psql -c \"create user root with superuser password 'changeme';\" --dbname=uaa -U postgres; psql -c 'show max_connections;' --dbname=uaa -U postgres;"
   elif [ "$db" = "mysql" ]  || [ "$db" = "mysql-5.6" ]; then
-    launchDB="(MYSQL_ROOT_PASSWORD=changeme bash /entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
-    testConnection="echo '\s;' | mysql -h 127.0.0.1 -u root --password='changeme' &>/dev/null"
-    initDB="mysql -uroot -pchangeme -e 'drop database if exists uaa;'; mysql -uroot -pchangeme -e 'CREATE DATABASE uaa DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;'; mysql -uroot -pchangeme -e \"SET PASSWORD FOR 'root'@'localhost' = 'changeme';\";"
+    launchDB="(MYSQL_DATABASE=uaa MYSQL_ROOT_HOST=127.0.0.1 MYSQL_ROOT_PASSWORD='changeme' bash /entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
+    testConnection="echo '\s;' | mysql &>/dev/null"
+    initDB=""
+  elif [ "$db" = "percona" ]; then
+    launchDB="bash /entrypoint.sh &> /var/log/mysql-boot.log"
+    testConnection="echo '\s;' | mysql &>/dev/null"
+    initDB="mysql -e \"CREATE USER 'root'@'127.0.0.1' IDENTIFIED BY 'changeme' ;\";
+         mysql -e \"GRANT ALL ON *.* TO 'root'@'127.0.0.1' WITH GRANT OPTION ;\";
+         mysql -e 'FLUSH PRIVILEGES ;';
+         mysql -uroot -pchangeme -e 'drop database if exists uaa;';
+         mysql -uroot -pchangeme -e 'CREATE DATABASE uaa DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;';
+         mysql -uroot -pchangeme -e \"SET PASSWORD FOR 'root'@'localhost' = 'changeme';\";
+    "
   elif [ "$db" = "sqlserver" ]; then
     launchDB="(/opt/mssql/bin/sqlservr &> /var/log/sqlserver-boot.log) &"
     testConnection="/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'changemeCHANGEME1234!' -d master -Q \"select 'hello'\""
