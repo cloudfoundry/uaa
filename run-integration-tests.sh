1dcb7eac278174ba7473a2c0678a406c8f16bde8
Ryan Richard <rrichard@pivotal.io>
2018-11-27 16:04:31 -0800

    Add an ENV db variable to specify which database provisioning strategy to use when running integration tests on a dev workstation
    Signed-off-by: Dennis Leon <dleon@pivotal.io>
    Co-authored-by: Dennis Leon <dleon@pivotal.io>

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -3,8 +3,16 @@
 set -xeu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-DB_IMAGE_NAME="$1"
+DB_IMAGE_NAME="${1}"
+PROFILE_NAME="${2}"
+DB="${3}"
+
 CONTAINER_SCRIPT_DIR='/root/uaa'
 GRADLE_LOCK_DIR='/root/uaa/.gradle/'
 
-docker run --privileged -t -i --shm-size=1G  -v "${SCRIPT_DIR}":"${CONTAINER_SCRIPT_DIR}" -v "${GRADLE_LOCK_DIR}" "cfidentity/uaa-${DB_IMAGE_NAME}" /root/uaa/scripts/integration-tests.sh "${DB_IMAGE_NAME}",default "${CONTAINER_SCRIPT_DIR}"
+docker run --privileged -t -i --shm-size=1G \
+  -v "${SCRIPT_DIR}":"${CONTAINER_SCRIPT_DIR}" \
+  -v "${GRADLE_LOCK_DIR}" \
+  --env DB=${DB} \
+  "cfidentity/uaa-${DB_IMAGE_NAME}" \
+  /root/uaa/scripts/integration-tests.sh "${PROFILE_NAME}",default "${CONTAINER_SCRIPT_DIR}"
