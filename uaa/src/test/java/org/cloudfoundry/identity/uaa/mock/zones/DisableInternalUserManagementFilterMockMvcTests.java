3ac966454ef930b57aa3a1b081f64c8a9fac36e9
Joshua Casey <jcasey@pivotal.io>
2019-01-11 07:31:36 -0600

    Test Refactor (DisableInternalUserManagementFilterMockMvcTests)
    - Don't use InjectedMockContextTest
    - Use Junit5
    - Apply IntelliJ sanitizations
    
    [Finishes #163146640] https://www.pivotaltracker.com/story/show/163146640

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/zones/DisableInternalUserManagementFilterMockMvcTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/zones/DisableInternalUserManagementFilterMockMvcTests.java
@@ -12,37 +12,69 @@
  *******************************************************************************/
 package org.cloudfoundry.identity.uaa.mock.zones;
 
-import org.cloudfoundry.identity.uaa.mock.InjectedMockContextTest;
+import org.cloudfoundry.identity.uaa.TestSpringContext;
 import org.cloudfoundry.identity.uaa.mock.util.MockMvcUtils;
-import org.junit.After;
-import org.junit.Test;
+import org.cloudfoundry.identity.uaa.test.HoneycombAuditEventTestListenerExtension;
+import org.cloudfoundry.identity.uaa.test.HoneycombJdbcInterceptorExtension;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
 
-public class DisableInternalUserManagementFilterMockMvcTests extends InjectedMockContextTest{
+@ExtendWith(SpringExtension.class)
+@ExtendWith(HoneycombJdbcInterceptorExtension.class)
+@ExtendWith(HoneycombAuditEventTestListenerExtension.class)
+@ActiveProfiles("default")
+@WebAppConfiguration
+@ContextConfiguration(classes = TestSpringContext.class)
+class DisableInternalUserManagementFilterMockMvcTests {
 
-    @After
-    public void resetInternalUserManagement() throws Exception {
-        MockMvcUtils.setDisableInternalUserManagement(false, getWebApplicationContext());
+    @Autowired
+    WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Value("${disableInternalUserManagement:false}")
+    private boolean disableInternalUserManagement;
+
+    @BeforeEach
+    void setUp(@Autowired FilterChainProxy springSecurityFilterChain) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilter(springSecurityFilterChain)
+                .build();
+
+        MockMvcUtils.setDisableInternalUserManagement(true, webApplicationContext);
+    }
+
+    @AfterEach
+    void resetInternalUserManagement() {
+        MockMvcUtils.setDisableInternalUserManagement(disableInternalUserManagement, webApplicationContext);
     }
 
     @Test
-    public void createAccountNotEnabled() throws Exception {
-        MockMvcUtils.setDisableInternalUserManagement(true, getWebApplicationContext());
-
-        getMockMvc().perform(get("/login"))
-            .andExpect(status().isOk())
-            .andExpect(xpath("//a[@href='/create_account']").doesNotExist());
+    void createAccountNotEnabled() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(xpath("//a[@href='/create_account']").doesNotExist());
     }
 
     @Test
-    public void resetPasswordNotEnabled() throws Exception {
-        MockMvcUtils.setDisableInternalUserManagement(true, getWebApplicationContext());
-
-        getMockMvc().perform(get("/login"))
-            .andExpect(status().isOk())
-            .andExpect(xpath("//a[@href='/forgot_password']").doesNotExist());
+    void resetPasswordNotEnabled() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(xpath("//a[@href='/forgot_password']").doesNotExist());
     }
 }
