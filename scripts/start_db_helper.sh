73b4c981b182734f17315caad606ee5feae68662
Dennis Leon <dleon@pivotal.io>
2019-02-01 11:41:15 -0800

    Make running postgres unit tests less flakey
    - Check docker startup during postgresql connection check.
    
    [#163663014] https://www.pivotaltracker.com/story/show/163663014
    
    Signed-off-by: Andrew Wittrock <awittrock@pivotal.io>
    Co-authored-by: Andrew Wittrock <awittrock@pivotal.io>

--- scripts/start_db_helper.sh
+++ scripts/start_db_helper.sh
@@ -8,7 +8,7 @@ function bootDB {
 
   if [ "$db" = "postgresql" ]; then
     launchDB="(/docker-entrypoint.sh postgres &> /var/log/postgres-boot.log) &"
-    testConnection="psql -h localhost -U postgres -c '\conninfo' &>/dev/null"
+    testConnection="(! ps aux | grep docker-entrypoint | grep -v 'grep') && psql -h localhost -U postgres -c '\conninfo' &>/dev/null"
     initDB="psql -c 'drop database if exists uaa;' -U postgres; psql -c 'create database uaa;' -U postgres; psql -c 'drop user if exists root;' --dbname=uaa -U postgres; psql -c \"create user root with superuser password 'changeme';\" --dbname=uaa -U postgres; psql -c 'show max_connections;' --dbname=uaa -U postgres;"
   elif [ "$db" = "mysql" ]  || [ "$db" = "mysql-5.6" ]; then
     launchDB="(MYSQL_DATABASE=uaa MYSQL_ROOT_HOST=127.0.0.1 MYSQL_ROOT_PASSWORD='changeme' bash /entrypoint.sh mysqld &> /var/log/mysql-boot.log) &"
