be51d1c4bbc2a5d3821bd10efaf423ff91dd6470
Florian Tack <florian.tack@sap.com>
2019-02-07 10:07:11 +0000

    Add Integration Tests for login_hint direct call
    [#162810381] https://www.pivotaltracker.com/story/show/162810381
    
    Signed-off-by: Torsten Luh <torsten.luh@sap.com>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/LoginIT.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/LoginIT.java
@@ -54,6 +54,7 @@ import java.util.List;
 import static org.cloudfoundry.identity.uaa.integration.util.IntegrationTestUtils.doesSupportZoneDNS;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -226,12 +227,14 @@ public class LoginIT {
         String ldapLoginHint = URLEncoder.encode("{\"origin\":\"ldap\"}", "UTF-8");
         webDriver.get(baseUrl + "/login?login_hint=" + ldapLoginHint);
         assertEquals("Cloud Foundry", webDriver.getTitle());
+        assertThat(webDriver.getPageSource(), not(containsString("or sign in with:")));
         attemptLogin(newUserEmail, USER_PASSWORD);
         assertThat(webDriver.findElement(By.className("alert-error")).getText(), containsString("Unable to verify email or password. Please try again."));
 
         String uaaLoginHint = URLEncoder.encode("{\"origin\":\"uaa\"}", "UTF-8");
         webDriver.get(baseUrl + "/login?login_hint=" + uaaLoginHint);
         assertEquals("Cloud Foundry", webDriver.getTitle());
+        assertThat(webDriver.getPageSource(), not(containsString("or sign in with:")));
         attemptLogin(newUserEmail, USER_PASSWORD);
         assertThat(webDriver.findElement(By.cssSelector("h1")).getText(), Matchers.containsString("Where to?"));
         webDriver.get(baseUrl + "/logout.do");
