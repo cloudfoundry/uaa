4bd098146cbe11a9911ed6acfb5417827f9ffdb8
Joshua Casey <jcasey@pivotal.io>
2019-02-20 22:56:46 -0600

    Refactor (LocalUaaRestTemplate)
    - Apply IntelliJ sanitizations
    - Inline internal getters
    
    [#163746990] https://www.pivotaltracker.com/story/show/163746990

--- server/src/main/java/org/cloudfoundry/identity/uaa/message/LocalUaaRestTemplate.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/message/LocalUaaRestTemplate.java
@@ -1,15 +1,3 @@
-/*******************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2015] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.message;
 
 import org.apache.http.conn.ssl.SSLContextBuilder;
@@ -30,7 +18,6 @@ import org.springframework.security.oauth2.client.resource.UserRedirectRequiredE
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.util.OAuth2Utils;
 import org.springframework.security.oauth2.provider.ClientDetails;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
@@ -39,8 +26,6 @@ import javax.net.ssl.SSLContext;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -52,77 +37,57 @@ public class LocalUaaRestTemplate extends OAuth2RestTemplate implements Initiali
     protected AuthorizationServerTokenServices tokenServices;
     protected String clientId;
     protected ClientServicesExtension clientDetailsService;
-    protected boolean verifySsl = true;
+    private boolean verifySsl = true;
 
-    public LocalUaaRestTemplate(OAuth2ProtectedResourceDetails resource) {
+    LocalUaaRestTemplate(OAuth2ProtectedResourceDetails resource) {
         super(resource);
-    }
-
-    public LocalUaaRestTemplate(OAuth2ProtectedResourceDetails resource, OAuth2ClientContext context) {
-        super(resource, context);
     }
 
     @Override
     public OAuth2AccessToken acquireAccessToken(OAuth2ClientContext oauth2Context) throws UserRedirectRequiredException {
-        ClientDetails client = clientDetailsService.loadClientByClientId(getClientId(), IdentityZoneHolder.get().getId());
+        ClientDetails client = clientDetailsService.loadClientByClientId(clientId, IdentityZoneHolder.get().getId());
         Set<String> scopes = new HashSet<>();
         for (GrantedAuthority authority : client.getAuthorities()) {
             scopes.add(authority.getAuthority());
         }
         Set<String> resourceIds = new HashSet<>();
         resourceIds.add(OriginKeys.UAA);
-        Map<String,String> requestParameters = new HashMap<>();
+        Map<String, String> requestParameters = new HashMap<>();
         requestParameters.put(OAuth2Utils.CLIENT_ID, "login");
         requestParameters.put(OAuth2Utils.GRANT_TYPE, GRANT_TYPE_CLIENT_CREDENTIALS);
         OAuth2Request request = new OAuth2Request(
-            requestParameters,
-            "login",
-            (Collection<? extends GrantedAuthority>)Collections.EMPTY_SET,
-            true,
-            scopes,
-            resourceIds,
-            null,
-            new HashSet<>(),
-            Collections.EMPTY_MAP);
+                requestParameters,
+                "login",
+                new HashSet<>(),
+                true,
+                scopes,
+                resourceIds,
+                null,
+                new HashSet<>(),
+                new HashMap<>());
         OAuth2Authentication authentication = new OAuth2Authentication(request, null);
         OAuth2AccessToken result = tokenServices.createAccessToken(authentication);
         oauth2Context.setAccessToken(result);
         return result;
     }
 
-    public AuthorizationServerTokenServices getTokenServices() {
-        return tokenServices;
-    }
-
     public void setTokenServices(AuthorizationServerTokenServices tokenServices) {
         this.tokenServices = tokenServices;
-    }
-
-    public String getClientId() {
-        return clientId;
     }
 
     public void setClientId(String clientId) {
         this.clientId = clientId;
     }
 
-    public ClientDetailsService getClientDetailsService() {
-        return clientDetailsService;
-    }
-
     public void setClientDetailsService(ClientServicesExtension clientDetailsService) {
         this.clientDetailsService = clientDetailsService;
-    }
-
-    public boolean isVerifySsl() {
-        return verifySsl;
     }
 
     public void setVerifySsl(boolean verifySsl) {
         this.verifySsl = verifySsl;
     }
 
-    protected void skipSslValidation() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+    private void skipSslValidation() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
         SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustSelfSignedStrategy()).build();
         CloseableHttpClient httpClient = HttpClients.custom().setSslcontext(sslContext).build();
         ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
@@ -131,16 +96,16 @@ public class LocalUaaRestTemplate extends OAuth2RestTemplate implements Initiali
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        if (tokenServices==null) {
+        if (tokenServices == null) {
             throw new NullPointerException("tokenServices property is null!");
         }
-        if (clientId==null) {
+        if (clientId == null) {
             throw new NullPointerException("clientId property is null!");
         }
-        if (clientDetailsService==null) {
+        if (clientDetailsService == null) {
             throw new NullPointerException("clientDetailsService property is null!");
         }
-        if (!isVerifySsl()) {
+        if (!verifySsl) {
             skipSslValidation();
         }
     }
