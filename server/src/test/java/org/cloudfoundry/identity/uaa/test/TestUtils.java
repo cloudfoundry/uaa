a47c3135e8220daa811cee6519c9c9ae340c5990
Ryan Richard <rrichard@pivotal.io>
2019-02-26 12:26:23 -0800

    Speed up the TestUtils method of cleaning the db
    - Instead of using flyway to clean and remigrate, just use sql to delete
    rows and reseed the UAA default zone
    
    Signed-off-by: Joshua Casey <jcasey@pivotal.io>
    Co-authored-by: Joshua Casey <jcasey@pivotal.io>
    Co-authored-by: Andrew Wittrock <awittrock@pivotal.io>
    
    [#164124761] https://www.pivotaltracker.com/story/show/164124761

--- server/src/test/java/org/cloudfoundry/identity/uaa/test/TestUtils.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/test/TestUtils.java
@@ -1,7 +1,7 @@
 package org.cloudfoundry.identity.uaa.test;
 
-import org.apache.commons.io.IOUtils;
 import org.cloudfoundry.identity.uaa.client.ClientAdminBootstrap;
+import org.cloudfoundry.identity.uaa.constants.OriginKeys;
 import org.cloudfoundry.identity.uaa.impl.config.IdentityProviderBootstrap;
 import org.cloudfoundry.identity.uaa.impl.config.IdentityZoneConfigurationBootstrap;
 import org.cloudfoundry.identity.uaa.mfa.MfaProviderBootstrap;
@@ -12,14 +12,13 @@ import org.cloudfoundry.identity.uaa.scim.bootstrap.ScimUserBootstrap;
 import org.cloudfoundry.identity.uaa.zone.IdentityZone;
 import org.cloudfoundry.identity.uaa.zone.IdentityZoneHolder;
 import org.cloudfoundry.identity.uaa.zone.JdbcIdentityZoneProvisioning;
-import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
+import java.sql.Timestamp;
+import java.util.*;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -48,18 +47,52 @@ public class TestUtils {
             return;
         }
 
-        Flyway flyway;
-
+        JdbcTemplate jdbcTemplate;
         try {
-            flyway = applicationContext.getBean(Flyway.class);
+            jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
         } catch (NoSuchBeanDefinitionException ignored) {
             return;
         }
 
-        flyway.clean();
-        flyway.migrate();
+        jdbcTemplate.update("DELETE FROM authz_approvals");
+        jdbcTemplate.update("DELETE FROM expiring_code_store");
+        jdbcTemplate.update("DELETE FROM external_group_mapping");
+        jdbcTemplate.update("DELETE FROM group_membership");
+        jdbcTemplate.update("DELETE FROM groups");
+        jdbcTemplate.update("DELETE FROM identity_provider");
+        jdbcTemplate.update("DELETE FROM identity_zone");
+        jdbcTemplate.update("DELETE FROM oauth_client_details");
+        jdbcTemplate.update("DELETE FROM oauth_code");
+        jdbcTemplate.update("DELETE FROM revocable_tokens");
+        jdbcTemplate.update("DELETE FROM sec_audit");
+        jdbcTemplate.update("DELETE FROM service_provider");
+        jdbcTemplate.update("DELETE FROM user_info");
+        jdbcTemplate.update("DELETE FROM users");
+        jdbcTemplate.update("DELETE FROM mfa_providers");
+
+        seedUaaZoneSimilarToHowTheRealFlywayMigrationDoesIt(jdbcTemplate);
 
         bootstrapDb(applicationContext);
+    }
+
+    private static void seedUaaZoneSimilarToHowTheRealFlywayMigrationDoesIt(JdbcTemplate jdbcTemplate) {
+        IdentityZone uaa = IdentityZone.getUaa();
+        Timestamp t = new Timestamp(uaa.getCreated().getTime());
+        jdbcTemplate.update("insert into identity_zone VALUES (?,?,?,?,?,?,?,?,?)", uaa.getId(),t,t,uaa.getVersion(),uaa.getSubdomain(),uaa.getName(),uaa.getDescription(),null,true);
+        Map<String,String> originMap = new HashMap<>();
+        Set<String> origins = new LinkedHashSet<>();
+        origins.addAll(Arrays.asList(new String[] {OriginKeys.UAA, OriginKeys.LOGIN_SERVER, OriginKeys.LDAP, OriginKeys.KEYSTONE}));
+        origins.addAll(jdbcTemplate.queryForList("SELECT DISTINCT origin from users", String.class));
+        for (String origin : origins) {
+            String identityProviderId = UUID.randomUUID().toString();
+            originMap.put(origin, identityProviderId);
+            jdbcTemplate.update("insert into identity_provider VALUES (?,?,?,0,?,?,?,?,null,?)",identityProviderId, t, t, uaa.getId(),origin,origin,origin,true);
+        }
+        jdbcTemplate.update("update oauth_client_details set identity_zone_id = ?",uaa.getId());
+        List<String> clientIds = jdbcTemplate.queryForList("SELECT client_id from oauth_client_details", String.class);
+        for (String clientId : clientIds) {
+            jdbcTemplate.update("insert into client_idp values (?,?) ",clientId,originMap.get(OriginKeys.UAA));
+        }
     }
 
     private static void bootstrapDb(ApplicationContext applicationContext) {
