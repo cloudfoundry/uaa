916c72d6bcfef1faf874016f398899ff45e97e1c
Ryan Richard <rrichard@pivotal.io>
2019-03-07 17:05:37 -0800

    Allow run-unit-tests.sh to run percona and hsqldb correctly
    Previously, it was not setting the spring profile to valid values,
    causing tests to fail which depend on the profile value.
    
    Signed-off-by: Andrew Edstrom <aedstrom@pivotal.io>
    Co-authored-by: Andrew Edstrom <aedstrom@pivotal.io>

--- run-unit-tests.sh
+++ run-unit-tests.sh
@@ -3,8 +3,28 @@
 set -xeu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-DB_IMAGE_NAME="$1"
 CONTAINER_SCRIPT_DIR='/root/uaa'
 GRADLE_LOCK_DIR='/root/uaa/.gradle/'
 
-docker run --privileged -t -i --shm-size=1G  -v "${SCRIPT_DIR}":"${CONTAINER_SCRIPT_DIR}" -v "${GRADLE_LOCK_DIR}" "cfidentity/uaa-${DB_IMAGE_NAME}" /root/uaa/scripts/unit-tests.sh "${DB_IMAGE_NAME}",default "${CONTAINER_SCRIPT_DIR}"
+case "$1" in
+    hsqldb)
+        DB_IMAGE_NAME=postgresql
+        PROFILE_NAME=hsqldb
+        ;;
+
+    percona)
+        DB_IMAGE_NAME=percona
+        PROFILE_NAME=mysql
+        ;;
+
+    postgresql|sqlserver|mysql)
+        DB_IMAGE_NAME=$1
+        PROFILE_NAME=$1
+        ;;
+
+    *)
+        echo $"$1 is not a known database type. Supported types are: hsqldb, percona, postgresql, sqlserver, mysql"
+        exit 1
+esac
+
+docker run --privileged -t -i --shm-size=1G  -v "${SCRIPT_DIR}":"${CONTAINER_SCRIPT_DIR}" -v "${GRADLE_LOCK_DIR}" "cfidentity/uaa-${DB_IMAGE_NAME}" /root/uaa/scripts/unit-tests.sh "${PROFILE_NAME}",default "${CONTAINER_SCRIPT_DIR}"
