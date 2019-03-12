e21a638b06c56409ca33f05180db1ef1fbc19fb9
Natalie Bennett <nbennett@pivotal.io>
2019-03-12 11:49:00 -0700

    Add usage instructions to integration test script
    Signed-off-by: Joshua Casey <jcasey@pivotal.io>
    Co-authored-by: Joshua Casey <jcasey@pivotal.io>
    
    [#164582226] https://www.pivotaltracker.com/story/show/164582226

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+print_usage_and_exit () {
+    echo "Requires 3 arguments: DB_IMAGE_NAME, PROFILE_NAME, DB"
+    echo "Example: ./run-integration-tests mysql mysql mysql"
+    exit 1
+}
+
+if [[ -z "$1" ]]
+  then
+    print_usage_and_exit
+fi
+
+if [[ -z "$2" ]]
+  then
+    print_usage_and_exit
+fi
+
+if [[ -z "$3" ]]
+  then
+    print_usage_and_exit
+fi
+
+
 set -xeu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
