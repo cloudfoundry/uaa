eb32793a3893e97a5b3aa0831e5927bc79663eec
Joshua Casey <jcasey@pivotal.io>
2019-03-20 13:14:11 -0500

    Test Refactor (EnvironmentMapFactoryBeanTests)
    - Use BeforeEach
    - Inline helper method and simplify
    
    [#164807949]

--- server/src/test/java/org/cloudfoundry/identity/uaa/config/EnvironmentMapFactoryBeanTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/config/EnvironmentMapFactoryBeanTests.java
@@ -2,67 +2,75 @@ package org.cloudfoundry.identity.uaa.config;
 
 import org.cloudfoundry.identity.uaa.impl.config.EnvironmentMapFactoryBean;
 import org.cloudfoundry.identity.uaa.impl.config.NestedMapPropertySource;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.env.StandardEnvironment;
-import org.springframework.util.StringUtils;
 
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EnvironmentMapFactoryBeanTests {
 
+    private EnvironmentMapFactoryBean factory;
+
+    @BeforeEach
+    void setUp() {
+        factory = new EnvironmentMapFactoryBean();
+    }
+
     @Test
-    void testDefaultProperties() {
-        EnvironmentMapFactoryBean factory = new EnvironmentMapFactoryBean();
-        factory.setDefaultProperties(getProperties("foo=foo"));
+    void defaultProperties() {
+        Map<String, String> inputProperties = new HashMap<>();
+        inputProperties.put("foo", "foo");
+
+        factory.setDefaultProperties(inputProperties);
         Map<String, ?> properties = factory.getObject();
         assertEquals("foo", properties.get("foo"));
     }
 
     @Test
-    void testRawPlaceholderProperties() {
-        EnvironmentMapFactoryBean factory = new EnvironmentMapFactoryBean();
-        factory.setDefaultProperties(getProperties("foo=${bar}"));
+    void rawPlaceholderProperties() {
+        Map<String, String> inputProperties = new HashMap<>();
+        inputProperties.put("foo", "${bar}");
+
+        factory.setDefaultProperties(inputProperties);
         Map<String, ?> properties = factory.getObject();
         assertEquals("${bar}", properties.get("foo"));
     }
 
     @Test
-    void testPlaceholderProperties() {
-        EnvironmentMapFactoryBean factory = new EnvironmentMapFactoryBean();
+    void placeholderProperties() {
+        Map<String, String> inputProperties = new HashMap<>();
+        inputProperties.put("foo", "baz");
+
+        Map<String, String> overrideProperties = new HashMap<>();
+        overrideProperties.put("bar", "${spam}");
+
         StandardEnvironment environment = new StandardEnvironment();
-        environment.getPropertySources().addLast(new NestedMapPropertySource("override", getProperties("bar=${spam}")));
+        environment.getPropertySources().addLast(new NestedMapPropertySource("override", overrideProperties));
         factory.setEnvironment(environment);
-        factory.setDefaultProperties(getProperties("foo=baz"));
+        factory.setDefaultProperties(inputProperties);
         Map<String, ?> properties = factory.getObject();
         assertEquals("baz", properties.get("foo"));
         assertEquals("${spam}", properties.get("bar"));
     }
 
     @Test
-    void testOverrideProperties() {
-        EnvironmentMapFactoryBean factory = new EnvironmentMapFactoryBean();
-        factory.setDefaultProperties(getProperties("foo=foo"));
+    void overrideProperties() {
+        Map<String, String> inputProperties = new HashMap<>();
+        inputProperties.put("foo", "foo");
+
+        Map<String, String> overrideProperties = new HashMap<>();
+        overrideProperties.put("foo", "bar");
+
+        factory.setDefaultProperties(inputProperties);
         StandardEnvironment environment = new StandardEnvironment();
-        environment.getPropertySources().addLast(new NestedMapPropertySource("override", getProperties("foo=bar")));
+        environment.getPropertySources().addLast(new NestedMapPropertySource("override", overrideProperties));
         factory.setEnvironment(environment);
         Map<String, ?> properties = factory.getObject();
         assertEquals("bar", properties.get("foo"));
-    }
-
-    private static Map<String, ?> getProperties(String input) {
-        HashMap<String, Object> result = new HashMap<>();
-        Properties properties = StringUtils.splitArrayElementsIntoProperties(
-                StringUtils.commaDelimitedListToStringArray(input), "=");
-        for (Enumeration<?> keys = properties.propertyNames(); keys.hasMoreElements(); ) {
-            String key = (String) keys.nextElement();
-            result.put(key, properties.getProperty(key));
-        }
-        return result;
     }
 
 }
