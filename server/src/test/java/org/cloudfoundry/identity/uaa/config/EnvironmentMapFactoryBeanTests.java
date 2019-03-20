4bb1a10497a4f4a65e395560138d2afc7fa23547
Joshua Casey <jcasey@pivotal.io>
2019-03-20 10:16:22 -0500

    Test Refactor (EnvironmentMapFactoryBeanTests)
    - Apply IntelliJ sanitizations
    
    [#164807949]

--- server/src/test/java/org/cloudfoundry/identity/uaa/config/EnvironmentMapFactoryBeanTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/config/EnvironmentMapFactoryBeanTests.java
@@ -1,23 +1,4 @@
-/*******************************************************************************
- *     Cloud Foundry 
- *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.config;
-
-import static org.junit.Assert.assertEquals;
-
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
 
 import org.cloudfoundry.identity.uaa.impl.config.EnvironmentMapFactoryBean;
 import org.cloudfoundry.identity.uaa.impl.config.NestedMapPropertySource;
@@ -25,14 +6,17 @@ import org.junit.Test;
 import org.springframework.core.env.StandardEnvironment;
 import org.springframework.util.StringUtils;
 
-/**
- * @author Dave Syer
- * 
- */
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.junit.Assert.assertEquals;
+
 public class EnvironmentMapFactoryBeanTests {
 
     @Test
-    public void testDefaultProperties() throws Exception {
+    public void testDefaultProperties() {
         EnvironmentMapFactoryBean factory = new EnvironmentMapFactoryBean();
         factory.setDefaultProperties(getProperties("foo=foo"));
         Map<String, ?> properties = factory.getObject();
@@ -40,7 +24,7 @@ public class EnvironmentMapFactoryBeanTests {
     }
 
     @Test
-    public void testRawPlaceholderProperties() throws Exception {
+    public void testRawPlaceholderProperties() {
         EnvironmentMapFactoryBean factory = new EnvironmentMapFactoryBean();
         factory.setDefaultProperties(getProperties("foo=${bar}"));
         Map<String, ?> properties = factory.getObject();
@@ -48,7 +32,7 @@ public class EnvironmentMapFactoryBeanTests {
     }
 
     @Test
-    public void testPlaceholderProperties() throws Exception {
+    public void testPlaceholderProperties() {
         EnvironmentMapFactoryBean factory = new EnvironmentMapFactoryBean();
         StandardEnvironment environment = new StandardEnvironment();
         environment.getPropertySources().addLast(new NestedMapPropertySource("override", getProperties("bar=${spam}")));
@@ -60,7 +44,7 @@ public class EnvironmentMapFactoryBeanTests {
     }
 
     @Test
-    public void testOverrideProperties() throws Exception {
+    public void testOverrideProperties() {
         EnvironmentMapFactoryBean factory = new EnvironmentMapFactoryBean();
         factory.setDefaultProperties(getProperties("foo=foo"));
         StandardEnvironment environment = new StandardEnvironment();
@@ -70,11 +54,11 @@ public class EnvironmentMapFactoryBeanTests {
         assertEquals("bar", properties.get("foo"));
     }
 
-    private Map<String, ?> getProperties(String input) {
-        HashMap<String, Object> result = new HashMap<String, Object>();
+    private static Map<String, ?> getProperties(String input) {
+        HashMap<String, Object> result = new HashMap<>();
         Properties properties = StringUtils.splitArrayElementsIntoProperties(
-                        StringUtils.commaDelimitedListToStringArray(input), "=");
-        for (Enumeration<?> keys = properties.propertyNames(); keys.hasMoreElements();) {
+                StringUtils.commaDelimitedListToStringArray(input), "=");
+        for (Enumeration<?> keys = properties.propertyNames(); keys.hasMoreElements(); ) {
             String key = (String) keys.nextElement();
             result.put(key, properties.getProperty(key));
         }
