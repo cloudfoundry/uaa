ebd935b540e0b30f2056ba19710d4906eef33215
Joshua Casey <jcasey@pivotal.io>
2019-03-21 16:18:36 -0500

    Refactor (JdbcRevocableTokenProvisioning)
    - Apply IntelliJ sanizations
    - Apply IntelliJ reformat
    
    [#164808269]

--- server/src/main/java/org/cloudfoundry/identity/uaa/oauth/token/JdbcRevocableTokenProvisioning.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/oauth/token/JdbcRevocableTokenProvisioning.java
@@ -1,15 +1,3 @@
-/*******************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.oauth.token;
 
 import org.apache.commons.logging.Log;
@@ -34,36 +22,33 @@ import static org.springframework.util.StringUtils.isEmpty;
 public class JdbcRevocableTokenProvisioning implements RevocableTokenProvisioning, SystemDeletable {
 
     private final static String REFRESH_TOKEN_RESPONSE_TYPE = REFRESH_TOKEN.toString();
-    protected final static String FIELDS = "token_id,client_id,user_id,format,response_type,issued_at,expires_at,scope,data,identity_zone_id";
-    protected static final String UPDATE_FIELDS = FIELDS.substring(FIELDS.indexOf(',')+1, FIELDS.lastIndexOf(',')).replace(",","=?,") + "=?";
-    protected final static String TABLE = "revocable_tokens";
-    protected final static String GET_QUERY = "SELECT " + FIELDS +" FROM "+TABLE + " WHERE token_id=? AND identity_zone_id=?";
-    protected final static String GET_BY_USER_QUERY = "SELECT " + FIELDS +" FROM "+TABLE + " WHERE user_id=? AND identity_zone_id=?";
-    protected final static String GET_BY_CLIENT_QUERY = "SELECT " + FIELDS +" FROM "+TABLE + " WHERE client_id=? AND identity_zone_id=?";
-    protected final static String UPDATE_QUERY = "UPDATE "+TABLE+" SET "+UPDATE_FIELDS+" WHERE token_id=? and identity_zone_id=?";
-    protected final static String INSERT_QUERY = "INSERT INTO " + TABLE + " ("+FIELDS+") VALUES (?,?,?,?,?,?,?,?,?,?)";
-    protected final static String DELETE_QUERY = "DELETE FROM " + TABLE + " WHERE token_id=? and identity_zone_id=?";
-    protected final static String DELETE_EXPIRED_QUERY = "DELETE FROM " + TABLE + " WHERE expires_at < ?";
-    protected final static String DELETE_REFRESH_TOKEN_QUERY = "DELETE FROM " + TABLE + " WHERE user_id=? AND client_id=? AND response_type='" +REFRESH_TOKEN_RESPONSE_TYPE+ "' AND identity_zone_id=?";
-    protected final static String DELETE_BY_CLIENT_QUERY = "DELETE FROM " + TABLE + " WHERE client_id = ? AND identity_zone_id=?";
-    protected final static String DELETE_BY_USER_QUERY = "DELETE FROM " + TABLE + " WHERE user_id = ? AND identity_zone_id=?";
-    protected final static String DELETE_BY_ZONE_QUERY = "DELETE FROM " + TABLE + " WHERE identity_zone_id=?";
+    private final static String FIELDS = "token_id,client_id,user_id,format,response_type,issued_at,expires_at,scope,data,identity_zone_id";
+    private static final String UPDATE_FIELDS = FIELDS.substring(FIELDS.indexOf(',') + 1, FIELDS.lastIndexOf(',')).replace(",", "=?,") + "=?";
+    private final static String TABLE = "revocable_tokens";
+    private final static String GET_QUERY = "SELECT " + FIELDS + " FROM " + TABLE + " WHERE token_id=? AND identity_zone_id=?";
+    private final static String GET_BY_USER_QUERY = "SELECT " + FIELDS + " FROM " + TABLE + " WHERE user_id=? AND identity_zone_id=?";
+    private final static String GET_BY_CLIENT_QUERY = "SELECT " + FIELDS + " FROM " + TABLE + " WHERE client_id=? AND identity_zone_id=?";
+    private final static String UPDATE_QUERY = "UPDATE " + TABLE + " SET " + UPDATE_FIELDS + " WHERE token_id=? and identity_zone_id=?";
+    private final static String INSERT_QUERY = "INSERT INTO " + TABLE + " (" + FIELDS + ") VALUES (?,?,?,?,?,?,?,?,?,?)";
+    private final static String DELETE_QUERY = "DELETE FROM " + TABLE + " WHERE token_id=? and identity_zone_id=?";
+    private final static String DELETE_REFRESH_TOKEN_QUERY = "DELETE FROM " + TABLE + " WHERE user_id=? AND client_id=? AND response_type='" + REFRESH_TOKEN_RESPONSE_TYPE + "' AND identity_zone_id=?";
+    private final static String DELETE_BY_CLIENT_QUERY = "DELETE FROM " + TABLE + " WHERE client_id = ? AND identity_zone_id=?";
+    private final static String DELETE_BY_USER_QUERY = "DELETE FROM " + TABLE + " WHERE user_id = ? AND identity_zone_id=?";
+    private final static String DELETE_BY_ZONE_QUERY = "DELETE FROM " + TABLE + " WHERE identity_zone_id=?";
 
-
-    protected static final Log logger = LogFactory.getLog(JdbcRevocableTokenProvisioning.class);
-    protected final RowMapper<RevocableToken> rowMapper;
-    protected final JdbcTemplate template;
-    protected final LimitSqlAdapter limitSqlAdapter;
+    private static final Log logger = LogFactory.getLog(JdbcRevocableTokenProvisioning.class);
+    private final RowMapper<RevocableToken> rowMapper;
+    private final JdbcTemplate template;
+    private final LimitSqlAdapter limitSqlAdapter;
     private TimeService timeService;
 
-    protected AtomicLong lastExpiredCheck = new AtomicLong(0);
-    protected long expirationCheckInterval = 30000; //30 seconds
-    private long maxExpirationRuntime = 2500l;
+    AtomicLong lastExpiredCheck = new AtomicLong(0);
+    private long maxExpirationRuntime = 2500L;
 
     public JdbcRevocableTokenProvisioning(JdbcTemplate jdbcTemplate,
                                           LimitSqlAdapter limitSqlAdapter,
                                           TimeService timeService) {
-        this.rowMapper =  new RevocableTokenRowMapper();
+        this.rowMapper = new RevocableTokenRowMapper();
         this.template = jdbcTemplate;
         this.limitSqlAdapter = limitSqlAdapter;
         this.timeService = timeService;
@@ -73,7 +58,6 @@ public class JdbcRevocableTokenProvisioning implements RevocableTokenProvisionin
     public List<RevocableToken> retrieveAll(String zoneId) {
         return null;
     }
-
 
     public RevocableToken retrieve(String id, boolean checkExpired, String zoneId) {
         if (checkExpired) {
@@ -92,45 +76,41 @@ public class JdbcRevocableTokenProvisioning implements RevocableTokenProvisionin
         return retrieve(id, true, zoneId);
     }
 
-
     @Override
     public int deleteRefreshTokensForClientAndUserId(String clientId, String userId, String zoneId) {
-        int deleted_rows = template.update(DELETE_REFRESH_TOKEN_QUERY, userId, clientId, zoneId);
-        return deleted_rows;
+        return template.update(DELETE_REFRESH_TOKEN_QUERY, userId, clientId, zoneId);
     }
-
-
 
     @Override
     public RevocableToken create(RevocableToken t, String zoneId) {
         checkExpired();
         template.update(INSERT_QUERY,
-                        t.getTokenId(),
-                        t.getClientId(),
-                        t.getUserId(),
-                        t.getFormat(),
-                        t.getResponseType().toString(),
-                        t.getIssuedAt(),
-                        t.getExpiresAt(),
-                        t.getScope(),
-                        t.getValue(),
-                        zoneId);
+                t.getTokenId(),
+                t.getClientId(),
+                t.getUserId(),
+                t.getFormat(),
+                t.getResponseType().toString(),
+                t.getIssuedAt(),
+                t.getExpiresAt(),
+                t.getScope(),
+                t.getValue(),
+                zoneId);
         return retrieve(t.getTokenId(), false, zoneId);
     }
 
     @Override
     public RevocableToken update(String id, RevocableToken t, String zoneId) {
         template.update(UPDATE_QUERY,
-                        t.getClientId(),
-                        t.getUserId(),
-                        t.getFormat(),
-                        t.getResponseType().toString(),
-                        t.getIssuedAt(),
-                        t.getExpiresAt(),
-                        t.getScope(),
-                        t.getValue(),
-                        id,
-                        zoneId);
+                t.getClientId(),
+                t.getUserId(),
+                t.getFormat(),
+                t.getResponseType().toString(),
+                t.getIssuedAt(),
+                t.getExpiresAt(),
+                t.getScope(),
+                t.getValue(),
+                id,
+                zoneId);
         return retrieve(id, false, zoneId);
     }
 
@@ -179,11 +159,12 @@ public class JdbcRevocableTokenProvisioning implements RevocableTokenProvisionin
         return template.query(GET_BY_CLIENT_QUERY, rowMapper, clientId, zoneId);
     }
 
-    public long getExpirationCheckInterval() {
-        return expirationCheckInterval;
+    private long getExpirationCheckInterval() {
+        //30 seconds
+        return 30_000L;
     }
 
-    public void checkExpired() {
+    private void checkExpired() {
         long now = timeService.getCurrentTimeMillis();
         long lastCheck = lastExpiredCheck.get();
         if ((now - lastCheck) > getExpirationCheckInterval() && lastExpiredCheck.compareAndSet(lastCheck, now)) {
@@ -191,23 +172,22 @@ public class JdbcRevocableTokenProvisioning implements RevocableTokenProvisionin
                 lastExpiredCheck.set(0);
             }
         }
-
     }
 
     /**
      * @param now
      * @return true if the last delete action deleted the max rows, this also signals there could be more rows to be deleted.
      */
-    protected boolean runDeleteExpired(long now) {
+    private boolean runDeleteExpired(long now) {
         final int maxRows = 500;
         String sql = limitSqlAdapter.getDeleteExpiredQuery(
-            TABLE, "token_id", "expires_at", maxRows
+                TABLE, "token_id", "expires_at", maxRows
         );
         int removed;
         do {
             removed = template.update(sql, now);
             logger.info("Removed " + removed + " expired revocable tokens.");
-        } while (removed > 0 && (timeService.getCurrentTimeMillis()-now)< maxExpirationRuntime);
+        } while (removed > 0 && (timeService.getCurrentTimeMillis() - now) < maxExpirationRuntime);
         return removed >= maxRows;
     }
 
@@ -227,7 +207,7 @@ public class JdbcRevocableTokenProvisioning implements RevocableTokenProvisionin
             revocableToken.setUserId(rs.getString(pos++));
             revocableToken.setFormat(rs.getString(pos++));
             String responseType = rs.getString(pos++);
-            if(StringUtils.hasText(responseType)) {
+            if (StringUtils.hasText(responseType)) {
                 revocableToken.setResponseType(RevocableToken.TokenType.valueOf(responseType));
             }
             revocableToken.setIssuedAt(rs.getLong(pos++));
