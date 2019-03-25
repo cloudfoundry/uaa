0b6bcf06c74e3cc9c47179ff951626b1a80294cd
Joshua Casey <jcasey@pivotal.io>
2019-03-24 21:42:30 -0500

    Refactor (MultitenantClientServices)
    - Pull out interface methods into interfaces
    
    [#164852336]

--- server/src/main/java/org/cloudfoundry/identity/uaa/zone/MultitenantClientServices.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/zone/MultitenantClientServices.java
@@ -4,51 +4,65 @@ import org.springframework.security.oauth2.provider.*;
 
 import java.util.List;
 
-public abstract class MultitenantClientServices implements ClientRegistrationService, ClientDetailsService {
+interface MultitenantClientRegistrationService extends ClientRegistrationService {
 
-    public abstract void addClientSecret(String clientId, String newSecret, String zoneId) throws NoSuchClientException;
+    void addClientDetails(ClientDetails clientDetails, String zoneId) throws ClientAlreadyExistsException;
 
-    public abstract void deleteClientSecret(String clientId, String zoneId) throws NoSuchClientException;
+    void updateClientDetails(ClientDetails clientDetails, String zoneId) throws NoSuchClientException;
+
+    void updateClientSecret(String clientId, String secret, String zoneId) throws NoSuchClientException;
+
+    void removeClientDetails(String clientId, String zoneId) throws NoSuchClientException;
+
+    List<ClientDetails> listClientDetails(String zoneId);
+
+}
+
+interface MultitenantClientDetailsService extends ClientDetailsService {
+
+    ClientDetails loadClientByClientId(String clientId, String zoneId) throws ClientRegistrationException;
+
+}
+
+interface MultitenantClientSecretService {
+
+    void addClientSecret(String clientId, String newSecret, String zoneId) throws NoSuchClientException;
+
+    void deleteClientSecret(String clientId, String zoneId) throws NoSuchClientException;
+}
+
+public abstract class MultitenantClientServices implements
+        MultitenantClientRegistrationService,
+        MultitenantClientDetailsService,
+        MultitenantClientSecretService {
 
     @Override
     public final void addClientDetails(ClientDetails clientDetails) throws ClientAlreadyExistsException {
         addClientDetails(clientDetails, IdentityZoneHolder.get().getId());
     }
 
-    public abstract void addClientDetails(ClientDetails clientDetails, String zoneId) throws ClientAlreadyExistsException;
-
     @Override
     public final void updateClientDetails(ClientDetails clientDetails) throws NoSuchClientException {
         updateClientDetails(clientDetails, IdentityZoneHolder.get().getId());
     }
-
-    public abstract void updateClientDetails(ClientDetails clientDetails, String zoneId) throws NoSuchClientException;
 
     @Override
     public final void updateClientSecret(String clientId, String secret) throws NoSuchClientException {
         updateClientSecret(clientId, secret, IdentityZoneHolder.get().getId());
     }
 
-    public abstract void updateClientSecret(String clientId, String secret, String zoneId) throws NoSuchClientException;
-
     @Override
     public final void removeClientDetails(String clientId) throws NoSuchClientException {
         removeClientDetails(clientId, IdentityZoneHolder.get().getId());
     }
-
-    public abstract void removeClientDetails(String clientId, String zoneId) throws NoSuchClientException;
 
     @Override
     public final List<ClientDetails> listClientDetails() {
         return listClientDetails(IdentityZoneHolder.get().getId());
     }
 
-    public abstract List<ClientDetails> listClientDetails(String zoneId);
-
     @Override
     public final ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
         return loadClientByClientId(clientId, IdentityZoneHolder.get().getId());
     }
-
-    public abstract ClientDetails loadClientByClientId(String clientId, String zoneId) throws ClientRegistrationException;
 }
