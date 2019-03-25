cf48c97e8a81f2d88e3fd25bb8603e5808dc6092
Joshua Casey <jcasey@pivotal.io>
2019-03-24 21:13:55 -0500

    Refactor (ClientServicesExtension)
    - Make it look prettier
    
    [#164852336]

--- server/src/main/java/org/cloudfoundry/identity/uaa/zone/ClientServicesExtension.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/zone/ClientServicesExtension.java
@@ -1,28 +1,10 @@
-/*******************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2017] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.zone;
 
-import org.springframework.security.oauth2.provider.ClientAlreadyExistsException;
-import org.springframework.security.oauth2.provider.ClientDetails;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.ClientRegistrationException;
-import org.springframework.security.oauth2.provider.ClientRegistrationService;
-import org.springframework.security.oauth2.provider.NoSuchClientException;
+import org.springframework.security.oauth2.provider.*;
 
 import java.util.List;
 
 public abstract class ClientServicesExtension implements ClientRegistrationService, ClientDetailsService {
-
 
     public abstract void addClientSecret(String clientId, String newSecret, String zoneId) throws NoSuchClientException;
 
@@ -67,5 +49,6 @@ public abstract class ClientServicesExtension implements ClientRegistrationServi
     public final ClientDetails loadClientByClientId(String clientId) throws ClientRegistrationException {
         return loadClientByClientId(clientId, IdentityZoneHolder.get().getId());
     }
+
     public abstract ClientDetails loadClientByClientId(String clientId, String zoneId) throws ClientRegistrationException;
 }
