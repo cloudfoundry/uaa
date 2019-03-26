92fc49bc07a084691fab13012ff80e6216efd099
Joshua Casey <jcasey@pivotal.io>
2019-03-25 19:37:10 -0500

    Test Refactor (MfaCheckerTests)
    - Use JUnit5
    
    [#164817168]

--- server/src/test/java/org/cloudfoundry/identity/uaa/mfa/MfaCheckerTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/mfa/MfaCheckerTests.java
@@ -1,16 +1,3 @@
-/*
- *  Cloud Foundry
- *  Copyright (c) [2009-2018] Pivotal Software, Inc. All Rights Reserved.
- *  <p/>
- *  This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *  You may not use this product except in compliance with the License.
- *  <p/>
- *  This product includes a number of subcomponents with
- *  separate copyright notices and license terms. Your use of these
- *  subcomponents is subject to the terms and conditions of the
- *  subcomponent's license, as noted in the LICENSE file
- */
-
 package org.cloudfoundry.identity.uaa.mfa;
 
 import com.google.common.collect.Lists;
@@ -18,9 +5,8 @@ import org.cloudfoundry.identity.uaa.provider.IdentityProviderProvisioning;
 import org.cloudfoundry.identity.uaa.zone.IdentityZone;
 import org.cloudfoundry.identity.uaa.zone.MfaConfig;
 import org.cloudfoundry.identity.uaa.zone.MultitenancyFixture;
-
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
@@ -29,37 +15,37 @@ import static org.cloudfoundry.identity.uaa.constants.OriginKeys.SAML;
 import static org.cloudfoundry.identity.uaa.constants.OriginKeys.UAA;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.mock;
 
-public class MfaCheckerTests {
+class MfaCheckerTests {
 
     private IdentityZone identityZone;
     private MfaChecker mfaChecker;
     private IdentityProviderProvisioning identityProviderProvisioning;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeEach
+    void setUp() {
         identityProviderProvisioning = mock(IdentityProviderProvisioning.class);
         identityZone = MultitenancyFixture.identityZone("id", "domain");
         mfaChecker = new MfaChecker(identityProviderProvisioning);
     }
 
     @Test
-    public void isMfaEnabled_WhenEnabled() {
+    void isMfaEnabled_WhenEnabled() {
         identityZone.getConfig().getMfaConfig().setEnabled(true);
         assertTrue(mfaChecker.isMfaEnabled(identityZone, UAA));
     }
 
     @Test
-    public void isMfaEnabled_WhenDisabled() {
+    void isMfaEnabled_WhenDisabled() {
         identityZone.getConfig().getMfaConfig().setEnabled(false);
         assertFalse(mfaChecker.isMfaEnabled(identityZone, UAA));
     }
 
     @Test
-    public void mfaIsRequiredWhenCorrectOriginsAreConfigured() {
+    void mfaIsRequiredWhenCorrectOriginsAreConfigured() {
         identityZone.getConfig().getMfaConfig().setIdentityProviders(
                 Lists.newArrayList("uaa", "ldap"));
 
@@ -68,7 +54,7 @@ public class MfaCheckerTests {
     }
 
     @Test
-    public void mfaConfig_getIdentityProviders_returnsUaaAndLdap() {
+    void mfaConfig_getIdentityProviders_returnsUaaAndLdap() {
         assertThat(MfaConfig.DEFAULT_MFA_IDENTITY_PROVIDERS, is(Arrays.asList(UAA, LDAP)));
 
         identityZone.getConfig().getMfaConfig().setIdentityProviders(
