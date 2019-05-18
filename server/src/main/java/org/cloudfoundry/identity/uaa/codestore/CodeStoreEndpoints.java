b064cb7aa4f3640ff755c1705e501d7ba2803a5e
Joshua Casey <jcasey@pivotal.io>
2019-05-18 15:38:56 -0500

    Refactor (CodeStoreEndpoints)
    - Apply IntelliJ sanitizations
    
    [#164967070]

--- server/src/main/java/org/cloudfoundry/identity/uaa/codestore/CodeStoreEndpoints.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/codestore/CodeStoreEndpoints.java
@@ -1,15 +1,3 @@
-/*******************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.codestore;
 
 import org.cloudfoundry.identity.uaa.web.ConvertingExceptionView;
@@ -20,13 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.View;
 
@@ -38,7 +20,7 @@ public class CodeStoreEndpoints {
     private ExpiringCodeStore expiringCodeStore;
 
     private HttpMessageConverter<?>[] messageConverters = new RestTemplate().getMessageConverters().toArray(
-                    new HttpMessageConverter<?>[0]);
+            new HttpMessageConverter<?>[0]);
 
     public void setMessageConverters(HttpMessageConverter<?>[] messageConverters) {
         this.messageConverters = messageConverters;
@@ -48,7 +30,7 @@ public class CodeStoreEndpoints {
         this.expiringCodeStore = expiringCodeStore;
     }
 
-    @RequestMapping(value = { "/Codes" }, method = RequestMethod.POST)
+    @RequestMapping(value = {"/Codes"}, method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public ExpiringCode generateCode(@RequestBody ExpiringCode expiringCode) {
@@ -66,7 +48,7 @@ public class CodeStoreEndpoints {
     @RequestMapping(value = "/Codes/{code}", method = RequestMethod.GET)
     @ResponseBody
     public ExpiringCode retrieveCode(@PathVariable String code) {
-        ExpiringCode result = null;
+        ExpiringCode result;
         try {
             result = expiringCodeStore.retrieveCode(code, IdentityZoneHolder.get().getId());
         } catch (NullPointerException e) {
@@ -89,7 +71,7 @@ public class CodeStoreEndpoints {
         // User can supply trace=true or just trace (unspecified) to get stack
         // traces
         boolean trace = request.getParameter("trace") != null && !request.getParameter("trace").equals("false");
-        return new ConvertingExceptionView(new ResponseEntity<ExceptionReport>(new ExceptionReport(e, trace),
-                        e.getStatus()), messageConverters);
+        return new ConvertingExceptionView(new ResponseEntity<>(new ExceptionReport(e, trace),
+                e.getStatus()), messageConverters);
     }
 }
