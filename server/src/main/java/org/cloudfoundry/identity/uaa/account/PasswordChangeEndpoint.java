4896eb80cef88e7f0cc378691c9453b5f12517c6
Joshua Casey <jcasey@pivotal.io>
2019-05-30 10:39:02 -0500

    Refactor (PasswordChangeEndpoint)
    - Apply IntelliJ sanitizations
    
    [nostory]

--- server/src/main/java/org/cloudfoundry/identity/uaa/account/PasswordChangeEndpoint.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/account/PasswordChangeEndpoint.java
@@ -1,22 +1,5 @@
-/*******************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.account;
 
-import org.cloudfoundry.identity.uaa.zone.beans.IdentityZoneManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.cloudfoundry.identity.uaa.web.ConvertingExceptionView;
-import org.cloudfoundry.identity.uaa.web.ExceptionReport;
 import org.cloudfoundry.identity.uaa.resources.ActionResult;
 import org.cloudfoundry.identity.uaa.scim.ScimUserProvisioning;
 import org.cloudfoundry.identity.uaa.scim.exception.InvalidPasswordException;
@@ -26,18 +9,18 @@ import org.cloudfoundry.identity.uaa.scim.validate.NullPasswordValidator;
 import org.cloudfoundry.identity.uaa.scim.validate.PasswordValidator;
 import org.cloudfoundry.identity.uaa.security.DefaultSecurityContextAccessor;
 import org.cloudfoundry.identity.uaa.security.SecurityContextAccessor;
+import org.cloudfoundry.identity.uaa.web.ConvertingExceptionView;
+import org.cloudfoundry.identity.uaa.web.ExceptionReport;
+import org.cloudfoundry.identity.uaa.zone.beans.IdentityZoneManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.View;
 
@@ -49,21 +32,21 @@ public class PasswordChangeEndpoint {
     private final Logger logger = LoggerFactory.getLogger(getClass());
     private final IdentityZoneManager identityZoneManager;
 
-    private ScimUserProvisioning dao;
+    private ScimUserProvisioning scimUserProvisioning;
 
     private PasswordValidator passwordValidator = new NullPasswordValidator();
 
     private SecurityContextAccessor securityContextAccessor = new DefaultSecurityContextAccessor();
 
     private HttpMessageConverter<?>[] messageConverters = new RestTemplate().getMessageConverters().toArray(
-                    new HttpMessageConverter<?>[0]);
+            new HttpMessageConverter<?>[0]);
 
     public PasswordChangeEndpoint(IdentityZoneManager identityZoneManager) {
         this.identityZoneManager = identityZoneManager;
     }
 
     public void setScimUserProvisioning(ScimUserProvisioning provisioning) {
-        this.dao = provisioning;
+        this.scimUserProvisioning = provisioning;
     }
 
     public void setPasswordValidator(PasswordValidator passwordValidator) {
@@ -92,11 +75,11 @@ public class PasswordChangeEndpoint {
         String newPassword = change.getPassword();
 
         throwIfPasswordChangeNotPermitted(userId, oldPassword, zoneId);
-        if (dao.checkPasswordMatches(userId, newPassword, zoneId)) {
+        if (scimUserProvisioning.checkPasswordMatches(userId, newPassword, zoneId)) {
             throw new InvalidPasswordException("Your new password cannot be the same as the old password.", UNPROCESSABLE_ENTITY);
         }
         passwordValidator.validate(newPassword);
-        dao.changePassword(userId, oldPassword, newPassword, zoneId);
+        scimUserProvisioning.changePassword(userId, oldPassword, newPassword, zoneId);
         return new ActionResult("ok", "password updated");
     }
 
@@ -106,10 +89,10 @@ public class PasswordChangeEndpoint {
         // caught and
         // logged (then ignored) by the caller.
         return new ConvertingExceptionView(
-                        new ResponseEntity<>(new ExceptionReport(
-                                        new BadCredentialsException("Invalid password change request"), false),
-                                        HttpStatus.UNAUTHORIZED),
-                        messageConverters);
+                new ResponseEntity<>(new ExceptionReport(
+                        new BadCredentialsException("Invalid password change request"), false),
+                        HttpStatus.UNAUTHORIZED),
+                messageConverters);
     }
 
     @ExceptionHandler(ScimException.class)
@@ -147,7 +130,7 @@ public class PasswordChangeEndpoint {
                 throw new InvalidPasswordException("Previous password is required");
             }
 
-            if (!dao.checkPasswordMatches(userId, oldPassword, zoneId)) {
+            if (!scimUserProvisioning.checkPasswordMatches(userId, oldPassword, zoneId)) {
                 throw new BadCredentialsException("Old password is incorrect");
             }
         }
