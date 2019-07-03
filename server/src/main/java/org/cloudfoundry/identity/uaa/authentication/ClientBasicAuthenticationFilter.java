6e938eb8f654a50bb57f7aca9cc9ab535631686d
Jeremy Morony <jmorony@pivotal.io>
2019-07-03 14:28:03 -0700

    Replace ClientBasicAuthenticationFilter.
    The UAA (and spring) do not properly handle the decoding of client id
    and secret when supplied as a basic auth header. The id and secret
    should have been url encoded but the UAA was not handling this subtlety.
    
    Before this commit, this filter extended the BasicAuthentication filter
    in order to apply some preprocessing. As far as we can tell, this
    preprocessing is no longer relevant.
    
    Now, we implement a new version of the BasicAuthentication filter which
    correctly handles the url decoding.
    
    [#166970393]
    
    Signed-off-by: Andrew Wittrock <awittrock@pivotal.io>

--- server/src/main/java/org/cloudfoundry/identity/uaa/authentication/ClientBasicAuthenticationFilter.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/authentication/ClientBasicAuthenticationFilter.java
@@ -1,33 +1,26 @@
-/*******************************************************************************
- * Cloud Foundry
- * Copyright (c) [2009-2015] Pivotal Software, Inc. All Rights Reserved.
- * <p>
- * This product is licensed to you under the Apache License, Version 2.0 (the "License").
- * You may not use this product except in compliance with the License.
- * <p>
- * This product includes a number of subcomponents with
- * separate copyright notices and license terms. Your use of these
- * subcomponents is subject to the terms and conditions of the
- * subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
+
 package org.cloudfoundry.identity.uaa.authentication;
 
-import java.sql.Timestamp;
-
-import org.cloudfoundry.identity.uaa.oauth.client.ClientConstants;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.crypto.codec.Base64;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.ClientRegistrationException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.util.Assert;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.Base64;
 
 
 /**
@@ -35,58 +28,105 @@ import java.io.IOException;
  * the client lockout policy and throws ClientLockoutException when the client attempting
  * to authenticate is locked out.
  */
-public class ClientBasicAuthenticationFilter extends BasicAuthenticationFilter {
+public class ClientBasicAuthenticationFilter extends OncePerRequestFilter {
+    private AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource = new WebAuthenticationDetailsSource();
+    private AuthenticationEntryPoint authenticationEntryPoint;
+    private AuthenticationManager authenticationManager;
 
-    protected ClientDetailsService clientDetailsService;
-
+    /**
+     * Creates an instance which will authenticate against the supplied
+     * {@code AuthenticationManager} and use the supplied {@code AuthenticationEntryPoint}
+     * to handle authentication failures.
+     *
+     * @param authenticationManager the bean to submit authentication requests to
+     * @param authenticationEntryPoint will be invoked when authentication fails.
+     * Typically an instance of {@link BasicAuthenticationEntryPoint}.
+     */
     public ClientBasicAuthenticationFilter(AuthenticationManager authenticationManager,
-            AuthenticationEntryPoint authenticationEntryPoint) {
+                                     AuthenticationEntryPoint authenticationEntryPoint) {
+        Assert.notNull(authenticationManager, "authenticationManager cannot be null");
+        Assert.notNull(authenticationEntryPoint,
+                "authenticationEntryPoint cannot be null");
+        this.authenticationManager = authenticationManager;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
 
-        super(authenticationManager, authenticationEntryPoint);
+    public void setAuthenticationDetailsSource(
+            AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource) {
+        Assert.notNull(authenticationDetailsSource,
+                "AuthenticationDetailsSource required");
+        this.authenticationDetailsSource = authenticationDetailsSource;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        Assert.notNull(this.authenticationManager,"An AuthenticationManager is required");
+        Assert.notNull(this.authenticationEntryPoint,"An AuthenticationEntryPoint is required");
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-            HttpServletResponse response, FilterChain chain)
-                    throws IOException, ServletException {
-        try{
-            String header = request.getHeader("Authorization");
-            if (header == null || !header.startsWith("Basic ")) {
-                chain.doFilter(request, response);
-                return;
+                                    HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        final boolean debug = this.logger.isDebugEnabled();
+
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.toLowerCase().startsWith("basic ")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            String[] tokens = extractAndDecodeHeader(header, request);
+            assert tokens.length == 2;
+            String clientId = URLDecoder.decode(tokens[0], getCredentialsCharset(request));
+            String clientSecret = URLDecoder.decode(tokens[1], getCredentialsCharset(request));
+
+            if (debug) {
+                this.logger
+                        .debug("Basic Authentication Authorization header found for user '"
+                                + clientId + "'");
             }
 
-            String[] decodedHeader = extractAndDecodeHeader(header, request);
-            //Validate against client lockout policy
-            String clientId = decodedHeader[0];
+            UsernamePasswordAuthenticationToken authRequest =
+                    new UsernamePasswordAuthenticationToken(clientId, clientSecret);
+            authRequest.setDetails(this.authenticationDetailsSource.buildDetails(request));
+            Authentication authResult = this.authenticationManager.authenticate(authRequest);
 
-            //Validate against client secret expiration in the zone configured client secret policy
-            Timestamp lastModified = (Timestamp) clientDetailsService.loadClientByClientId(clientId).getAdditionalInformation().get(ClientConstants.LAST_MODIFIED);
-        } catch(BadCredentialsException e) {
-            super.getAuthenticationEntryPoint().commence(request, response, e);
-            return;
-        } catch(ClientRegistrationException e) {
-            logger.debug(e.getMessage());
+            if (debug) {
+                this.logger.debug("Authentication success: " + authResult);
+            }
+
+            SecurityContextHolder.getContext().setAuthentication(authResult);
         }
-        //call parent class to authenticate
-        super.doFilterInternal(request, response, chain);
+        catch (AuthenticationException failed) {
+            SecurityContextHolder.clearContext();
+
+            if (debug) {
+                this.logger.debug("Authentication request for failed: " + failed);
+            }
+
+            this.authenticationEntryPoint.commence(request, response, failed);
+            return;
+        }
+
+        chain.doFilter(request, response);
     }
 
-    public ClientDetailsService getClientDetailsService() {
-        return clientDetailsService;
-    }
-
-    public void setClientDetailsService(ClientDetailsService clientDetailsService) {
-        this.clientDetailsService = clientDetailsService;
-    }
-
+    /**
+     * Decodes the header into a username and password.
+     *
+     * @throws BadCredentialsException if the Basic header is not present or is not valid
+     * Base64
+     */
     private String[] extractAndDecodeHeader(String header, HttpServletRequest request)
             throws IOException {
 
         byte[] base64Token = header.substring(6).getBytes("UTF-8");
         byte[] decoded;
         try {
-            decoded = Base64.decode(base64Token);
+            decoded = Base64.getDecoder().decode(base64Token);
         }
         catch (IllegalArgumentException e) {
             throw new BadCredentialsException(
@@ -101,5 +141,9 @@ public class ClientBasicAuthenticationFilter extends BasicAuthenticationFilter {
             throw new BadCredentialsException("Invalid basic authentication token");
         }
         return new String[] { token.substring(0, delim), token.substring(delim + 1) };
+    }
+
+    private String getCredentialsCharset(HttpServletRequest httpRequest) {
+        return "UTF-8";
     }
 }
--- server/src/test/java/org/cloudfoundry/identity/uaa/authentication/ClientBasicAuthenticationFilterTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/authentication/ClientBasicAuthenticationFilterTests.java
@@ -1,60 +1,46 @@
 package org.cloudfoundry.identity.uaa.authentication;
 
-import org.cloudfoundry.identity.uaa.oauth.client.ClientConstants;
-import org.cloudfoundry.identity.uaa.zone.ClientSecretPolicy;
 import org.cloudfoundry.identity.uaa.zone.IdentityZone;
 import org.cloudfoundry.identity.uaa.zone.IdentityZoneHolder;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
-import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
-import java.sql.Timestamp;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Base64;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
 
-import static org.cloudfoundry.identity.uaa.provider.IdentityProviderValidationRequest.UsernamePasswordAuthentication;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 class ClientBasicAuthenticationFilterTests {
-    private static final String CREDENTIALS_HEADER_STRING =
-            new String(Base64.getEncoder().encode("app:appclientsecret".getBytes()));
-
     private ClientBasicAuthenticationFilter filter;
     private AuthenticationManager clientAuthenticationManager;
-    private ClientDetailsService clientDetailsService;
+    private AuthenticationDetailsSource<HttpServletRequest, ?> uaaAuthenticationDetailsSource;
 
     @BeforeEach
     void setUp() {
-        tearDown();
-        clientAuthenticationManager = mock(AuthenticationManager.class);
-        AuthenticationEntryPoint authenticationEntryPoint = mock(AuthenticationEntryPoint.class);
-        clientDetailsService = mock(ClientDetailsService.class);
-
-        filter = new ClientBasicAuthenticationFilter(clientAuthenticationManager, authenticationEntryPoint);
-        filter.setClientDetailsService(clientDetailsService);
-
+        SecurityContextHolder.clearContext();
         IdentityZone testZone = new IdentityZone();
-        testZone.getConfig().setClientSecretPolicy(new ClientSecretPolicy(0,255,0,0,0,0,6));
         IdentityZoneHolder.set(testZone);
+
+        clientAuthenticationManager = mock(AuthenticationManager.class);
+        uaaAuthenticationDetailsSource = mock(UaaAuthenticationDetailsSource.class);
+
+        filter = new ClientBasicAuthenticationFilter(clientAuthenticationManager, mock(AuthenticationEntryPoint.class));
+        filter.setAuthenticationDetailsSource(uaaAuthenticationDetailsSource);
     }
 
     @AfterAll
@@ -64,63 +50,31 @@ class ClientBasicAuthenticationFilterTests {
     }
 
     @Nested
-    class WhenClientSecretHasExpired {
+    class ByDefault {
+        private void addBasicAuthHeader(MockHttpServletRequest request, String clientId, String clientSecret) throws UnsupportedEncodingException {
+            String encodedClientId = URLEncoder.encode(clientId, "UTF-8");
+            String encodedClientSecret = URLEncoder.encode(clientSecret, "UTF-8");
+            String encodedCredentials = new String(
+                Base64.getEncoder().encode((encodedClientId + ":" + encodedClientSecret).getBytes())
+            );
+
+            request.addHeader("Authorization", "Basic " + encodedCredentials);
+        }
+
         @Test
-        void continuesWithFilterChain() throws IOException, ServletException {
-            BaseClientDetails clientDetails = new BaseClientDetails("client-1", "none", "uaa.none", "client_credentials", "http://localhost:5000/uaadb" );
-
-
-            Calendar expiredDate = Calendar.getInstance();
-            expiredDate.set(2016, 1, 1);
-            clientDetails.setAdditionalInformation(createTestAdditionalInformation(expiredDate));
-
-            when(clientDetailsService.loadClientByClientId(Mockito.matches("app"))).thenReturn(clientDetails);
-
+        void urlDecodesClientIdAndClientSecret() throws IOException, ServletException {
+            String clientId = "app|whatever";
+            String clientSecret = "sec|ret";
             MockFilterChain chain = mock(MockFilterChain.class);
             MockHttpServletRequest request = new MockHttpServletRequest();
-            request.addHeader("Authorization", "Basic " + CREDENTIALS_HEADER_STRING);
             MockHttpServletResponse response = new MockHttpServletResponse();
+            addBasicAuthHeader(request, clientId, clientSecret);
 
             filter.doFilter(request, response, chain);
 
-            verify(clientAuthenticationManager).authenticate(any(Authentication.class));
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(clientId, clientSecret);
+            token.setDetails(uaaAuthenticationDetailsSource.buildDetails(request));
+            verify(clientAuthenticationManager).authenticate(token);
         }
-    }
-
-    @Nested
-    class WhenClientSecretHasNotExpired {
-        @Test
-        void continuesWithFilterChain() throws IOException, ServletException {
-            BaseClientDetails clientDetails = new BaseClientDetails("client-1", "none", "uaa.none", "client_credentials",
-                    "http://localhost:5000/uaadb" );
-
-            Calendar previousDay = Calendar.getInstance();
-            previousDay.roll(Calendar.DATE, -1);
-
-            clientDetails.setAdditionalInformation(createTestAdditionalInformation(previousDay));
-
-            when(clientDetailsService.loadClientByClientId(Mockito.matches("app"))).thenReturn(clientDetails);
-
-            UsernamePasswordAuthentication authResult =
-                    new UsernamePasswordAuthentication("app","appclientsecret");
-            authResult.setAuthenticated(true);
-            when(clientAuthenticationManager.authenticate(any())).thenReturn(authResult);
-
-            MockFilterChain chain = mock(MockFilterChain.class);
-            MockHttpServletRequest request = new MockHttpServletRequest();
-            request.addHeader("Authorization", "Basic " + CREDENTIALS_HEADER_STRING);
-            MockHttpServletResponse response = new MockHttpServletResponse();
-
-            filter.doFilter(request, response, chain);
-
-            verify(clientAuthenticationManager).authenticate(any(Authentication.class));
-        }
-    }
-
-    private Map<String, Object> createTestAdditionalInformation(Calendar calendar) {
-        Map<String,Object> additionalInformation = new HashMap<>();
-        additionalInformation.put(ClientConstants.LAST_MODIFIED, new Timestamp(calendar.getTimeInMillis()));
-
-        return additionalInformation;
     }
 }
