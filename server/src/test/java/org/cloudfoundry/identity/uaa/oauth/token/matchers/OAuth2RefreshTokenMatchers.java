dd4b877d3f06e3a29d819a8e1709d3cac9dae8af
Joshua Casey <jcasey@pivotal.io>
2019-08-01 11:42:36 -0500

    Upgrade hamcrest to 2.1
    - @Factory can be removed
    - See https://github.com/hamcrest/JavaHamcrest/blob/master/CHANGES.md#breaking-changes-for-21
    
    [nostory]

--- server/src/test/java/org/cloudfoundry/identity/uaa/oauth/token/matchers/OAuth2RefreshTokenMatchers.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/oauth/token/matchers/OAuth2RefreshTokenMatchers.java
@@ -2,7 +2,6 @@ package org.cloudfoundry.identity.uaa.oauth.token.matchers;
 
 import org.cloudfoundry.identity.uaa.oauth.token.ClaimConstants;
 import org.hamcrest.Description;
-import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
 import org.springframework.security.oauth2.common.OAuth2RefreshToken;
 
@@ -39,82 +38,66 @@ public class OAuth2RefreshTokenMatchers extends AbstractOAuth2AccessTokenMatcher
         }
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> issuerUri(Matcher<Object> issuerUri) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.ISS, issuerUri);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> clientId(Matcher<Object> clientId) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.CLIENT_ID, clientId);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> userId(Matcher<Object> userId) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.USER_ID, userId);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> subject(Matcher<Object> clientId) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.SUB, clientId);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> cid(Matcher<Object> clientId) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.CID, clientId);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> scope(Matcher<Object> scopes) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.GRANTED_SCOPES, scopes);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> audience(Matcher<Object> resourceIds) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.AUD, resourceIds);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> jwtId(Matcher<String> jti) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.JTI, jti);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> issuedAt(Matcher<Integer> iat) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.IAT, iat);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> expiry(Matcher<Integer> expiry) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.EXP, expiry);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> username(Matcher<Object> username) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.USER_NAME, username);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> zoneId(Matcher<Object> zoneId) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.ZONE_ID, zoneId);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> origin(Matcher<Object> origin) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.ORIGIN, origin);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> revocationSignature(Matcher<Object> signature) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.REVOCATION_SIGNATURE, signature);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> email(Matcher<Object> email) {
         return new OAuth2RefreshTokenMatchers(ClaimConstants.EMAIL, email);
     }
 
-    @Factory
     public static Matcher<OAuth2RefreshToken> validFor(Matcher<?> validFor) {
         return new AbstractOAuth2AccessTokenMatchers<OAuth2RefreshToken>() {
 
