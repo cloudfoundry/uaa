ba8b126596a2e2cd96e4aa6ed5d06c42b0e1a36a
Joshua Casey <jcasey@pivotal.io>
2019-09-13 13:01:32 -0500

    Test Refactor - IntrospectEndpointMockMvcTest
    - IntelliJ autoformat
    - In preparation for [#167050374]
    
    [nostory]

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/oauth/IntrospectEndpointMockMvcTest.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/oauth/IntrospectEndpointMockMvcTest.java
@@ -1,32 +1,26 @@
 package org.cloudfoundry.identity.uaa.oauth;
 
-import java.util.Map;
-
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.apache.commons.codec.binary.Base64;
 import org.cloudfoundry.identity.uaa.mock.token.AbstractTokenMockMvcTests;
 import org.cloudfoundry.identity.uaa.oauth.token.TokenConstants;
 import org.cloudfoundry.identity.uaa.util.JsonUtils;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import org.apache.commons.codec.binary.Base64;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.oauth2.common.util.OAuth2Utils;
 import org.springframework.web.util.HtmlUtils;
 
+import java.util.Map;
+
 import static org.cloudfoundry.identity.uaa.oauth.token.TokenConstants.GRANT_TYPE_PASSWORD;
 import static org.cloudfoundry.identity.uaa.oauth.token.TokenConstants.TokenFormat.OPAQUE;
 import static org.springframework.http.HttpHeaders.ACCEPT;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
-import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class IntrospectEndpointMockMvcTest extends AbstractTokenMockMvcTests {
 
@@ -40,18 +34,18 @@ class IntrospectEndpointMockMvcTest extends AbstractTokenMockMvcTests {
         String username = createUserForPasswordGrant(jdbcScimUserProvisioning, jdbcScimGroupMembershipManager, jdbcScimGroupProvisioning, generator);
 
         String content = mockMvc.perform(
-            post("/oauth/token")
-                .param("client_id", CLIENTID)
-                .param("client_secret", CLIENTSECRET)
-                .param(OAuth2Utils.GRANT_TYPE, GRANT_TYPE_PASSWORD)
-                .param("username", username)
-                .param("password", SECRET)
-                .param(TokenConstants.REQUEST_TOKEN_FORMAT, OPAQUE.getStringValue())
-                .param("response_type", "id_token")
-                .accept(APPLICATION_JSON)
-                .contentType(APPLICATION_FORM_URLENCODED))
-            .andExpect(status().isOk())
-            .andReturn().getResponse().getContentAsString();
+                post("/oauth/token")
+                        .param("client_id", CLIENTID)
+                        .param("client_secret", CLIENTSECRET)
+                        .param(OAuth2Utils.GRANT_TYPE, GRANT_TYPE_PASSWORD)
+                        .param("username", username)
+                        .param("password", SECRET)
+                        .param(TokenConstants.REQUEST_TOKEN_FORMAT, OPAQUE.getStringValue())
+                        .param("response_type", "id_token")
+                        .accept(APPLICATION_JSON)
+                        .contentType(APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
         Map<String, Object> tokenMap = JsonUtils.readValue(content, new TypeReference<Map<String, Object>>() {
         });
         token = (String) tokenMap.get("access_token");
@@ -61,44 +55,44 @@ class IntrospectEndpointMockMvcTest extends AbstractTokenMockMvcTests {
     @Test
     void introspectToken_withValidToken() throws Exception {
         mockMvc.perform(
-            post("/introspect")
-                .header("Authorization", "Basic " + basic)
-                .header(ACCEPT, APPLICATION_JSON_VALUE)
-                .header(CONTENT_TYPE, APPLICATION_FORM_URLENCODED_VALUE)
-                .param("token", token))
-            .andDo(log())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.active").value("true"))
-            .andExpect(jsonPath("$.client_id").value(CLIENTID))
-            .andExpect(header().string(CONTENT_TYPE, "application/json;charset=UTF-8"));
+                post("/introspect")
+                        .header("Authorization", "Basic " + basic)
+                        .header(ACCEPT, APPLICATION_JSON_VALUE)
+                        .header(CONTENT_TYPE, APPLICATION_FORM_URLENCODED_VALUE)
+                        .param("token", token))
+                .andDo(log())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.active").value("true"))
+                .andExpect(jsonPath("$.client_id").value(CLIENTID))
+                .andExpect(header().string(CONTENT_TYPE, "application/json;charset=UTF-8"));
     }
 
 
     @Test
     void introspectToken_withInvalidToken() throws Exception {
         mockMvc.perform(
-            post("/introspect")
-                .header("Authorization", "Basic " + basic)
-                .header(ACCEPT, APPLICATION_JSON_VALUE)
-                .header(CONTENT_TYPE, APPLICATION_FORM_URLENCODED_VALUE)
-                .param("token", "invalid-token"))
-            .andDo(log())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.active").value("false"))
-            .andExpect(header().string(CONTENT_TYPE, "application/json;charset=UTF-8"));
+                post("/introspect")
+                        .header("Authorization", "Basic " + basic)
+                        .header(ACCEPT, APPLICATION_JSON_VALUE)
+                        .header(CONTENT_TYPE, APPLICATION_FORM_URLENCODED_VALUE)
+                        .param("token", "invalid-token"))
+                .andDo(log())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.active").value("false"))
+                .andExpect(header().string(CONTENT_TYPE, "application/json;charset=UTF-8"));
     }
 
     @Test
     void check_token_delete() throws Exception {
         mockMvc.perform(
-            delete("/introspect")
-                .header("Authorization", "Basic " + basic)
-                .header(ACCEPT, APPLICATION_JSON_VALUE)
-                .header(CONTENT_TYPE, APPLICATION_FORM_URLENCODED_VALUE)
-                .param("token", token))
-            .andDo(log())
-            .andExpect(status().isMethodNotAllowed())
-            .andExpect(jsonPath("$.error").value("method_not_allowed"))
-            .andExpect(jsonPath("$.error_description").value(HtmlUtils.htmlEscape("Request method 'DELETE' not supported", "ISO-8859-1")));
+                delete("/introspect")
+                        .header("Authorization", "Basic " + basic)
+                        .header(ACCEPT, APPLICATION_JSON_VALUE)
+                        .header(CONTENT_TYPE, APPLICATION_FORM_URLENCODED_VALUE)
+                        .param("token", token))
+                .andDo(log())
+                .andExpect(status().isMethodNotAllowed())
+                .andExpect(jsonPath("$.error").value("method_not_allowed"))
+                .andExpect(jsonPath("$.error_description").value(HtmlUtils.htmlEscape("Request method 'DELETE' not supported", "ISO-8859-1")));
     }
 }
