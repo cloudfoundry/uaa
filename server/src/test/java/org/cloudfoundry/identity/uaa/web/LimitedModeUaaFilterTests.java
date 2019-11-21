42297541e13a051d9306c514bc915afef5399095
Joshua Casey <jcasey@pivotal.io>
2019-11-21 14:08:50 -0600

    Test Refactor - LimitedModeUaaFilterTests
    - Use JUnit5
    
    [nostory]

--- server/src/test/java/org/cloudfoundry/identity/uaa/web/LimitedModeUaaFilterTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/web/LimitedModeUaaFilterTests.java
@@ -2,9 +2,9 @@ package org.cloudfoundry.identity.uaa.web;
 
 import org.cloudfoundry.identity.uaa.util.JsonUtils;
 import org.cloudfoundry.identity.uaa.util.TimeService;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
@@ -17,9 +17,9 @@ import java.util.concurrent.atomic.AtomicLong;
 
 import static javax.servlet.http.HttpServletResponse.SC_SERVICE_UNAVAILABLE;
 import static org.cloudfoundry.identity.uaa.web.LimitedModeUaaFilter.STATUS_INTERVAL_MS;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.mock;
@@ -34,122 +34,122 @@ import static org.springframework.http.HttpMethod.POST;
 
 public class LimitedModeUaaFilterTests {
 
-    private MockHttpServletRequest request;
-    private MockHttpServletResponse response;
-    private FilterChain chain;
+    private MockHttpServletRequest mockHttpServletRequest;
+    private MockHttpServletResponse mockHttpServletResponse;
+    private FilterChain mockFilterChain;
     private LimitedModeUaaFilter filter;
     private File statusFile;
     private final AtomicLong time = new AtomicLong(System.currentTimeMillis());
     private TimeService timeService;
 
-    @Before
-    public void setup() throws Exception {
+    @BeforeEach
+    void setUp() throws Exception {
         timeService = new TimeService() {
             @Override
             public long getCurrentTimeMillis() {
                 return time.get();
             }
         };
-        request = new MockHttpServletRequest();
-        request.addHeader(ACCEPT, "*/*");
-        response = new MockHttpServletResponse();
-        chain = mock(FilterChain.class);
+        mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.addHeader(ACCEPT, "*/*");
+        mockHttpServletResponse = new MockHttpServletResponse();
+        mockFilterChain = mock(FilterChain.class);
         filter = new LimitedModeUaaFilter();
         statusFile = File.createTempFile("uaa-limited-mode.", ".status");
     }
 
-    @After
-    public void teardown() {
+    @AfterEach
+    void tearDown() {
         statusFile.delete();
     }
 
     @Test
-    public void disabled() throws Exception {
-        filter.doFilterInternal(request, response, chain);
-        verify(chain, times(1)).doFilter(same(request), same(response));
+    void disabled() throws Exception {
+        filter.doFilterInternal(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+        verify(mockFilterChain, times(1)).doFilter(same(mockHttpServletRequest), same(mockHttpServletResponse));
         assertFalse(filter.isEnabled());
     }
 
     @Test
-    public void enabledNoWhitelistPost() throws Exception {
-        request.setMethod(POST.name());
+    void enabledNoWhitelistPost() throws Exception {
+        mockHttpServletRequest.setMethod(POST.name());
         filter.setStatusFile(statusFile);
-        filter.doFilterInternal(request, response, chain);
-        verifyNoInteractions(chain);
-        assertEquals(SC_SERVICE_UNAVAILABLE, response.getStatus());
+        filter.doFilterInternal(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+        verifyNoInteractions(mockFilterChain);
+        assertEquals(SC_SERVICE_UNAVAILABLE, mockHttpServletResponse.getStatus());
     }
 
     @Test
-    public void enabledNoWhitelistGet() throws Exception {
-        request.setMethod(GET.name());
+    void enabledNoWhitelistGet() throws Exception {
+        mockHttpServletRequest.setMethod(GET.name());
         filter.setStatusFile(statusFile);
         filter.setPermittedMethods(new HashSet<>(Collections.singletonList(GET.toString())));
-        filter.doFilterInternal(request, response, chain);
-        verify(chain, times(1)).doFilter(same(request), same(response));
+        filter.doFilterInternal(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+        verify(mockFilterChain, times(1)).doFilter(same(mockHttpServletRequest), same(mockHttpServletResponse));
     }
 
     @Test
-    public void enabledMatchingUrlPost() throws Exception {
-        request.setMethod(POST.name());
+    void enabledMatchingUrlPost() throws Exception {
+        mockHttpServletRequest.setMethod(POST.name());
         filter.setPermittedEndpoints(Collections.singleton("/oauth/token/**"));
         filter.setStatusFile(statusFile);
         for (String pathInfo : Arrays.asList("/oauth/token", "/oauth/token/alias/something")) {
-            setPathInfo(pathInfo, request);
-            reset(chain);
-            filter.doFilterInternal(request, response, chain);
-            verify(chain, times(1)).doFilter(same(request), same(response));
+            setPathInfo(pathInfo, mockHttpServletRequest);
+            reset(mockFilterChain);
+            filter.doFilterInternal(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+            verify(mockFilterChain, times(1)).doFilter(same(mockHttpServletRequest), same(mockHttpServletResponse));
         }
     }
 
     @Test
-    public void enabledNotMatchingPost() throws Exception {
-        request.setMethod(POST.name());
+    void enabledNotMatchingPost() throws Exception {
+        mockHttpServletRequest.setMethod(POST.name());
         filter.setPermittedEndpoints(Collections.singleton("/oauth/token/**"));
         filter.setStatusFile(statusFile);
         for (String pathInfo : Arrays.asList("/url", "/other/url")) {
-            response = new MockHttpServletResponse();
-            setPathInfo(pathInfo, request);
-            reset(chain);
-            filter.doFilterInternal(request, response, chain);
-            verifyNoInteractions(chain);
-            assertEquals(SC_SERVICE_UNAVAILABLE, response.getStatus());
+            mockHttpServletResponse = new MockHttpServletResponse();
+            setPathInfo(pathInfo, mockHttpServletRequest);
+            reset(mockFilterChain);
+            filter.doFilterInternal(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+            verifyNoInteractions(mockFilterChain);
+            assertEquals(SC_SERVICE_UNAVAILABLE, mockHttpServletResponse.getStatus());
         }
     }
 
     @Test
-    public void errorIsJson() throws Exception {
+    void errorIsJson() throws Exception {
         filter.setPermittedEndpoints(Collections.singleton("/oauth/token/**"));
         filter.setStatusFile(statusFile);
         for (String accept : Arrays.asList("application/json", "text/html,*/*")) {
-            request = new MockHttpServletRequest();
-            response = new MockHttpServletResponse();
-            setPathInfo("/not/allowed", request);
-            request.setMethod(POST.name());
-            request.addHeader(ACCEPT, accept);
-            filter.doFilterInternal(request, response, chain);
-            assertEquals(SC_SERVICE_UNAVAILABLE, response.getStatus());
-            assertEquals(JsonUtils.writeValueAsString(filter.getErrorData()), response.getContentAsString());
+            mockHttpServletRequest = new MockHttpServletRequest();
+            mockHttpServletResponse = new MockHttpServletResponse();
+            setPathInfo("/not/allowed", mockHttpServletRequest);
+            mockHttpServletRequest.setMethod(POST.name());
+            mockHttpServletRequest.addHeader(ACCEPT, accept);
+            filter.doFilterInternal(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+            assertEquals(SC_SERVICE_UNAVAILABLE, mockHttpServletResponse.getStatus());
+            assertEquals(JsonUtils.writeValueAsString(filter.getErrorData()), mockHttpServletResponse.getContentAsString());
         }
     }
 
     @Test
-    public void errorIsNot() throws Exception {
+    void errorIsNot() throws Exception {
         filter.setPermittedEndpoints(Collections.singleton("/oauth/token/**"));
         filter.setStatusFile(statusFile);
         for (String accept : Arrays.asList("text/html", "text/plain")) {
-            request = new MockHttpServletRequest();
-            response = new MockHttpServletResponse();
-            setPathInfo("/not/allowed", request);
-            request.setMethod(POST.name());
-            request.addHeader(ACCEPT, accept);
-            filter.doFilterInternal(request, response, chain);
-            assertEquals(SC_SERVICE_UNAVAILABLE, response.getStatus());
-            assertEquals(filter.getErrorData().get("description"), response.getErrorMessage());
+            mockHttpServletRequest = new MockHttpServletRequest();
+            mockHttpServletResponse = new MockHttpServletResponse();
+            setPathInfo("/not/allowed", mockHttpServletRequest);
+            mockHttpServletRequest.setMethod(POST.name());
+            mockHttpServletRequest.addHeader(ACCEPT, accept);
+            filter.doFilterInternal(mockHttpServletRequest, mockHttpServletResponse, mockFilterChain);
+            assertEquals(SC_SERVICE_UNAVAILABLE, mockHttpServletResponse.getStatus());
+            assertEquals(filter.getErrorData().get("description"), mockHttpServletResponse.getErrorMessage());
         }
     }
 
     @Test
-    public void disableEnableUsesCacheToAvoidFileAccess() {
+    void disableEnableUsesCacheToAvoidFileAccess() {
         File spy = spy(statusFile);
         doCallRealMethod().when(spy).exists();
         filter.setTimeService(timeService);
@@ -163,7 +163,7 @@ public class LimitedModeUaaFilterTests {
     }
 
     @Test
-    public void settingsFileChangesCache() {
+    void settingsFileChangesCache() {
         disableEnableUsesCacheToAvoidFileAccess();
         filter.setStatusFile(null);
         assertFalse(filter.isEnabled());
