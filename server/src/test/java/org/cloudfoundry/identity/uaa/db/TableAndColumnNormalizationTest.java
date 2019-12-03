46163bb989f5ed79458a4339dcf3310f55cfc18e
Joshua Casey <jcasey@pivotal.io>
2019-12-03 13:22:51 -0600

    Test Refactor - TableAndColumnNormalizationTest
    - Apply IntelliJ sanitizations
    
    [#170083097]

--- server/src/test/java/org/cloudfoundry/identity/uaa/db/TableAndColumnNormalizationTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/db/TableAndColumnNormalizationTest.java
@@ -1,23 +1,11 @@
-/*******************************************************************************
- *     Cloud Foundry 
- *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.db;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.cloudfoundry.identity.uaa.test.JdbcTestBase;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
@@ -34,8 +22,8 @@ public class TableAndColumnNormalizationTest extends JdbcTestBase {
     @Before
     public void checkMysqlOrPostgresqlProfile() {
         Assume.assumeTrue(
-            Arrays.asList(webApplicationContext.getEnvironment().getActiveProfiles()).contains("mysql") ||
-            Arrays.asList(webApplicationContext.getEnvironment().getActiveProfiles()).contains("postgresql")
+                Arrays.asList(webApplicationContext.getEnvironment().getActiveProfiles()).contains("mysql") ||
+                        Arrays.asList(webApplicationContext.getEnvironment().getActiveProfiles()).contains("postgresql")
         );
     }
 
