e061513d22f1736cf445712a9dd9d235e2502c4e
Joshua Casey <jcasey@pivotal.io>
2019-12-03 13:21:29 -0600

    Test Refactor - UserInfoTableTest
    - Use WithDatabaseContext
    - Use JUnit5
    
    [#170083097]

--- server/src/test/java/org/cloudfoundry/identity/uaa/db/UserInfoTableTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/db/UserInfoTableTest.java
@@ -1,33 +1,26 @@
 package org.cloudfoundry.identity.uaa.db;
 
-import org.cloudfoundry.identity.uaa.test.JdbcTestBase;
-import org.junit.Test;
-import org.springframework.mock.env.MockEnvironment;
+import org.cloudfoundry.identity.uaa.annotations.WithDatabaseContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class UserInfoTableTest extends JdbcTestBase {
+@WithDatabaseContext
+class UserInfoTableTest {
 
     private static List<TestColumn> TEST_COLUMNS = Arrays.asList(
             new TestColumn("user_id", "varchar", 36),
             new TestColumn("info", "longvarchar/mediumtext", 0)
     );
-
-    @Override
-    public void setUp() {
-        MockEnvironment environment = new MockEnvironment();
-        if (System.getProperty("spring.active.profiles") != null) {
-            environment.setActiveProfiles(System.getProperty("spring.active.profiles"));
-        }
-        setUp(environment);
-    }
 
     private static boolean testColumn(String name, String type, int size) {
         return testColumn(TEST_COLUMNS, name, type, size);
@@ -45,7 +38,9 @@ public class UserInfoTableTest extends JdbcTestBase {
     }
 
     @Test
-    public void validate_table() throws Exception {
+    void validate_table(
+            @Autowired DataSource dataSource
+    ) throws Exception {
         try (Connection connection = dataSource.getConnection()) {
             DatabaseMetaData meta = connection.getMetaData();
             boolean foundTable = false;
@@ -57,14 +52,14 @@ public class UserInfoTableTest extends JdbcTestBase {
                 String rscolumnName = rs.getString("COLUMN_NAME");
                 int columnSize = rs.getInt("COLUMN_SIZE");
                 if (tableName.equalsIgnoreCase(rstableName)) {
-                    assertTrue("Testing column:" + rscolumnName, testColumn(rscolumnName, rs.getString("TYPE_NAME"), columnSize));
+                    assertTrue(testColumn(rscolumnName, rs.getString("TYPE_NAME"), columnSize), "Testing column:" + rscolumnName);
                     foundTable = true;
                     foundColumn++;
                 }
             }
             rs.close();
-            assertTrue("Table " + tableName + " not found!", foundTable);
-            assertEquals("Table " + tableName + " is missing columns!", TEST_COLUMNS.size(), foundColumn);
+            assertTrue(foundTable, "Table " + tableName + " not found!");
+            assertEquals(TEST_COLUMNS.size(), foundColumn, "Table " + tableName + " is missing columns!");
 
             rs = meta.getIndexInfo(connection.getCatalog(), null, tableName, false, false);
             if (!rs.next()) {
