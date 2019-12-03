fad5ca9f7d01d933d725d4a273e9e7d3e0d63bb4
Joshua Casey <jcasey@pivotal.io>
2019-12-03 14:20:57 -0600

    Test Refactor - ClientDetailsHasRequiredUserScopes
    - Turns out this class wasn't Parameterized
    - Hardcode all the parameterized stuff
    - Apply IntelliJ sanitizations
    
    [#170083097]

--- server/src/test/java/org/cloudfoundry/identity/uaa/db/ClientDetailsHasRequiredUserScopes.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/db/ClientDetailsHasRequiredUserScopes.java
@@ -1,72 +1,25 @@
-/*
- * ****************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2017] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- * ****************************************************************************
- */
 package org.cloudfoundry.identity.uaa.db;
 
 import org.cloudfoundry.identity.uaa.test.JdbcTestBase;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.springframework.mock.env.MockEnvironment;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.util.Arrays;
-import java.util.Collection;
 
+import static org.hamcrest.Matchers.in;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isIn;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(Parameterized.class)
 public class ClientDetailsHasRequiredUserScopes extends JdbcTestBase {
 
-    private String springProfile;
-    private String tableName;
-    private String columnName;
-
-    public ClientDetailsHasRequiredUserScopes(String springProfile, String tableName, String columnName) {
-        this.springProfile = springProfile;
-        this.tableName = tableName;
-        this.columnName = columnName;
-    }
-
-    @Parameterized.Parameters(name = "{index}: org.cloudfoundry.identity.uaa.db[{0}]; table[{1}]")
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-            {null, "oauth_client_details", "required_user_groups"},
-//            {"mysql", "oauth_client_details", "required_user_groups"},
-//            {"hsqldb", "oauth_client_details", "required_user_groups"},
-//            {"postgresql", "oauth_client_details", "required_user_groups"},
-        });
-    }
-
-    @Override
-    public void setUp() {
-        MockEnvironment environment = new MockEnvironment();
-        if ( springProfile!=null ) {
-            environment.setActiveProfiles(springProfile);
-        }
-        setUp(environment);
-    }
-
-
     @Test
-    public void test_That_required_user_groups_is_1024() throws Exception {
+    public void requiredUserGroupsIs1024() throws Exception {
         try (Connection connection = dataSource.getConnection()) {
             DatabaseMetaData meta = connection.getMetaData();
             boolean foundTable = false;
@@ -76,19 +29,19 @@ public class ClientDetailsHasRequiredUserScopes extends JdbcTestBase {
                 String rstableName = rs.getString("TABLE_NAME");
                 String rscolumnName = rs.getString("COLUMN_NAME");
                 int columnSize = rs.getInt("COLUMN_SIZE");
-                if ((foundTable = tableName.equalsIgnoreCase(rstableName)) && columnName.equalsIgnoreCase(rscolumnName)) {
+                if ((foundTable = "oauth_client_details".equalsIgnoreCase(rstableName)) && "required_user_groups".equalsIgnoreCase(rscolumnName)) {
                     assertEquals("Table:" + rstableName + " Column:" + rscolumnName + " should be 1024 in size.", 1024, columnSize);
                     foundColumn = true;
                     String columnType = rs.getString("TYPE_NAME");
                     assertNotNull("Table:" + rstableName + " Column:" + rscolumnName + " should have a column type.", columnType);
-                    assertThat("Table:" + rstableName + " Column:" + rscolumnName + " should be varchar", columnType.toLowerCase(), isIn(Arrays.asList("varchar", "nvarchar")));
+                    assertThat("Table:" + rstableName + " Column:" + rscolumnName + " should be varchar", columnType.toLowerCase(), is(in(Arrays.asList("varchar", "nvarchar"))));
                     break;
                 }
             }
             rs.close();
 
-            assertTrue("[" + springProfile + "] I was expecting to find table:" + tableName, foundTable);
-            assertTrue("[" + springProfile + "] I was expecting to find column: " + columnName, foundColumn);
+            assertTrue("I was expecting to find table: oauth_client_details", foundTable);
+            assertTrue("I was expecting to find column: required_user_groups", foundColumn);
         }
     }
 }
