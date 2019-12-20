37d9317b0c89e5dca240159caf5ecd92174f5df7
Andrew Wittrock <awittrock@pivotal.io>
2019-12-19 17:46:13 -0800

    Add test that provides non-default config
    [#169712923]
    
    Signed-off-by: Andrew Edstrom <aedstrom@pivotal.io>
    Co-authored-by: Andrew Edstrom <aedstrom@pivotal.io>

--- src/k8s/deployment_test.go
+++ src/k8s/deployment_test.go
@@ -13,18 +13,29 @@ import (
 var _ = Describe("Deployment", func() {
 	It("Sets the image sha for the UAA container", func() {
 		command := exec.Command("ytt", "-f", "./templates/deployment.yml", "-f", "./templates/values/values.yml")
-		session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
-		Expect(err).NotTo(HaveOccurred())
-		Eventually(session).Should(gexec.Exit())
-
-		stdOut := session.Wait().Out.Contents()
-		decode := scheme.Codecs.UniversalDeserializer().Decode
-		obj, _, err := decode(stdOut, nil, nil)
-		if err != nil {
-			fmt.Printf("%#v", err)
-		}
-		deployment := obj.(*v1.Deployment)
-
+		deployment := interpolateAndUnmarshalDeployment(command)
 		Expect(deployment.Spec.Template.Spec.Containers[0].Image).To(Equal("cfidentity/uaa@sha256:93b70b26fbb3de88d93728b0daf1ea7b001fde89a24e283c3db36bf4c6af087c"))
 	})
+
+	When("provided a different image sha", func() {
+		It("Sets the image sha for the UAA container", func() {
+			command := exec.Command("ytt", "-f", "./templates/deployment.yml", "-f", "./templates/values/values.yml", "--data-value", "image_sha=other-sha")
+			deployment := interpolateAndUnmarshalDeployment(command)
+			Expect(deployment.Spec.Template.Spec.Containers[0].Image).To(Equal("cfidentity/uaa@sha256:other-sha"))
+		})
+	})
 })
+
+func interpolateAndUnmarshalDeployment(command *exec.Cmd) *v1.Deployment {
+	session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
+	Expect(err).NotTo(HaveOccurred())
+	Eventually(session).Should(gexec.Exit())
+
+	stdOut := session.Wait().Out.Contents()
+	decode := scheme.Codecs.UniversalDeserializer().Decode
+	obj, _, err := decode(stdOut, nil, nil)
+	if err != nil {
+		fmt.Printf("%#v", err)
+	}
+	return obj.(*v1.Deployment)
+}
