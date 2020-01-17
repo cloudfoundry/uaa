98efda7e2d3f784b6291ab9f494a6ea1e024c310
Andrew Wittrock <awittrock@pivotal.io>
2020-01-17 14:22:33 -0800

    Test Refactor - XOAuthAuthenticationFilterTest
    - Use JUnit5 and MockitoExtension
    
    [#170636851]
    
    Signed-off-by: Joshua Casey <jcasey@pivotal.io>
    Co-authored-by: Joshua Casey <jcasey@pivotal.io>

--- server/src/test/java/org/cloudfoundry/identity/uaa/provider/oauth/XOAuthAuthenticationFilterTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/provider/oauth/XOAuthAuthenticationFilterTest.java
@@ -4,15 +4,19 @@ import org.cloudfoundry.identity.uaa.authentication.UaaAuthentication;
 import org.cloudfoundry.identity.uaa.authentication.UaaAuthenticationDetails;
 import org.cloudfoundry.identity.uaa.authentication.UaaPrincipal;
 import org.cloudfoundry.identity.uaa.constants.OriginKeys;
+import org.cloudfoundry.identity.uaa.extensions.PollutionPreventionExtension;
 import org.cloudfoundry.identity.uaa.login.AccountSavingAuthenticationSuccessHandler;
 import org.cloudfoundry.identity.uaa.zone.IdentityZoneHolder;
 import org.hamcrest.Matchers;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
@@ -23,39 +27,36 @@ import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 
 import static java.util.Collections.emptyList;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class XOAuthAuthenticationFilterTest {
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(PollutionPreventionExtension.class)
+class XOAuthAuthenticationFilterTest {
 
+    @Mock
     private AccountSavingAuthenticationSuccessHandler mockAccountSavingAuthenticationSuccessHandler;
+    @Mock
     private XOAuthAuthenticationManager mockXOAuthAuthenticationManager;
+    @InjectMocks
     private XOAuthAuthenticationFilter filter;
 
-    @Before
-    public void setUp() throws Exception {
-        mockAccountSavingAuthenticationSuccessHandler = mock(AccountSavingAuthenticationSuccessHandler.class);
-        mockXOAuthAuthenticationManager = mock(XOAuthAuthenticationManager.class);
-
-        filter = new XOAuthAuthenticationFilter(
-                mockXOAuthAuthenticationManager,
-                mockAccountSavingAuthenticationSuccessHandler);
-    }
-
-    @Before
-    @After
-    public void clearContext() {
+    @BeforeEach
+    @AfterEach
+    void clearContext() {
         SecurityContextHolder.clearContext();
     }
 
     @Test
-    public void shouldAuthenticate() {
+    void shouldAuthenticate() {
         MockHttpServletRequest request = new MockHttpServletRequest();
         shouldAuthenticate(filter, request, "code");
         shouldAuthenticate(filter, request, "id_token");
@@ -74,10 +75,10 @@ public class XOAuthAuthenticationFilterTest {
     }
 
     @Test
-    public void getIdTokenInResponse() throws Exception {
+    void getIdTokenInResponse() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);
         when(request.getRequestURL()).thenReturn(new StringBuffer("http://localhost/uaa/login/callback/the_origin"));
-        when(request.getServletPath()).thenReturn("/login/callback/the_origin");
+        when(request.getParameter(anyString())).thenReturn(null);
         when(request.getParameter("id_token")).thenReturn("the_id_token");
         when(request.getParameter("access_token")).thenReturn("the_access_token");
         when(request.getParameter("code")).thenReturn("the_code");
@@ -103,10 +104,10 @@ public class XOAuthAuthenticationFilterTest {
     }
 
     @Test
-    public void getXOAuthCodeTokenFromRequest() throws Exception {
+    void getXOAuthCodeTokenFromRequest() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);
         when(request.getRequestURL()).thenReturn(new StringBuffer("http://localhost/uaa/login/callback/the_origin"));
-        when(request.getServletPath()).thenReturn("/login/callback/the_origin");
+        when(request.getParameter(anyString())).thenReturn(null);
         when(request.getParameter("code")).thenReturn("the_code");
 
         UaaAuthentication authentication = new UaaAuthentication(new UaaPrincipal("id", "username", "email@email.com", OriginKeys.UAA, null, IdentityZoneHolder.get().getId()), emptyList(), new UaaAuthenticationDetails(request));
@@ -130,17 +131,17 @@ public class XOAuthAuthenticationFilterTest {
     }
 
     @Test
-    public void redirectsToErrorPageInCaseOfException() throws Exception {
+    void redirectsToErrorPageInCaseOfException() throws Exception {
         HttpServletRequest request = mock(HttpServletRequest.class);
         FilterChain chain = mock(FilterChain.class);
         MockHttpServletResponse response = new MockHttpServletResponse();
 
         when(request.getRequestURL()).thenReturn(new StringBuffer("http://localhost/uaa/login/callback/the_origin"));
-        when(request.getServletPath()).thenReturn("/login/callback/the_origin");
+        when(request.getParameter(anyString())).thenReturn(null);
         when(request.getParameter("code")).thenReturn("the_code");
 
         Mockito.doThrow(new HttpClientErrorException(HttpStatus.BAD_REQUEST, "error from oauth server")).when(mockXOAuthAuthenticationManager).authenticate(any());
         filter.doFilter(request, response, chain);
-        Assert.assertThat(response.getHeader("Location"), Matchers.containsString(request.getContextPath() + "/oauth_error?error=There+was+an+error+when+authenticating+against+the+external+identity+provider%3A"));
+        assertThat(response.getHeader("Location"), Matchers.containsString(request.getContextPath() + "/oauth_error?error=There+was+an+error+when+authenticating+against+the+external+identity+provider%3A"));
     }
 }
