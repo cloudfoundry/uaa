48c365d0d7f0c497cc92cb39000ca872150c8096
Andrew Edstrom <aedstrom@pivotal.io>
2020-01-21 16:37:04 -0800

    k8s release sets spring profile based on db scheme
    [#169718758]
    
    Signed-off-by: Andrew Wittrock <awittrock@pivotal.io>
    Signed-off-by: Andrew Edstrom <aedstrom@pivotal.io>
    Signed-off-by: Joshua Casey <jcasey@pivotal.io>

--- k8s/test/deployment_test.go
+++ k8s/test/deployment_test.go
@@ -7,15 +7,18 @@ import (
 )
 
 var _ = Describe("Deployment", func() {
-	var deploymentPath, valuesPath string
+	var templates []string
 
 	BeforeEach(func() {
-		deploymentPath = pathToTemplate("deployment.yml")
-		valuesPath = pathToTemplate(filepath.Join("..", "values", "default-values.yml"))
+		templates = []string{
+			pathToTemplate("deployment.yml"),
+			pathToTemplate(filepath.Join("..", "values", "default-values.yml")),
+			pathToTemplate("deployment_functions.star"),
+		}
 	})
 
 	It("Renders a deployment for the UAA", func() {
-		ctx := NewRenderingContext(deploymentPath, valuesPath)
+		ctx := NewRenderingContext(templates...)
 
 		Expect(ctx).To(
 			ProduceYAML(
@@ -28,5 +31,32 @@ var _ = Describe("Deployment", func() {
 				}),
 			),
 		)
+	})
+
+	When("provided with custom values", func() {
+		var (
+			databaseScheme string
+			ctx            RenderingContext
+		)
+
+		BeforeEach(func() {
+			databaseScheme = "postgresql"
+			ctx = NewRenderingContext(templates...).WithData(map[string]string{
+				"database.scheme": databaseScheme,
+			})
+		})
+
+		It("Renders a deployment with the custom values interpolated", func() {
+			Expect(ctx).To(
+				ProduceYAML(
+					RepresentingDeployment().WithPodMatching(func(pod *PodMatcher) {
+						pod.WithContainerMatching(func(container *ContainerMatcher) {
+							container.WithName("uaa")
+							container.WithEnvVar("spring_profiles", databaseScheme)
+						})
+					}),
+				),
+			)
+		})
 	})
 })
