bcf85e8f69fc4d6bcb75e61b9b9be18f30738b09
Joshua Casey <jcasey@pivotal.io>
2020-02-01 16:37:30 -0600

    Refactor - GeneralIdentityZoneValidator
    - Apply IntelliJ autoformat
    
    [nostory]

--- server/src/main/java/org/cloudfoundry/identity/uaa/zone/GeneralIdentityZoneValidator.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/zone/GeneralIdentityZoneValidator.java
@@ -1,17 +1,4 @@
-/*******************************************************************************
- * Cloud Foundry
- * Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
- * <p>
- * This product is licensed to you under the Apache License, Version 2.0 (the "License").
- * You may not use this product except in compliance with the License.
- * <p>
- * This product includes a number of subcomponents with
- * separate copyright notices and license terms. Your use of these
- * subcomponents is subject to the terms and conditions of the
- * subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.zone;
-
 
 import org.springframework.util.StringUtils;
 
@@ -34,7 +21,7 @@ public class GeneralIdentityZoneValidator implements IdentityZoneValidator {
         try {
             identityZone.setConfig(configValidator.validate(identityZone, mode));
         } catch (InvalidIdentityZoneConfigurationException ex) {
-            String configErrorMessage = StringUtils.hasText(ex.getMessage())?ex.getMessage():"";
+            String configErrorMessage = StringUtils.hasText(ex.getMessage()) ? ex.getMessage() : "";
             throw new InvalidIdentityZoneDetailsException("The zone configuration is invalid. " + configErrorMessage, ex);
         }
         return identityZone;
