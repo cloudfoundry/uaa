8fe214c998392ca42aba3d8d3b79239ff0dce503
Joshua Casey <jcasey@pivotal.io>
2020-02-07 15:39:58 -0600

    Refactor - MfaConfigValidatorTests
    - Apply IntelliJ autoformat
    
    [nostory]

--- server/src/main/java/org/cloudfoundry/identity/uaa/zone/MfaConfigValidator.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/zone/MfaConfigValidator.java
@@ -1,8 +1,8 @@
 package org.cloudfoundry.identity.uaa.zone;
 
+import org.cloudfoundry.identity.uaa.mfa.MfaProviderProvisioning;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.cloudfoundry.identity.uaa.mfa.MfaProviderProvisioning;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.util.StringUtils;
 
@@ -19,10 +19,10 @@ public class MfaConfigValidator {
     }
 
     public void validate(MfaConfig config, String zoneId) throws InvalidIdentityZoneConfigurationException {
-        if(config.isEnabled() || StringUtils.hasText(config.getProviderName())) {
+        if (config.isEnabled() || StringUtils.hasText(config.getProviderName())) {
             try {
                 mfaProviderProvisioning.retrieveByName(config.getProviderName(), zoneId);
-            } catch(EmptyResultDataAccessException e){
+            } catch (EmptyResultDataAccessException e) {
                 logger.debug(String.format("Provider with name %s not found", config.getProviderName()));
                 throw new InvalidIdentityZoneConfigurationException("Active MFA Provider not found with name: " + config.getProviderName());
             }
