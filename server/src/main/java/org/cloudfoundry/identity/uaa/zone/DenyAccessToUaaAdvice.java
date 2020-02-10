cc4541d445af4290074dcb774fafd257caef3612
Joshua Casey <jcasey@pivotal.io>
2020-02-09 22:24:50 -0600

    Refactor - DenyAccessToUaaAdvice
    - Use annotations instead of XML for AOP wiring
    
    [nostory]

--- server/src/main/java/org/cloudfoundry/identity/uaa/zone/DenyAccessToUaaAdvice.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/zone/DenyAccessToUaaAdvice.java
@@ -1,17 +1,26 @@
 package org.cloudfoundry.identity.uaa.zone;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 
 @Component("denyAccessToUaaAdvice")
+@Aspect
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class DenyAccessToUaaAdvice {
-    
+
+    @Before("execution(* *..IdentityZoneEndpoints+.updateIdentityZone(..)) and args(identityZone,*)")
     public void checkIdentityZone(IdentityZone identityZone) {
         if (identityZone != null && identityZone.isUaa()) {
             throw new AccessDeniedException("Access to UAA is not allowed.");
         }
     }
-    
+
+    @Before("execution(* *..IdentityZoneEndpoints+.updateIdentityZone(..)) and args(*,identityZoneId)" +
+            " or execution(* *..IdentityZoneEndpoints+.createClient(..)) and args(identityZoneId,*)" +
+            " or execution(* *..IdentityZoneEndpoints+.deleteClient(..)) and args(identityZoneId,*)")
     public void checkIdentityZoneId(String identityZoneId) {
         if (IdentityZone.getUaaZoneId().equals(identityZoneId)) {
             throw new AccessDeniedException("Access to UAA is not allowed.");
