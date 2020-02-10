55ee424cdc3b95854ad9c4663b6589868421880f
Joshua Casey <jcasey@pivotal.io>
2020-02-09 20:54:59 -0600

    Test Refactor - IdentityProviderConfigValidationDelegatorTest
    - Use JUnit5
    
    [nostory]

--- server/src/test/java/org/cloudfoundry/identity/uaa/provider/IdentityProviderConfigValidationDelegatorTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/provider/IdentityProviderConfigValidationDelegatorTest.java
@@ -3,10 +3,8 @@ package org.cloudfoundry.identity.uaa.provider;
 import org.cloudfoundry.identity.uaa.provider.ldap.LdapIdentityProviderConfigValidator;
 import org.cloudfoundry.identity.uaa.provider.oauth.ExternalOAuthIdentityProviderConfigValidator;
 import org.cloudfoundry.identity.uaa.provider.uaa.UaaIdentityProviderConfigValidator;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
@@ -15,13 +13,14 @@ import static org.cloudfoundry.identity.uaa.constants.OriginKeys.LDAP;
 import static org.cloudfoundry.identity.uaa.constants.OriginKeys.OAUTH20;
 import static org.cloudfoundry.identity.uaa.constants.OriginKeys.OIDC10;
 import static org.cloudfoundry.identity.uaa.constants.OriginKeys.UAA;
+import static org.cloudfoundry.identity.uaa.util.AssertThrowsWithMessage.assertThrowsWithMessageThat;
 import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 
-public class IdentityProviderConfigValidationDelegatorTest {
+class IdentityProviderConfigValidationDelegatorTest {
 
     private IdentityProviderConfigValidationDelegator validator;
     private UaaIdentityProviderConfigValidator uaaValidator;
@@ -29,8 +28,8 @@ public class IdentityProviderConfigValidationDelegatorTest {
     private IdentityProvider<AbstractIdentityProviderDefinition> provider;
     private ExternalOAuthIdentityProviderConfigValidator externalOAuthValidator;
 
-    @Before
-    public void setup() {
+    @BeforeEach
+    void setup() {
         uaaValidator = mock(UaaIdentityProviderConfigValidator.class);
         externalOAuthValidator = mock(ExternalOAuthIdentityProviderConfigValidator.class);
         ldapValidator = mock(LdapIdentityProviderConfigValidator.class);
@@ -42,18 +41,17 @@ public class IdentityProviderConfigValidationDelegatorTest {
         );
     }
 
-    @Rule
-    public ExpectedException exception = ExpectedException.none();
-
     @Test
-    public void null_identity_provider() {
-        exception.expect(IllegalArgumentException.class);
-        exception.expectMessage("Provider cannot be null");
-        validator.validate(null);
+    void null_identity_provider() {
+        assertThrowsWithMessageThat(
+                IllegalArgumentException.class,
+                () -> validator.validate(null),
+                org.hamcrest.Matchers.is("Provider cannot be null")
+        );
     }
 
     @Test
-    public void uaa_validator_with_nodefinition_is_invoked() {
+    void uaa_validator_with_nodefinition_is_invoked() {
         provider.setType(UAA);
         provider.setOriginKey(UAA);
         validator.validate(provider);
@@ -63,7 +61,7 @@ public class IdentityProviderConfigValidationDelegatorTest {
     }
 
     @Test
-    public void ldap_validator_with_definition_is_invoked() {
+    void ldap_validator_with_definition_is_invoked() {
         provider.setType(LDAP);
         provider.setOriginKey(LDAP);
         validator.validate(provider);
@@ -73,7 +71,7 @@ public class IdentityProviderConfigValidationDelegatorTest {
     }
 
     @Test
-    public void externalOAuth_validator_with_definition_is_invoked() {
+    void externalOAuth_validator_with_definition_is_invoked() {
         for (String type : Arrays.asList(OAUTH20, OIDC10)) {
             provider.setType(type);
             provider.setOriginKey("any");
