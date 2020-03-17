e504a06a60f697167554b2ad1a4184f699efdbce
Tim Hausler <thausler@pivotal.io>
2020-03-17 15:52:04 -0500

    Refactor K8s tests - move RenderingContext
    [#171343212]
    
    Signed-off-by: Joshua Casey <jcasey@pivotal.io>

--- k8s/matchers/produce_yaml_matcher.go
+++ k8s/matchers/produce_yaml_matcher.go
@@ -11,20 +11,6 @@ import (
 	"os/exec"
 )
 
-type RenderingContext struct {
-	templates []string
-	data      map[string]string
-}
-
-func (r RenderingContext) WithData(data map[string]string) RenderingContext {
-	r.data = data
-	return r
-}
-
-func NewRenderingContext(templates ...string) RenderingContext {
-	return RenderingContext{templates, nil}
-}
-
 type ProduceYAMLMatcher struct {
 	matcher  types.GomegaMatcher
 	rendered string
