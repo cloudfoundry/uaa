ee4cf6446d72c7b746567f509751d07c1cb3c4dd
Joshua Casey <jcasey@pivotal.io>
2020-04-15 12:55:49 -0500

    Test Refactor - K8s Secrets tests
    - Apply Contexts for clarity around what is being tested
    
    [nostory]

--- k8s/test/secrets_test.go
+++ k8s/test/secrets_test.go
@@ -11,139 +11,145 @@ var _ = Describe("Secrets", func() {
 
 	var templates []string
 
-	It("Renders with SMTP credentials", func() {
-		templates = []string{
-			pathToFile(filepath.Join("values", "_values.yml")),
-			pathToFile(filepath.Join("secrets", "smtp_credentials.yml")),
-		}
+	Context("SMTP Credentials", func() {
+		It("Renders with SMTP credentials", func() {
+			templates = []string{
+				pathToFile(filepath.Join("values", "_values.yml")),
+				pathToFile(filepath.Join("secrets", "smtp_credentials.yml")),
+			}
 
-		renderingContext := NewRenderingContext(templates...).WithData(
-			map[string]string{
-				"smtp.user":     "my smtp username",
-				"smtp.password": "my smtp password",
-			})
+			renderingContext := NewRenderingContext(templates...).WithData(
+				map[string]string{
+					"smtp.user":     "my smtp username",
+					"smtp.password": "my smtp password",
+				})
 
-		smtp_secrets := `smtp:
+			smtp_secrets := `smtp:
   user: my smtp username
   password: my smtp password
 `
 
-		Expect(renderingContext).To(
-			ProduceYAML(RepresentingASecret().
-				WithName("uaa-smtp-credentials").
-				WithStringData("smtp_credentials.yml", smtp_secrets)),
-		)
+			Expect(renderingContext).To(
+				ProduceYAML(RepresentingASecret().
+					WithName("uaa-smtp-credentials").
+					WithStringData("smtp_credentials.yml", smtp_secrets)),
+			)
+		})
+
+		It("Does not render with empty SMTP credentials", func() {
+			templates = []string{
+				pathToFile(filepath.Join("values", "_values.yml")),
+				pathToFile(filepath.Join("secrets", "smtp_credentials.yml")),
+			}
+
+			renderingContext := NewRenderingContext(templates...)
+
+			Expect(renderingContext).To(ProduceEmptyYAML())
+		})
 	})
 
-	It("Does not render with empty SMTP credentials", func() {
-		templates = []string{
-			pathToFile(filepath.Join("values", "_values.yml")),
-			pathToFile(filepath.Join("secrets", "smtp_credentials.yml")),
-		}
+	Context("Database Credentials", func() {
+		It("Renders with Database credentials", func() {
+			templates = []string{
+				pathToFile(filepath.Join("values", "_values.yml")),
+				pathToFile(filepath.Join("secrets", "database_credentials.yml")),
+			}
 
-		renderingContext := NewRenderingContext(templates...)
+			renderingContext := NewRenderingContext(templates...).WithData(
+				map[string]string{
+					"database.username": "my database username",
+					"database.password": "my database password",
+				})
 
-		Expect(renderingContext).To(ProduceEmptyYAML())
-	})
-
-	It("Renders with Database credentials", func() {
-		templates = []string{
-			pathToFile(filepath.Join("values", "_values.yml")),
-			pathToFile(filepath.Join("secrets", "database_credentials.yml")),
-		}
-
-		renderingContext := NewRenderingContext(templates...).WithData(
-			map[string]string{
-				"database.username": "my database username",
-				"database.password": "my database password",
-			})
-
-		database_credentials := `database:
+			database_credentials := `database:
   username: my database username
   password: my database password
 `
 
-		Expect(renderingContext).To(
-			ProduceYAML(RepresentingASecret().
-				WithName("uaa-database-credentials").
-				WithStringData("database_credentials.yml", database_credentials)),
-		)
-	})
+			Expect(renderingContext).To(
+				ProduceYAML(RepresentingASecret().
+					WithName("uaa-database-credentials").
+					WithStringData("database_credentials.yml", database_credentials)),
+			)
+		})
 
-	It("Renders with Different Database credentials", func() {
-		templates = []string{
-			pathToFile(filepath.Join("values", "_values.yml")),
-			pathToFile(filepath.Join("secrets", "database_credentials.yml")),
-		}
+		It("Renders with Different Database credentials", func() {
+			templates = []string{
+				pathToFile(filepath.Join("values", "_values.yml")),
+				pathToFile(filepath.Join("secrets", "database_credentials.yml")),
+			}
 
-		renderingContext := NewRenderingContext(templates...).WithData(
-			map[string]string{
-				"database.username": "my other database username",
-				"database.password": "my other database password",
-			})
+			renderingContext := NewRenderingContext(templates...).WithData(
+				map[string]string{
+					"database.username": "my other database username",
+					"database.password": "my other database password",
+				})
 
-		database_credentials := `database:
+			database_credentials := `database:
   username: my other database username
   password: my other database password
 `
 
-		Expect(renderingContext).To(
-			ProduceYAML(RepresentingASecret().
-				WithName("uaa-database-credentials").
-				WithStringData("database_credentials.yml", database_credentials)),
-		)
+			Expect(renderingContext).To(
+				ProduceYAML(RepresentingASecret().
+					WithName("uaa-database-credentials").
+					WithStringData("database_credentials.yml", database_credentials)),
+			)
+		})
+
+		It("Does Not Render with Missing Database credentials", func() {
+			templates = []string{
+				pathToFile(filepath.Join("values", "_values.yml")),
+				pathToFile(filepath.Join("secrets", "database_credentials.yml")),
+			}
+
+			renderingContext := NewRenderingContext(templates...).WithData(
+				map[string]string{
+					"database.username": "",
+					"database.password": "",
+				})
+
+			Expect(renderingContext).To(ProduceEmptyYAML())
+		})
 	})
 
-	It("Does Not Render with Missing Database credentials", func() {
-		templates = []string{
-			pathToFile(filepath.Join("values", "_values.yml")),
-			pathToFile(filepath.Join("secrets", "database_credentials.yml")),
-		}
+	Context("Admin Client Credentials", func() {
+		It("Renders with admin client credentials", func() {
+			templates = []string{
+				pathToFile(filepath.Join("values", "_values.yml")),
+				pathToFile(filepath.Join("secrets", "admin_client_credentials.yml")),
+			}
 
-		renderingContext := NewRenderingContext(templates...).WithData(
-			map[string]string{
-				"database.username": "",
-				"database.password": "",
-			})
+			renderingContext := NewRenderingContext(templates...).WithData(
+				map[string]string{
+					"admin.client_secret": "my admin client secret",
+				})
 
-		Expect(renderingContext).To(ProduceEmptyYAML())
-	})
-
-	It("Renders with admin client credentials", func() {
-		templates = []string{
-			pathToFile(filepath.Join("values", "_values.yml")),
-			pathToFile(filepath.Join("secrets", "admin_client_credentials.yml")),
-		}
-
-		renderingContext := NewRenderingContext(templates...).WithData(
-			map[string]string{
-				"admin.client_secret": "my admin client secret",
-			})
-
-		adminClientCredentials := `oauth:
+			adminClientCredentials := `oauth:
   clients:
     admin:
       secret: my admin client secret
 `
 
-		Expect(renderingContext).To(
-			ProduceYAML(RepresentingASecret().
-				WithName("uaa-admin-client-credentials").
-				WithStringData("admin_client_credentials.yml", adminClientCredentials)),
-		)
-	})
+			Expect(renderingContext).To(
+				ProduceYAML(RepresentingASecret().
+					WithName("uaa-admin-client-credentials").
+					WithStringData("admin_client_credentials.yml", adminClientCredentials)),
+			)
+		})
 
-	It("Admin client credentials are required", func() {
-		templates = []string{
-			pathToFile(filepath.Join("values", "_values.yml")),
-			pathToFile(filepath.Join("secrets", "admin_client_credentials.yml")),
-		}
+		It("Admin client credentials are required", func() {
+			templates = []string{
+				pathToFile(filepath.Join("values", "_values.yml")),
+				pathToFile(filepath.Join("secrets", "admin_client_credentials.yml")),
+			}
 
-		renderingContext := NewRenderingContext(templates...)
+			renderingContext := NewRenderingContext(templates...)
 
-		Expect(renderingContext).To(
-			ThrowError("admin.client_secret is required"),
-		)
+			Expect(renderingContext).To(
+				ThrowError("admin.client_secret is required"),
+			)
+		})
 	})
 
 })
