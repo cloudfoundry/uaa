13b9a9ba9a2cb2846cb2b311b3b6e6f893fa596b
Joshua Casey <jcasey@pivotal.io>
2020-04-26 16:15:03 -0500

    Test Refactor - AuditListenerTests
    - Use MockitoExtension
    
    [nostory]

--- server/src/test/java/org/cloudfoundry/identity/uaa/audit/event/AuditListenerTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/audit/event/AuditListenerTests.java
@@ -2,39 +2,40 @@ package org.cloudfoundry.identity.uaa.audit.event;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
 import org.cloudfoundry.identity.uaa.audit.AuditEvent;
 import org.cloudfoundry.identity.uaa.audit.UaaAuditService;
-import org.cloudfoundry.identity.uaa.authentication.AuthzAuthenticationRequest;
-import org.cloudfoundry.identity.uaa.authentication.UaaAuthenticationDetails;
 import org.cloudfoundry.identity.uaa.authentication.event.UserAuthenticationFailureEvent;
 import org.cloudfoundry.identity.uaa.authentication.event.UserAuthenticationSuccessEvent;
 import org.cloudfoundry.identity.uaa.authentication.event.UserNotFoundEvent;
+import org.cloudfoundry.identity.uaa.extensions.PollutionPreventionExtension;
 import org.cloudfoundry.identity.uaa.user.UaaUser;
 import org.cloudfoundry.identity.uaa.zone.IdentityZoneHolder;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.Authentication;
 
+@ExtendWith(PollutionPreventionExtension.class)
+@ExtendWith(MockitoExtension.class)
 class AuditListenerTests {
 
+    @Mock
     private UaaAuditService mockUaaAuditService;
+
+    @Mock
     private UaaUser mockUser;
+
+    @Mock
     private Authentication mockAuthentication;
+
+    @InjectMocks
     private AuditListener auditListener;
-
-    @BeforeEach
-    void setUp() {
-        mockAuthentication = mock(Authentication.class);
-
-        mockUser = mock(UaaUser.class);
-        mockUaaAuditService = mock(UaaAuditService.class);
-        auditListener = new AuditListener(mockUaaAuditService);
-    }
 
     @Test
     void userNotFoundIsAudited() {
