e90a2b0af9b4f399637a4ad06fc87fbb68e1c433
Joshua Casey <jcasey@pivotal.io>
2020-04-26 19:38:34 -0500

    Refactor - JdbcUnsuccessfulLoginCountingAuditService
    - Apply IntelliJ autoformat
    
    [nostory]

--- server/src/main/java/org/cloudfoundry/identity/uaa/audit/JdbcUnsuccessfulLoginCountingAuditService.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/audit/JdbcUnsuccessfulLoginCountingAuditService.java
@@ -1,15 +1,3 @@
-/*******************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.audit;
 
 import org.cloudfoundry.identity.uaa.util.TimeService;
@@ -30,7 +18,7 @@ import static org.cloudfoundry.identity.uaa.audit.AuditEventType.UserAuthenticat
 public class JdbcUnsuccessfulLoginCountingAuditService extends JdbcAuditService {
 
     private int saveDataPeriodMillis = 24 * 3600 * 1000; // 24hr
-    private long timeBetweenDeleteMillis = 1000*30;
+    private long timeBetweenDeleteMillis = 1000 * 30;
 
     private AtomicLong lastDelete = new AtomicLong(0);
     private TimeService timeService = new TimeServiceImpl();
@@ -85,8 +73,8 @@ public class JdbcUnsuccessfulLoginCountingAuditService extends JdbcAuditService 
         long lastCheck = lastDelete.get();
         if (now - lastCheck > timeBetweenDeleteMillis && lastDelete.compareAndSet(lastCheck, now)) {
             getJdbcTemplate().update("delete from sec_audit where created < ?",
-                                     new Timestamp(System.currentTimeMillis()
-                                                       - saveDataPeriodMillis));
+                    new Timestamp(System.currentTimeMillis()
+                            - saveDataPeriodMillis));
         }
     }
 
