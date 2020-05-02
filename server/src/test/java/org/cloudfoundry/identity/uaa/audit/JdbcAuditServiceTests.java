e58dfb6b99d085385be624ed658a0ecd3a6f92a5
Joshua Casey <jcasey@pivotal.io>
2020-05-02 16:34:04 -0500

    Test Refactor - JdbcAuditServiceTests
    - Apply IntelliJ autoformat
    
    [nostory]

--- server/src/test/java/org/cloudfoundry/identity/uaa/audit/JdbcAuditServiceTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/audit/JdbcAuditServiceTests.java
@@ -1,15 +1,3 @@
-/*******************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.audit;
 
 import org.cloudfoundry.identity.uaa.test.JdbcTestBase;
@@ -24,6 +12,7 @@ import java.util.List;
 import static org.cloudfoundry.identity.uaa.audit.AuditEventType.PrincipalAuthenticationFailure;
 import static org.cloudfoundry.identity.uaa.audit.AuditEventType.UserAuthenticationFailure;
 import static org.junit.Assert.assertEquals;
+
 public class JdbcAuditServiceTests extends JdbcTestBase {
 
     private JdbcAuditService auditService;
