59cbfbd4b21dbb51f6808cb0fc24b02e6c7611a1
Joshua Casey <jcasey@pivotal.io>
2020-05-02 13:23:20 -0500

    Test Refactor - TestDataSourcePool
    - Apply IntelliJ autoformat
    
    [nostory]

--- server/src/test/java/org/cloudfoundry/identity/uaa/db/TestDataSourcePool.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/db/TestDataSourcePool.java
@@ -1,23 +1,16 @@
-/**
- * 
- */
 package org.cloudfoundry.identity.uaa.db;
-
-import static org.junit.Assert.*;
 
 import org.cloudfoundry.identity.uaa.test.JdbcTestBase;
 import org.junit.Test;
 
-/**
- * @author nmalp
- *
- */
+import static org.junit.Assert.assertEquals;
+
 public class TestDataSourcePool extends JdbcTestBase {
 
-	@Test
-	public void testValidationQuery() {
-		int i = jdbcTemplate.queryForObject(this.validationQuery, Integer.class);
-		assertEquals(1, i);
-	}
+    @Test
+    public void testValidationQuery() {
+        int i = jdbcTemplate.queryForObject(this.validationQuery, Integer.class);
+        assertEquals(1, i);
+    }
 
 }
