1ad1a145c2177c381d5c4479d7f4220408fe0055
Joshua Casey <jcasey@pivotal.io>
2020-05-02 16:51:57 -0500

    Test Refactor - GroupMembershipAuthoritiesNullableTest
    - Apply IntelliJ autoformat
    
    [nostory]

--- server/src/test/java/org/cloudfoundry/identity/uaa/db/GroupMembershipAuthoritiesNullableTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/db/GroupMembershipAuthoritiesNullableTest.java
@@ -1,17 +1,3 @@
-/*
- * ****************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2017] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- * ****************************************************************************
- */
 package org.cloudfoundry.identity.uaa.db;
 
 import org.cloudfoundry.identity.uaa.test.JdbcTestBase;
@@ -31,13 +17,12 @@ public class GroupMembershipAuthoritiesNullableTest extends JdbcTestBase {
     public void testAuthoritiesNullable() throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
             DatabaseMetaData meta = connection.getMetaData();
-            String profile = environment.getProperty("spring.profiles.active");
             ResultSet rs;
             rs = meta.getColumns(connection.getCatalog(), null, null, null);
             boolean call = false;
-            while(rs.next()) {
-                if("GROUP_MEMBERSHIP".equalsIgnoreCase(rs.getString("TABLE_NAME")) &&
-                    "AUTHORITIES".equalsIgnoreCase(rs.getString("COLUMN_NAME"))) {
+            while (rs.next()) {
+                if ("GROUP_MEMBERSHIP".equalsIgnoreCase(rs.getString("TABLE_NAME")) &&
+                        "AUTHORITIES".equalsIgnoreCase(rs.getString("COLUMN_NAME"))) {
                     call = true;
                     assertEquals("YES", rs.getString("IS_NULLABLE").toUpperCase());
                 }
