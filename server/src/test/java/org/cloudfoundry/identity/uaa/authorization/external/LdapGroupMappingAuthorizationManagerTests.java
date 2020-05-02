c6557f5062d5fa59fd9bb697205660339af23e16
Joshua Casey <jcasey@pivotal.io>
2020-05-02 16:43:26 -0500

    Test Refactor - LdapGroupMappingAuthorizationManagerTests
    - Apply IntelliJ autoformat
    
    [nostory]

--- server/src/test/java/org/cloudfoundry/identity/uaa/authorization/external/LdapGroupMappingAuthorizationManagerTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/authorization/external/LdapGroupMappingAuthorizationManagerTests.java
@@ -1,15 +1,3 @@
-/*******************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.authorization.external;
 
 import org.cloudfoundry.identity.uaa.authorization.LdapGroupMappingAuthorizationManager;
@@ -56,14 +44,14 @@ public class LdapGroupMappingAuthorizationManagerTests extends JdbcTestBase {
     private Set<? extends GrantedAuthority> ldapGroups;
 
     private LdapAuthority
-        la1 = new LdapAuthority("engineering","cn=Engineering,ou=groups,dc=example,dc=com"),
-        la2 = new LdapAuthority("HR","cn=HR,ou=groups,dc=example,dc=com"),
-        la3 = new LdapAuthority("mgmt","cn=mgmt,ou=groups,dc=example,dc=com");
+            la1 = new LdapAuthority("engineering", "cn=Engineering,ou=groups,dc=example,dc=com"),
+            la2 = new LdapAuthority("HR", "cn=HR,ou=groups,dc=example,dc=com"),
+            la3 = new LdapAuthority("mgmt", "cn=mgmt,ou=groups,dc=example,dc=com");
 
     private SimpleGrantedAuthority
-        sa1 = new SimpleGrantedAuthority("acme"),
-        sa2 = new SimpleGrantedAuthority("acme.dev"),
-        sa3 = new SimpleGrantedAuthority("acme.notmapped");
+            sa1 = new SimpleGrantedAuthority("acme"),
+            sa2 = new SimpleGrantedAuthority("acme.dev"),
+            sa3 = new SimpleGrantedAuthority("acme.notmapped");
 
     @Before
     public void initLdapGroupMappingAuthorizationManagerTests() throws Exception {
@@ -91,21 +79,21 @@ public class LdapGroupMappingAuthorizationManagerTests extends JdbcTestBase {
         bootstrap.setExternalGroupMaps(originMap);
         bootstrap.afterPropertiesSet();
 
-        ldapGroups = new HashSet<>(Arrays.asList(new LdapAuthority[] {la1,la2,la3}));
-        nonLdapGroups = new HashSet<>(Arrays.asList(new SimpleGrantedAuthority[] {sa1,sa2,sa3}));
+        ldapGroups = new HashSet<>(Arrays.asList(new LdapAuthority[]{la1, la2, la3}));
+        nonLdapGroups = new HashSet<>(Arrays.asList(new SimpleGrantedAuthority[]{sa1, sa2, sa3}));
     }
 
     @Test
     public void testAllLdapGroups() {
         Set<? extends GrantedAuthority> result = manager.findScopesFromAuthorities(ldapGroups);
-        String[] list = getAuthorities(Arrays.asList(sa1,sa2));
+        String[] list = getAuthorities(Arrays.asList(sa1, sa2));
         assertThat(list, arrayContainingInAnyOrder(getAuthorities(result)));
     }
 
     @Test
     public void testAllNonLdapGroups() {
         Set<? extends GrantedAuthority> result = manager.findScopesFromAuthorities(nonLdapGroups);
-        String[] list = getAuthorities(Arrays.asList(sa1,sa2,sa3));
+        String[] list = getAuthorities(Arrays.asList(sa1, sa2, sa3));
         assertThat(list, arrayContainingInAnyOrder(getAuthorities(result)));
     }
 
@@ -116,14 +104,14 @@ public class LdapGroupMappingAuthorizationManagerTests extends JdbcTestBase {
         mixed.add(sa3);
         mixed.add(la1);
         Set<? extends GrantedAuthority> result = manager.findScopesFromAuthorities(nonLdapGroups);
-        String[] list = getAuthorities(Arrays.asList(sa1,sa2,sa3));
+        String[] list = getAuthorities(Arrays.asList(sa1, sa2, sa3));
         assertThat(list, arrayContainingInAnyOrder(getAuthorities(result)));
     }
 
     public String[] getAuthorities(Collection<? extends GrantedAuthority> authorities) {
-        String[] result = new String[authorities!=null?authorities.size():0];
-        if (result.length>0) {
-            int index=0;
+        String[] result = new String[authorities != null ? authorities.size() : 0];
+        if (result.length > 0) {
+            int index = 0;
             for (GrantedAuthority a : authorities) {
                 result[index++] = a.getAuthority();
             }
