b8560ed78d900ed54918532c1e02525bdab019c7
Markus Strehle <markus.strehle@sap.com>
2020-06-08 17:47:38 +0200

    test for issue https://github.com/cloudfoundry/uaa/issues/1318

--- server/src/test/java/org/cloudfoundry/identity/uaa/zone/MultitenantJdbcClientDetailsServiceTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/zone/MultitenantJdbcClientDetailsServiceTests.java
@@ -207,6 +207,12 @@ class MultitenantJdbcClientDetailsServiceTests {
     }
 
     @Test
+    void loadingClientForNullByteClientId() {
+        assertThrows(NoSuchClientException.class,
+            () -> service.loadClientByClientId("nonExistingClientIdD".replace('D', '\u0000')));
+    }
+
+    @Test
     void loadingClientIdWithNoDetails() {
         int rowsInserted = jdbcTemplate.update(INSERT_SQL,
                 "clientIdWithNoDetails", null, null,
