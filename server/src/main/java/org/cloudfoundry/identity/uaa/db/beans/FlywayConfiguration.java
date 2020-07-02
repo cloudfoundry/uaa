7bac40bb265c47480a5e61004c68bcd84f8b3777
Samuel Waggoner <swaggoner@pivotal.io>
2020-07-02 16:28:03 -0700

    Format file
    [#172504836]
    
    Signed-off-by: Ryker Reed <rreed@pivotal.io>
    Co-authored-by: Ryker Reed <rreed@pivotal.io>

--- server/src/main/java/org/cloudfoundry/identity/uaa/db/beans/FlywayConfiguration.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/db/beans/FlywayConfiguration.java
@@ -16,72 +16,74 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 @Configuration
 public class FlywayConfiguration {
 
+  /**
+   * In Flyway 5, the default version table name changed to flyway_schema_history
+   * https://flywaydb.org/documentation/releaseNotes#5.0.0
+   * https://github.com/flyway/flyway/issues/1848
+   * <p>
+   * We need to maintain backwards compatibility due to {@link FixFailedBackportMigrations_4_0_4}
+   */
+  static final String VERSION_TABLE = "schema_version";
+
+  @Bean
+  public Flyway baseFlyway(
+      DataSource dataSource,
+      DataSourceAccessor dataSourceAccessor,
+      @Qualifier("platform") String platform) {
+    Flyway flyway = Flyway.configure()
+        .baselineOnMigrate(true)
+        .dataSource(dataSource)
+        .locations("classpath:org/cloudfoundry/identity/uaa/db/" + platform + "/")
+        .baselineVersion("1.5.2")
+        .validateOnMigrate(false)
+        .table(VERSION_TABLE)
+        .load();
+    flyway.repair();
+    flyway.migrate();
+    return flyway;
+  }
+
+  @Configuration
+  @Conditional(FlywayConfigurationWithMigration.ConfiguredWithMigrations.class)
+  public static class FlywayConfigurationWithMigration {
+    static class ConfiguredWithMigrations implements Condition {
+
+      @Override
+      public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        return Boolean
+            .parseBoolean(context.getEnvironment().getProperty("uaa.migrationsEnabled", "true"));
+      }
+    }
+
     /**
-     * In Flyway 5, the default version table name changed to flyway_schema_history
-     * https://flywaydb.org/documentation/releaseNotes#5.0.0
-     * https://github.com/flyway/flyway/issues/1848
-     *
-     * We need to maintain backwards compatibility due to {@link FixFailedBackportMigrations_4_0_4}
+     * @param dataSourceAccessor This bean does NOT need need an instance of {@link DataSourceAccessor}.
+     *                           However, other Flyway objects (example {@link V1_5_3__InitialDBScript}
+     *                           DO make use of {@link DataSourceAccessor}
      */
-    static final String VERSION_TABLE = "schema_version";
+    @Bean
+    public Flyway flyway(@Qualifier("baseFlyway") Flyway flyway) {
+      flyway.repair();
+      flyway.migrate();
+      return flyway;
+    }
+  }
+
+  @Configuration
+  @Conditional(FlywayConfigurationWithoutMigrations.ConfiguredWithoutMigrations.class)
+  static class FlywayConfigurationWithoutMigrations {
+
+    static class ConfiguredWithoutMigrations implements Condition {
+
+      @Override
+      public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        return !Boolean
+            .parseBoolean(context.getEnvironment().getProperty("uaa.migrationsEnabled", "true"));
+      }
+    }
 
     @Bean
-    public Flyway baseFlyway(
-        DataSource dataSource,
-        DataSourceAccessor dataSourceAccessor,
-        @Qualifier("platform") String platform) {
-        Flyway flyway = Flyway.configure()
-            .baselineOnMigrate(true)
-            .dataSource(dataSource)
-            .locations("classpath:org/cloudfoundry/identity/uaa/db/" + platform + "/")
-            .baselineVersion("1.5.2")
-            .validateOnMigrate(false)
-            .table(VERSION_TABLE)
-            .load();
-        flyway.repair();
-        flyway.migrate();
-        return flyway;
+    public Flyway flyway(@Qualifier("baseFlyway") Flyway flyway) {
+      return flyway;
     }
-
-    @Configuration
-    @Conditional(FlywayConfigurationWithMigration.ConfiguredWithMigrations.class)
-    public static class FlywayConfigurationWithMigration {
-        static class ConfiguredWithMigrations implements Condition {
-
-            @Override
-            public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-                return Boolean.parseBoolean(context.getEnvironment().getProperty("uaa.migrationsEnabled", "true"));
-            }
-        }
-
-        /**
-         * @param dataSourceAccessor This bean does NOT need need an instance of {@link DataSourceAccessor}.
-         *                           However, other Flyway objects (example {@link V1_5_3__InitialDBScript}
-         *                           DO make use of {@link DataSourceAccessor}
-         */
-        @Bean
-        public Flyway flyway(@Qualifier("baseFlyway") Flyway flyway) {
-            flyway.repair();
-            flyway.migrate();
-            return flyway;
-        }
-    }
-
-    @Configuration
-    @Conditional(FlywayConfigurationWithoutMigrations.ConfiguredWithoutMigrations.class)
-    static class FlywayConfigurationWithoutMigrations {
-
-        static class ConfiguredWithoutMigrations implements Condition {
-
-            @Override
-            public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-                return !Boolean.parseBoolean(context.getEnvironment().getProperty("uaa.migrationsEnabled", "true"));
-            }
-        }
-
-        @Bean
-        public Flyway flyway(@Qualifier("baseFlyway") Flyway flyway) {
-            return flyway;
-        }
-    }
+  }
 }
