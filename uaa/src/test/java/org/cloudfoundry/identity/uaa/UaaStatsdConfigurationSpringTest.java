2b5024f9fe632aa9bfbbd23dcff16414b4bd965c
Ryker Reed <rreed@pivotal.io>
2020-07-09 16:25:44 -0700

    Fix tests broken by previous changes
    We removed some beans that were only relied upon within tests.
    We have shifted this dependency to be on the ✨*~actually required~*✨ class
    that wraps the previous beans.
    
    [#173728966]
    
    Signed-off-by: Samuel Waggoner <swaggoner@pivotal.io>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/UaaStatsdConfigurationSpringTest.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/UaaStatsdConfigurationSpringTest.java
@@ -1,6 +1,10 @@
 package org.cloudfoundry.identity.uaa;
 
-import com.timgroup.statsd.StatsDClient;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+
+import org.cloudfoundry.identity.statsd.UaaMetricsEmitter;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,9 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 class UaaStatsdConfigurationSpringTest {
 
@@ -20,8 +21,8 @@ class UaaStatsdConfigurationSpringTest {
     @TestPropertySource(properties = "statsd.enabled=true")
     class WithStatsdEnabled {
         @Test
-        void statsdIsNotAvailable(@Autowired(required = false) StatsDClient statsDClient) {
-            assertNotNull(statsDClient, "statsDClient must be available");
+        void statsdIsNotAvailable(@Autowired(required = false) UaaMetricsEmitter statsDClientWrapper) {
+            assertNotNull(statsDClientWrapper, "statsDClientWrapper must be available");
         }
     }
 
@@ -31,8 +32,8 @@ class UaaStatsdConfigurationSpringTest {
     @TestPropertySource(properties = "statsd.enabled=baz")
     class WithStatsdExplicitlyDisabled {
         @Test
-        void statsdIsNotAvailable(@Autowired(required = false) StatsDClient statsDClient) {
-            assertNull(statsDClient, "statsDClient must not be available");
+        void statsdIsNotAvailable(@Autowired(required = false) UaaMetricsEmitter statsDClientWrapper) {
+            assertNull(statsDClientWrapper, "statsDClientWrapper must not be available");
         }
     }
 
@@ -41,8 +42,8 @@ class UaaStatsdConfigurationSpringTest {
     @ContextConfiguration(classes = UaaStatsdConfiguration.class)
     class WithStatsdDisabledByDefault {
         @Test
-        void statsdIsNotAvailable(@Autowired(required = false) StatsDClient statsDClient) {
-            assertNull(statsDClient, "statsDClient must not be available");
+        void statsdIsNotAvailable(@Autowired(required = false) UaaMetricsEmitter statsDClientWrapper) {
+            assertNull(statsDClientWrapper, "statsDClientWrapper must not be available");
         }
     }
 
