211d417bd7a5ef1dc94cee7694eaf2cbb76de543
aram price <pricear@vmware.com>
2020-08-05 16:00:26 -0700

    Ensure we build each time
    Co-authored-by: aram price <aprice@pivotal.io>

--- server/migrations/push_image.sh
+++ server/migrations/push_image.sh
@@ -6,7 +6,7 @@ UAA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 DOCKER_ROOT="${UAA_DIR}/server/migrations"
 DOCKER_IMAGE="${IMAGE_TAG:-"swaggoner/uaa_flyway_migrations"}"
 
-#"${UAA_DIR}/scripts/gradle" clean shadowJar
+"${UAA_DIR}/scripts/gradle" clean shadowJar
 
 cp -v "${UAA_DIR}/server/build/libs/UAA-FlywayMigrationRunner-0.0.0-all.jar" "${DOCKER_ROOT}"
 
