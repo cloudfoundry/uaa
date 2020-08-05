72ff040e273d5beb91b40df356401f8556c39a69
aram price <pricear@vmware.com>
2020-08-04 17:54:36 -0700

    Rework `push_image.sh`
    - move to `server/migrations`
    - allow image tag configuration via `DOCKER_IMAGE` env var
    - can now be runable from any location
    
    Co-authored-by: aram price <aprice@pivotal.io>

---
+++ server/migrations/push_image.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+UAA_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+
+DOCKER_ROOT="${UAA_DIR}/server/migrations"
+DOCKER_IMAGE="${IMAGE_TAG:-"swaggoner/uaa_flyway_migrations"}"
+
+#"${UAA_DIR}/scripts/gradle" clean shadowJar
+
+cp -v "${UAA_DIR}/server/build/libs/UAA-FlywayMigrationRunner-0.0.0-all.jar" "${DOCKER_ROOT}"
+
+docker build --tag ${DOCKER_IMAGE} "${DOCKER_ROOT}"
+
+#docker push "${DOCKER_IMAGE}"
