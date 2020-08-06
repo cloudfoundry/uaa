966a2dede095ea9dd0c6fa251bf65cf8d3d34874
Tim Hausler <thausler@pivotal.io>
2020-08-05 17:31:08 -0700

    Run migrations with params from ENV
    Signed-off-by: aram price <aprice@pivotal.io>
    Co-authored-by: aram price <aprice@pivotal.io>

--- server/src/main/java/org/cloudfoundry/identity/uaa/db/FlywayMigrationRunner.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/db/FlywayMigrationRunner.java
@@ -5,6 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class FlywayMigrationRunner {
 
   public static void main(String[] args) {
+    // TODO ensure that we do this safely and fail with a reasonable error
+    System.setProperty("database.username", System.getenv("DB_USERNAME"));
+    System.setProperty("database.password", System.getenv("DB_PASSWORD"));
+    System.setProperty("database.url", System.getenv("DB_URL"));
+    System.setProperty("spring.profiles.active", System.getenv("DB_SCHEME"));
     new ClassPathXmlApplicationContext(
         "spring/env.xml",
         "spring/data-source.xml",
