40c747dca151045479763042cf99273e26f81e67
aram price <pricear@vmware.com>
2020-08-24 16:07:36 -0700

    Test serialization of UaaAuthentication.authenticatedTime

--- server/src/test/java/org/cloudfoundry/identity/uaa/authentication/UaaAuthenticationSerializerDeserializerTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/authentication/UaaAuthenticationSerializerDeserializerTest.java
@@ -19,6 +19,7 @@ public class UaaAuthenticationSerializerDeserializerTest {
         UaaAuthentication auth = new UaaAuthentication(p, UaaAuthority.USER_AUTHORITIES, new UaaAuthenticationDetails(false, "clientId", OriginKeys.ORIGIN,"sessionId"));
         auth.setAuthenticationMethods(Collections.singleton("pwd"));
         auth.setAuthContextClassRef(Collections.singleton("test:uri"));
+        auth.setAuthenticatedTime(1485314434675L);
         auth.setLastLoginSuccessTime(1485305759366l);
 
         UaaAuthentication deserializedUaaAuthentication = JsonUtils.readValue(JsonUtils.writeValueAsString(auth), UaaAuthentication.class);
