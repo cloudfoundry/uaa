64772d279c1618debdfb600842d82621bf143927
aram price <pricear@vmware.com>
2020-08-28 11:43:31 -0700

    Rename `scripts/uaa.log.sh` => `scripts/tail-uaa-log`
    simplify implementation

--- scripts/uaa.log.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-UAA_LOG=$(grep --text /webapps/uaa.war uaa/build/reports/tests/uaa-server.log | cut -d "[" -f2 | cut -d "]" -f1 | sed 's/webapps\/uaa.war/logs\/uaa.log/')
-
-echo $UAA_LOG
-
-tail -f $UAA_LOG
