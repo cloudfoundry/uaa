a9c68d86475dac8bae61f34f5570577dc92d894d
Jeremy Morony <jmorony@pivotal.io>
2020-08-28 14:25:43 -0700

    collect more metrics

--- server/src/main/java/org/cloudfoundry/identity/uaa/web/beans/UaaMemorySessionConfig.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/web/beans/UaaMemorySessionConfig.java
@@ -9,9 +9,13 @@ import org.springframework.context.annotation.*;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.lang.NonNull;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.session.MapSession;
 import org.springframework.session.MapSessionRepository;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+
+import java.util.HashMap;
 
 @Configuration
 @Conditional(UaaMemorySessionConfig.MemoryConfigured.class)
@@ -36,16 +40,28 @@ public class UaaMemorySessionConfig extends UaaSessionConfig {
             @Autowired PurgeableSessionMap purgeableSessionMap
     ) {
         MapSessionRepository sessionRepository = new MapSessionRepository(purgeableSessionMap) {
+            private HashMap<Class, Integer> stats = new HashMap<>();
+
             @Override
             public void save(MapSession session) {
                 super.save(session);
-                if (session.getAttribute(SessionUtils.SPRING_SECURITY_CONTEXT) != null) {
-                    logger.info("********we have a spring sec context");
+
+                SecurityContext securityContext = (SecurityContext) session.getAttribute(SessionUtils.SPRING_SECURITY_CONTEXT);
+                if (securityContext != null) {
+                    Authentication authentication = securityContext.getAuthentication();
+                    if (authentication != null) {
+                        Integer usages = stats.getOrDefault(authentication.getClass(), 0);
+                        stats.put(authentication.getClass(), usages + 1);
+                    }
                 }
             }
 
             public void destroy() {
-                logger.info("****************** destroyed");
+                logger.info("******************");
+                stats.keySet().forEach(c -> {
+                    logger.info(String.format("*** %s: %d", c.getCanonicalName(), stats.get(c)));
+                });
+                logger.info("******************");
             }
         };
         sessionRepository.setDefaultMaxInactiveInterval(idleTimeout);
