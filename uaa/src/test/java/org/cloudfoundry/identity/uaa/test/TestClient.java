44c9bd13ba20bae0bbc0e69832bb0c5112aa917a
UAA Team <pcf-uaa@pivotal.io>
2020-11-02 11:58:22 -0800

    Revert "Add logging to saml key rotation tests and testClient in order to debug intermittent unit test failures in the pipeline. [#175443804]"
    This reverts commit 7cbcce104f0537351817e48450ab0470e9cdaf6c.

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/test/TestClient.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/test/TestClient.java
@@ -22,32 +22,25 @@ public class TestClient {
     }
 
     public String getClientCredentialsOAuthAccessToken(String clientId, String clientSecret, String scope) throws Exception {
-        System.out.println("in the oauth client creds token fn in testclient.java constructor without subdomain");
         return getClientCredentialsOAuthAccessToken(clientId, clientSecret, scope, null);
     }
 
     public String getClientCredentialsOAuthAccessToken(String clientId, String clientSecret, String scope, String subdomain)
         throws Exception {
-        System.out.println("in the main oauth client creds token fn in testclient.java");
         String basicDigestHeaderValue = "Basic "
             + new String(Base64.encodeBase64((clientId + ":" + clientSecret).getBytes()));
-        System.out.println("in the main oauth client creds token fn in testclient.java 222");
         MockHttpServletRequestBuilder oauthTokenPost = post("/oauth/token")
             .header("Authorization", basicDigestHeaderValue)
             .param("grant_type", "client_credentials")
             .param("client_id", clientId)
             .param(TokenConstants.REQUEST_TOKEN_FORMAT, OPAQUE.getStringValue())
             .param("scope", scope);
-        System.out.println("in the main oauth client creds token fn in testclient.java 333");
         if (subdomain != null && !subdomain.equals(""))
             oauthTokenPost.with(new SetServerNameRequestPostProcessor(subdomain + ".localhost"));
-        System.out.println("in the main oauth client creds token fn in testclient.java 444");
         MvcResult result = mockMvc.perform(oauthTokenPost)
             .andExpect(status().isOk())
             .andReturn();
-        System.out.println("in the main oauth client creds token fn in testclient.java 555");
         OAuthToken oauthToken = JsonUtils.readValue(result.getResponse().getContentAsString(), OAuthToken.class);
-        System.out.println("in the main oauth client creds token fn in testclient.java 666");
         return oauthToken.accessToken;
     }
 
