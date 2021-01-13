10cf8db2b38f3dba591688a085ef44cf002f0aba
Markus Strehle <markus.strehle@sap.com>
2021-01-13 11:38:07 +0100

    Rework jasmine-test.sh after PR #1435
    The test run showed, the the needed docker image is used, but
    the ./scripts/jasmine-test.sh script is executed locally in the worker.
    The worker has installed locally node 8.x with npm 5.x. The errors shows this.
    The docker image cfidentity/uaa-singular has node 12.x with npm 6.4.
    
    Finally: error occurs now, because new jasmine depends on newer node, e.g. node 10 or high.
    Before the local test was working also with a node 8.x
    
    Ensure that jasmine-test.sh check if it runs in docker and if not, start itself in cfidentity/uaa-singular
    
    npm ci should be used instead npm install therefore update package-lock.json

--- scripts/jasmine-tests.sh
+++ scripts/jasmine-tests.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 set -xeu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONTAINER_SCRIPT_DIR='/root/uaa'
+DOCKER_IMAGE='cfidentity/uaa-singular'
 
-pushd ${SCRIPT_DIR}/../uaa
-  npm install
-  npm test
-popd
+if grep -q docker /proc/1/cgroup; then
+   echo inside docker
+   pushd ${CONTAINER_SCRIPT_DIR}/uaa
+   npm ci
+   npm test
+   popd
+else
+   echo on host
+   pushd ${SCRIPT_DIR}
+   docker run \
+  --privileged \
+  --tty \
+  --interactive \
+  --volume "${SCRIPT_DIR}/..":"${CONTAINER_SCRIPT_DIR}" \
+   "${DOCKER_IMAGE}" \
+   /root/uaa/scripts/jasmine-tests.sh
+   popd
+fi
