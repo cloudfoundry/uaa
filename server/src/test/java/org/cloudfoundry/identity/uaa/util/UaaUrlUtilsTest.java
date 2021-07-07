de0f664d4c4c0b5b2a2ed1554fba7bba093da74a
Hongchol Sinn <hsinn@vmware.com>
2021-07-07 15:48:11 -0700

    fix: Backport Open Redirect Security Issue fix
    - For logout.do, etc.
    
    Co-authored-by: Cristhian Peña <cpena@vmware.com>
    
    [#178677678]

--- server/src/test/java/org/cloudfoundry/identity/uaa/util/UaaUrlUtilsTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/util/UaaUrlUtilsTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -322,6 +324,65 @@ class UaaUrlUtilsTest {
         String fallback = "http://fallback.to/this";
         String matchingRedirectUri5 = UaaUrlUtils.findMatchingRedirectUri(Collections.singleton(pattern3), redirect5, fallback);
         assertThat(matchingRedirectUri5, equalTo(fallback));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "http://example.com/*, http://example.com/?param=value",
+            "http://example.com/*, http://example.com/page#1",
+            "http://example.com/**/mypage*, http://example.com/a/b/mypage?a=b",
+            "http://abc?.example.com, http://abcd.example.com",
+            "http://www.*.example.com, http://www.tv.example.com",
+            "a/**, a/b/c",
+            "a/b/*, a/b/c",
+            "ab?/*, abc/def",
+            "/abc/*, /abc/ab",
+            "http://foo.bar.com:8080, http://foo.bar.com:8080",
+            "http://foo.bar.com:8080/**, http://foo.bar.com:8080/app/foo",
+            "http://*.bar.com:8080/**, http://foo.bar.com:8080/app/foo",
+            "http://*.bar.com*, http://foo.bar.com:80",
+            "https://*.bar.com*, https://foo.bar.com:80"
+    })
+    void findMatchingRedirectUri_urlParametersShouldResolveInIncomingUrl(
+            String allowedRedirectUrl,
+            String incomingRedirectUrl) {
+        final String fallbackRedirectUrl = "http://fallback.to/this";
+        Set<String> allowedRedirectUrlGlobPatterns = Collections.singleton(allowedRedirectUrl);
+
+        assertEquals(incomingRedirectUrl, UaaUrlUtils.findMatchingRedirectUri(
+                allowedRedirectUrlGlobPatterns,
+                incomingRedirectUrl,
+                fallbackRedirectUrl
+        ));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "http://*.example.com, http://attacker.com?.example.com",
+            "http://*.example.com, http://attacker.com\\.example.com",
+            "http://*.example.com, http://attacker.com/.example.com",
+            "http://*.example.com, http://attacker.com#.example.com",
+            "http://example.com, http://tv.example.com",
+            "http://www.*.example.com, http://www.attacker.com?.example.com",
+            "a/**/c, a/b/c/d",
+            "a/b/*, a/b/c/d",
+            "ab?/*, abcd/ef",
+            "/abc/*, a/abc/ab",
+            "http://*.bar.com:8080, http://attacker.com?.bar.com:8080",
+            "http://*.bar.com:8080/**, http://attacker.com#foo.bar.com:8080/app/foo",
+            "https://*.bar.com:8080/**, https://attacker.com#foo.bar.com:8080/app/foo"
+    })
+    void findMatchingRedirectUri_badRedirectUrlShouldResolveInFallbackUrl(
+            String allowedRedirectUrl,
+            String incomingMaliciousRedirectUrl) {
+        final String fallbackRedirectUrl = "http://fallback.to/this";
+        Set<String> allowedRedirectUrlGlobPatterns = Collections.singleton(allowedRedirectUrl);
+
+        assertEquals(fallbackRedirectUrl, UaaUrlUtils.findMatchingRedirectUri(
+                allowedRedirectUrlGlobPatterns,
+                incomingMaliciousRedirectUrl,
+                fallbackRedirectUrl
+        ));
     }
 
     @Test
