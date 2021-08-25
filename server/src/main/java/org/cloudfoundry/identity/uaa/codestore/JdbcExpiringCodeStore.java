50d933b1509c68305bacaea7d1519cd6f501a80c
Anoop Karollil <anoop.karollil@ge.com>
2021-08-25 15:09:32 -0700

    Refactor common code in JdbcExpiringCodeStore functions
    peek and retrieve code functions are exactly the same except for
    database update being done only when retrieving and not when peeking.

--- server/src/main/java/org/cloudfoundry/identity/uaa/codestore/JdbcExpiringCodeStore.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/codestore/JdbcExpiringCodeStore.java
@@ -111,27 +111,7 @@ public class JdbcExpiringCodeStore implements ExpiringCodeStore {
         return null;
     }
 
-    @Override
-    public ExpiringCode peekCode(String code, String zoneId) {
-        cleanExpiredEntries();
-
-        if (code == null) {
-            throw new NullPointerException();
-        }
-
-        try {
-            ExpiringCode expiringCode = jdbcTemplate.queryForObject(selectAllFields, rowMapper, code, zoneId);
-            if (expiringCode != null && expiringCode.getExpiresAt().getTime() < timeService.getCurrentTimeMillis()) {
-                expiringCode = null;
-            }
-            return expiringCode;
-        } catch (EmptyResultDataAccessException x) {
-            return null;
-        }
-    }
-
-    @Override
-    public ExpiringCode retrieveCode(String code, String zoneId) {
+    private ExpiringCode getCode(String code, String zoneId, boolean peek) {
         cleanExpiredEntries();
 
         if (code == null) {
@@ -141,7 +121,9 @@ public class JdbcExpiringCodeStore implements ExpiringCodeStore {
         try {
             ExpiringCode expiringCode = jdbcTemplate.queryForObject(selectAllFields, rowMapper, code, zoneId);
             if (expiringCode != null) {
-                jdbcTemplate.update(delete, code, zoneId);
+                if (!peek) {
+                    jdbcTemplate.update(delete, code, zoneId);
+                }
                 if (expiringCode.getExpiresAt().getTime() < timeService.getCurrentTimeMillis()) {
                     expiringCode = null;
                 }
@@ -150,6 +132,16 @@ public class JdbcExpiringCodeStore implements ExpiringCodeStore {
         } catch (EmptyResultDataAccessException x) {
             return null;
         }
+    }
+
+    @Override
+    public ExpiringCode peekCode(String code, String zoneId) {
+        return getCode(code, zoneId, true);
+    }
+
+    @Override
+    public ExpiringCode retrieveCode(String code, String zoneId) {
+        return getCode(code, zoneId, false);
     }
 
     @Override
