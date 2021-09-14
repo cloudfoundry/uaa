51821a2eaa1a2663611230a9229fb59fe91d2efa
Markus Strehle <markus.strehle@sap.com>
2021-09-14 17:43:34 +0200

    Fix issue #1628
    Add character validation for client_id parameter, forbid now / and \ because they will break its REST api

--- server/src/main/java/org/cloudfoundry/identity/uaa/client/ClientAdminEndpointsValidator.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/client/ClientAdminEndpointsValidator.java
@@ -75,6 +75,8 @@ public class ClientAdminEndpointsValidator implements InitializingBean, ClientDe
 
     private Set<String> reservedClientIds = StringUtils.commaDelimitedListToSet(OriginKeys.UAA);
 
+    private final Set<Character> invalidClientIdsCharacters = Set.of('/', '\\');
+
     public ClientAdminEndpointsValidator(final SecurityContextAccessor securityContextAccessor) {
         this.securityContextAccessor = securityContextAccessor;
     }
@@ -111,8 +113,13 @@ public class ClientAdminEndpointsValidator implements InitializingBean, ClientDe
 
         client.setAdditionalInformation(prototype.getAdditionalInformation());
         String clientId = client.getClientId();
-        if (create && reservedClientIds.contains(clientId)) {
-            throw new InvalidClientDetailsException("Not allowed: " + clientId + " is a reserved client_id");
+        if (create) {
+            if (reservedClientIds.contains(clientId)) {
+                throw new InvalidClientDetailsException("Not allowed: " + clientId + " is a reserved client_id");
+            }
+            if (invalidClientIdsCharacters.stream().filter(c -> clientId.indexOf(c) > -1).findAny().isPresent()) {
+                throw new InvalidClientDetailsException("Not allowed characters: " + clientId + " must not contain any of " + invalidClientIdsCharacters);
+            }
         }
 
         validateClientRedirectUri(client);
