c6223514fffb18be116a318fb7598dc49ff3f2d1
Hongchol Sinn <hsinn@vmware.com>
2021-12-08 10:31:42 -0800

    Upgrade Flyway to 7
    - borrowed migration syntax from: https://github.com/cloudfoundry/credhub/pull/183/commits/11c4051fb1784303941d62ff1e8c145bbeb0be08
    (the gist is that the parameter for `migrate` method in these migration
    files is now Flyway `Context`, so we just have to obtain the JDBC template or
    connection from the `Context`.
    - renamed some classes because Flyway 7 now requires the migration class
      that extends the `BaseJavaMigration` to start with letters like `V` +
      some version strings
    
    [#179739511]
    
    Co-authored-by: Peter Chen <peterch@vmware.com>

--- server/src/main/java/org/cloudfoundry/identity/uaa/db/mysql/V4_9_2__AddPrimaryKeysIfMissing.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/db/mysql/V4_9_2__AddPrimaryKeysIfMissing.java
@@ -1,16 +1,20 @@
 package org.cloudfoundry.identity.uaa.db.mysql;
 
-import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 
-public class V4_9_2__AddPrimaryKeysIfMissing implements SpringJdbcMigration {
+public class V4_9_2__AddPrimaryKeysIfMissing extends BaseJavaMigration {
 
     private final String checkPrimaryKeyExists = "SELECT COUNT(*) FROM information_schema.KEY_COLUMN_USAGE WHERE TABLE_SCHEMA = ? AND TABLE_NAME = ? AND CONSTRAINT_NAME = 'PRIMARY'";
 
     @Override
-    public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
+    public void migrate(Context context) throws Exception {
         String[] tables = {"group_membership", "external_group_mapping", "oauth_code", "sec_audit"};
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(new SingleConnectionDataSource(
+                context.getConnection(), true));
         for (String table : tables) {
             int count = jdbcTemplate.queryForObject(checkPrimaryKeyExists, Integer.class, jdbcTemplate.getDataSource().getConnection().getCatalog(), table);
             if (count == 0) {
