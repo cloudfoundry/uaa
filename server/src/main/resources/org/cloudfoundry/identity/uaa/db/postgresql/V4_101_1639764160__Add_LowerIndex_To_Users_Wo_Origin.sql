0fca94861c34193b01a08411330d9c3e818c86c7
Torsten Luh <torsten-sap@users.noreply.github.com>
2021-12-20 08:47:49 +0100

    Update V4_101_1639764160__Add_LowerIndex_To_Users_Wo_Origin.sql

--- server/src/main/resources/org/cloudfoundry/identity/uaa/db/postgresql/V4_101_1639764160__Add_LowerIndex_To_Users_Wo_Origin.sql
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/postgresql/V4_101_1639764160__Add_LowerIndex_To_Users_Wo_Origin.sql
@@ -1,1 +1,1 @@
-CREATE INDEX concurrently IF NOT EXISTS users_key_lower_wo_origin ON users (LOWER(username),LOWER(identity_zone_id));
+CREATE INDEX CONCURRENTLY IF NOT EXISTS users_key_lower_wo_origin ON users (LOWER(username),LOWER(identity_zone_id));
