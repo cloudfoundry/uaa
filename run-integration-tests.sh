7343fec14d96357ed7df48e004ccbdf5ef41c3a2
Bruce Ricard <bricard@vmware.com>
2022-03-28 12:14:15 -0500

    refactor: use long parameter name
    Co-authored-by: Danny Faught <dfaught@vmware.com>

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -69,6 +69,6 @@ docker run --privileged --tty --interactive --shm-size=1G \
   --volume "${CONTAINER_GRADLE_LOCK_DIR}" \
   --env DB="${DB}" \
   --env RUN_TESTS="${RUN_TESTS:-true}" \
-  -p 8081:8080 \
+  --publish 8081:8080 \
   "${DOCKER_IMAGE}" \
   "${CONTAINER_SCRIPT_DIR}/integration-tests.sh" "${PROFILE_NAME},default"
