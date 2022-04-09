7dcdcce245da7f3ef1d2c248aba9da313c90c163
Hongchol Sinn <hsinn@vmware.com>
2022-04-08 17:53:53 -0700

    Backfill test for session page
    - Check for Javascript error when the HTML page is loaded
    
    [#179533204]
    
    Co-authored-by: Danny Faught <dfaught@vmware.com>

---
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/SessionControllerIntegrationTests.java
@@ -0,0 +1,60 @@
+package org.cloudfoundry.identity.uaa.integration;
+
+import org.cloudfoundry.identity.uaa.integration.feature.DefaultIntegrationTestConfig;
+import org.cloudfoundry.identity.uaa.integration.feature.TestClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.client.test.TestAccounts;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.logging.Level;
+
+import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = DefaultIntegrationTestConfig.class)
+public class SessionControllerIntegrationTests {
+    @Autowired
+    TestClient testClient;
+    @Autowired
+    TestAccounts testAccounts;
+    @Autowired
+    WebDriver webDriver;
+    @Value("${integration.test.base_url}")
+    String baseUrl;
+
+    @BeforeEach
+    @AfterEach
+    public void logout_and_clear_cookies() {
+        try {
+            webDriver.get(baseUrl + "/logout.do");
+        }catch (org.openqa.selenium.TimeoutException x) {
+            //try again - this should not be happening - 20 second timeouts
+            webDriver.get(baseUrl + "/logout.do");
+        }
+        webDriver.manage().deleteAllCookies();
+    }
+
+    @Test
+    public void sessionPageLoadedWithoutError() {
+        webDriver.get(baseUrl +
+                "/session?clientId=admin&messageOrigin=http://localhost:8080");
+
+        LogEntries entries = webDriver.manage().logs().get(LogType.BROWSER);
+        List<LogEntry> entryList = entries.getAll().stream()
+                .filter(entry -> entry.getLevel().intValue() >= Level.SEVERE.intValue())
+                .collect(toList());
+        assertTrue(entryList.isEmpty());
+    }
+}
