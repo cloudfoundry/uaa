21694bb99a1dddd2d3953d03a5a508eeeb437924
d036670 <markus.strehle@sap.com>
2022-05-18 19:09:04 +0200

    [WIP] test on session tracking mode

--- statsd-lib/src/main/java/org/cloudfoundry/identity/statsd/Log4jContextInitializer.java
+++ statsd-lib/src/main/java/org/cloudfoundry/identity/statsd/Log4jContextInitializer.java
@@ -18,6 +18,7 @@ import org.slf4j.MDC;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.EnumSet;
 
 /**
  * Simple context listener that adds an MDC entry for the context path. Can be referenced using <code>%X{context}</code>
@@ -34,6 +35,7 @@ public class Log4jContextInitializer implements ServletContextListener, Filter {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         MDC.put("context", sce.getServletContext().getContextPath());
+        sce.getServletContext().setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
     }
 
     @Override
