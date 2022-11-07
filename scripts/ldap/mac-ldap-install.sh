2b4d92dcbdd5c940f435b279e940a76da701ef03
Alicia Yingling <yalicia@vmware.com>
2022-11-07 16:33:35 -0600

    fix: providing mac setup for local ldap server instance
    - Allows quick install and startup of openldap with brew on Mac using minimal config
    
    Co-authored-by: Alex Rocha <alexr1@vmware.com>

--- scripts/ldap/mac-ldap-install.sh
+++ scripts/ldap/mac-ldap-install.sh
@@ -5,10 +5,3 @@ sudo cp -v ./slapd.conf /etc/openldap
 sudo mkdir -vp /var/lib/ldap
 sudo cp -v /private/etc/openldap/DB_CONFIG.example /var/lib/ldap
 
-# run ldap server with debug output enabled ( -d3 switch )
-if test "$1" == "debug"
-then
-  sudo /usr/libexec/slapd -d3
-else
-  sudo /usr/libexec/slapd
-fi
