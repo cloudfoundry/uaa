525c612d55463b0ccf2b3db1111efe028eee9aec
yogeshkumar <yogeshkumar.savita@ge.com>
2023-01-05 18:44:35 +0530

    Add configs for Orchestrator Integration Tests
    - Create spring beans of utility classes using spring component
      scan on java package in `iam-k8s-utils` git submodule repo.
    
    - Add environment variables configurations required by above
      utility beans for performing aws login and accessing kubernetes
      namespaces.

---
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/orchestrator/OrchestratorIntegrationTestConfig.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ *     Cloud Foundry
+ *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
+ *
+ *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
+ *     You may not use this product except in compliance with the License.
+ *
+ *     This product includes a number of subcomponents with
+ *     separate copyright notices and license terms. Your use of these
+ *     subcomponents is subject to the terms and conditions of the
+ *     subcomponent's license, as noted in the LICENSE file.
+ *******************************************************************************/
+package org.cloudfoundry.identity.uaa.orchestrator;
+
+import org.cloudfoundry.identity.uaa.NestedMapPropertySourceFactory;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@PropertySource(value = "classpath:orchestrator-integration.yml", factory = NestedMapPropertySourceFactory.class)
+@ComponentScan(basePackages = "com.ge.predix.iam.kubernetes")
+public class OrchestratorIntegrationTestConfig {
+
+}
