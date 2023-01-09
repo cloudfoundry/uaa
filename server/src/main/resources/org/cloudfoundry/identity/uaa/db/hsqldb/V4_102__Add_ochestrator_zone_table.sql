be7356aa71e7dab9727e67dd87e4a6a3a9a7aa18
Jeyaraj, Johnson <johnson.jeyaraj@ge.com>
2023-01-09 21:01:21 +0530

    Add flyway script to create orchestrator_zone table
    Add flyway script for postgresql and hsqldb to add orchestrator_zone
    table.
    Create unique key for orchestrator_zone_name to ensure only one entry
    for an orchestrator zone
    Also create unique key for identity_zone_id to make sure only one entry
     for an identity_zone. there should be one-to-one mapping between
     orchestrator_zone and identity_zone
    create a foreign key for faster retrieval with join query.

---
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/hsqldb/V4_102__Add_ochestrator_zone_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE orchestrator_zone (
+    id INTEGER IDENTITY NOT NULL PRIMARY KEY,
+    identity_zone_id VARCHAR(36)  NOT NULL,
+    orchestrator_zone_name  VARCHAR(255)  NOT NULL,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    lastmodified TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE UNIQUE INDEX identity_zone_id_unique_key ON orchestrator_zone (identity_zone_id);
+
+CREATE UNIQUE INDEX orchestrator_name_unique_key ON orchestrator_zone (orchestrator_zone_name);
+
+ALTER TABLE orchestrator_zone ADD CONSTRAINT fk_orchestrator_identity_zone FOREIGN KEY (identity_zone_id)
+    REFERENCES identity_zone (id);
