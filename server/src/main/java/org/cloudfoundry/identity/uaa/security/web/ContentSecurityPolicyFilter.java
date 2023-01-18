76c45b1ad56a6e0e3b1a8abc5155161bdd42966e
James Hochadel <james.hochadel@gsa.gov>
2023-01-18 11:36:52 -0500

    Support setting custom CSP script-src policy
    72565fb disallowed inline scripts, but some users load scripts from other sources to add functionality to, for example, the login page. This change adds a new optional section to uaa.yml that allows custom script sources.
    
    Addresses https://github.com/cloudfoundry/uaa/issues/2086.

--- server/src/main/java/org/cloudfoundry/identity/uaa/security/web/ContentSecurityPolicyFilter.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/security/web/ContentSecurityPolicyFilter.java
@@ -8,15 +8,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class ContentSecurityPolicyFilter extends OncePerRequestFilter {
+
+    private ContentSecurityPolicyConfiguration configuration = new ContentSecurityPolicyConfiguration();
+    private String cspHeader = null;
+
     @Override
     public void doFilterInternal(HttpServletRequest request,
-                                 HttpServletResponse response, FilterChain chain)
+            HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
-        response.setHeader("Content-Security-Policy", "script-src 'self'");
+        // If cspHeader were set when the class was instantiated, it would use
+        // the values in `configuration` before they're possibly overwritten
+        // by spring-servlet.xml, which itself reads values from uaa.yml. To
+        // avoid this, we set it lazily here instead.
+        if (this.cspHeader == null) {
+            cspHeader = cspHeaderValue();
+        }
+        response.setHeader("Content-Security-Policy", cspHeader);
         chain.doFilter(request, response);
     }
 
@@ -30,5 +42,16 @@ public class ContentSecurityPolicyFilter extends OncePerRequestFilter {
 
         return pathsWithHtmlInlineScripts.stream()
                 .anyMatch(requestPath::startsWith);
+    }
+
+    private String cspHeaderValue() {
+        StringBuilder b = new StringBuilder();
+        b.append("script-src ");
+        b.append(String.join(" ", configuration.getAllowedScriptSrc()));
+        return b.toString();
+    }
+
+    public void setCspAllowedScriptSrc(List<String> cspAllowedScriptSrc) {
+        this.configuration.setAllowedScriptSrc(new HashSet<String>(cspAllowedScriptSrc));
     }
 }
