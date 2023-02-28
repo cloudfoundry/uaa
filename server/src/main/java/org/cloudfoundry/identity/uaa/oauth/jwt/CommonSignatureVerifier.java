382446c6a50d3fbae5b5791a10fed6b1568d158b
strehle <markus.strehle@sap.com>
2023-02-28 15:36:56 +0100

    more flexible key parser

--- server/src/main/java/org/cloudfoundry/identity/uaa/oauth/jwt/CommonSignatureVerifier.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/oauth/jwt/CommonSignatureVerifier.java
@@ -17,6 +17,7 @@ package org.cloudfoundry.identity.uaa.oauth.jwt;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.RSAKey;
 import org.cloudfoundry.identity.uaa.oauth.jwk.JsonWebKey;
 import org.springframework.security.jwt.crypto.sign.EllipticCurveVerifier;
@@ -36,7 +37,7 @@ public class CommonSignatureVerifier implements SignatureVerifier {
             throw new IllegalArgumentException("verificationKey cannot be null");
         } else if(verificationKey.getKty() == JsonWebKey.KeyType.RSA) {
             try {
-                RSAKey rsaKey = RSAKey.parse(verificationKey.getKeyProperties());
+                RSAKey rsaKey = verificationKey.getValue() != null ? JWK.parseFromPEMEncodedObjects(verificationKey.getValue()).toRSAKey() : RSAKey.parse(verificationKey.getKeyProperties());;
                 String jwtAlg = Optional.ofNullable(verificationKey.getAlgorithm()).orElse(JWSAlgorithm.RS256.getName());
                 delegate = new RsaVerifier(rsaKey.toRSAPublicKey(), JwtAlgorithms.sigAlgJava(jwtAlg));
             } catch (ParseException | JOSEException e) {
