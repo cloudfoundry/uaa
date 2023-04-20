c51d2f50bb7f0dcbab553567d1025d9c3c0ef902
strehle <markus.strehle@sap.com>
2023-04-20 12:16:24 +0200

    rebase from develop
    add test with many roles
    some sonar smells in addition, but no logic change

--- server/src/test/java/org/cloudfoundry/identity/uaa/user/UserInfoTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/user/UserInfoTest.java
@@ -3,10 +3,11 @@ package org.cloudfoundry.identity.uaa.user;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class UserInfoTest {
 
@@ -14,59 +15,78 @@ class UserInfoTest {
     class Equals {
 
         @Test
+        @SuppressWarnings("java:S5863")
         void expectTrueWhenBothUserInfoObjectsAreSame() {
             UserInfo userInfo = new UserInfo();
-            assertTrue(userInfo.equals(userInfo));
+            assertEquals(userInfo, userInfo);
         }
 
         @Test
         void expectFalseWhenUserInfoIsComparedWithOtherObject() {
             UserInfo userInfo = new UserInfo();
-            assertFalse(userInfo.equals(new Object()));
+            assertNotEquals(userInfo, new Object());
         }
 
         @Test
         void expectTrueWhenBothUserInfoWithoutAnyRole() {
             UserInfo u1 = new UserInfo();
             UserInfo u2 = new UserInfo();
-            assertTrue(u1.equals(u2));
+            assertEquals(u1, u2);
         }
 
         @Test
         void expectFalseWhenOnlyOneUserInfoHasRole() {
             UserInfo u1 = userInfoWithRoles(List.of("group1"));
             UserInfo u2 = new UserInfo();
-            assertFalse(u1.equals(u2));
+            assertNotEquals(u1, u2);
         }
 
         @Test
         void expectTrueWhenBothUserInfoHaveSameRole() {
             UserInfo u1 = userInfoWithRoles(List.of("group1"));
             UserInfo u2 = userInfoWithRoles(List.of("group1"));
-            assertTrue(u1.equals(u2));
+            assertEquals(u1, u2);
         }
 
         @Test
         void expectFalseWhenBothUserInfoHaveDifferentRoles() {
             UserInfo u1 = userInfoWithRoles(List.of("group1"));
             UserInfo u2 = userInfoWithRoles(List.of("group2"));
-            assertFalse(u1.equals(u2));
+            assertNotEquals(u1, u2);
         }
 
         @Test
         void expectFalseWhenBothUserInfoHaveMultipleRolesAndFewAreCommon() {
             UserInfo u1 = userInfoWithRoles(List.of("group1", "group2", "group3"));
             UserInfo u2 = userInfoWithRoles(List.of("group2", "group3", "group4"));
-            assertFalse(u1.equals(u2));
+            assertNotEquals(u1, u2);
         }
 
         @Test
         void expectTrueWhenBothUserInfoHaveMultipleRolesAndAllAreCommon() {
             UserInfo u1 = userInfoWithRoles(List.of("group1", "group2", "group3"));
             UserInfo u2 = userInfoWithRoles(List.of("group1", "group2", "group3"));
-            assertTrue(u1.equals(u2));
+            assertEquals(u1, u2);
         }
 
+        @Test
+        void expectTrueWhenBothUserInfoHaveManyMultipleRolesAndAllAreCommon() {
+            // Given
+            int count = 10000;
+            List<String> roleList1 = new ArrayList<>(count);
+            List<String> roleList2 = new ArrayList<>(count);
+            for (int i=1; i <= count; i++ ) {
+                roleList1.add("groups".concat(Integer.toString(i)));
+            }
+            for (int i=count; 0 < i; i-- ) {
+                roleList2.add("groups".concat(Integer.toString(i)));
+            }
+            // When
+            UserInfo u1 = userInfoWithRoles(roleList1);
+            UserInfo u2 = userInfoWithRoles(roleList2);
+            // Then
+            assertEquals(u1, u2);
+        }
     }
 
     private UserInfo userInfoWithRoles(List<String> roles) {
