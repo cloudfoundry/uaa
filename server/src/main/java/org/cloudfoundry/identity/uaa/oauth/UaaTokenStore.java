2e7fe1ef3b2b7585833b62f73225e448a247757e
Bruce Ricard <bruce.ricard@gmail.com>
2023-05-19 12:32:02 -0400

    refactor: rename
    * this name represents better what this variable is about: the attempt number for the retry loop

--- server/src/main/java/org/cloudfoundry/identity/uaa/oauth/UaaTokenStore.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/oauth/UaaTokenStore.java
@@ -99,9 +99,9 @@ public class UaaTokenStore implements AuthorizationCodeServices {
         final int maxTries = 3;
         performExpirationClean();
         JdbcTemplate template = new JdbcTemplate(dataSource);
-        int tries = 0;
+        int attempt = 0;
         while (true) {
-            tries++;
+            attempt++;
             try {
                 String code = generator.generate();
                 Instant expiresAt = Instant.now().plus(getExpirationTime());
@@ -118,7 +118,7 @@ public class UaaTokenStore implements AuthorizationCodeServices {
                 }
                 return code;
             } catch (DataIntegrityViolationException exists) {
-                if (tries>=maxTries) throw exists;
+                if (attempt>=maxTries) throw exists;
             }
         }
     }
