cloudfoundry/uaa

commit 5c6f7bdc2ca8b7e0e817e090626e0f48f4780491

KeyInfo fixes (#2284)
* Fix KeyInfo

* tests

* more flexible key parser

* sonar

* some more tests

* renamed

* renamed

* remove deprecated zone holder

* sonar issues

* KeyInfo fixes

* cleanup

* small refactorings

* rebase

* rebase

* review

* sonar

model/src/main/java/org/cloudfoundry/identity/uaa/oauth/jwk/JsonWebKey.java
@@ -19,26 +19,29 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.nimbusds.jose.HeaderParameterNames;
 import com.nimbusds.jose.jwk.JWKParameterNames;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.BaseNCodec;
+import org.cloudfoundry.identity.uaa.util.UaaStringUtils;
 
 import java.math.BigInteger;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.RSAPublicKeySpec;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.apache.commons.codec.binary.BaseNCodec.PEM_CHUNK_SIZE;
 import static org.cloudfoundry.identity.uaa.oauth.jwk.JsonWebKey.KeyType.MAC;
 import static org.cloudfoundry.identity.uaa.oauth.jwk.JsonWebKey.KeyType.RSA;
-import static org.springframework.util.StringUtils.hasText;
+import static org.cloudfoundry.identity.uaa.oauth.jwk.JsonWebKey.KeyType.oct;
 
 /**
  * See https://tools.ietf.org/html/rfc7517
@@ -48,7 +51,8 @@ import static org.springframework.util.StringUtils.hasText;
 @JsonSerialize(using = JsonWebKeySerializer.class)
 public class JsonWebKey {
 
-    private static final java.util.Base64.Encoder base64encoder = java.util.Base64.getMimeEncoder(BaseNCodec.PEM_CHUNK_SIZE, "\n".getBytes());
+    private static final Base64.Encoder base64encoder = Base64.getMimeEncoder(PEM_CHUNK_SIZE, "\n".getBytes(Charset.defaultCharset()));
+    private static final Base64.Decoder base64decoder = Base64.getUrlDecoder();
 
     // value is not defined in RFC 7517
     public static final String PUBLIC_KEY_VALUE = "value";
@@ -58,9 +62,12 @@ public class JsonWebKey {
         enc
     }
 
+    // RFC 7518
     public enum KeyType {
         RSA,
-        MAC
+        EC,
+        MAC,
+        oct
     }
 
     public enum KeyOperation {
@@ -81,7 +88,7 @@ public class JsonWebKey {
             throw new IllegalArgumentException("kty field is required");
         }
         KeyType.valueOf((String) json.get(JWKParameterNames.KEY_TYPE));
-        this.json = new HashMap(json);
+        this.json = new HashMap<>(json);
     }
 
     public Map<String, Object> getKeyProperties() {
@@ -122,7 +129,7 @@ public class JsonWebKey {
     public final KeyUse getUse() {
         String use = (String) getKeyProperties().get(JWKParameterNames.PUBLIC_KEY_USE);
         KeyUse result = null;
-        if (hasText(use)) {
+        if (UaaStringUtils.isNotEmpty(use)) {
             result = KeyUse.valueOf(use);
         }
         return result;
@@ -153,10 +160,10 @@ public class JsonWebKey {
     public String getValue() {
         String result = (String) getKeyProperties().get(PUBLIC_KEY_VALUE);
         if (result == null) {
-            if (RSA.equals(getKty())) {
-                result = pemEncodePublicKey(getRsaPublicKey(this));
+            if (RSA == getKty()) {
+                result = pemEncodePublicKey(getRsaPublicKey(this)).orElse(UaaStringUtils.EMPTY_STRING);
                 this.json.put(PUBLIC_KEY_VALUE, result);
-            } else if (MAC.equals(getKty())) {
+            } else if (MAC == getKty() || oct == getKty()) {
                 result = (String) getKeyProperties().get(JWKParameterNames.OCT_KEY_VALUE);
                 this.json.put(PUBLIC_KEY_VALUE, result);
             }
@@ -172,27 +179,29 @@ public class JsonWebKey {
         return result.stream().map(KeyOperation::valueOf).collect(Collectors.toSet());
     }
 
-    public static String pemEncodePublicKey(PublicKey publicKey) {
+    public static Optional<String> pemEncodePublicKey(PublicKey publicKey) {
+        if (publicKey == null) {
+            return Optional.empty();
+        }
         String begin = "-----BEGIN PUBLIC KEY-----\n";
         String end = "\n-----END PUBLIC KEY-----";
-        byte[] data = publicKey.getEncoded();
-        String base64encoded = new String(base64encoder.encode(data));
 
-        return begin + base64encoded + end;
+        return Optional.of(begin + base64encoder.encodeToString(publicKey.getEncoded()) + end);
     }
 
-    public static PublicKey getRsaPublicKey(JsonWebKey key) {
-        final Base64 decoder = new Base64(true);
+    protected static PublicKey getRsaPublicKey(JsonWebKey key) {
         String e = (String) key.getKeyProperties().get(JWKParameterNames.RSA_EXPONENT);
         String n = (String) key.getKeyProperties().get(JWKParameterNames.RSA_MODULUS);
-        BigInteger modulus = new BigInteger(1, decoder.decode(n.getBytes(StandardCharsets.UTF_8)));
-        BigInteger exponent = new BigInteger(1, decoder.decode(e.getBytes(StandardCharsets.UTF_8)));
-        try {
-            return KeyFactory.getInstance(RSA.name()).generatePublic(
-                new RSAPublicKeySpec(modulus, exponent)
-            );
-        } catch (InvalidKeySpecException | NoSuchAlgorithmException e1) {
-            throw new IllegalStateException(e1);
+
+        if (e != null && n != null) {
+            BigInteger modulus = new BigInteger(1, base64decoder.decode(n.getBytes(StandardCharsets.UTF_8)));
+            BigInteger exponent = new BigInteger(1, base64decoder.decode(e.getBytes(StandardCharsets.UTF_8)));
+            try {
+                return KeyFactory.getInstance(RSA.name()).generatePublic(new RSAPublicKeySpec(modulus, exponent));
+            } catch (InvalidKeySpecException | NoSuchAlgorithmException e1) {
+                throw new IllegalStateException(e1);
+            }
         }
+        return null;
     }
 }

server/src/main/java/org/cloudfoundry/identity/uaa/oauth/jwt/CommonSignatureVerifier.java
@@ -14,25 +14,47 @@
  */
 package org.cloudfoundry.identity.uaa.oauth.jwt;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.RSAKey;
+import org.cloudfoundry.identity.uaa.oauth.jwk.JsonWebKey;
+import org.springframework.security.jwt.crypto.sign.EllipticCurveVerifier;
 import org.springframework.security.jwt.crypto.sign.MacSigner;
 import org.springframework.security.jwt.crypto.sign.RsaVerifier;
 import org.springframework.security.jwt.crypto.sign.SignatureVerifier;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.text.ParseException;
+import java.util.Optional;
+
 public class CommonSignatureVerifier implements SignatureVerifier {
     private final SignatureVerifier delegate;
 
-    public CommonSignatureVerifier(String verificationKey) {
+    public CommonSignatureVerifier(JsonWebKey verificationKey) {
         if(verificationKey == null) {
             throw new IllegalArgumentException("verificationKey cannot be null");
-        } else if(isAssymetricKey(verificationKey)) {
-            delegate = new RsaVerifier(verificationKey);
+        } else if(verificationKey.getKty() == JsonWebKey.KeyType.RSA) {
+            try {
+                RSAKey rsaKey = verificationKey.getValue() != null ? JWK.parseFromPEMEncodedObjects(verificationKey.getValue()).toRSAKey() : RSAKey.parse(verificationKey.getKeyProperties());
+                String jwtAlg = Optional.ofNullable(verificationKey.getAlgorithm()).orElse(JWSAlgorithm.RS256.getName());
+                delegate = new RsaVerifier(rsaKey.toRSAPublicKey(), JwtAlgorithms.sigAlgJava(jwtAlg));
+            } catch (ParseException | JOSEException e) {
+                throw new IllegalArgumentException(e);
+            }
+        } else if(verificationKey.getKty() == JsonWebKey.KeyType.EC) {
+            try {
+                ECKey ecKey = ECKey.parse(verificationKey.getKeyProperties());
+                String jwtAlg = Optional.ofNullable(verificationKey.getAlgorithm()).orElse(JWSAlgorithm.ES256.getName());
+                delegate = new EllipticCurveVerifier(ecKey.toECPublicKey(), JwtAlgorithms.sigAlgJava(jwtAlg));
+            } catch (ParseException | JOSEException e) {
+                throw new IllegalArgumentException(e);
+            }
         } else {
-            delegate = new MacSigner(verificationKey);
+            String jwtAlg = Optional.ofNullable(verificationKey.getAlgorithm()).orElse(JWSAlgorithm.HS256.getName());
+            delegate = new MacSigner(JwtAlgorithms.sigAlgJava(jwtAlg), new SecretKeySpec(verificationKey.getValue().getBytes(), jwtAlg));
         }
-    }
-
-    private static boolean isAssymetricKey(String key) {
-        return key.startsWith("-----BEGIN");
     }
 
     @Override

model/src/main/java/org/cloudfoundry/identity/uaa/util/UaaStringUtils.java
@@ -286,6 +286,8 @@ public final class UaaStringUtils {
         return input == null || input.length() == 0;
     }
 
+    public static boolean isNotEmpty(final String input) { return !isNullOrEmpty(input); }
+
     public static String convertISO8859_1_to_UTF_8(String s) {
         if (s==null) {
             return null;

model/src/test/java/org/cloudfoundry/identity/uaa/oauth/jwk/JsonWebKeyDeserializerTest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import org.cloudfoundry.identity.uaa.util.JsonUtils;
 import org.junit.jupiter.api.Test;
 
+import static org.cloudfoundry.identity.uaa.oauth.jwk.JsonWebKey.KeyType.EC;
+import static org.cloudfoundry.identity.uaa.oauth.jwk.JsonWebKey.KeyType.oct;
 import static org.cloudfoundry.identity.uaa.test.ModelTestUtils.getResourceAsString;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -17,6 +19,10 @@ class JsonWebKeyDeserializerTest {
   private static final String uaaLegacyJwkSet = getResourceAsString(JsonWebKeyDeserializerTest.class, "JwkSet-LegacyUaa.json");
   // Keycloak server configuration https://www.keycloak.org/docs/latest/server_admin/, e.g. jwks_uri: http://localhost:8080/realms/{realm-name}/protocol/openid-connect/certs
   private static final String keyCloakJwkSet = getResourceAsString(JsonWebKeyDeserializerTest.class, "JwkSet-Keycloak.json");
+  // HMAC standard attributes
+  private static final String keyOctedJwkSet = getResourceAsString(JsonWebKeyDeserializerTest.class, "JwkSet-Hmac.json");
+  // elliptic cure
+  private static final String keyECJwkSet = getResourceAsString(JsonWebKeyDeserializerTest.class, "JwkSet-ECProvider.json");
 
   @Test
   void testWebKeysMicrosoft() {
@@ -61,6 +67,40 @@ class JsonWebKeyDeserializerTest {
       assertNotNull(key.getX5t());
       assertEquals("m-ERKoK9FRe8S9gP0eMI3OP4oljfQMOa3bukzi8ASmM", key.getKid());
       assertEquals("Zv-dxo0VbAZrjp7gBP97yyjdxC8", key.getX5t());
+    }
+  }
+
+  @Test
+  void testWebKeysOcted() {
+    JsonWebKeySet<JsonWebKey> keys = JsonUtils.readValue(keyOctedJwkSet, new TypeReference<JsonWebKeySet<JsonWebKey>>() {
+    });
+    assertNotNull(keys);
+    assertNotNull(keys.getKeys());
+    assertEquals(1, keys.getKeys().size());
+    for (JsonWebKey key : keys.getKeys()) {
+      assertNotNull(key);
+      assertEquals(oct, key.getKty());
+      assertEquals("tokenKey", key.getValue());
+      assertEquals("legacy-token-key", key.getKid());
+    }
+  }
+
+  @Test
+  void testWebKeysEllipticCurve() {
+    JsonWebKeySet<JsonWebKey> keys = JsonUtils.readValue(keyECJwkSet, new TypeReference<JsonWebKeySet<JsonWebKey>>() {
+    });
+    assertNotNull(keys);
+    assertNotNull(keys.getKeys());
+    assertEquals(1, keys.getKeys().size());
+    for (JsonWebKey key : keys.getKeys()) {
+      assertNotNull(key);
+      assertNull(key.getValue());
+      assertEquals(EC, key.getKty());
+      assertEquals("ES256", key.getAlgorithm());
+      assertEquals("ec-key-1", key.getKid());
+      assertEquals("gI0GAILBdu7T53akrFmMyGcsF3n5dO7MmwNBHKW5SV0", key.getKeyProperties().get("x"));
+      assertEquals("SLW_xSffzlPWrHEVI30DHM_4egVwt3NQqeUD7nMFpps", key.getKeyProperties().get("y"));
+      assertEquals("P-256", key.getKeyProperties().get("crv"));
     }
   }
 }

model/src/test/java/org/cloudfoundry/identity/uaa/util/UaaStringUtilsTest.java
@@ -405,6 +405,12 @@ class UaaStringUtilsTest {
     }
 
     @ParameterizedTest
+    @NullAndEmptySource
+    void isNotEmpty_ShouldReturnFalse(final String input) {
+        Assertions.assertThat(UaaStringUtils.isNotEmpty(input)).isFalse();
+    }
+
+    @ParameterizedTest
     @ValueSource(strings = { " ", "  ", "\t", "\n", "abc" })
     void isNullOrEmpty_ShouldReturnFalse(final String input) {
         Assertions.assertThat(UaaStringUtils.isNullOrEmpty(input)).isFalse();

commit 8076c41584ed65df16009d4bf22e19dd8b533c43

Fix RSA public key parser
Issue is a regression with #2284. Before the spring-security-jwt parser was used and now nimbus, but is stricter in parser of key content.
Implemented now the less strict parser from the library. We want remove spring-security-jwt in near future, thus did not used library anymore
Added test with example of YAML or DB config based oauth2 provider in parameter tokenKey. The key there does not have line break but spaces or other chars.

model/src/main/java/org/cloudfoundry/identity/uaa/oauth/jwk/JsonWebKey.java
@@ -24,11 +24,15 @@ import org.cloudfoundry.identity.uaa.util.UaaStringUtils;
 import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.security.spec.RSAPublicKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 import java.util.Collections;
 import java.util.HashMap;
@@ -36,6 +40,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static org.apache.commons.codec.binary.BaseNCodec.PEM_CHUNK_SIZE;
@@ -53,6 +59,8 @@ public class JsonWebKey {
 
     private static final Base64.Encoder base64encoder = Base64.getMimeEncoder(PEM_CHUNK_SIZE, "\n".getBytes(Charset.defaultCharset()));
     private static final Base64.Decoder base64decoder = Base64.getUrlDecoder();
+    private static final Pattern PEM_DATA = Pattern.compile("-----BEGIN (.*)-----(.*)-----END (.*)-----", Pattern.DOTALL);
+    private static final int PATTERN_CONTENT = 2;
 
     // value is not defined in RFC 7517
     public static final String PUBLIC_KEY_VALUE = "value";
@@ -203,5 +211,17 @@ public class JsonWebKey {
             }
         }
         return null;
+    }
+
+    public static RSAPublicKey getRsaPublicKey(String jsonData) throws GeneralSecurityException {
+        Matcher m = PEM_DATA.matcher(jsonData.trim());
+
+        if (!m.matches()) {
+            throw new IllegalArgumentException("String is not PEM encoded data");
+        }
+        byte[] content = Base64.getMimeDecoder().decode(m.group(PATTERN_CONTENT).getBytes(StandardCharsets.UTF_8));
+        KeySpec keySpec = new X509EncodedKeySpec(content);
+        KeyFactory fact = KeyFactory.getInstance("RSA");
+        return (RSAPublicKey) fact.generatePublic(keySpec);
     }
 }

server/src/main/java/org/cloudfoundry/identity/uaa/oauth/jwt/CommonSignatureVerifier.java
@@ -17,7 +17,6 @@ package org.cloudfoundry.identity.uaa.oauth.jwt;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.ECKey;
-import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.RSAKey;
 import org.cloudfoundry.identity.uaa.oauth.jwk.JsonWebKey;
 import org.springframework.security.jwt.crypto.sign.EllipticCurveVerifier;
@@ -26,6 +25,8 @@ import org.springframework.security.jwt.crypto.sign.RsaVerifier;
 import org.springframework.security.jwt.crypto.sign.SignatureVerifier;
 
 import javax.crypto.spec.SecretKeySpec;
+import java.security.GeneralSecurityException;
+import java.security.interfaces.RSAPublicKey;
 import java.text.ParseException;
 import java.util.Optional;
 
@@ -37,10 +38,10 @@ public class CommonSignatureVerifier implements SignatureVerifier {
             throw new IllegalArgumentException("verificationKey cannot be null");
         } else if(verificationKey.getKty() == JsonWebKey.KeyType.RSA) {
             try {
-                RSAKey rsaKey = verificationKey.getValue() != null ? JWK.parseFromPEMEncodedObjects(verificationKey.getValue()).toRSAKey() : RSAKey.parse(verificationKey.getKeyProperties());
+                RSAPublicKey rsaKey = verificationKey.getValue() != null ? JsonWebKey.getRsaPublicKey(verificationKey.getValue()) : RSAKey.parse(verificationKey.getKeyProperties()).toRSAPublicKey();
                 String jwtAlg = Optional.ofNullable(verificationKey.getAlgorithm()).orElse(JWSAlgorithm.RS256.getName());
-                delegate = new RsaVerifier(rsaKey.toRSAPublicKey(), JwtAlgorithms.sigAlgJava(jwtAlg));
-            } catch (ParseException | JOSEException e) {
+                delegate = new RsaVerifier(rsaKey, JwtAlgorithms.sigAlgJava(jwtAlg));
+            } catch (ParseException | JOSEException | GeneralSecurityException e) {
                 throw new IllegalArgumentException(e);
             }
         } else if(verificationKey.getKty() == JsonWebKey.KeyType.EC) {

model/src/test/java/org/cloudfoundry/identity/uaa/oauth/jwk/JsonWebKeyTests.java
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import org.cloudfoundry.identity.uaa.util.JsonUtils;
 import org.junit.jupiter.api.Test;
 
+import java.security.GeneralSecurityException;
+import java.security.PublicKey;
 import java.util.Map;
 
 import static org.cloudfoundry.identity.uaa.test.ModelTestUtils.getResourceAsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThrows;
 
 class JsonWebKeyTests {
@@ -35,4 +38,22 @@ class JsonWebKeyTests {
     assertThrows(IllegalArgumentException.class, () -> new JsonWebKey(jsonMap));
   }
 
+  @Test
+  void testGetRsaPublicKeyFromConfig() throws GeneralSecurityException {
+    // given
+    String tokenKey = "-----BEGIN PUBLIC KEY----- MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxMi4Z4FBfQEOdNYLmzxk YJvP02TSeapZMKMQo90JQRL07ttIKcDMP6pGcirOGSQWWBBpvdo5EnVOiNzViu9J CJP2IWbHJ4sRe0S1dySYdBRVV/ZkgWOrj7Cr2yT0ZVvCCzH7NAWmlA6LUV19Mnp+ ugeGoxK+fsk8SRLS/Z9JdyxgOb3tPxdDas3MZweMZ6HqujoAAG9NASBGjFNXbhMc krEfecwm3OJzsjGFxhqXRqkTsGEHvzETMxfvSkTkldOzmErnjpwyoOPLrXcWIs1w vdXHakfVHSvyb3T4gm3ZfOOoUf6lrd2w1pF/PkA88NkjN2+W9fQmbUzNgVjEQiXo 4wIDAQAB -----END PUBLIC KEY-----";
+    // when
+    PublicKey key = JsonWebKey.getRsaPublicKey(tokenKey);
+    // then
+    assertNotNull(key);
+    assertEquals("RSA", key.getAlgorithm());
+  }
+
+  @Test
+  void testGetRsaPublicKeyFromConfigFails() throws GeneralSecurityException {
+    // given
+    String tokenKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA";
+    // when
+    assertThrows(IllegalArgumentException.class, () -> JsonWebKey.getRsaPublicKey(tokenKey));
+  }
 }