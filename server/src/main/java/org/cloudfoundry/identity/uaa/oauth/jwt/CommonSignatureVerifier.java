2b4f32cea8bb1ce8a52e6ee18226e697d08da365
strehle <markus.strehle@sap.com>
2023-05-20 08:43:24 +0200

    refactorings

--- server/src/main/java/org/cloudfoundry/identity/uaa/oauth/jwt/CommonSignatureVerifier.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/oauth/jwt/CommonSignatureVerifier.java
@@ -17,9 +17,9 @@ package org.cloudfoundry.identity.uaa.oauth.jwt;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.jwk.ECKey;
-import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.RSAKey;
 import org.cloudfoundry.identity.uaa.oauth.jwk.JsonWebKey;
+import org.cloudfoundry.identity.uaa.oauth.jwk.JsonWebKeyHelper;
 import org.springframework.security.jwt.crypto.sign.EllipticCurveVerifier;
 import org.springframework.security.jwt.crypto.sign.MacSigner;
 import org.springframework.security.jwt.crypto.sign.RsaVerifier;
@@ -28,15 +28,8 @@ import org.springframework.security.jwt.crypto.sign.SignatureVerifier;
 import javax.crypto.spec.SecretKeySpec;
 import java.text.ParseException;
 import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class CommonSignatureVerifier implements SignatureVerifier {
-
-    private static final Pattern PEM_DATA = Pattern.compile("-----BEGIN (.*)-----(.*)-----END (.*)-----", Pattern.DOTALL);
-    private static final Pattern WS_DATA = Pattern.compile("\\s", Pattern.UNICODE_CHARACTER_CLASS);
-    private static final int PATTERN_TYPE = 1;
-    private static final int PATTERN_CONTENT = 2;
     private final SignatureVerifier delegate;
 
     public CommonSignatureVerifier(JsonWebKey verificationKey) {
@@ -44,7 +37,7 @@ public class CommonSignatureVerifier implements SignatureVerifier {
             throw new IllegalArgumentException("verificationKey cannot be null");
         } else if(verificationKey.getKty() == JsonWebKey.KeyType.RSA) {
             try {
-                RSAKey rsaKey = verificationKey.getValue() != null ? getRsaKey(verificationKey.getValue()) : RSAKey.parse(verificationKey.getKeyProperties());
+                RSAKey rsaKey = verificationKey.getValue() != null ? JsonWebKeyHelper.getJsonWebKey(verificationKey.getValue()).toRSAKey() : RSAKey.parse(verificationKey.getKeyProperties());
                 String jwtAlg = Optional.ofNullable(verificationKey.getAlgorithm()).orElse(JWSAlgorithm.RS256.getName());
                 delegate = new RsaVerifier(rsaKey.toRSAPublicKey(), JwtAlgorithms.sigAlgJava(jwtAlg));
             } catch (ParseException | JOSEException e) {
@@ -72,15 +65,5 @@ public class CommonSignatureVerifier implements SignatureVerifier {
     @Override
     public String algorithm() {
         return delegate.algorithm();
-    }
-
-    private static RSAKey getRsaKey(String jsonData) throws JOSEException {
-        Matcher m = PEM_DATA.matcher(jsonData.trim());
-        if (!m.matches()) {
-            throw new IllegalArgumentException("String is not PEM encoded data");
-        }
-        String begin = "-----BEGIN " + m.group(PATTERN_TYPE) + "-----\n";
-        String end = "\n-----END " + m.group(PATTERN_TYPE) + "-----";
-        return JWK.parseFromPEMEncodedObjects(begin + WS_DATA.matcher(m.group(PATTERN_CONTENT).trim()).replaceAll("\n") + end).toRSAKey();
     }
 }
