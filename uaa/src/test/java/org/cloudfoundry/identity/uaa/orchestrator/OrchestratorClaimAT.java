cedd6f82c727b6b1018eb54bff83f312399ca549
a223033813 <Baleno@94a>
2023-06-20 12:35:12 +0530

    Add Acceptance Tests for Orchestrator Integration
    Add automated tests for testing end to end integration with Orchestrator UAA claim.
    
    Add tests to create, get and delete UAA instance using UAA claim
    with subdomain and without subdomain.
    Add negative tests to test claim for Mandatory Missing fields.
    Update AcceptanceEKSJenkinsfile to export required env vars for
    Orchestrator Integration Acceptance Tests [1] and add ips to /etc/hosts
    to allow aws login through gosammer3.
    [1] https://github.devtools.predix.io/predix-security/uaa-k8s-deploy/pull/43

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/orchestrator/OrchestratorClaimAT.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/orchestrator/OrchestratorClaimAT.java
@@ -1,124 +1,278 @@
-/*******************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
 package org.cloudfoundry.identity.uaa.orchestrator;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ge.predix.iam.kubernetes.enums.ApiEnvironment;
+import com.ge.predix.iam.kubernetes.enums.ServiceName;
+import com.ge.predix.iam.kubernetes.exception.ServiceInstanceProviderException;
+import com.ge.predix.iam.kubernetes.model.Claim;
+import com.ge.predix.iam.kubernetes.model.uaa.UaaClaimSpec;
+import com.ge.predix.iam.kubernetes.model.uaa.UaaForProvider;
+import com.ge.predix.iam.kubernetes.model.uaa.UaaRequest;
+import com.ge.predix.iam.kubernetes.utils.OrchestratorUtil;
+import io.kubernetes.client.openapi.ApiException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.StringUtils;
+import org.cloudfoundry.identity.uaa.integration.util.IntegrationTestUtils;
+import org.cloudfoundry.identity.uaa.zone.model.OrchestratorZoneResponse;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
-import com.ge.predix.iam.kubernetes.enums.ServiceName;
-import com.ge.predix.iam.kubernetes.exception.ServiceInstanceProviderException;
-import com.ge.predix.iam.kubernetes.model.Claim;
-import com.ge.predix.iam.kubernetes.utils.AssumeRoleUtil;
-import com.ge.predix.iam.kubernetes.utils.OrchestratorUtil;
+import java.util.Random;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = OrchestratorIntegrationTestConfig.class)
+@Slf4j
 public class OrchestratorClaimAT {
+    protected static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    @Value("${kubernetes.config.secondary-namespace}")
-    private String namespace;
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrchestratorClaimAT.class);
+
+    private static final String UAA_ADMIN_CLIENT_SECRET = "ZHVtbXlBZG1pblNlY3JldDEyMyMh";
+
+    private static final String UAA_CLAIM_NAME_PREFIX = "uaa-it-";
+
+    public static final String ZONES_URI = "/orchestrator/zones";
+
+    private static HttpHeaders headers;
+
+    private Random random = new Random();
+
+    private final String UAA_SUBDOMAIN = "rest-uaa-subdomain" + random.nextInt(1000);
+
+    @Value("${uaa.url}")
+    private String uaaUrl;
+
+    @Value("${uaa.orchestrator.clientId}")
+    private String clientId;
+
+    @Value("${uaa.orchestrator.clientSecret}")
+    private String clientSecret;
+
+    @Value("${kubernetes.config.env}")
+    private String environment;
 
     @Autowired
     private OrchestratorUtil orchestratorUtil;
 
-    @Autowired
-    private AssumeRoleUtil assumeRoleUtil;
+    protected static OAuth2RestTemplate trustedIssuerValidScopesRestTemplate;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrchestratorClaimAT.class);
-    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    @Value("${kubernetes.config.secondary-namespace:iamqa-svc}")
+    private String namespace;
 
-    @BeforeClass
-    public static void setupClassLevelData() {
-        LOGGER.info("In OrchestratorClaimAT.setupClassLevelData()");
-    }
+    private Integer uaaInstanceCount = random.nextInt(1000);
 
-    @Before
-    public void setupTestLevelData() {
-        LOGGER.info("In OrchestratorClaimAT.setupTestLevelData()");
+    // region Positive Tests
+
+    @Test
+    public void testCreateZoneWithSubdomain() throws Exception {
+        testCreateZone(UAA_SUBDOMAIN, UAA_ADMIN_CLIENT_SECRET);
     }
 
     @Test
-    public void dummyTestTryAwsLogin() throws Exception {
-        LOGGER.info("In OrchestratorClaimAT.dummyTestTryAwsLogin()");
-        LOGGER.info("Print loaded env vars and autowired beans");
-        LOGGER.info("AssumeRoleUtil: {}", assumeRoleUtil);
-        LOGGER.info("OrchestratorUtil: {}", orchestratorUtil);
-        LOGGER.info("Loaded env var kubernetes.config.secondary-namespace: {}", namespace);
-
-        LOGGER.info("\nPerform gossamer aws login and assume role\n");
-        LOGGER.info("\nK8S Api Client returned: {}", assumeRoleUtil.getKubernetesApiClient());
+    public void testCreateZoneWithOutSubdomain() throws Exception {
+        testCreateZone(null, UAA_ADMIN_CLIENT_SECRET);
     }
 
-    // This is dummy test added to validate OrchestratorUtil functionality, temporarily expecting
-    // ServiceInstanceProviderException as UAA is not onboarded yet and can be removed later
-    // once UAA is onboarded and when we add actual uaa orchestrator integration tests.
-    @Test(expected = ServiceInstanceProviderException.class)
-    public void testDummyUaaCreateZone() throws Exception {
-        LOGGER.info("In OrchestratorClaimAT.testDummyUaaCreateZone()");
-
-        String claimName = "test-uaa-claim";
+    public void testCreateZone(String subDomain, String adminSecret) throws Exception {
+        UaaRequest uaaRequest = new UaaRequest();
+        uaaRequest.setSubdomain(subDomain);
+        uaaRequest.setAdminClientSecret(adminSecret);
+        String claimName;
+        OrchestratorZoneResponse getZone = null;
+        String zoneName = null;
+        claimName = (UAA_CLAIM_NAME_PREFIX + (uaaInstanceCount)).toLowerCase();
         Claim claim = null;
         boolean isClaimCreated = false;
         try {
-            isClaimCreated = createZone(claimName, null);
-            claim = getZone(claimName);
-            LOGGER.info("Fetched claim: {}", claim);
+            isClaimCreated = createZone(claimName, uaaRequest);
+            Thread.sleep(120000);
+            claim = getZone(claimName, ServiceName.UAA);
+            assertNotNull(claim);
+            assertEquals(claimName, claim.getMetadata().getName());
+
+            zoneName = namespace + "-" + claimName;
+            String getResponse = getZoneByName(zoneName, HttpStatus.OK, null);
+            getZone = OBJECT_MAPPER.readValue(getResponse, OrchestratorZoneResponse.class);
+
+            //assertions from get API
+            assertNotNull(getResponse);
+            assertNotNull(getZone);
+            assertEquals(zoneName, getZone.getName());
+            assertNotNull(getZone.getConnectionDetails().getDashboardUri());
+            assertNotNull(getZone.getConnectionDetails().getUri());
+            assertNotNull(getZone.getConnectionDetails().getIssuerId());
+            assertNotNull(getZone.getConnectionDetails().getZone().getHttpHeaderValue());
+            assertNotNull(getZone.getConnectionDetails().getZone().getHttpHeaderName());
+            if (uaaRequest.getSubdomain() != null) {
+                assertEquals(uaaRequest.getSubdomain(), getZone.getConnectionDetails().getSubdomain());
+            } else {
+                assertNotNull(getZone.getConnectionDetails().getSubdomain());
+            }
+            JsonNode outputSecret = orchestratorUtil.readSecret(claimName, ServiceName.UAA);
+            String outputSecretData = StringEscapeUtils.unescapeJava(outputSecret.toString());
+            assertNotNull(outputSecret);
+            assertNotNull(outputSecretData);
+            JsonNode connection = outputSecret.get("connectionDetails");
+            JsonNode connectionSecret = OBJECT_MAPPER.readTree(connection.asText());
+
+            // if claim and outputSecret(created by Orchestrator as result of successful instance creation) both are not null
+            // that means uaa zone is successfully created in uaa service.
+            UaaForProvider uaaforProvider = ((UaaClaimSpec) claim.getSpec()).getUaaForProvider();
+
+            //assertions from Claim.
+            assertNotNull(connection);
+            if (uaaRequest.getSubdomain() != null) {
+                assertEquals(uaaRequest.getSubdomain(), connectionSecret.get("subdomain").asText());
+            } else {
+                assertNotNull(connectionSecret.get("subdomain"));
+            }
+            assertEquals(getZone.getConnectionDetails().getUri(), connectionSecret.get("uri").asText());
+            assertEquals(getZone.getConnectionDetails().getDashboardUri(), connectionSecret.get("dashboardUrl").asText());
+            assertEquals(getZone.getConnectionDetails().getIssuerId(), connectionSecret.get("issuerId").asText());
+            assertEquals(getZone.getConnectionDetails().getZone().getHttpHeaderName(), connectionSecret.get("zone").get("http-header-name").asText());
+            assertEquals(getZone.getConnectionDetails().getZone().getHttpHeaderValue(), connectionSecret.get("zone").get("http-header-value").asText());
         } finally {
             if (isClaimCreated) {
-                deleteZone(claimName);
+                deleteZone(claimName, ServiceName.UAA);
+            }
+            if (getZone != null) {
+                Thread.sleep(20000);
+                getZoneByName(zoneName, HttpStatus.NOT_FOUND, "Zone[" + zoneName + "] not found.");
+            }
+        }
+    }
+    // end region Positive Tests
+
+    // region Negative Tests"
+    @Test
+    public void testCreateZoneWithBadInput() throws Exception {
+        UaaRequest uaaRequest = new UaaRequest();
+        uaaRequest.setSubdomain(UAA_SUBDOMAIN);
+        uaaRequest.setAdminClientSecret(UAA_ADMIN_CLIENT_SECRET);
+        String claimName = null;
+        boolean isClaimCreated = false;
+        try {
+            isClaimCreated = createZone(claimName, uaaRequest);
+            Claim claim = getZone(claimName, ServiceName.UAA);
+            assertNull("Orchestrator should not create UAA instance for Bad Input", claim);
+        } catch (Exception e) {
+            assertTrue(e.getMessage().contains("Kubernetes API Exception occurred during Claim creation"));
+        } finally {
+            if (isClaimCreated) {
+                deleteZone(claimName, ServiceName.UAA);
             }
         }
     }
 
-    private boolean createZone(final String claimName, final Object request)
-        throws ServiceInstanceProviderException {
+    @Test
+    public void testGetZoneForNonExisting() throws Exception {
+        String dummyClaimName = (UAA_CLAIM_NAME_PREFIX + "Non_existent_run_id_12345").toLowerCase();
         try {
-            orchestratorUtil.createService(claimName, ServiceName.UAA, OBJECT_MAPPER.valueToTree(request));
+            Claim claim = getZone(dummyClaimName, ServiceName.UAA);
+            assertNull("Orchestrator should return null for Non existing zone", claim);
+        } catch (Exception e) {
+            assertTrue(e.getMessage().contains("Service claim " + dummyClaimName + " does not exist"));
+        }
+    }
+
+    @Test
+    public void testDeleteZoneForNonExisting() throws Exception {
+        String dummyClaimName = (UAA_CLAIM_NAME_PREFIX + "Non_existent_run_id_12345").toLowerCase();
+        try {
+            deleteZone(dummyClaimName, ServiceName.UAA);
+        } catch (Exception e) {
+            LOGGER.error("error while deleting non existing zone", e);
+            assertTrue(e.getMessage().contains("Service claim " + dummyClaimName + " does not exist"));
+        }
+    }
+    // end region Negative Tests
+
+    private boolean createZone(final String claimName, final UaaRequest uaaRequest)
+            throws ServiceInstanceProviderException, ApiException {
+        try {
+            orchestratorUtil.createService(claimName, ServiceName.UAA, OBJECT_MAPPER.valueToTree(uaaRequest), ApiEnvironment.valueOf(environment));
             return true;
-        } catch (ServiceInstanceProviderException e) {
-            LOGGER.error("UAA Claim create failed for claimName: {}, request:{}, errorMessage: {}",
-                         claimName, request, e.getMessage(), e);
+        } catch (ServiceInstanceProviderException | ApiException e) {
+            LOGGER.error("UAA Claim create failed for claimName: {}, uaaRequest:{}, errorMessage: {}", claimName, uaaRequest, e.getMessage(), e);
             throw e;
         }
     }
 
-    private Claim getZone(final String claimName) throws ServiceInstanceProviderException {
+    private Claim getZone(final String claimName, final ServiceName serviceName) throws ServiceInstanceProviderException {
         try {
-            return orchestratorUtil.readClaim(claimName, ServiceName.UAA);
+            return orchestratorUtil.readClaim(claimName, ServiceName.UAA, ApiEnvironment.valueOf(environment));
         } catch (ServiceInstanceProviderException e) {
-            LOGGER.error("UAA Claim read failed for claimName: {}, errorMessage: {}", claimName,
-                         e.getMessage(), e);
+            LOGGER.error("UAA Claim read failed for claimName: {}, errorMessage: {}", claimName, e.getMessage(), e);
             throw e;
         }
     }
 
-    private boolean deleteZone(final String claimName) throws ServiceInstanceProviderException {
+    private boolean deleteZone(final String claimName, final ServiceName serviceName) throws ServiceInstanceProviderException {
         try {
-            orchestratorUtil.deleteClaim(claimName, ServiceName.UAA);
+            orchestratorUtil.deleteClaim(claimName, ServiceName.UAA, ApiEnvironment.valueOf(environment));
             return true;
         } catch (ServiceInstanceProviderException e) {
-            LOGGER.error("UAA Claim delete failed for claimName: {}, errorMessage: {}", claimName,
-                         e.getMessage(), e);
+            LOGGER.error("UAA Claim delete failed for claimName: {}, errorMessage: {}", claimName, e.getMessage(), e);
             throw e;
+        }
+    }
+
+    private String getZoneByName(final String zoneName, HttpStatus status, String expectedErrorMessage) throws Exception {
+        if (StringUtils.isEmpty(zoneName)) {
+            throw new Exception("zoneName can not be null");
+        }
+        String url = uaaUrl + ZONES_URI + "?name=" + zoneName;
+        return executeRequest(getZoneRestTemplate(), url, HttpMethod.GET, headers, status, expectedErrorMessage,
+                null);
+    }
+
+    protected RestTemplate getZoneRestTemplate() {
+        RestTemplate Zone = IntegrationTestUtils.getClientCredentialsTemplate(
+                IntegrationTestUtils.getClientCredentialsResource(uaaUrl, new String[0], clientId, clientSecret)
+        );
+        return Zone;
+    }
+
+    public static String executeRequest(RestTemplate tenantRestTemplate, String url, HttpMethod method,
+                                        HttpHeaders headers, HttpStatus expectedStatusCode,
+                                        String expectedMessage, String requestBody) {
+        String body = null;
+        if (!HttpMethod.GET.equals(method)) {
+            body = requestBody;
+        }
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        log.trace("Executing HTTP {} - url: {}, request:\n {}", method, url, request);
+        if (!expectedStatusCode.is2xxSuccessful()) {
+            ResponseEntity<OrchestratorZoneResponse> responseEntity = tenantRestTemplate.exchange(url, method, request, OrchestratorZoneResponse.class);
+            log.trace("HTTP Response: {}", responseEntity);
+            assertNotNull(responseEntity);
+            assertEquals("Unexpected response: " + responseEntity.getBody(),
+                    expectedStatusCode, responseEntity.getStatusCode());
+            OrchestratorZoneResponse restResponse = responseEntity.getBody();
+            assertNotNull(restResponse);
+            String message = restResponse.getMessage();
+            assertEquals(expectedMessage, message);
+            return message;
+        } else {
+            log.debug("Request: {}", request);
+            ResponseEntity<String> response = tenantRestTemplate.exchange(url, method, request, String.class);
+            log.debug("Response: {}", response);
+            assertNotNull(response);
+            assertEquals("Unexpected response: " + response.getBody(), expectedStatusCode, response.getStatusCode());
+            return response.getBody();
         }
     }
 }
