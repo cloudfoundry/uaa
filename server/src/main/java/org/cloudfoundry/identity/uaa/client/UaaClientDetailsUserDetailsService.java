819d274ca9aac1c0bf396e799a780e40d496da19
Markus Strehle <11627201+strehle@users.noreply.github.com>
2023-09-14 11:06:54 +0200

    refactor: prepare for private_key_jwt in oauth_client_details (#2433)
    * refactor: prepare for private_key_jwt in oauth_client_details
    
    BaseClientDetails from spring security oauth2 cannot be changed, therefore more to UaaClientDetails for client details load
    
    * Add column client_jwt_config and do some refactoring for UaaClientDetails usage
    
    * Sonar findings
    
    * cleanup
    
    * remove not needed method.
    
    Even if UaaClientDetails is used the addClientDetails method can be used and therefore it does not make sense to have 2 add methods
    
    * review

--- server/src/main/java/org/cloudfoundry/identity/uaa/client/UaaClientDetailsUserDetailsService.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/client/UaaClientDetailsUserDetailsService.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.NoSuchClientException;
 
@@ -25,9 +24,9 @@ public class UaaClientDetailsUserDetailsService implements UserDetailsService {
   }
 
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    ClientDetails clientDetails;
+    UaaClientDetails clientDetails;
     try {
-      clientDetails = clientDetailsService.loadClientByClientId(username);
+      clientDetails = (UaaClientDetails) clientDetailsService.loadClientByClientId(username);
     } catch (NoSuchClientException e) {
       throw new UsernameNotFoundException(e.getMessage(), e);
     }
