734710692232233d588beb1050d998ce91619d13
Danny Faught <dfaught@vmware.com>
2023-09-15 16:27:09 -0700

    refactor testSimpleSamlPhpLoginDisplaysLastLogin to use page objects

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/pageObjects/HomePage.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/pageObjects/HomePage.java
@@ -1,17 +1,27 @@
 package org.cloudfoundry.identity.uaa.integration.pageObjects;
 
 import org.hamcrest.Matchers;
+import org.joda.time.DateTime;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.opensaml.xml.encryption.Public;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.endsWith;
+import static org.junit.Assert.assertNotNull;
 
 public class HomePage extends Page {
     public HomePage(WebDriver driver) {
         super(driver);
         assertThat("Should be on the home page", driver.getCurrentUrl(), endsWith("/"));
         assertThat(driver.getPageSource(), Matchers.containsString("Where to?"));
+    }
+
+    public boolean hasLastLoginTime() {
+        WebElement lastLoginTime = driver.findElement(By.id("last_login_time"));
+        String loginTime = lastLoginTime.getText();
+        return loginTime != null && ! loginTime.isBlank();
     }
 }
 
