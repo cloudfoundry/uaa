e66421da6652e95a2fa912def9a0213322014046
Danny Faught <dfaught@vmware.com>
2023-09-26 15:23:32 -0700

    fix: simplesamlphp server logout URL everywhere
    * Already fixed in one place, these are the other copypastas of the same thing.
    
    Co-authored-by: Peter Chen <peterch@vmware.com>

---
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/pageObjects/SamlWelcomePage.java
@@ -0,0 +1,19 @@
+package org.cloudfoundry.identity.uaa.integration.pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.endsWith;
+
+public class SamlWelcomePage extends Page {
+    static final protected String urlPath = "module.php/core/welcome";
+
+    public SamlWelcomePage(WebDriver driver) {
+        super(driver);
+        validateUrl(driver, endsWith(urlPath));
+    }
+
+}
+
