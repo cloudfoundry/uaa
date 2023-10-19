4fba1e4c0142691ab25cb313d60dd26b53774686
Markus Strehle <11627201+strehle@users.noreply.github.com>
2023-10-19 08:18:39 +0200

    fix sonar findings (#2527)
    Potential resource leaks

--- server/src/main/java/org/cloudfoundry/identity/uaa/util/PasswordValidatorUtil.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/util/PasswordValidatorUtil.java
@@ -15,7 +15,6 @@
 
 package org.cloudfoundry.identity.uaa.util;
 
-import org.apache.commons.io.IOUtils;
 import org.cloudfoundry.identity.uaa.authentication.GenericPasswordPolicy;
 import org.passay.CharacterRule;
 import org.passay.EnglishCharacterData;
@@ -33,20 +32,17 @@ import java.util.Properties;
 
 public final class PasswordValidatorUtil {
 
+    private PasswordValidatorUtil() {}
+
     public static PropertiesMessageResolver messageResolver(String messagesResourcePath) {
         final Properties props = new Properties();
-        InputStream in = null;
-        try {
-            in = PasswordValidatorUtil.class.getResourceAsStream(
-                    messagesResourcePath);
+        try (InputStream in = PasswordValidatorUtil.class.getResourceAsStream(messagesResourcePath)) {
             props.load(in);
             return new PropertiesMessageResolver(props);
         } catch (Exception e) {
             throw new IllegalStateException(
                     "Error loading default message properties.",
                     e);
-        } finally {
-            IOUtils.closeQuietly(in); // null safe
         }
     }
 
