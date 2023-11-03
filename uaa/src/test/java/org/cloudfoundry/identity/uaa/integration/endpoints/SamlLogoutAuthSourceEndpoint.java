b6948abc22764f63863e482bfeb583632cf7304b
Danny Faught <dfaught@vmware.com>
2023-11-03 16:48:54 -0700

    refactor: move endpoint classes to a separate package
    * They're associated with web API endpoints, so they don't belong with the pages.

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/endpoints/SamlLogoutAuthSourceEndpoint.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/endpoints/SamlLogoutAuthSourceEndpoint.java
@@ -1,13 +1,11 @@
-package org.cloudfoundry.identity.uaa.integration.pageObjects;
+package org.cloudfoundry.identity.uaa.integration.endpoints;
 
+import org.cloudfoundry.identity.uaa.integration.pageObjects.Page;
+import org.cloudfoundry.identity.uaa.integration.pageObjects.SamlWelcomePage;
 import org.openqa.selenium.WebDriver;
 
-public class SamlLogoutAuthSourceEndpoint extends Page {
+public class SamlLogoutAuthSourceEndpoint {
     static final private String urlPath = "/module.php/core/logout";
-
-    public SamlLogoutAuthSourceEndpoint(WebDriver driver) {
-        super(driver);
-    }
 
     static public SamlWelcomePage logoutAuthSource_goesToSamlWelcomePage(WebDriver driver, String baseUrl, String authSource) {
         driver.get(baseUrl + urlPath + "/" + authSource);
