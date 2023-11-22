b7addddca85e00c65b1e0c5ff81a731d2e1b4dcf
D023954 <klaus.kiefer@sap.com>
2023-11-22 12:14:47 +0100

    add testResultingAllowedGroups

--- model/src/test/java/org/cloudfoundry/identity/uaa/zone/UserConfigTest.java
+++ model/src/test/java/org/cloudfoundry/identity/uaa/zone/UserConfigTest.java
@@ -3,6 +3,7 @@ package org.cloudfoundry.identity.uaa.zone;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -11,10 +12,19 @@ public class UserConfigTest {
     @Test
     public void testDefaultConfig() {
         UserConfig userConfig = new UserConfig();
-        assertTrue(userConfig.getDefaultGroups() != null);
-        assertTrue(userConfig.getDefaultGroups().contains("uaa.user"));
+        assertTrue(userConfig.getDefaultGroups().contains("openid"));
         assertTrue(userConfig.getAllowedGroups() == null);       // all groups allowed
         assertTrue(userConfig.resultingAllowedGroups() == null); // all groups allowed
+    }
+
+    @Test
+    public void testResultingAllowedGroups() {
+        UserConfig userConfig = new UserConfig();
+        userConfig.setDefaultGroups(List.of("openid"));
+        userConfig.setAllowedGroups(List.of("uaa.user"));
+        assertTrue(userConfig.getDefaultGroups().equals(List.of("openid")));
+        assertTrue(userConfig.getAllowedGroups().equals(List.of("uaa.user")));
+        assertTrue(userConfig.resultingAllowedGroups().equals(Set.of("openid", "uaa.user")));
     }
 
     @Test
@@ -23,10 +33,8 @@ public class UserConfigTest {
         userConfig.setDefaultGroups(null);
         userConfig.setAllowedGroups(List.of("uaa.user"));
         assertTrue(userConfig.getDefaultGroups() == null);
-        assertTrue(userConfig.getAllowedGroups() != null);
-        assertTrue(userConfig.getAllowedGroups().contains("uaa.user"));
-        assertTrue(userConfig.resultingAllowedGroups() != null);
-        assertTrue(userConfig.resultingAllowedGroups().contains("uaa.user"));
+        assertTrue(userConfig.getAllowedGroups().equals(List.of("uaa.user")));
+        assertTrue(userConfig.resultingAllowedGroups().equals(Set.of("uaa.user")));
     }
 
     @Test
