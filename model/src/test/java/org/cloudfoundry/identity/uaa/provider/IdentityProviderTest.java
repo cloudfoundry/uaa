d329dcb19797b5bb6bcb94b879cd46a68901486a
Adrian Hoelzl <adrian.hoelzl@sap.com>
2023-12-22 11:10:21 +0100

    Add unit test for IdentityProvider.equals and hashCode

--- model/src/test/java/org/cloudfoundry/identity/uaa/provider/IdentityProviderTest.java
+++ model/src/test/java/org/cloudfoundry/identity/uaa/provider/IdentityProviderTest.java
@@ -30,4 +30,32 @@ class IdentityProviderTest {
         assertThat(idp).hasToString("IdentityProvider{id='12345', originKey='some-origin', name='some-name', type='oidc1.0', active=true, aliasId='id-of-mirrored-idp', aliasZid='custom-zone'}");
     }
 
+    @Test
+    void testEqualsAndHashCode() {
+        final IdentityProvider<OIDCIdentityProviderDefinition> idp2 = new IdentityProvider<>();
+        idp2.setId("12345");
+        idp2.setName("some-name");
+        idp2.setOriginKey("some-origin");
+        idp2.setAliasZid("custom-zone");
+        idp2.setAliasId("id-of-mirrored-idp");
+        idp2.setActive(true);
+        idp2.setIdentityZoneId(UAA);
+        final OIDCIdentityProviderDefinition config = new OIDCIdentityProviderDefinition();
+        config.setIssuer("issuer");
+        idp2.setConfig(config);
+
+        idp2.setCreated(idp.getCreated());
+        idp2.setLastModified(idp.getLastModified());
+
+        assertThat(idp.equals(idp2)).isTrue();
+        assertThat(idp).hasSameHashCodeAs(idp2);
+
+        idp2.setAliasZid(null);
+        assertThat(idp.equals(idp2)).isFalse();
+
+        idp2.setAliasZid("custom-zone");
+        idp2.setAliasId(null);
+        assertThat(idp.equals(idp2)).isFalse();
+    }
+
 }
