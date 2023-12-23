23734c64e0c1b87069f22c1e5245f3a40bf0c498
Danny Faught <danny.faught@broadcom.com>
2023-12-22 18:13:04 -0800

    refactor: optimize imports

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/pageObjects/CustomErrorPage.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/pageObjects/CustomErrorPage.java
@@ -3,8 +3,6 @@ package org.cloudfoundry.identity.uaa.integration.pageObjects;
 import org.hamcrest.Matcher;
 import org.openqa.selenium.WebDriver;
 
-import static org.hamcrest.Matchers.endsWith;
-
 public class CustomErrorPage extends Page {
 
     public CustomErrorPage(WebDriver driver, Matcher urlMatcher) {
