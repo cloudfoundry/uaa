b53bcc1445cfbf1ad6ac85f9ba3c8f1a5d47375f
Hongchol Sinn <hongchol.sinn@broadcom.com>
2024-01-03 18:25:30 -0800

    Replace SAML2 bearer grant integration test cases with a mock mvc test.
    - The integration test was getting the IDP metadata from actual UAA server, which needs be bypassed as UAA does not have the SAML IDP endpoint anymore.
    - With that, the existing integration test basically becomes the same as `TokenEndpointDocs.getTokenUsingSaml2BearerGrant()`.
    - So removed the existing integration test cases and created a new mock mvc test based on the `TokenEndpointDocs` code.
    
    [#182118433]
    
    Co-authored-by: Bruce Ricard <bruce.ricard@broadcom.com>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/SamlLoginIT.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/SamlLoginIT.java
@@ -14,10 +14,8 @@ package org.cloudfoundry.identity.uaa.integration.feature;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.URI;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -29,15 +27,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.test.TestAccounts;
 import org.springframework.security.oauth2.common.util.RandomValueStringGenerator;
 import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
@@ -91,7 +86,6 @@ import org.openqa.selenium.WebElement;
 import org.opensaml.saml2.core.AuthnContext;
 import org.opensaml.xml.ConfigurationException;
 
-import static org.cloudfoundry.identity.uaa.authentication.AbstractClientParametersAuthenticationFilter.CLIENT_SECRET;
 import static org.cloudfoundry.identity.uaa.integration.util.IntegrationTestUtils.SAML_AUTH_SOURCE;
 import static org.cloudfoundry.identity.uaa.integration.util.IntegrationTestUtils.SIMPLESAMLPHP_LOGIN_PROMPT_XPATH_EXPR;
 import static org.cloudfoundry.identity.uaa.integration.util.IntegrationTestUtils.createSimplePHPSamlIDP;
@@ -100,7 +94,6 @@ import static org.cloudfoundry.identity.uaa.integration.util.IntegrationTestUtil
 import static org.cloudfoundry.identity.uaa.integration.util.IntegrationTestUtils.isMember;
 import static org.cloudfoundry.identity.uaa.oauth.token.ClaimConstants.USER_ATTRIBUTES;
 import static org.cloudfoundry.identity.uaa.oauth.token.TokenConstants.GRANT_TYPE_AUTHORIZATION_CODE;
-import static org.cloudfoundry.identity.uaa.oauth.token.TokenConstants.GRANT_TYPE_SAML2_BEARER;
 import static org.cloudfoundry.identity.uaa.provider.ExternalIdentityProviderDefinition.EMAIL_ATTRIBUTE_NAME;
 import static org.cloudfoundry.identity.uaa.provider.ExternalIdentityProviderDefinition.GROUP_ATTRIBUTE_NAME;
 import static org.cloudfoundry.identity.uaa.provider.ExternalIdentityProviderDefinition.USER_ATTRIBUTE_PREFIX;
@@ -115,11 +108,8 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
-import static org.springframework.security.oauth2.common.util.OAuth2Utils.CLIENT_ID;
-import static org.springframework.security.oauth2.common.util.OAuth2Utils.GRANT_TYPE;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = DefaultIntegrationTestConfig.class)
@@ -992,115 +982,6 @@ public class SamlLoginIT {
         assertThat(managerData, containsInAnyOrder(JOHN_THE_SLOTH, KARI_THE_ANT_EATER));
 
     }
-
-//    @Test
-//    public void two_zone_saml_bearer_grant_url_metadata() throws Exception {
-//        Map<String, Object> claims = two_zone_saml_bearer_grant(true, "testzone4");
-//        String samlClientId = (String)claims.get(ClaimConstants.CLIENT_ID);
-//        ArrayList<String> auds = (ArrayList<String>)claims.get(ClaimConstants.AUD);
-//        Assert.assertTrue("aud claim must contain own client id: " + samlClientId, auds.contains(samlClientId));
-//    }
-
-//    @Test
-//    public void two_zone_saml_bearer_grant_xml_metadata() throws Exception {
-//        assertNotNull(two_zone_saml_bearer_grant(false, "testzone3"));
-//    }
-
-//    public Map<String, Object> two_zone_saml_bearer_grant(boolean urlMetadata, String zoneName) throws Exception {
-//        //ensure we are able to resolve DNS for hostname testzone1.localhost
-//        String zoneUrl = baseUrl.replace("localhost", zoneName +".localhost");
-//
-//
-//        //identity client token
-//        RestTemplate identityClient = IntegrationTestUtils.getClientCredentialsTemplate(
-//            IntegrationTestUtils.getClientCredentialsResource(baseUrl, new String[]{"zones.write", "zones.read", "scim.zones"}, "identity", "identitysecret")
-//        );
-//
-//        String adminToken = IntegrationTestUtils.getClientCredentialsToken(serverRunning, "admin", "adminsecret");
-//
-//        //create the zone
-//
-//        IdentityZone zone = IntegrationTestUtils.createZoneOrUpdateSubdomain(identityClient, baseUrl,
-//                zoneName,
-//                zoneName, null);
-//
-//        String idpMetadataUrl = zoneUrl + "/saml/idp/metadata";
-//
-//        String idpMetadata = urlMetadata ? idpMetadataUrl : new RestTemplate().getForObject(idpMetadataUrl, String.class);
-//
-//        String idpOrigin = zone.getSubdomain() + ".cloudfoundry-saml-login";
-//
-//        String uaaZoneId = IdentityZone.getUaaZoneId();
-//        SamlIdentityProviderDefinition def = new SamlIdentityProviderDefinition()
-//            .setZoneId(uaaZoneId)
-//            .setMetaDataLocation(idpMetadata)
-//            .setNameID("urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress")
-//            .setAssertionConsumerIndex(0)
-//            .setMetadataTrustCheck(false)
-//            .setShowSamlLink(false)
-//            .setIdpEntityAlias(idpOrigin);
-//
-//        IdentityProvider provider = new IdentityProvider();
-//        provider.setConfig(def);
-//        provider.setActive(true);
-//        provider.setIdentityZoneId(uaaZoneId);
-//        provider.setName(idpOrigin);
-//        provider.setOriginKey(idpOrigin);
-//
-//        IntegrationTestUtils.createOrUpdateProvider(adminToken, baseUrl, provider);
-//
-//        String clientId = new RandomValueStringGenerator().generate().toLowerCase();
-//        String username = "saml2bearerUser";
-//        BaseClientDetails saml2BearerClient = new BaseClientDetails(clientId,
-//                                                                    null,
-//                                                                    "openid",
-//                                                                    GRANT_TYPE_SAML2_BEARER,
-//                                                                    "uaa.resource",
-//                                                                    null);
-//        saml2BearerClient.setAutoApproveScopes(Collections.singletonList("true"));
-//        saml2BearerClient.setClientSecret("secret");
-//        saml2BearerClient = IntegrationTestUtils.createClient(adminToken, baseUrl, saml2BearerClient);
-//
-//
-//        String audienceEntityID = "cloudfoundry-saml-login";
-//        String spAudienceEndpoint = baseUrl + "/oauth/token/alias/"+audienceEntityID;
-//        String assertion = samlTestUtils.mockAssertionEncoded(
-//            zone.getSubdomain()+".cloudfoundry-saml-login",
-//            "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified",
-//            username,
-//            spAudienceEndpoint,
-//            audienceEntityID
-//        );
-
-//        MultiValueMap<String, String> postBody = new LinkedMultiValueMap<>();
-//        postBody.add(CLIENT_ID, saml2BearerClient.getClientId());
-//        postBody.add(CLIENT_SECRET, "secret");
-//        postBody.add(GRANT_TYPE, GRANT_TYPE_SAML2_BEARER);
-//        postBody.add("assertion", assertion);
-
-//        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
-//        headers.add(HttpHeaders.ACCEPT, APPLICATION_JSON_VALUE);
-//        headers.add(HttpHeaders.CONTENT_TYPE, APPLICATION_FORM_URLENCODED_VALUE);
-
-//        System.out.println("curl"+
-//            " -H\"Accept: application/json\""+
-//            " -d \"client_id="+saml2BearerClient.getClientId()+"\""+
-//            " -d \"client_secret=secret\""+
-//            " -d \"grant_type=urn:ietf:params:oauth:grant-type:saml2-bearer\"" +
-//            " -d \"assertion="+assertion+"\"" +
-//            " " + spAudienceEndpoint+"\n\n"
-//        );
-
-//        ResponseEntity<String> response = new RestTemplate().exchange(new URI(spAudienceEndpoint), HttpMethod.POST, new HttpEntity<MultiValueMap>(postBody, headers), String.class);
-//
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//        Map<String, Object> tokenResponse = JsonUtils.readValue(response.getBody(), new TypeReference<Map<String, Object>>() {
-//        });
-//        String jwtAccessToken = (String)tokenResponse.get("access_token");
-//        assertNotNull("Expecting access_token to be present in response", jwtAccessToken);
-//        Jwt idTokenClaims = JwtHelper.decode(jwtAccessToken);
-//        return JsonUtils.readValue(idTokenClaims.getClaims(), new TypeReference<Map<String, Object>>() { });
-//    }
 
     @Test
     public void testSamlLogin_Email_In_ID_Token_When_UserID_IsNotEmail() {
