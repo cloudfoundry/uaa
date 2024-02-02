1326570906df4c73645de2744cd46222e65a52a8
Adrian Hoelzl <adrian.hoelzl@sap.com>
2024-02-02 13:32:58 +0100

    Add getter for maxUsers in ScimUserEndpoints

--- server/src/main/java/org/cloudfoundry/identity/uaa/scim/endpoints/ScimUserEndpoints.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/scim/endpoints/ScimUserEndpoints.java
@@ -93,6 +93,8 @@ import java.util.stream.Collectors;
 import static org.cloudfoundry.identity.uaa.codestore.ExpiringCodeType.REGISTRATION;
 import static org.springframework.util.StringUtils.isEmpty;
 
+import lombok.Getter;
+
 /**
  * User provisioning and query endpoints. Implements the core API from the
  * Simple Cloud Identity Management (SCIM)
@@ -122,6 +124,7 @@ public class ScimUserEndpoints implements InitializingBean, ApplicationEventPubl
     private final UserMfaCredentialsProvisioning mfaCredentialsProvisioning;
     private final ApprovalStore approvalStore;
     private final ScimGroupMembershipManager membershipManager;
+    @Getter
     private final int userMaxCount;
     private final HttpMessageConverter<?>[] messageConverters;
     private final AtomicInteger scimUpdates;
