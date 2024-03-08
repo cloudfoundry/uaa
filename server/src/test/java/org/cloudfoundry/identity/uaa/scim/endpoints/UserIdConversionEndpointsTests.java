2644f537be1d123a4c5db04877070d1a112963c4
Adrian Hoelzl <adrian.hoelzl@sap.com>
2024-03-08 10:21:53 +0100

    Rework

--- server/src/test/java/org/cloudfoundry/identity/uaa/scim/endpoints/UserIdConversionEndpointsTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/scim/endpoints/UserIdConversionEndpointsTests.java
@@ -16,13 +16,10 @@ package org.cloudfoundry.identity.uaa.scim.endpoints;
 import static java.util.Collections.singletonList;
 import static java.util.UUID.randomUUID;
 import static java.util.stream.Collectors.toList;
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -36,16 +33,14 @@ import org.cloudfoundry.identity.uaa.scim.ScimUser;
 import org.cloudfoundry.identity.uaa.scim.ScimUserProvisioning;
 import org.cloudfoundry.identity.uaa.scim.exception.ScimException;
 import org.cloudfoundry.identity.uaa.security.beans.SecurityContextAccessor;
-import org.cloudfoundry.identity.uaa.zone.IdentityZone;
-import org.cloudfoundry.identity.uaa.zone.IdentityZoneHolder;
 import org.cloudfoundry.identity.uaa.zone.beans.IdentityZoneManager;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-import org.mockito.MockedStatic;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -54,74 +49,48 @@ import org.springframework.security.core.authority.AuthorityUtils;
  * @author Dave Syer
  * @author Luke Taylor
  */
-public class UserIdConversionEndpointsTests {
-
-    @Rule
-    public ExpectedException expected = ExpectedException.none();
-
+@ExtendWith(MockitoExtension.class)
+class UserIdConversionEndpointsTests {
     private UserIdConversionEndpoints endpoints;
 
+    @Mock
     private SecurityContextAccessor mockSecurityContextAccessor;
-
-    private final ScimUserEndpoints scimUserEndpoints = mock(ScimUserEndpoints.class);
-
-    private final ScimUserProvisioning scimUserProvisioning = mock(ScimUserProvisioning.class);
-
-    private final MockedStatic<IdentityZoneHolder> idzHolderMockedStatic;
-    private final IdentityZoneManager identityZoneManager = mock(IdentityZoneManager.class);
+    @Mock
+    private ScimUserEndpoints scimUserEndpoints;
+    @Mock
+    private ScimUserProvisioning scimUserProvisioning;
+    @Mock
+    private IdentityZoneManager identityZoneManager;
 
     @SuppressWarnings("rawtypes")
     private final Collection authorities = AuthorityUtils
             .commaSeparatedStringToAuthorityList("orgs.foo,uaa.user");
 
-    public UserIdConversionEndpointsTests() {
-        this.idzHolderMockedStatic = mockStatic(IdentityZoneHolder.class);
-    }
-
     @SuppressWarnings("unchecked")
-    @Before
+    @BeforeEach
     public void init() {
-        mockSecurityContextAccessor = mock(SecurityContextAccessor.class);
         endpoints = new UserIdConversionEndpoints(mockSecurityContextAccessor, scimUserEndpoints, scimUserProvisioning, identityZoneManager, true);
-        when(mockSecurityContextAccessor.getAuthorities()).thenReturn(authorities);
-        when(mockSecurityContextAccessor.getAuthenticationInfo()).thenReturn("mock object");
-        when(scimUserEndpoints.getUserMaxCount()).thenReturn(10_000);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        idzHolderMockedStatic.close();
+        lenient().when(mockSecurityContextAccessor.getAuthorities()).thenReturn(authorities);
+        lenient().when(mockSecurityContextAccessor.getAuthenticationInfo()).thenReturn("mock object");
+        lenient().when(scimUserEndpoints.getUserMaxCount()).thenReturn(10_000);
     }
 
     @Test
-    public void testHappyDay() {
+    void testHappyDay() {
         arrangeCurrentIdentityZone("uaa");
-        endpoints.findUsers("userName eq \"marissa\"", "ascending", 0, 100, false);
+        assertThatNoException()
+                .isThrownBy(() -> endpoints.findUsers("userName eq \"marissa\"", "ascending", 0, 100, false));
     }
 
     @Test
-    public void testSanitizeExceptionInFilter() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(is("Invalid filter '&lt;svg onload=alert(document.domain)&gt;'"));
-        endpoints.findUsers("<svg onload=alert(document.domain)>", "ascending", 0, 100, false);
+    void testSanitizeExceptionInFilter() {
+        assertThatExceptionOfType(ScimException.class)
+                .isThrownBy(() -> endpoints.findUsers("<svg onload=alert(document.domain)>", "ascending", 0, 100, false))
+                .withMessage("Invalid filter '&lt;svg onload=alert(document.domain)&gt;'");
     }
 
     @Test
-    public void testBadFieldInFilter() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Invalid filter"));
-        endpoints.findUsers("emails.value eq \"foo@bar.org\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testBadFilterWithGroup() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Invalid filter"));
-        endpoints.findUsers("groups.display eq \"foo\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testGoodFilter_IncludeInactive() {
+    void testGoodFilter_IncludeInactive() {
         final String idzId = randomUUID().toString();
         arrangeCurrentIdentityZone(idzId);
 
@@ -148,7 +117,7 @@ public class UserIdConversionEndpointsTests {
     }
 
     @Test
-    public void testGoodFilter_OnlyActive() {
+    void testGoodFilter_OnlyActive() {
         final String idzId = randomUUID().toString();
         arrangeCurrentIdentityZone(idzId);
 
@@ -168,27 +137,14 @@ public class UserIdConversionEndpointsTests {
         assertEndpointReturnsCorrectResult(filter, 10, expectedUsers, false);
     }
 
-    @Test
-    public void testGoodFilter1() {
-        testGoodFilter("(id eq \"foo\" or username eq \"bar\") and origin eq \"uaa\"");
-    }
-
-    @Test
-    public void testGoodFilter2() {
-        testGoodFilter("origin eq \"uaa\" and (id eq \"foo\" or username eq \"bar\")");
-    }
-
-    @Test
-    public void testGoodFilter3() {
-        testGoodFilter("(id eq \"foo\" and username eq \"bar\") or id eq \"bar\"");
-    }
-
-    @Test
-    public void testGoodFilter4() {
-        testGoodFilter("id eq \"bar\" and (id eq \"foo\" and username eq \"bar\")");
-    }
-
-    private void testGoodFilter(final String filter) {
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "(id eq \"foo\" or username eq \"bar\") and origin eq \"uaa\"",
+            "origin eq \"uaa\" and (id eq \"foo\" or username eq \"bar\")",
+            "(id eq \"foo\" and username eq \"bar\") or id eq \"bar\"",
+            "id eq \"bar\" and (id eq \"foo\" and username eq \"bar\")"
+    })
+    void testGoodFilter(final String filter) {
         arrangeCurrentIdentityZone("uaa");
         final ResponseEntity<Object> response = endpoints.findUsers(
                 filter,
@@ -197,109 +153,74 @@ public class UserIdConversionEndpointsTests {
                 100,
                 false
         );
-        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "id co \"foo\"",
+            "id sw \"foo\"",
+            "id pr",
+            "id eq \"foo\" or origin co \"uaa\""
+    })
+    void testBadFilter_WildcardsNotAllowed(final String filter) {
+        assertThatExceptionOfType(ScimException.class)
+                .isThrownBy(() -> endpoints.findUsers(filter, "ascending", 0, 100, false))
+                .withMessage("Wildcards are not allowed in filter.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "id gt \"foo\"",
+            "id le \"foo\"",
+            "id lt \"foo\""
+    })
+    void testBadFilter_UnsupportedOperator(final String filter) {
+        assertThatExceptionOfType(ScimException.class)
+                .isThrownBy(() -> endpoints.findUsers(filter, "ascending", 0, 100, false))
+                .withMessage("Invalid operator.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "id sq \"foo\""
+    })
+    void testBadFilter_UnrecognizedOperator(final String filter) {
+        assertThatExceptionOfType(ScimException.class)
+                .isThrownBy(() -> endpoints.findUsers(filter, "ascending", 0, 100, false))
+                .withMessageStartingWith("Invalid filter '");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "origin eq \"uaa\"",
+            "emails.value eq \"foo@bar.org\"",
+            "origin eq \"uaa\" or origin eq \"bar\"",
+            "groups.display eq \"foo\""
+    })
+    void testBadFilter_DoesNotContainClauseWithIdOrUserName(final String filter) {
+        assertThatExceptionOfType(ScimException.class)
+                .isThrownBy(() -> endpoints.findUsers(filter, "ascending", 0, 100, false))
+                .withMessage("Invalid filter attribute.");
     }
 
     @Test
-    public void testBadFilter1() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Wildcards are not allowed in filter."));
-        endpoints.findUsers("id co \"foo\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testBadFilter2() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Invalid filter"));
-        endpoints.findUsers("id sq \"foo\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testBadFilter3() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Wildcards are not allowed in filter."));
-        endpoints.findUsers("id sw \"foo\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testBadFilter4() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Wildcards are not allowed in filter."));
-        endpoints.findUsers("id pr", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testBadFilter5() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Invalid operator."));
-        endpoints.findUsers("id gt \"foo\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testBadFilter6() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Invalid operator."));
-        endpoints.findUsers("id gt \"foo\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testBadFilter7() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Invalid operator."));
-        endpoints.findUsers("id lt \"foo\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testBadFilter8() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Invalid operator."));
-        endpoints.findUsers("id le \"foo\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testBadFilter9() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Invalid filter"));
-        endpoints.findUsers("origin eq \"uaa\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testBadFilter10() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Wildcards are not allowed in filter."));
-
-        // illegal operator in right operand of root-level "or" -> all branches need to be checked even if left operands are valid
-        endpoints.findUsers("id eq \"foo\" or origin co \"uaa\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testBadFilter11() {
-        expected.expect(ScimException.class);
-        expected.expectMessage(containsString("Invalid filter"));
-        endpoints.findUsers("origin eq \"uaa\" or origin eq \"bar\"", "ascending", 0, 100, false);
-    }
-
-    @Test
-    public void testDisabled() {
-        arrangeCurrentIdentityZone("uaa");
+    void testDisabled() {
         endpoints = new UserIdConversionEndpoints(mockSecurityContextAccessor, scimUserEndpoints, scimUserProvisioning, identityZoneManager, false);
         ResponseEntity<Object> response = endpoints.findUsers("id eq \"foo\"", "ascending", 0, 100, false);
-        Assert.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        Assert.assertEquals("Illegal Operation: Endpoint not enabled.", response.getBody());
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        assertThat(response.getBody()).isEqualTo("Illegal Operation: Endpoint not enabled.");
     }
 
     @Test
-    public void noActiveIdps_ReturnsEmptyResources() {
+    void noActiveIdps_ReturnsEmptyResources() {
         arrangeCurrentIdentityZone("uaa");
         SearchResults<?> searchResults = (SearchResults<?>) endpoints.findUsers("username eq \"foo\"", "ascending", 0, 100, false).getBody();
-        assertTrue(searchResults.getResources().isEmpty());
+        assertThat(searchResults).isNotNull();
+        assertThat(searchResults.getResources()).isEmpty();
     }
 
     private void arrangeCurrentIdentityZone(final String idzId) {
-        final IdentityZone identityZone = new IdentityZone();
-        identityZone.setId(idzId);
-        idzHolderMockedStatic.when(IdentityZoneHolder::get).thenReturn(identityZone);
-        idzHolderMockedStatic.when(IdentityZoneHolder::getCurrentZoneId).thenReturn(idzId);
         when(identityZoneManager.getCurrentIdentityZoneId()).thenReturn(idzId);
     }
 
