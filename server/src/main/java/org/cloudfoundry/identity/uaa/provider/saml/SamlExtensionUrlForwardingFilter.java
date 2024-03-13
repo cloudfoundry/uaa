50196cfe873f1f44ca5c8080840a10ac3eb80d7f
Alicia Yingling <yalicia@vmware.com>
2024-03-13 13:42:14 -0500

    feat: Supply metadata through /saml/metadata
    - Adds back endpoint and incorporates forwarding for new pattern saml2 endpoints, Still has some wip elements WithHttpsNotRequired > samlMetadataReturnsOk still red RelyingPartyRegistration is hardcoded in xml, /saml/metadata/ with trailing slash not working missing parity with develop
    
    [#186986697]
    
    Co-authored-by: Peter Chen <peter-h.chen@broadcom.com>

--- server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/SamlExtensionUrlForwardingFilter.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/SamlExtensionUrlForwardingFilter.java
@@ -47,9 +47,6 @@ public class SamlExtensionUrlForwardingFilter extends OncePerRequestFilter {
             return;
         }
         String forwardUrl = urlMapping.get(request.getServletPath());
-        if (forwardUrl == null) {
-            forwardUrl = urlMapping.get(request.getRequestURI());
-        }
         RequestDispatcher dispatcher = request.getRequestDispatcher(forwardUrl);
         dispatcher.forward(request, response);
     }
