cloudfoundry/uaa

commit d10922a8f606b7d4ed8a7eb6bc585d9226e70f9e

delete: saml/idp directory & code
* Comment out / delete code to get the project to work
* status: all unit tests passing (./gradlew test), local server starts (./gradlew run) successfully with the UAA login page working in browser

[#182118433]

Co-authored-by: Alicia Yingling <alicia.yingling@broadcom.com>
Co-authored-by: Bruce Ricard <bruce.ricard@broadcom.com>
Co-authored-by: Danny Faught <danny.faught@broadcom.com>
Co-authored-by: Peter Chen <peter-h.chen@broadcom.com>

uaa/src/main/webapp/WEB-INF/spring/saml-providers.xml
@@ -19,7 +19,7 @@
             <security:filter-chain pattern="/saml/login/**" filters="samlEntryPoint"/>
             <security:filter-chain pattern="/saml/logout/**" filters="samlLogoutFilter"/>
             <security:filter-chain pattern="/saml/metadata/**" filters="metadataDisplayFilter"/>
-            <security:filter-chain pattern="/saml/idp/metadata/**" filters="idpMetadataDisplayFilter"/>
+<!--            <security:filter-chain pattern="/saml/idp/metadata/**" filters="idpMetadataDisplayFilter"/>-->
             <security:filter-chain pattern="/saml/SSO/**"
                                    filters="samlSecurityContextPersistenceFilter,samlWebSSOProcessingFilter"/>
             <security:filter-chain pattern="/saml/SingleLogout/**"

server/src/main/java/org/cloudfoundry/identity/uaa/provider/SamlServiceProviderEndpoints.java
@@ -1,123 +1,123 @@
-package org.cloudfoundry.identity.uaa.provider;
-
-import org.cloudfoundry.identity.uaa.provider.saml.idp.SamlServiceProvider;
-import org.cloudfoundry.identity.uaa.provider.saml.idp.SamlServiceProviderConfigurator;
-import org.cloudfoundry.identity.uaa.provider.saml.idp.SamlServiceProviderProvisioning;
-import org.cloudfoundry.identity.uaa.provider.saml.idp.SamlSpAlreadyExistsException;
-import org.cloudfoundry.identity.uaa.util.JsonUtils;
-import org.cloudfoundry.identity.uaa.zone.IdentityZoneHolder;
-import org.opensaml.saml2.metadata.provider.MetadataProviderException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-import static org.springframework.http.HttpStatus.OK;
-import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
-import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-import static org.springframework.web.bind.annotation.RequestMethod.PUT;
-
-@RequestMapping("/saml/service-providers")
-@RestController
-public class SamlServiceProviderEndpoints {
-
-    protected static Logger logger = LoggerFactory.getLogger(SamlServiceProviderEndpoints.class);
-
-    private final SamlServiceProviderProvisioning serviceProviderProvisioning;
-    private final SamlServiceProviderConfigurator samlConfigurator;
-
-    public SamlServiceProviderEndpoints(
-            final @Qualifier("serviceProviderProvisioning") SamlServiceProviderProvisioning serviceProviderProvisioning,
-            final @Qualifier("spMetaDataProviders") SamlServiceProviderConfigurator samlConfigurator) {
-        this.serviceProviderProvisioning = serviceProviderProvisioning;
-        this.samlConfigurator = samlConfigurator;
-    }
-
-    @RequestMapping(method = POST)
-    public ResponseEntity<SamlServiceProvider> createServiceProvider(@RequestBody SamlServiceProvider body)
-            throws MetadataProviderException {
-        String zoneId = IdentityZoneHolder.get().getId();
-        body.setIdentityZoneId(zoneId);
-        samlConfigurator.validateSamlServiceProvider(body);
-        SamlServiceProvider createdSp = serviceProviderProvisioning.create(body, zoneId);
-        return new ResponseEntity<>(createdSp, HttpStatus.CREATED);
-    }
-
-    @RequestMapping(value = "{id}", method = PUT)
-    public ResponseEntity<SamlServiceProvider> updateServiceProvider(@PathVariable String id,
-                                                                     @RequestBody SamlServiceProvider body) throws MetadataProviderException {
-        SamlServiceProvider existing = serviceProviderProvisioning.retrieve(id, IdentityZoneHolder.get().getId());
-        String zoneId = IdentityZoneHolder.get().getId();
-        body.setId(id);
-        body.setIdentityZoneId(zoneId);
-        if (!body.configIsValid()) {
-            return new ResponseEntity<>(UNPROCESSABLE_ENTITY);
-        }
-        body.setEntityId(existing.getEntityId());
-
-        samlConfigurator.validateSamlServiceProvider(body);
-
-        SamlServiceProvider updatedSp = serviceProviderProvisioning.update(body, zoneId);
-        return new ResponseEntity<>(updatedSp, OK);
-    }
-
-    @RequestMapping(method = GET)
-    public ResponseEntity<List<SamlServiceProvider>> retrieveServiceProviders(
-            @RequestParam(value = "active_only", required = false) String activeOnly) {
-        boolean retrieveActiveOnly = Boolean.parseBoolean(activeOnly);
-        List<SamlServiceProvider> serviceProviderList =
-                serviceProviderProvisioning.retrieveAll(retrieveActiveOnly,
-                        IdentityZoneHolder.get().getId());
-        return new ResponseEntity<>(serviceProviderList, OK);
-    }
-
-    @RequestMapping(value = "{id}", method = GET)
-    public ResponseEntity<SamlServiceProvider> retrieveServiceProvider(@PathVariable String id) {
-        SamlServiceProvider serviceProvider = serviceProviderProvisioning.retrieve(id, IdentityZoneHolder.get().getId());
-        return new ResponseEntity<>(serviceProvider, OK);
-    }
-
-    @RequestMapping(value = "{id}", method = DELETE)
-    public ResponseEntity<SamlServiceProvider> deleteServiceProvider(@PathVariable String id) {
-        SamlServiceProvider serviceProvider = serviceProviderProvisioning.retrieve(id, IdentityZoneHolder.get().getId());
-        serviceProviderProvisioning.delete(id, IdentityZoneHolder.get().getId());
-        return new ResponseEntity<>(serviceProvider, OK);
-    }
-
-    @ExceptionHandler(MetadataProviderException.class)
-    public ResponseEntity<String> handleMetadataProviderException(MetadataProviderException e) {
-        if (e.getMessage().contains("Duplicate")) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
-        } else {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @ExceptionHandler(JsonUtils.JsonUtilException.class)
-    public ResponseEntity<String> handleMetadataProviderException() {
-        return new ResponseEntity<>("Invalid provider configuration.", HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity<String> handleProviderNotFoundException() {
-        return new ResponseEntity<>("Provider not found.", HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(SamlSpAlreadyExistsException.class)
-    public ResponseEntity<String> handleDuplicateServiceProvider() {
-        return new ResponseEntity<>("SAML SP with the same entity id already exists.", HttpStatus.CONFLICT);
-    }
-
-}
+//package org.cloudfoundry.identity.uaa.provider;
+//
+////import org.cloudfoundry.identity.uaa.provider.saml.idp.SamlServiceProvider;
+////import org.cloudfoundry.identity.uaa.provider.saml.idp.SamlServiceProviderConfigurator;
+////import org.cloudfoundry.identity.uaa.provider.saml.idp.SamlServiceProviderProvisioning;
+////import org.cloudfoundry.identity.uaa.provider.saml.idp.SamlSpAlreadyExistsException;
+//import org.cloudfoundry.identity.uaa.util.JsonUtils;
+//import org.cloudfoundry.identity.uaa.zone.IdentityZoneHolder;
+//import org.opensaml.saml2.metadata.provider.MetadataProviderException;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.dao.EmptyResultDataAccessException;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//import static org.springframework.http.HttpStatus.OK;
+//import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+//import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
+//import static org.springframework.web.bind.annotation.RequestMethod.GET;
+//import static org.springframework.web.bind.annotation.RequestMethod.POST;
+//import static org.springframework.web.bind.annotation.RequestMethod.PUT;
+//
+//@RequestMapping("/saml/service-providers")
+//@RestController
+//public class SamlServiceProviderEndpoints {
+//
+//    protected static Logger logger = LoggerFactory.getLogger(SamlServiceProviderEndpoints.class);
+//
+//    private final SamlServiceProviderProvisioning serviceProviderProvisioning;
+//    private final SamlServiceProviderConfigurator samlConfigurator;
+//
+//    public SamlServiceProviderEndpoints(
+//            final @Qualifier("serviceProviderProvisioning") SamlServiceProviderProvisioning serviceProviderProvisioning,
+//            final @Qualifier("spMetaDataProviders") SamlServiceProviderConfigurator samlConfigurator) {
+//        this.serviceProviderProvisioning = serviceProviderProvisioning;
+//        this.samlConfigurator = samlConfigurator;
+//    }
+//
+//    @RequestMapping(method = POST)
+//    public ResponseEntity<SamlServiceProvider> createServiceProvider(@RequestBody SamlServiceProvider body)
+//            throws MetadataProviderException {
+//        String zoneId = IdentityZoneHolder.get().getId();
+//        body.setIdentityZoneId(zoneId);
+//        samlConfigurator.validateSamlServiceProvider(body);
+//        SamlServiceProvider createdSp = serviceProviderProvisioning.create(body, zoneId);
+//        return new ResponseEntity<>(createdSp, HttpStatus.CREATED);
+//    }
+//
+//    @RequestMapping(value = "{id}", method = PUT)
+//    public ResponseEntity<SamlServiceProvider> updateServiceProvider(@PathVariable String id,
+//                                                                     @RequestBody SamlServiceProvider body) throws MetadataProviderException {
+//        SamlServiceProvider existing = serviceProviderProvisioning.retrieve(id, IdentityZoneHolder.get().getId());
+//        String zoneId = IdentityZoneHolder.get().getId();
+//        body.setId(id);
+//        body.setIdentityZoneId(zoneId);
+//        if (!body.configIsValid()) {
+//            return new ResponseEntity<>(UNPROCESSABLE_ENTITY);
+//        }
+//        body.setEntityId(existing.getEntityId());
+//
+//        samlConfigurator.validateSamlServiceProvider(body);
+//
+//        SamlServiceProvider updatedSp = serviceProviderProvisioning.update(body, zoneId);
+//        return new ResponseEntity<>(updatedSp, OK);
+//    }
+//
+//    @RequestMapping(method = GET)
+//    public ResponseEntity<List<SamlServiceProvider>> retrieveServiceProviders(
+//            @RequestParam(value = "active_only", required = false) String activeOnly) {
+//        boolean retrieveActiveOnly = Boolean.parseBoolean(activeOnly);
+//        List<SamlServiceProvider> serviceProviderList =
+//                serviceProviderProvisioning.retrieveAll(retrieveActiveOnly,
+//                        IdentityZoneHolder.get().getId());
+//        return new ResponseEntity<>(serviceProviderList, OK);
+//    }
+//
+//    @RequestMapping(value = "{id}", method = GET)
+//    public ResponseEntity<SamlServiceProvider> retrieveServiceProvider(@PathVariable String id) {
+//        SamlServiceProvider serviceProvider = serviceProviderProvisioning.retrieve(id, IdentityZoneHolder.get().getId());
+//        return new ResponseEntity<>(serviceProvider, OK);
+//    }
+//
+//    @RequestMapping(value = "{id}", method = DELETE)
+//    public ResponseEntity<SamlServiceProvider> deleteServiceProvider(@PathVariable String id) {
+//        SamlServiceProvider serviceProvider = serviceProviderProvisioning.retrieve(id, IdentityZoneHolder.get().getId());
+//        serviceProviderProvisioning.delete(id, IdentityZoneHolder.get().getId());
+//        return new ResponseEntity<>(serviceProvider, OK);
+//    }
+//
+//    @ExceptionHandler(MetadataProviderException.class)
+//    public ResponseEntity<String> handleMetadataProviderException(MetadataProviderException e) {
+//        if (e.getMessage().contains("Duplicate")) {
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+//        } else {
+//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+//        }
+//    }
+//
+//    @ExceptionHandler(JsonUtils.JsonUtilException.class)
+//    public ResponseEntity<String> handleMetadataProviderException() {
+//        return new ResponseEntity<>("Invalid provider configuration.", HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @ExceptionHandler(EmptyResultDataAccessException.class)
+//    public ResponseEntity<String> handleProviderNotFoundException() {
+//        return new ResponseEntity<>("Provider not found.", HttpStatus.NOT_FOUND);
+//    }
+//
+//    @ExceptionHandler(SamlSpAlreadyExistsException.class)
+//    public ResponseEntity<String> handleDuplicateServiceProvider() {
+//        return new ResponseEntity<>("SAML SP with the same entity id already exists.", HttpStatus.CONFLICT);
+//    }
+//
+//}

server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/idp/IdpExtendedMetadata.java
@@ -1,46 +1,0 @@
-/*******************************************************************************
- *     Cloud Foundry
- *     Copyright (c) [2009-2016] Pivotal Software, Inc. All Rights Reserved.
- *
- *     This product is licensed to you under the Apache License, Version 2.0 (the "License").
- *     You may not use this product except in compliance with the License.
- *
- *     This product includes a number of subcomponents with
- *     separate copyright notices and license terms. Your use of these
- *     subcomponents is subject to the terms and conditions of the
- *     subcomponent's license, as noted in the LICENSE file.
- *******************************************************************************/
-package org.cloudfoundry.identity.uaa.provider.saml.idp;
-
-import org.springframework.security.saml.metadata.ExtendedMetadata;
-
-/**
- * A SAML IdP needs information beyond what the standard ExtendedMetadata provides.
- * This class exists to provide that extra information.
- */
-public class IdpExtendedMetadata extends ExtendedMetadata {
-
-    private boolean assertionsSigned = true;
-    private int assertionTimeToLiveSeconds = 500;
-
-    public boolean isAssertionsSigned() {
-        return assertionsSigned;
-    }
-
-    public void setAssertionsSigned(boolean assertionsSigned) {
-        this.assertionsSigned = assertionsSigned;
-    }
-
-    public int getAssertionTimeToLiveSeconds() {
-        return assertionTimeToLiveSeconds;
-    }
-
-    public void setAssertionTimeToLiveSeconds(int assertionTimeToLiveSeconds) {
-        this.assertionTimeToLiveSeconds = assertionTimeToLiveSeconds;
-    }
-
-    @Override
-    public IdpExtendedMetadata clone() {
-        return (IdpExtendedMetadata) super.clone();
-    }
-}

commit af42c14372d18f4b727c453ede05e075332b5c63

fix: saml signatureAlgorithm in AuthnRequest
* This fixes a regression introduced in commit https://github.com/cloudfoundry/uaa/commit/d10922a8f606b7d4ed8a7eb6bc585d9226e70f9e where we stopped reading the signatureAlgorithm from the properties file and only used the SHA1 default.

[#187250012]

Co-authored-by: Hongchol Sinn <hongchol.sinn@broadcom.com>

server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/SamlConfigurationBean.java
@@ -27,6 +27,10 @@ public class SamlConfigurationBean implements InitializingBean {
     signatureAlgorithm = s;
   }
 
+  public SignatureAlgorithm getSignatureAlgorithm() {
+    return signatureAlgorithm;
+  }
+
   @Override
   public void afterPropertiesSet() {
     BasicSecurityConfiguration config = (BasicSecurityConfiguration) Configuration.getGlobalSecurityConfiguration();

uaa/src/main/webapp/WEB-INF/spring/saml-providers.xml
@@ -301,6 +301,10 @@
 
     <bean id="fixedHttpMetaDataProvider" class="org.cloudfoundry.identity.uaa.provider.saml.FixedHttpMetaDataProvider" />
 
+    <bean id="defaultSamlConfig" class="org.cloudfoundry.identity.uaa.provider.saml.SamlConfigurationBean">
+        <property name="signatureAlgorithm" value="${login.saml.signatureAlgorithm:SHA1}"/>
+    </bean>
+
     <beans profile="fileMetadata">
         <bean id="metaDataUrl" class="java.lang.String">
             <constructor-arg value="${login.idpMetadataFile:null}"/>

uaa/src/test/java/org/cloudfoundry/identity/uaa/login/BootstrapTests.java
@@ -6,6 +6,7 @@ import org.cloudfoundry.identity.uaa.impl.config.IdentityZoneConfigurationBootst
 import org.cloudfoundry.identity.uaa.impl.config.YamlServletProfileInitializer;
 import org.cloudfoundry.identity.uaa.provider.SamlIdentityProviderDefinition;
 import org.cloudfoundry.identity.uaa.provider.saml.BootstrapSamlIdentityProviderData;
+import org.cloudfoundry.identity.uaa.provider.saml.SamlConfigurationBean;
 import org.cloudfoundry.identity.uaa.scim.ScimGroup;
 import org.cloudfoundry.identity.uaa.scim.ScimGroupProvisioning;
 import org.cloudfoundry.identity.uaa.util.PredicateMatcher;
@@ -20,6 +21,11 @@ import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.ResourceEntityResolver;
@@ -42,6 +48,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -49,6 +56,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class SystemPropertiesCleanupExtension implements BeforeAllCallback, AfterAllCallback {
 
@@ -165,6 +173,28 @@ class BootstrapTests {
         assertEquals(
                 SamlIdentityProviderDefinition.MetadataLocation.URL,
                 defs.get(defs.size() - 1).getType()
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("samlSignatureParameterProvider")
+    void samlSignatureAlgorithm(String yamlFile, SamlConfigurationBean.SignatureAlgorithm algorithm) {
+        // When we override the SHA1 default for login.saml.signatureAlgorithm in the yaml, make sure it works.
+        context = getServletContext("default", yamlFile);
+
+        SamlConfigurationBean samlConfig = context.getBean("defaultSamlConfig", SamlConfigurationBean.class);
+        assertEquals(
+                algorithm,
+                samlConfig.getSignatureAlgorithm(),
+                "The SAML signature algorithm in the yaml file is set in the bean"
+        );
+    }
+
+    static Stream<Arguments> samlSignatureParameterProvider() {
+        final String yamlPath = "test/config/";
+        return Stream.of(
+                arguments(yamlPath + "saml-algorithm-sha256.yml", SamlConfigurationBean.SignatureAlgorithm.SHA256),
+                arguments(yamlPath + "saml-algorithm-sha512.yml", SamlConfigurationBean.SignatureAlgorithm.SHA512)
         );
     }
 

uaa/src/test/resources/test/config/saml-algorithm-sha256.yml
@@ -1,0 +1,3 @@
+login:
+  saml:
+    signatureAlgorithm: SHA256

uaa/src/test/resources/test/config/saml-algorithm-sha512.yml
@@ -1,0 +1,3 @@
+login:
+  saml:
+    signatureAlgorithm: SHA512