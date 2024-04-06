a885d2fac961788d6cc8c8da313b008d5f7d1c72
d036670 <markus.strehle@sap.com>
2024-04-06 12:28:49 +0200

    Import move of Client interfaces

--- server/src/test/java/org/cloudfoundry/identity/uaa/oauth/ClientAccessTokenValidityTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/oauth/ClientAccessTokenValidityTest.java
@@ -2,6 +2,8 @@ package org.cloudfoundry.identity.uaa.oauth;
 
 import org.cloudfoundry.identity.uaa.extensions.PollutionPreventionExtension;
 import org.cloudfoundry.identity.uaa.login.util.RandomValueStringGenerator;
+import org.cloudfoundry.identity.uaa.oauth.provider.ClientDetails;
+import org.cloudfoundry.identity.uaa.oauth.provider.ClientRegistrationException;
 import org.cloudfoundry.identity.uaa.zone.IdentityZone;
 import org.cloudfoundry.identity.uaa.zone.IdentityZoneConfiguration;
 import org.cloudfoundry.identity.uaa.zone.MultitenantClientServices;
@@ -16,8 +18,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.cloudfoundry.identity.uaa.oauth.provider.ClientDetails;
-import org.springframework.security.oauth2.provider.ClientRegistrationException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
