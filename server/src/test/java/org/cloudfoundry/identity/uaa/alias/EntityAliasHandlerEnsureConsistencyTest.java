767c967bb7630a0dcc772e73d5baaea1093818a3
Adrian Hoelzl <adrian.hoelzl@sap.com>
2024-04-11 10:29:42 +0200

    Add inner classes for different combinations of existing/non-existing alias and alias feature enabled/disabled

--- server/src/test/java/org/cloudfoundry/identity/uaa/alias/EntityAliasHandlerEnsureConsistencyTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/alias/EntityAliasHandlerEnsureConsistencyTest.java
@@ -6,4 +6,16 @@ import org.cloudfoundry.identity.uaa.EntityWithAlias;
 
 public abstract class EntityAliasHandlerEnsureConsistencyTest<T extends EntityWithAlias> {
     protected final String customZoneId = UUID.randomUUID().toString();
+
+    protected abstract class NoExistingAlias_AliasFeatureEnabled {
+    }
+
+    protected abstract class NoExistingAlias_AliasFeatureDisabled {
+    }
+
+    protected abstract class ExistingAlias_AliasFeatureEnabled {
+    }
+
+    protected abstract class ExistingAlias_AliasFeatureDisabled {
+    }
 }
