900c423d8da401cfbd51dfa242618a26f954a8f0
Alicia Yingling <yalicia@vmware.com>
2024-04-16 14:49:52 -0500

    wip: updating to non forwarding for /saml/metadata to the example default
    - Updated to use direct GetMapping
    
    [#186986697]
    
    Co-authored-by: Filip Hanik <fhanik@vmware.com>

--- server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/SamlConfiguration.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/SamlConfiguration.java
@@ -1,39 +1,8 @@
 package org.cloudfoundry.identity.uaa.provider.saml;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.saml2.provider.service.metadata.OpenSamlMetadataResolver;
-import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
-import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
-import org.springframework.security.saml2.provider.service.web.DefaultRelyingPartyRegistrationResolver;
-import org.springframework.security.saml2.provider.service.web.Saml2MetadataFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SamlConfiguration {
-    public static final String AGGREGATE_SPRING_SECURITY_FILTER_CHAIN_ID = "aggregateSpringSecurityFilterChain";
-
-//    @Bean(AGGREGATE_SPRING_SECURITY_FILTER_CHAIN_ID)
-    @Bean(name = AGGREGATE_SPRING_SECURITY_FILTER_CHAIN_ID)
-    @Lazy
-    Saml2MetadataFilter aggregateSpringSecurityFilterChain(
-//            WebSecurityConfiguration webSecurityConfiguration,
-            @Autowired RelyingPartyRegistrationRepository relyingPartyRegistrationRepository) {
-
-        Converter<HttpServletRequest, RelyingPartyRegistration> relyingPartyRegistrationResolver =
-                new DefaultRelyingPartyRegistrationResolver(relyingPartyRegistrationRepository);
-        Saml2MetadataFilter filter = new Saml2MetadataFilter(
-                relyingPartyRegistrationResolver,
-                new OpenSamlMetadataResolver());
-        filter.setRequestMatcher(new AntPathRequestMatcher("/saml/metadata/**/{registrationId}", "GET"));
-
-        return filter;
-    }
 
 }
