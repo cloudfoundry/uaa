5449fc2a50e8f00e291f3a21696eaef80ebff072
Alicia Yingling <yalicia@vmware.com>
2024-04-16 10:16:03 -0500

    wip: ensuring the saml metadata endpoint for metadata works in Mock MVC Tests
    - /saml/metadata/ is not returning xml
    
    [#186986697]
    
    Co-authored-by: Filip Hanik <fhanik@vmware.com>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/saml/SamlMetadataMockMvcTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/saml/SamlMetadataMockMvcTests.java
@@ -1,61 +1,18 @@
 package org.cloudfoundry.identity.uaa.mock.saml;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.function.Consumer;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.security.oauth2.common.util.RandomValueStringGenerator;
-import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.web.context.WebApplicationContext;
-
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.appender.AbstractAppender;
-import org.apache.logging.log4j.core.config.Configurator;
 import org.cloudfoundry.identity.uaa.DefaultTestContext;
-import org.cloudfoundry.identity.uaa.audit.LoggingAuditService;
-import org.cloudfoundry.identity.uaa.authentication.SamlResponseLoggerBinding;
-import org.cloudfoundry.identity.uaa.constants.OriginKeys;
-import org.cloudfoundry.identity.uaa.mock.util.InterceptingLogger;
-import org.cloudfoundry.identity.uaa.mock.util.MockMvcUtils;
-import org.cloudfoundry.identity.uaa.provider.IdentityProvider;
-import org.cloudfoundry.identity.uaa.provider.JdbcIdentityProviderProvisioning;
-import org.cloudfoundry.identity.uaa.provider.SamlIdentityProviderDefinition;
-import org.cloudfoundry.identity.uaa.scim.ScimUser;
-import org.cloudfoundry.identity.uaa.scim.jdbc.JdbcScimUserProvisioning;
-import org.cloudfoundry.identity.uaa.zone.IdentityZone;
-import org.cloudfoundry.identity.uaa.zone.IdentityZoneHolder;
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Nested;
+//import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.owasp.esapi.ESAPI;
-import org.owasp.esapi.reference.DefaultSecurityConfiguration;
-import org.slf4j.Logger;
 
-import static org.apache.logging.log4j.Level.DEBUG;
-import static org.apache.logging.log4j.Level.WARN;
-import static org.cloudfoundry.identity.uaa.authentication.SamlResponseLoggerBinding.X_VCAP_REQUEST_ID_HEADER;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.emptyOrNullString;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.not;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.HttpHeaders.HOST;
+import static java.util.function.Predicate.not;
+//import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.hamcrest.text.IsEmptyString.emptyOrNullString;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -73,16 +30,29 @@ class SamlMetadataMockMvcTests {
     }
 
     @Test
+    void testSamlMetadataDefaultNoEndingSlash() throws Exception {
+        mockMvc.perform(get(new URI("/saml/metadata/example")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testSamlMetadataDefaultWithEndingSlash() throws Exception {
+        mockMvc.perform(get(new URI("/saml/metadata/example/")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+//    @Disabled("Returning a 404, but it curls 200 and payload look good. It should not be a forwardedURL but direct")
     void testSamlMetadataDefault() throws Exception {
         ResultActions response = null;
 
-        ResultActions xml = mockMvc.perform(get(new URI("/saml/metadata/example/")))
+        ResultActions xml = mockMvc.perform(get(new URI("/saml/metadata/example")))
                 .andExpect(status().isOk());
 //                .andExpect(content().string(not(emptyOrNullString())));
 
         String x = xml.andReturn().getResponse().getContentAsString();
-        int y = 4;
-//                    .andExpect(xpath("/md:EntityDescriptor/@entityID").string("cloudfoundry-saml-login"));
+//        int y = 4;
+//                    andExpect(xpath("/md:EntityDescriptor/@entityID").string("cloudfoundry-saml-login"));
 
 
 //            xpath("...ds:DigestMethod/@Algorithm").string("http://www.w3.org/2001/04/xmlenc#sha256");
