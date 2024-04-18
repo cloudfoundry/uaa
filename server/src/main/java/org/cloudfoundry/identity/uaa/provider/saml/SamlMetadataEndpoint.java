8aa2fd91a1682798ec70efd7df2ce7d554f3ac2b
Alicia Yingling <yalicia@vmware.com>
2024-04-18 15:54:02 -0500

    wip: Adding in signature elements for SAML metadata.xml endpoint payload
    - Need to fix credential type being empty
    
    Caused by: java.lang.IllegalArgumentException: credentials types cannot be empty
    ....(SamlRelyingPartyRegistrationRepository.java:84)
    [#186986697]
    
    Co-authored-by: Duane May <duane.may@broadcom.com>

--- server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/SamlMetadataEndpoint.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/SamlMetadataEndpoint.java
@@ -6,6 +6,9 @@ import org.opensaml.saml.common.xml.SAMLConstants;
 import org.opensaml.saml.saml2.metadata.EntityDescriptor;
 import org.opensaml.saml.saml2.metadata.SPSSODescriptor;
 import org.opensaml.saml.saml2.metadata.impl.IDPSSODescriptorBuilder;
+import org.opensaml.xmlsec.signature.XMLSignatureBuilder;
+import org.opensaml.xmlsec.signature.impl.SignatureImpl;
+import org.opensaml.xmlsec.signature.support.SignatureConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.convert.converter.Converter;
@@ -29,12 +32,19 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.crypto.dsig.*;
+import javax.xml.crypto.dsig.keyinfo.KeyInfo;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.util.List;
 import java.util.function.Consumer;
 
 import org.opensaml.saml.saml2.metadata.SPSSODescriptor;
+import org.opensaml.xmlsec.signature.Signature;
 
 
 
@@ -60,6 +70,39 @@ public class SamlMetadataEndpoint {
             SPSSODescriptor spssodescriptor = descriptor.getSPSSODescriptor(SAMLConstants.SAML20P_NS);
             spssodescriptor.setWantAssertionsSigned(true);
             spssodescriptor.setAuthnRequestsSigned(true);
+//            try {
+//                XMLSignatureFactory xmlSignatureFactory = XMLSignatureFactory.getInstance("DOM", "XMLDSig");
+//                CanonicalizationMethod c14nMethod = xmlSignatureFactory.newCanonicalizationMethod("http://www.w3.org/2001/10/xml-exc-c14n#", null);
+//                DigestMethod digestMethod = xmlSignatureFactory.newDigestMethod("http://www.w3.org/2001/04/xmlenc#sha256", null);
+//                SignatureMethod signMethod = xmlSignatureFactory.newSignatureMethod("http://www.w3.org/2001/04/xmldsig-more#rsa-sha512", null);
+//
+//                List<Transform> transforms = List.of(
+//                        xmlSignatureFactory.newTransform("http://www.w3.org/2000/09/xmldsig#enveloped-signature", null),
+//                        xmlSignatureFactory.newTransform("http://www.w3.org/2001/10/xml-exc-c14n#", null)
+//                );
+//
+//                Reference referenceDoc = xmlSignatureFactory.newReference("", digestMethod, transforms, null, null);
+//                List<Reference> references = List.of(referenceDoc);
+//
+//                SignedInfo signedInfo = xmlSignatureFactory.newSignedInfo(c14nMethod, signMethod, references);
+//                KeyInfo keyInfo = createKeyInfo(xmlSignatureFactory);
+//                XMLSignature xmlSignature = xmlSignatureFactory.newXMLSignature(signedInfo, keyInfo, null, null, null);
+//
+//
+//            } catch (NoSuchProviderException e) {
+//                throw new RuntimeException(e);
+//            } catch (InvalidAlgorithmParameterException e) {
+//                throw new RuntimeException(e);
+//            } catch (NoSuchAlgorithmException e) {
+//                throw new RuntimeException(e);
+//            }
+////            XMLSignatureBuilder xmlSigBuilder = new XMLSignatureBuilder
+
+//            descriptor.setSignature(Signature new Signature());
+
+//            Signature signature = new SignatureImpl(SignatureConstants.XMLSIG_NS); //, "localName", "ds");
+//            Signature signature = descriptor.getSignature();
+//            signature.setSchemaLocation(SignatureConstants.XMLSIG_NS);
         }
     }
 
--- server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/SamlRelyingPartyRegistrationRepository.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/SamlRelyingPartyRegistrationRepository.java
@@ -1,17 +1,29 @@
 package org.cloudfoundry.identity.uaa.provider.saml;
 
 import org.apache.commons.io.IOUtils;
+//import org.hsqldb.lib.StringInputStream;
+import org.hsqldb.lib.StringInputStream;
 import org.opensaml.saml.saml2.metadata.SPSSODescriptor;
+import org.opensaml.xmlsec.signature.support.SignatureConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.saml2.core.Saml2X509Credential;
 import org.springframework.security.saml2.provider.service.registration.InMemoryRelyingPartyRegistrationRepository;
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrations;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+//import java.io.StringInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
 @Component
 public class SamlRelyingPartyRegistrationRepository {
 
@@ -32,12 +44,33 @@ public class SamlRelyingPartyRegistrationRepository {
     private String samlSpNameID;
 
     @Bean
-    RelyingPartyRegistrationRepository relyingPartyRegistrationRepository() {
+    RelyingPartyRegistrationRepository relyingPartyRegistrationRepository() throws CertificateException {
 
         String CLASSPATH_DUMMY_SAML_IDP_METADATA_XML = "classpath:dummy-saml-idp-metadata.xml";
         String samlEntityID = "integration-saml-entity-id";
         String samlNameIDFormat = "example-NAME_ID_FORMAT";
 
+//        X509Certificate cert = null;
+        String certString = new String("-----BEGIN CERTIFICATE-----\nMIIDSTCCArKgAwIBAgIBADANBgkqhkiG9w0BAQQFADB8MQswCQYDVQQGEwJhdzEOMAwGA1UECBMF\n" +
+        "                    YXJ1YmExDjAMBgNVBAoTBWFydWJhMQ4wDAYDVQQHEwVhcnViYTEOMAwGA1UECxMFYXJ1YmExDjAM\n" +
+        "                    BgNVBAMTBWFydWJhMR0wGwYJKoZIhvcNAQkBFg5hcnViYUBhcnViYS5hcjAeFw0xNTExMjAyMjI2\n" +
+        "                    MjdaFw0xNjExMTkyMjI2MjdaMHwxCzAJBgNVBAYTAmF3MQ4wDAYDVQQIEwVhcnViYTEOMAwGA1UE\n" +
+        "                    ChMFYXJ1YmExDjAMBgNVBAcTBWFydWJhMQ4wDAYDVQQLEwVhcnViYTEOMAwGA1UEAxMFYXJ1YmEx\n" +
+        "                    HTAbBgkqhkiG9w0BCQEWDmFydWJhQGFydWJhLmFyMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKB\n" +
+        "                    gQDHtC5gUXxBKpEqZTLkNvFwNGnNIkggNOwOQVNbpO0WVHIivig5L39WqS9u0hnA+O7MCA/KlrAR\n" +
+        "                    4bXaeVVhwfUPYBKIpaaTWFQR5cTR1UFZJL/OF9vAfpOwznoD66DDCnQVpbCjtDYWX+x6imxn8HCY\n" +
+        "                    xhMol6ZnTbSsFW6VZjFMjQIDAQABo4HaMIHXMB0GA1UdDgQWBBTx0lDzjH/iOBnOSQaSEWQLx1sy\n" +
+        "                    GDCBpwYDVR0jBIGfMIGcgBTx0lDzjH/iOBnOSQaSEWQLx1syGKGBgKR+MHwxCzAJBgNVBAYTAmF3\n" +
+        "                    MQ4wDAYDVQQIEwVhcnViYTEOMAwGA1UEChMFYXJ1YmExDjAMBgNVBAcTBWFydWJhMQ4wDAYDVQQL\n" +
+        "                    EwVhcnViYTEOMAwGA1UEAxMFYXJ1YmExHTAbBgkqhkiG9w0BCQEWDmFydWJhQGFydWJhLmFyggEA\n" +
+        "                    MAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEEBQADgYEAYvBJ0HOZbbHClXmGUjGs+GS+xC1FO/am\n" +
+        "                    2suCSYqNB9dyMXfOWiJ1+TLJk+o/YZt8vuxCKdcZYgl4l/L6PxJ982SRhc83ZW2dkAZI4M0/Ud3o\n" +
+        "                    ePe84k8jm3A7EvH5wi5hvCkKRpuRBwn3Ei+jCRouxTbzKPsuCVB+1sNyxMTXzf0=\n-----END CERTIFICATE-----");
+        InputStream stream = new ByteArrayInputStream(certString.getBytes(StandardCharsets.UTF_8));
+        CertificateFactory cf = CertificateFactory. getInstance("X.509");
+        X509Certificate cert = (X509Certificate) cf. generateCertificate(stream);
+
+        X509Certificate finalCert = cert;
         RelyingPartyRegistration relyingPartyRegistration = RelyingPartyRegistrations
                 .fromMetadataLocation(CLASSPATH_DUMMY_SAML_IDP_METADATA_XML)
                 .entityId(samlEntityID)
@@ -46,7 +79,11 @@ public class SamlRelyingPartyRegistrationRepository {
                 .assertingPartyDetails(details -> details
                         .entityId(samlEntityID)
                         .wantAuthnRequestsSigned(true)
-                        .signingAlgorithms(algos -> algos.add(""))
+                        .signingAlgorithms((sign) -> sign.add(SignatureConstants.ALGO_ID_SIGNATURE_RSA_SHA1))  // 512 by default?
+                        )
+                .signingX509Credentials( (cred) -> cred
+                        .add(new Saml2X509Credential(finalCert)
+                        )
                 )
                 .build();
 
