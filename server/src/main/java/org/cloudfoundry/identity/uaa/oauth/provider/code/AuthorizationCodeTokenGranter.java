1f0db2cbc4b36a638c51af52772da5858ce785ed
d036670 <markus.strehle@sap.com>
2024-04-19 14:41:04 +0200

    Remove duplicate Code
    PkceEnhancedAuthorizationCodeTokenGranter uses duplicated
    code from AuthorizationCodeTokenGranter

--- server/src/main/java/org/cloudfoundry/identity/uaa/oauth/provider/code/AuthorizationCodeTokenGranter.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/oauth/provider/code/AuthorizationCodeTokenGranter.java
@@ -1,12 +1,16 @@
 package org.cloudfoundry.identity.uaa.oauth.provider.code;
 
 import org.cloudfoundry.identity.uaa.oauth.common.util.OAuth2Utils;
+import org.cloudfoundry.identity.uaa.oauth.pkce.PkceValidationException;
+import org.cloudfoundry.identity.uaa.oauth.pkce.PkceValidationService;
 import org.cloudfoundry.identity.uaa.oauth.provider.OAuth2Authentication;
 import org.cloudfoundry.identity.uaa.oauth.provider.OAuth2Request;
 import org.cloudfoundry.identity.uaa.oauth.provider.OAuth2RequestFactory;
 import org.cloudfoundry.identity.uaa.oauth.provider.TokenRequest;
 import org.cloudfoundry.identity.uaa.oauth.provider.token.AbstractTokenGranter;
 import org.cloudfoundry.identity.uaa.oauth.provider.token.AuthorizationServerTokenServices;
+import org.cloudfoundry.identity.uaa.oauth.token.ClaimConstants;
+import org.cloudfoundry.identity.uaa.util.UaaSecurityContextUtils;
 import org.springframework.security.core.Authentication;
 import org.cloudfoundry.identity.uaa.oauth.common.exceptions.InvalidClientException;
 import org.cloudfoundry.identity.uaa.oauth.common.exceptions.InvalidGrantException;
@@ -45,6 +49,11 @@ public class AuthorizationCodeTokenGranter extends AbstractTokenGranter {
 
 	@Override
 	protected OAuth2Authentication getOAuth2Authentication(ClientDetails client, TokenRequest tokenRequest) {
+		return getOAuth2Authentication(client, tokenRequest, authorizationCodeServices, null);
+	}
+
+	protected static OAuth2Authentication getOAuth2Authentication(ClientDetails client, TokenRequest tokenRequest,
+			AuthorizationCodeServices authorizationCodeServices, PkceValidationService pkceService) {
 
 		Map<String, String> parameters = tokenRequest.getRequestParameters();
 		String authorizationCode = parameters.get("code");
@@ -56,14 +65,37 @@ public class AuthorizationCodeTokenGranter extends AbstractTokenGranter {
 
 		OAuth2Authentication storedAuth = authorizationCodeServices.consumeAuthorizationCode(authorizationCode);
 		if (storedAuth == null) {
-			throw new InvalidGrantException("Invalid authorization code");
+			throw new InvalidGrantException("Invalid authorization code: " + authorizationCode);
+		}
+
+		if (pkceService != null) {
+			/*
+			 * PKCE code verifier parameter length and charset validation
+			 */
+			String codeVerifier = parameters.get(PkceValidationService.CODE_VERIFIER);
+			if (codeVerifier != null && !PkceValidationService.isCodeVerifierParameterValid(codeVerifier)) {
+				throw new InvalidRequestException("Code verifier length must between 43 and 128 and use only [A-Z],[a-z],[0-9],_,.,-,~ characters.");
+			}
+			/*
+			 * PKCE code verifier parameter verification
+			 */
+			try {
+				if (!pkceService.checkAndValidate(storedAuth.getOAuth2Request().getRequestParameters(), codeVerifier, client)) {
+					// has PkceValidation service and validation failed
+					throw new InvalidGrantException("Invalid code verifier: " + codeVerifier);
+				}
+			} catch (PkceValidationException exception) {
+				// during the validation one of the PKCE parameters missing
+				throw new InvalidGrantException("PKCE error: "+ exception.getMessage());
+			}
+			// No pkceService defined or Pkce validation successfully passed
 		}
 
 		OAuth2Request pendingOAuth2Request = storedAuth.getOAuth2Request();
 		// https://jira.springsource.org/browse/SECOAUTH-333
-		// This might be null, if the authorization was done without the redirect_uri parameter
-		String redirectUriApprovalParameter = pendingOAuth2Request.getRequestParameters().get(
-				OAuth2Utils.REDIRECT_URI);
+		// This might be null, if the authorization was done without the redirect_uri
+		// parameter
+		String redirectUriApprovalParameter = pendingOAuth2Request.getRequestParameters().get(OAuth2Utils.REDIRECT_URI);
 
 		if ((redirectUri != null || redirectUriApprovalParameter != null)
 				&& !pendingOAuth2Request.getRedirectUri().equals(redirectUri)) {
@@ -76,23 +108,27 @@ public class AuthorizationCodeTokenGranter extends AbstractTokenGranter {
 			// just a sanity check.
 			throw new InvalidClientException("Client ID mismatch");
 		}
-
 		// Secret is not required in the authorization request, so it won't be available
-		// in the pendingAuthorizationRequest. We do want to check that a secret is provided
+		// in the pendingAuthorizationRequest. We do want to check that a secret is
+		// provided
 		// in the token request, but that happens elsewhere.
 
-		Map<String, String> combinedParameters = new HashMap<String, String>(pendingOAuth2Request
-				.getRequestParameters());
-		// Combine the parameters adding the new ones last so they override if there are any clashes
+		Map<String, String> combinedParameters = new HashMap<String, String>(
+				pendingOAuth2Request.getRequestParameters());
+		// Combine the parameters adding the new ones last so they override if there are
+		// any clashes
 		combinedParameters.putAll(parameters);
-		
+
 		// Make a new stored request with the combined parameters
 		OAuth2Request finalStoredOAuth2Request = pendingOAuth2Request.createOAuth2Request(combinedParameters);
-		
+
 		Authentication userAuth = storedAuth.getUserAuthentication();
-		
+
+		String clientAuthentication = UaaSecurityContextUtils.getClientAuthenticationMethod();
+		if (clientAuthentication != null) {
+			finalStoredOAuth2Request.getExtensions().put(ClaimConstants.CLIENT_AUTH_METHOD, clientAuthentication);
+		}
+
 		return new OAuth2Authentication(finalStoredOAuth2Request, userAuth);
-
 	}
-
 }
