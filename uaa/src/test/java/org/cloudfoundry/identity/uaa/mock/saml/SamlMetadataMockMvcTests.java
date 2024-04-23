3a40ff9c95da95bf6f97d8be6e55334dc93c3463
Duane May <duane.may@broadcom.com>
2024-04-23 11:26:13 -0400

    Refactor tests: formatting, andExpectAll and assertThat
    Use assertThat
    Use textblocks
    
    Co-authored-by: Duane May <duane.may@broadcom.com>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/saml/SamlMetadataMockMvcTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/saml/SamlMetadataMockMvcTests.java
@@ -5,8 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.cloudfoundry.identity.uaa.DefaultTestContext;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.Matchers.containsString;
@@ -20,10 +20,9 @@ class SamlMetadataMockMvcTests {
     @Autowired
     private MockMvc mockMvc;
 
-
     @Test
     void legacyMetadataRoot() throws Exception {
-        ResultActions xml = mockMvc.perform(get(new URI("/saml/metadata")))
+        mockMvc.perform(get(new URI("/saml/metadata")))
                 .andExpect(forwardedUrl("/saml/metadata/example"));
     }
 
@@ -38,7 +37,6 @@ class SamlMetadataMockMvcTests {
         mockMvc.perform(get(new URI("/saml/metadata/")))
                 .andExpect(status().isOk());
     }
-
 
     @Test
     void testSamlMetadataDefaultNoEndingSlash() throws Exception {
@@ -57,29 +55,33 @@ class SamlMetadataMockMvcTests {
 
         mockMvc.perform(get(new URI("/saml/metadata/example")))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_DISPOSITION, containsString("filename=\"saml-sp-metadata.xml\";")))
-                .andExpect(xpath("/EntityDescriptor/@entityID").string("integration-saml-entity-id")) // matches UAA config login.entityID
-                .andExpect(xpath("/EntityDescriptor/SPSSODescriptor/@AuthnRequestsSigned").booleanValue(true)) // matches UAA config login.saml.signRequest
-                .andExpect(xpath("/EntityDescriptor/SPSSODescriptor/@WantAssertionsSigned").booleanValue(true))
-                .andExpect(xpath("/EntityDescriptor/SPSSODescriptor/NameIDFormat").string("urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress")); // matches UAA config login.saml.NameID
+                .andExpectAll(
+                        status().isOk(),
+                        header().string(HttpHeaders.CONTENT_DISPOSITION, containsString("filename=\"saml-sp-metadata.xml\";")),
+                        xpath("/EntityDescriptor/@entityID").string("integration-saml-entity-id"), // matches UAA config login.entityID
+                        xpath("/EntityDescriptor/SPSSODescriptor/@AuthnRequestsSigned").booleanValue(true), // matches UAA config login.saml.signRequest
+                        xpath("/EntityDescriptor/SPSSODescriptor/@WantAssertionsSigned").booleanValue(true),
+                        xpath("/EntityDescriptor/SPSSODescriptor/NameIDFormat").string("urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress")  // matches UAA config login.saml.NameID
+                );
     }
-}
 
-@DefaultTestContext
-@TestPropertySource(properties = "login.saml.signRequest = false")
-class SamlMetadataAlternativeConfigsMockMvcTests {
+    @Nested
+    @DefaultTestContext
+    @TestPropertySource(properties = "login.saml.signRequest = false")
+    class SamlMetadataAlternativeConfigsMockMvcTests {
+        @Autowired
+        private MockMvc mockMvc;
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    void testSamlMetadataXMLValidation() throws Exception {
-        mockMvc.perform(get(new URI("/saml/metadata/example")))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.CONTENT_DISPOSITION, containsString("filename=\"saml-sp-metadata.xml\";")))
-                .andExpect(xpath("/EntityDescriptor/SPSSODescriptor/@AuthnRequestsSigned").booleanValue(false)) // matches UAA config login.saml.signRequest
-                .andExpect(xpath("/EntityDescriptor/SPSSODescriptor/@WantAssertionsSigned").booleanValue(true));
+        @Test
+        void testSamlMetadataAuthnRequestsSignedIsFalse() throws Exception {
+            mockMvc.perform(get(new URI("/saml/metadata/example")))
+                    .andDo(print())
+                    .andExpectAll(
+                            status().isOk(),
+                            header().string(HttpHeaders.CONTENT_DISPOSITION, containsString("filename=\"saml-sp-metadata.xml\";")),
+                            xpath("/EntityDescriptor/SPSSODescriptor/@AuthnRequestsSigned").booleanValue(false), // matches UAA config login.saml.signRequest
+                            xpath("/EntityDescriptor/SPSSODescriptor/@WantAssertionsSigned").booleanValue(true)
+                    );
+        }
     }
 }
