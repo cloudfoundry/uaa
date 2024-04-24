f2d399edbf001b8572e1fdbf34e05bf36bc22a65
Peter Chen <peter-h.chen@broadcom.com>
2024-04-23 18:49:52 -0700

    feat: saml sp metadata field - signing cert
    - also: refactor the UAA config used in mock mvc tests
    (/uaa/src/test/resources/integration_test_properties.yml)
    from the deprecated saml key fields (eg: login.serviceProviderKey)
    to the new ones (eg: login.saml.keys), so that we test for the
    new fields.
      - also fix the api docs test so that it now correctly marks
      the retrieve id zones response's `config.samlConfig.certificate`
      as optional (this field is only returned if you use the
      deprecated saml key config fields)
    
    [#186986697]
    
    Co-authored-by: Duane May <duane.may@broadcom.com>

--- server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/SamlRelyingPartyRegistrationRepository.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/provider/saml/SamlRelyingPartyRegistrationRepository.java
@@ -1,5 +1,7 @@
 package org.cloudfoundry.identity.uaa.provider.saml;
 
+import org.cloudfoundry.identity.uaa.saml.SamlKey;
+import org.cloudfoundry.identity.uaa.util.KeyWithCert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,18 +13,7 @@ import org.springframework.security.saml2.provider.service.registration.RelyingP
 import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrations;
 import org.springframework.stereotype.Component;
 
-import java.math.BigInteger;
-import java.security.NoSuchAlgorithmException;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.security.KeyFactory;
 import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.RSAPrivateKeySpec;
 
 @Component
 public class SamlRelyingPartyRegistrationRepository {
@@ -45,39 +36,14 @@ public class SamlRelyingPartyRegistrationRepository {
     @Value("${login.saml.signRequest:true}")
     private Boolean samlSignRequest;
 
+    @Autowired
+    private SamlKeysConfig samlKeysConfig;
+
     @Bean
-    RelyingPartyRegistrationRepository relyingPartyRegistrationRepository() throws CertificateException, NoSuchAlgorithmException, InvalidKeySpecException {
+    RelyingPartyRegistrationRepository relyingPartyRegistrationRepository() throws CertificateException {
 
-        String certString = """
-                -----BEGIN CERTIFICATE-----
-                MIIDSTCCArKgAwIBAgIBADANBgkqhkiG9w0BAQQFADB8MQswCQYDVQQGEwJhdzEOMAwGA1UECBMF
-                YXJ1YmExDjAMBgNVBAoTBWFydWJhMQ4wDAYDVQQHEwVhcnViYTEOMAwGA1UECxMFYXJ1YmExDjAM
-                BgNVBAMTBWFydWJhMR0wGwYJKoZIhvcNAQkBFg5hcnViYUBhcnViYS5hcjAeFw0xNTExMjAyMjI2
-                MjdaFw0xNjExMTkyMjI2MjdaMHwxCzAJBgNVBAYTAmF3MQ4wDAYDVQQIEwVhcnViYTEOMAwGA1UE
-                ChMFYXJ1YmExDjAMBgNVBAcTBWFydWJhMQ4wDAYDVQQLEwVhcnViYTEOMAwGA1UEAxMFYXJ1YmEx
-                HTAbBgkqhkiG9w0BCQEWDmFydWJhQGFydWJhLmFyMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKB
-                gQDHtC5gUXxBKpEqZTLkNvFwNGnNIkggNOwOQVNbpO0WVHIivig5L39WqS9u0hnA+O7MCA/KlrAR
-                4bXaeVVhwfUPYBKIpaaTWFQR5cTR1UFZJL/OF9vAfpOwznoD66DDCnQVpbCjtDYWX+x6imxn8HCY
-                xhMol6ZnTbSsFW6VZjFMjQIDAQABo4HaMIHXMB0GA1UdDgQWBBTx0lDzjH/iOBnOSQaSEWQLx1sy
-                GDCBpwYDVR0jBIGfMIGcgBTx0lDzjH/iOBnOSQaSEWQLx1syGKGBgKR+MHwxCzAJBgNVBAYTAmF3
-                MQ4wDAYDVQQIEwVhcnViYTEOMAwGA1UEChMFYXJ1YmExDjAMBgNVBAcTBWFydWJhMQ4wDAYDVQQL
-                EwVhcnViYTEOMAwGA1UEAxMFYXJ1YmExHTAbBgkqhkiG9w0BCQEWDmFydWJhQGFydWJhLmFyggEA
-                MAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEEBQADgYEAYvBJ0HOZbbHClXmGUjGs+GS+xC1FO/am
-                2suCSYqNB9dyMXfOWiJ1+TLJk+o/YZt8vuxCKdcZYgl4l/L6PxJ982SRhc83ZW2dkAZI4M0/Ud3o
-                ePe84k8jm3A7EvH5wi5hvCkKRpuRBwn3Ei+jCRouxTbzKPsuCVB+1sNyxMTXzf0=
-                -----END CERTIFICATE-----""";
-        InputStream stream = new ByteArrayInputStream(certString.getBytes(StandardCharsets.UTF_8));
-        CertificateFactory cf = CertificateFactory. getInstance("X.509");
-        X509Certificate cert = (X509Certificate) cf. generateCertificate(stream);
-
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-
-        RSAPrivateKeySpec privateKeySpec = new RSAPrivateKeySpec(
-                new BigInteger("57791d5430d593164082036ad8b29fb157791d5430d593164082036ad8b29fb157791d5430d593164082036ad8b29fb157791d5430d593164082036ad8b29fb1", 16),
-                new BigInteger("57791d5430d593164082036ad8b29fb157791d5430d593164082036ad8b29fb157791d5430d593164082036ad8b29fb157791d5430d593164082036ad8b29fb1", 16)
-        );
-
-        RSAPrivateKey privateKey = (RSAPrivateKey) keyFactory.generatePrivate(privateKeySpec);
+        SamlKey activeSamlKey = samlKeysConfig.getActiveSamlKey();
+        KeyWithCert keyWithCert = new KeyWithCert(activeSamlKey.getKey(), activeSamlKey.getPassphrase(), activeSamlKey.getCertificate());
 
         RelyingPartyRegistration relyingPartyRegistration = RelyingPartyRegistrations
                 .fromMetadataLocation(CLASSPATH_DUMMY_SAML_IDP_METADATA_XML)
@@ -88,7 +54,7 @@ public class SamlRelyingPartyRegistrationRepository {
                         .wantAuthnRequestsSigned(samlSignRequest)
                         )
                 .signingX509Credentials( cred -> cred
-                        .add(Saml2X509Credential.signing( privateKey, cert))
+                        .add(Saml2X509Credential.signing( keyWithCert.getPrivateKey(), keyWithCert.getCertificate()))
                 )
                 .build();
 
