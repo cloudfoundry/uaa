170f2241d212a932cabbcb0563495e4dad568bd2
Bruce Ricard <bruce.ricard@gmail.com>
2024-04-29 13:39:12 -0400

    Fix: performance issue in MySQL
    * Revert "Merge pull request #2704 from cloudfoundry/fix/optimize-ids-users-endpoint"
    
    * This reverts commit 1324a81f8948250e993193f4be62dc5b9854965a, reversing
      changes made to b8826e1456b98ae179b85cab9e5261a64edc2d25.
    
    * The original PR is suspected to cause the performance issues. We
      will try to bring this change back later.

--- server/src/main/java/org/cloudfoundry/identity/uaa/resources/jdbc/SimpleSearchQueryConverter.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/resources/jdbc/SimpleSearchQueryConverter.java
@@ -9,7 +9,6 @@ import org.cloudfoundry.identity.uaa.resources.SimpleAttributeNameMapper;
 import org.cloudfoundry.identity.uaa.util.AlphanumericRandomValueStringGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.lang.Nullable;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
@@ -126,12 +125,9 @@ public class SimpleSearchQueryConverter implements SearchQueryConverter {
         }
     }
 
-    /**
-     * @return the WHERE and (optional) ORDER BY clauses WITHOUT the "WHERE" keyword in the beginning
-     */
     private String getWhereClause(
-            @Nullable final String filter,
-            @Nullable final String sortBy,
+            final String filter,
+            final String sortBy,
             final boolean ascending,
             final Map<String, Object> values,
             final AttributeNameMapper mapper,
