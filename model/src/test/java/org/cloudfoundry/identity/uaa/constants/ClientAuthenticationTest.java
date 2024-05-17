cd40ecab5db63f486ecc7af5c63679bc00a9bd63
d036670 <markus.strehle@sap.com>
2024-05-17 12:10:25 +0200

    Test refactored

--- model/src/test/java/org/cloudfoundry/identity/uaa/constants/ClientAuthenticationTest.java
+++ model/src/test/java/org/cloudfoundry/identity/uaa/constants/ClientAuthenticationTest.java
@@ -27,19 +27,26 @@ class ClientAuthenticationTest {
   }
 
   @Test
-  void isValidMethod() {
+  void isValidMethodTrue() {
     assertTrue(ClientAuthentication.isValidMethod(NONE, false, false));
     assertTrue(ClientAuthentication.isValidMethod(PRIVATE_KEY_JWT, false, true));
     assertTrue(ClientAuthentication.isValidMethod(CLIENT_SECRET_POST, true, false));
     assertTrue(ClientAuthentication.isValidMethod(CLIENT_SECRET_BASIC, true, false));
-    assertFalse(ClientAuthentication.isValidMethod(CLIENT_SECRET_BASIC, false, false));
-    assertFalse(ClientAuthentication.isValidMethod(CLIENT_SECRET_POST, false, false));
-    assertFalse(ClientAuthentication.isValidMethod(NONE, true, false));
-    assertFalse(ClientAuthentication.isValidMethod(PRIVATE_KEY_JWT, true, true));
     // legacy checks, no method passed
     assertTrue(ClientAuthentication.isValidMethod(null, false, false));
     assertTrue(ClientAuthentication.isValidMethod(null, true, false));
     assertTrue(ClientAuthentication.isValidMethod(null, false, true));
+
+  }
+
+  @Test
+  void isValidMethodFalse() {
+    assertFalse(ClientAuthentication.isValidMethod(CLIENT_SECRET_BASIC, false, false));
+    assertFalse(ClientAuthentication.isValidMethod(CLIENT_SECRET_POST, false, false));
+    assertFalse(ClientAuthentication.isValidMethod(NONE, true, false));
+    assertFalse(ClientAuthentication.isValidMethod(PRIVATE_KEY_JWT, true, true));
+    assertFalse(ClientAuthentication.isValidMethod(CLIENT_SECRET_BASIC, true, true));
+    // legacy checks, no method passed
     assertFalse(ClientAuthentication.isValidMethod(null, true, true));
   }
 
@@ -54,7 +61,7 @@ class ClientAuthenticationTest {
   }
 
   @Test
-  void isAuthMethodEqual() {
+  void isAuthMethodEqualTrue() {
     assertTrue(ClientAuthentication.isAuthMethodEqual(NONE, NONE));
     assertTrue(ClientAuthentication.isAuthMethodEqual(CLIENT_SECRET_BASIC, CLIENT_SECRET_POST));
     assertTrue(ClientAuthentication.isAuthMethodEqual(CLIENT_SECRET_POST, CLIENT_SECRET_BASIC));
@@ -62,6 +69,14 @@ class ClientAuthenticationTest {
     assertTrue(ClientAuthentication.isAuthMethodEqual(CLIENT_SECRET_POST, CLIENT_SECRET_POST));
     assertTrue(ClientAuthentication.isAuthMethodEqual(PRIVATE_KEY_JWT, PRIVATE_KEY_JWT));
     assertTrue(ClientAuthentication.isAuthMethodEqual(null, null));
+    assertTrue(ClientAuthentication.isAuthMethodEqual(null, CLIENT_SECRET_BASIC));
+    assertTrue(ClientAuthentication.isAuthMethodEqual(null, CLIENT_SECRET_POST));
+    assertTrue(ClientAuthentication.isAuthMethodEqual(CLIENT_SECRET_BASIC, null));
+  }
+
+  @Test
+  void isAuthMethodEqualFalse() {
+    assertFalse(ClientAuthentication.isAuthMethodEqual(PRIVATE_KEY_JWT, null));
     assertFalse(ClientAuthentication.isAuthMethodEqual(PRIVATE_KEY_JWT, CLIENT_SECRET_BASIC));
     assertFalse(ClientAuthentication.isAuthMethodEqual(PRIVATE_KEY_JWT, NONE));
   }
