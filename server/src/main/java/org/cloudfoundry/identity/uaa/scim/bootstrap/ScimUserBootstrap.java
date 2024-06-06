ba3fdf99bfd155a3029a05992750efaf90271237
d036670 <markus.strehle@sap.com>
2024-06-06 21:12:04 +0200

    fix: use origin for addGroups always
    Without this, new groups are assigned to UAA origin
    With external IdPs this is a bug

--- server/src/main/java/org/cloudfoundry/identity/uaa/scim/bootstrap/ScimUserBootstrap.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/scim/bootstrap/ScimUserBootstrap.java
@@ -167,19 +167,19 @@ public class ScimUserBootstrap implements
         if (updateGroups) {
             Collection<String> newGroups = convertToGroups(updatedUser.getAuthorities());
             logger.debug("Adding new groups " + newGroups);
-            addGroups(id, newGroups);
+            addGroups(id, updatedUser.getOrigin(), newGroups);
         }
     }
 
     private void createNewUser(UaaUser user) {
         logger.debug("Registering new user account: " + user);
         ScimUser newScimUser = scimUserProvisioning.createUser(convertToScimUser(user), user.getPassword(), IdentityZoneHolder.get().getId());
-        addGroups(newScimUser.getId(), convertToGroups(user.getAuthorities()));
+        addGroups(newScimUser.getId(), user.getOrigin(), convertToGroups(user.getAuthorities()));
     }
 
-    private void addGroups(String scimUserid, Collection<String> groups) {
+    private void addGroups(String scimUserid, String origin, Collection<String> groups) {
         for (String group : groups) {
-            addToGroup(scimUserid, group);
+            addToGroup(scimUserid, origin, group);
         }
     }
 
@@ -248,8 +248,8 @@ public class ScimUserBootstrap implements
         }
     }
 
-    private void addToGroup(String scimUserId, String gName) {
-        addToGroup(scimUserId, gName, OriginKeys.UAA, true);
+    private void addToGroup(String scimUserId, String origin, String gName) {
+        addToGroup(scimUserId, gName, origin, true);
     }
 
     private void addToGroup(String scimUserId, String gName, String origin, boolean addGroup) {
