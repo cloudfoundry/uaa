fe4a35c142869dc2cf521dbbc89f6f84df66438d
Adrian Hoelzl <adrian.hoelzl@sap.com>
2024-06-25 15:54:41 +0200

    Add HSQL DB migrations for removing old alias_zid indexes and adding new one on aliasZid and identity_zone_id

---
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/hsqldb/V4_110__Drop_AliasZid_Indexes.sql
@@ -0,0 +1,2 @@
+DROP INDEX IF EXISTS identity_provider_alias_zid__idx;
+DROP INDEX IF EXISTS users_alias_zid__idx;
---
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/hsqldb/V4_111__Create_AliasZid_And_IdzId_Indexes.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS identity_provider_alias_zid_idz_id__idx on identity_provider (alias_zid, identity_zone_id);
+CREATE INDEX IF NOT EXISTS users_alias_zid_idz_id__idx on users (alias_zid, identity_zone_id);
