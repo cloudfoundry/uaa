7feee63f502a5006442f81fbd288197590e0b051
d036670 <markus.strehle@sap.com>
2024-06-26 11:21:37 +0200

    Refactorings
    - shorten the index name. Similar name pattern then existing

--- server/src/main/resources/org/cloudfoundry/identity/uaa/db/mysql/V4_110__Add_ExternalKey_To_Identity_Provider.sql
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/mysql/V4_110__Add_ExternalKey_To_Identity_Provider.sql
@@ -1,3 +1,3 @@
 -- add column external_key for oauth2,oidc,saml2 IdPs
 ALTER TABLE identity_provider ADD COLUMN external_key TEXT DEFAULT NULL;
-CREATE UNIQUE INDEX identity_provider_ext_key_zid__idx on identity_provider (identity_zone_id(36),type(8),external_key(724));
+CREATE UNIQUE INDEX external_key_in_zone on identity_provider (identity_zone_id(36),type(8),external_key(724));
--- server/src/main/resources/org/cloudfoundry/identity/uaa/db/postgresql/V4_110__Add_ExternalKey_To_Identity_Provider.sql
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/postgresql/V4_110__Add_ExternalKey_To_Identity_Provider.sql
@@ -1,3 +1,3 @@
 -- add column external_key for oauth2,oidc,saml2 IdPs
 ALTER TABLE identity_provider ADD COLUMN external_key TEXT DEFAULT NULL;
-CREATE UNIQUE INDEX identity_provider_ext_key_zid__idx on identity_provider (identity_zone_id,type,external_key) WHERE external_key IS NOT NULL;
+CREATE UNIQUE INDEX external_key_in_zone on identity_provider (identity_zone_id,type,external_key) WHERE external_key IS NOT NULL;
