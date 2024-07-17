da2667d45804fa8d66503cb3a2c68c2cd3361b6d
d036670 <markus.strehle@sap.com>
2024-07-17 20:56:06 +0200

    test coverage

--- server/src/test/java/org/cloudfoundry/identity/uaa/provider/JdbcIdentityProviderProvisioningTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/provider/JdbcIdentityProviderProvisioningTests.java
@@ -16,8 +16,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
@@ -221,12 +219,12 @@ class JdbcIdentityProviderProvisioningTests {
     }
 
     @Test
-    void retrieveOAuth2IdentityProviderWithoutExternalId() throws MalformedURLException {
-        String tokenEndPointUrl = "https://oauth2.issuer.domain.org";
+    void retrieveOAuth2IdentityProviderWithoutExternalId() {
+        String issuerURI = "https://oauth2.issuer.domain.org";
         IdentityProvider<RawExternalOAuthIdentityProviderDefinition> idp = MultitenancyFixture.identityProvider(origin, uaaZoneId);
         String providerDescription = "Test Description";
         RawExternalOAuthIdentityProviderDefinition rawExternalOAuthIdentityProviderDefinition = new RawExternalOAuthIdentityProviderDefinition();
-        rawExternalOAuthIdentityProviderDefinition.setTokenUrl(URI.create(tokenEndPointUrl).toURL());
+        rawExternalOAuthIdentityProviderDefinition.setIssuer(issuerURI);
         idp.setConfig(rawExternalOAuthIdentityProviderDefinition);
         idp.getConfig().setProviderDescription(providerDescription);
         idp.setType(OAUTH20);
@@ -239,9 +237,7 @@ class JdbcIdentityProviderProvisioningTests {
         assertEquals(idp.getType(), readAgain.getType());
         assertEquals(providerDescription, readAgain.getConfig().getProviderDescription());
         RawExternalOAuthIdentityProviderDefinition readAgainConfig = (RawExternalOAuthIdentityProviderDefinition) readAgain.getConfig();
-        assertNotNull(tokenEndPointUrl, readAgainConfig.getTokenUrl().toString());
-        // OAuth2 allows to omit issuer, but tokenEncPointUrl is used then for check, e.g. checkIssuer in JwtTokenSignedByThisUAA
-        assertNull(readAgainConfig.getIssuer());
+        assertEquals(issuerURI, readAgainConfig.getIssuer());
     }
 
     @Test
