b76ba8c8a007fabb91d74daa6a35565a0cf77b06
strehle <markus.strehle@sap.com>
2024-11-15 18:16:55 +0100

    Cleanup test failure
    Changed, because of hack with defaults.

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/saml/SamlAuthenticationMockMvcTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/mock/saml/SamlAuthenticationMockMvcTests.java
@@ -301,7 +301,7 @@ class SamlAuthenticationMockMvcTests {
     void receiveAuthnResponseFromIdpToLegacyAliasUrl() throws Exception {
 
         String encodedSamlResponse = serializedResponse(responseWithAssertions());
-        mockMvc.perform(post("/uaa/saml/SSO/alias/%s".formatted("testsaml-post-binding"))
+        mockMvc.perform(post("/uaa/saml/SSO/alias/%s".formatted("integration-saml-entity-id"))
                         .contextPath("/uaa")
                         .header(HOST, "localhost:8080")
                         .param(SAML_RESPONSE, encodedSamlResponse)
@@ -477,7 +477,7 @@ class SamlAuthenticationMockMvcTests {
             Response response = responseWithAssertions("https://some.idp.test/saml/idp");
             response.setInResponseTo("incorrect");
             String encodedSamlResponse = serializedResponse(response);
-            mockMvc.perform(post("/uaa/saml/SSO/alias/%s".formatted("testsaml-post-binding"))
+            mockMvc.perform(post("/uaa/saml/SSO/alias/%s".formatted("integration-saml-entity-id"))
                             .contextPath("/uaa")
                             .header(HOST, "localhost:8080")
                             .param(SAML_RESPONSE, encodedSamlResponse)
