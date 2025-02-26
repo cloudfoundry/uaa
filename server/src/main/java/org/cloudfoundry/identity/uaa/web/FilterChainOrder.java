4857b4fdbe9f6f57f54a606aa6461689983640b5
Daniel Garnier-Moiroux <daniel.garnier-moiroux@broadcom.com>
2025-02-26 16:31:12 +0100

    Fix FilterChainOrder, include all filter chains from login-ui

--- server/src/main/java/org/cloudfoundry/identity/uaa/web/FilterChainOrder.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/web/FilterChainOrder.java
@@ -7,16 +7,18 @@ package org.cloudfoundry.identity.uaa.web;
 public class FilterChainOrder {
 
     // Order of filters in login-ui.xml
-    public static final int AUTOLOGIN = 1200;
-    public static final int INVITATIONS = 1201;
-    public static final int INVITE = 1202;
-    public static final int RESET_PASSWORD = 1203;
-    public static final int FORGOT_PASSWORD = 1204;
-    public static final int DELETE_SAVED_ACCOUNT = 1205;
-    public static final int VERIFY_EMAIL = 1206;
-    public static final int VERIFY_USER = 1207;
-    public static final int INVITATIONS_ACCEPT = 1208;
-    public static final int SAML_IDP_SSO = 1209;
-    public static final int UI_SECURITY = 1210;
+    public static final int AUTOLOGIN_AUTHORIZE = 1200;
+    public static final int AUTOLOGIN_CODE = 1201;
+    public static final int AUTOLOGIN = 1202;
+    public static final int INVITATIONS = 1203;
+    public static final int INVITE = 1204;
+    public static final int RESET_PASSWORD = 1205;
+    public static final int FORGOT_PASSWORD = 1206;
+    public static final int DELETE_SAVED_ACCOUNT = 1207;
+    public static final int VERIFY_EMAIL = 1208;
+    public static final int VERIFY_USER = 1209;
+    public static final int INVITATIONS_ACCEPT = 1210;
+    public static final int SAML_IDP_SSO = 1211;
+    public static final int UI_SECURITY = 1212;
 
 }
