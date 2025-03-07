1b171d8003a052e543be23a06b99938fdc52e2a4
Filip Hanik <fhanik@vmware.com>
2025-03-07 10:56:18 -0800

    Fix filter orders

--- server/src/main/java/org/cloudfoundry/identity/uaa/web/FilterChainOrder.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/web/FilterChainOrder.java
@@ -7,21 +7,14 @@ package org.cloudfoundry.identity.uaa.web;
 public class FilterChainOrder {
 
     // Order of filters in login-ui.xml
-    public static final int AUTOLOGIN_AUTHORIZE = 1200;
-    public static final int AUTOLOGIN_CODE = 1201;
-    public static final int AUTOLOGIN = 1202;
-    public static final int INVITATIONS = 1203;
-    public static final int INVITE = 1204;
-    public static final int RESET_PASSWORD = 1205;
-    public static final int FORGOT_PASSWORD = 1206;
-    public static final int DELETE_SAVED_ACCOUNT = 1207;
-    public static final int VERIFY_EMAIL = 1208;
-    public static final int VERIFY_USER = 1209;
-    public static final int INVITATIONS_ACCEPT = 1210;
-    public static final int SAML_IDP_SSO = 1211;
-    public static final int SCIM_GROUP = 1212;
-    public static final int SCIM_USER_PASSWORD = 1213;
-    public static final int SCIM_USER = 1214;
-    public static final int UI_SECURITY = 1215;
+    public static final int AUTOLOGIN_CODE = 1200;
+    public static final int AUTOLOGIN = 1201;
+    public static final int INVITATIONS = 1202;
+    public static final int INVITE = 1203;
+    public static final int LOGIN_PUBLIC_OPERATIONS = 1204;
+    public static final int SCIM_GROUP = 1205;
+    public static final int SCIM_USER_PASSWORD = 1206;
+    public static final int SCIM_USER = 1207;
+    public static final int UI_SECURITY = 1208;
 
 }
