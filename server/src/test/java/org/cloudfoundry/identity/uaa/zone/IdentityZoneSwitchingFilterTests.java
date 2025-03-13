67c0362444f146e5c76e45e8e222f45c3799f3e8
strehle <markus.strehle@sap.com>
2025-03-13 20:50:14 +0100

    fix http 500
    Test

--- server/src/test/java/org/cloudfoundry/identity/uaa/zone/IdentityZoneSwitchingFilterTests.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/zone/IdentityZoneSwitchingFilterTests.java
@@ -14,13 +14,29 @@
 
 package org.cloudfoundry.identity.uaa.zone;
 
+import org.cloudfoundry.identity.uaa.oauth.UaaOauth2Authentication;
 import org.cloudfoundry.identity.uaa.oauth.common.util.RandomValueStringGenerator;
+import org.cloudfoundry.identity.uaa.oauth.provider.OAuth2Authentication;
+import org.cloudfoundry.identity.uaa.oauth.provider.OAuth2Request;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class IdentityZoneSwitchingFilterTests {
+
+    @AfterEach
+    void tearDown() {
+        SecurityContextHolder.clearContext();
+    }
 
     @Test
     void stripPrefix() {
@@ -33,4 +49,23 @@ class IdentityZoneSwitchingFilterTests {
         assertThat(filter.stripPrefix("zones." + zoneId + ".idps.read", zoneId)).isEqualTo("idps.read");
     }
 
+    @Test
+    void getAuthenticationForZone() {
+        // Setup
+        OAuth2Request oAuth2Request = new OAuth2Request(Map.of("client_id", "id"), "id", Collections.emptyList(), true, Set.of("client"),
+                Set.of(), null, null, Map.of("extra", "param"));
+        IdentityZoneSwitchingFilter filter = new IdentityZoneSwitchingFilter(mock(IdentityZoneProvisioning.class));
+        UaaOauth2Authentication uaaAauth2Authentication = mock(UaaOauth2Authentication.class);
+        OAuth2Authentication oAuth2Authentication = mock(OAuth2Authentication.class);
+        when(uaaAauth2Authentication.getOAuth2Request()).thenReturn(oAuth2Request);
+        when(oAuth2Authentication.getOAuth2Request()).thenReturn(oAuth2Request);
+        // When
+        SecurityContextHolder.getContext().setAuthentication(uaaAauth2Authentication);
+        // Then
+        assertThat(filter.getAuthenticationForZone("uaa", new MockHttpServletRequest())).isInstanceOf(UaaOauth2Authentication.class);
+        // When
+        SecurityContextHolder.getContext().setAuthentication(oAuth2Authentication);
+        // Then
+        assertThat(filter.getAuthenticationForZone("uaa", new MockHttpServletRequest())).isInstanceOf(OAuth2Authentication.class);
+    }
 }
