feec1f78e5713e72990ce41923d4db2d1d53a1ca
rohit-armorikar <armorikar.rohit@gmail.com>
2025-03-18 22:13:30 +0530

    Update V4_113__Add_Index_user_group_membership_groups.sql
    Corrected the issues with queries as per MySql 8 version.

--- server/src/main/resources/org/cloudfoundry/identity/uaa/db/mysql/V4_113__Add_Index_user_group_membership_groups.sql
+++ server/src/main/resources/org/cloudfoundry/identity/uaa/db/mysql/V4_113__Add_Index_user_group_membership_groups.sql
@@ -1,14 +1,9 @@
-DROP INDEX IF EXISTS idx_users_zone_id_created ON users;
-ALTER TABLE users ADD INDEX idx_users_zone_id_created (identity_zone_id, created);
+CREATE INDEX idx_users_zone_id_created ON users (identity_zone_id, created);
 
-DROP INDEX IF EXISTS idx_group_membership_zone_member_group ON group_membership;
-ALTER TABLE group_membership ADD INDEX idx_group_membership_zone_member_group (identity_zone_id, member_id, group_id);
+CREATE INDEX idx_group_membership_zone_member_group ON group_membership (identity_zone_id, member_id, group_id);
 
-DROP INDEX IF EXISTS idx_group_membership_id_zone_id ON group_membership;
-ALTER TABLE group_membership ADD INDEX idx_group_membership_id_zone_id (group_id, identity_zone_id);
+CREATE INDEX idx_group_membership_id_zone_id ON group_membership (group_id, identity_zone_id);
 
-DROP INDEX IF EXISTS idx_groups_zone_id_created ON groups;
-ALTER TABLE groups ADD INDEX idx_groups_zone_id_created (identity_zone_id, created);
+CREATE INDEX idx_groups_zone_id_created ON `groups` (identity_zone_id, created);
 
-DROP INDEX IF EXISTS idx_external_group_zone_id_created ON external_group_mapping;
-ALTER TABLE external_group_mapping ADD INDEX idx_external_group_zone_id_created (group_id, identity_zone_id);
+CREATE INDEX idx_external_group_zone_id_created ON external_group_mapping (group_id, identity_zone_id);
