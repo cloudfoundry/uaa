daf2596008688a523162177fca0b071d6978a11b
Filip Hanik <fhanik@vmware.com>
2025-03-21 14:43:10 -0700

    Remove the parser for <authorization-server>

--- server/src/main/java/org/cloudfoundry/identity/uaa/oauth/provider/config/xml/OAuth2SecurityNamespaceHandler.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/oauth/provider/config/xml/OAuth2SecurityNamespaceHandler.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
 public class OAuth2SecurityNamespaceHandler extends NamespaceHandlerSupport {
 
     public void init() {
-        registerBeanDefinitionParser("authorization-server", new AuthorizationServerBeanDefinitionParser());
         registerBeanDefinitionParser("resource-server", new ResourceServerBeanDefinitionParser());
         registerBeanDefinitionParser("client-details-service", new ClientDetailsServiceBeanDefinitionParser());
         registerBeanDefinitionParser("client", new ClientBeanDefinitionParser());
