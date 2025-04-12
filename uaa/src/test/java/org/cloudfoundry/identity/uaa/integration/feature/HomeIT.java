3b2b591fe9065ce164e77bc4c680a45bbf7e0f61
strehle <markus.strehle@sap.com>
2025-04-12 14:29:26 +0200

    refactor wait

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/HomeIT.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/integration/feature/HomeIT.java
@@ -94,11 +94,10 @@ class HomeIT {
 
     @Test
     void theHeaderDropdown() {
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
-        WebElement elm = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nav-dropdown-button")));
-        elm.click();
-        assertThat(asOnHomePage.getAccountSettingsElement().isDisplayed()).isTrue();
-        assertThat(asOnHomePage.getSignOutElement().isDisplayed()).isTrue();
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nav-dropdown-button"))).click();
+        assertThat(wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nav-dropdown-content-profile"))).isDisplayed()).isTrue();
+        assertThat(wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("nav-dropdown-content-logout"))).isDisplayed()).isTrue();
     }
 
     static class HomePagePerspective {
