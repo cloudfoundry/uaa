3e453b90a18fd678f94e5691ef34deef9531ffa0
Georgi Genchev <g.genchev@sap.com>
2026-04-22 01:02:28 +0300

    Remove WebMvcAutoConfiguration import and update test autoconfigure
    Spring Boot 4 reorganized autoconfigure packages.
    - Removed unused WebMvcAutoConfiguration import from WebConfig
    - Updated test annotation to use new autoconfigure package locations
    
    Related to Spring Boot 4 migration.
    
    # Conflicts:
    #	server/src/main/java/org/cloudfoundry/identity/uaa/web/WebConfig.java

--- server/src/main/java/org/cloudfoundry/identity/uaa/web/WebConfig.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/web/WebConfig.java
@@ -1,17 +1,19 @@
 package org.cloudfoundry.identity.uaa.web;
 
+import jakarta.annotation.PostConstruct;
 import org.cloudfoundry.identity.uaa.authentication.manager.AutologinRequestConverter;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import java.util.List;
 
 /**
- * Web stack configuration. It relies on Spring Boot's {@link WebMvcAutoConfiguration},
+ * Web stack configuration. It relies on Spring Boot's Web MVC auto-configuration,
  * with a few adjustments in a properties file to match the legacy behavior from UAA.
  */
 @Configuration
@@ -22,5 +24,13 @@ class WebConfig implements WebMvcConfigurer {
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(new AutologinRequestConverter());
+    }
+
+    @Autowired
+    private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
+
+    @PostConstruct
+    public void init() {
+        requestMappingHandlerAdapter.setIgnoreDefaultModelOnRedirect(false);
     }
 }
