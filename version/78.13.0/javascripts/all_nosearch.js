82a80dea07c7c48ccd00bcbbb1e029fdd811981e
Cloud Foundry Identity Team <cf-identity-eng@pivotal.io>
2026-04-23 21:21:10 +0000

    Update API docs

---
+++ version/78.13.0/javascripts/all_nosearch.js
@@ -0,0 +1,1 @@
+$(function(){loadToc($("#toc"),".toc-link",".toc-list-h2, .toc-list-h3, .toc-list-h4",10),setupLanguages($("body").data("languages")),$(".content").imagesLoaded(function(){window.recacheHeights(),window.refreshToc()})}),window.onpopstate=function(){activateLanguage(getLanguageFromQueryString())};
