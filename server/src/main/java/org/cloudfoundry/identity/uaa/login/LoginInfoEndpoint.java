74fc43ff7102bb7ee9ca2c80a4824426c45157fa
gdgenchev <gdgenchev97@gmail.com>
2026-05-06 23:59:26 +0300

    review: remove odd login hint set
    Indeed, we are guaranteed to be in the UAA case, so we can remove it..

--- server/src/main/java/org/cloudfoundry/identity/uaa/login/LoginInfoEndpoint.java
+++ server/src/main/java/org/cloudfoundry/identity/uaa/login/LoginInfoEndpoint.java
@@ -854,9 +854,6 @@ public class LoginInfoEndpoint {
         if (!StringUtils.hasText(skipDiscovery) && identityProviders.size() == 1) {
             IdentityProvider matchedIdp = identityProviders.getFirst();
             if (matchedIdp.getType().equals(UAA)) {
-                if (StringUtils.hasText(loginHint)) {
-                    model.addAttribute(LOGIN_HINT_ATTRIBUTE, loginHint);
-                }
                 model.addAttribute(LOGIN_HINT_ATTRIBUTE, new UaaLoginHint("uaa").toString());
                 return goToPasswordPage(email, model);
             } else {
