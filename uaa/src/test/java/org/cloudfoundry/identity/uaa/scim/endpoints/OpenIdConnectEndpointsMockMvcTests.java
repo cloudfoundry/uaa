e038d5dd1df8069bd10ebbcb80a792fda52e1126
rkoster <hi@rkoster.dev>
2026-07-06 13:10:56 +0200

    test: update OIDC discovery tests for mtls_endpoint_aliases and tls_client_auth
    - Add tls_client_auth to tokenAMR assertion in wellKnownEndpoint tests
    - Assert mtls_endpoint_aliases.token_endpoint in both MockMvc test variants
      (SUBDOMAIN and ZONE_PATH zone resolution modes)
    - Document mtls_endpoint_aliases.token_endpoint in REST Docs snippet

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/scim/endpoints/OpenIdConnectEndpointsMockMvcTests.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/scim/endpoints/OpenIdConnectEndpointsMockMvcTests.java
@@ -62,7 +62,7 @@ class OpenIdConnectEndpointsMockMvcTests {
                 assertThat(openIdConfiguration.getIssuer()).isEqualTo("http://" + host + ":8080/uaa/oauth/token");
                 assertThat(openIdConfiguration.getAuthUrl()).isEqualTo("http://" + host + "/oauth/authorize");
                 assertThat(openIdConfiguration.getTokenUrl()).isEqualTo("http://" + host + "/oauth/token");
-                assertThat(openIdConfiguration.getTokenAMR()).containsExactly(new String[]{"client_secret_basic", "client_secret_post", "private_key_jwt"});
+                assertThat(openIdConfiguration.getTokenAMR()).containsExactly(new String[]{"client_secret_basic", "client_secret_post", "private_key_jwt", "tls_client_auth"});
                 assertThat(openIdConfiguration.getTokenEndpointAuthSigningValues()).containsExactly(new String[]{"RS256", "HS256"});
                 assertThat(openIdConfiguration.getUserInfoUrl()).isEqualTo("http://" + host + "/userinfo");
                 assertThat(openIdConfiguration.getScopes()).containsExactly(new String[]{"openid", "profile", "email", "phone", ROLES, USER_ATTRIBUTES});
@@ -74,6 +74,8 @@ class OpenIdConnectEndpointsMockMvcTests {
                 assertThat(openIdConfiguration.isClaimsParameterSupported()).isFalse();
                 assertThat(openIdConfiguration.getServiceDocumentation()).isEqualTo("http://docs.cloudfoundry.org/api/uaa/");
                 assertThat(openIdConfiguration.getUiLocalesSupported()).containsExactly(new String[]{"en-US"});
+                assertThat(openIdConfiguration.getMtlsEndpointAliases())
+                        .containsEntry("token_endpoint", "http://" + host + "/oauth/mtls/token");
             }
         }
     }
