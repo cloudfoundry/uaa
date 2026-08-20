15e8c50535032b4944ce4b9ec5e11cb6d88a2e52
rkoster <hi@rkoster.dev>
2026-08-20 12:08:08 +0200

    test: cover the fail-fast path when BCJSSE is not registered

--- server/src/test/java/org/cloudfoundry/identity/uaa/web/tomcat/BCJSSESSLContextTest.java
+++ server/src/test/java/org/cloudfoundry/identity/uaa/web/tomcat/BCJSSESSLContextTest.java
@@ -1,14 +1,25 @@
 package org.cloudfoundry.identity.uaa.web.tomcat;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.Security;
+
+import org.bouncycastle.jsse.provider.BouncyCastleJsseProvider;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BCJSSESSLContextTest {
 
     @BeforeEach
     void setUp() {
+        MtlsClientAuthTomcatCustomizer.ensureJsseProviderRegistered();
+    }
+
+    @AfterEach
+    void tearDown() {
         MtlsClientAuthTomcatCustomizer.ensureJsseProviderRegistered();
     }
 
@@ -18,5 +29,14 @@ class BCJSSESSLContextTest {
         context.init(null, null, null);
 
         assertThat(context.getSupportedSSLParameters().getProtocols()).contains("TLSv1.3", "TLSv1.2");
+    }
+
+    @Test
+    void throwsWhenTheBcjsseProviderIsNotRegistered() {
+        Security.removeProvider(BouncyCastleJsseProvider.PROVIDER_NAME);
+
+        assertThatThrownBy(() -> new BCJSSESSLContext("TLS"))
+                .isInstanceOf(NoSuchAlgorithmException.class)
+                .hasMessageContaining("ensureJsseProviderRegistered");
     }
 }
